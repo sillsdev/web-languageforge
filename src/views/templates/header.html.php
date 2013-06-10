c316a6949cd9be1b3cf1e68f647252ebc8d9d200
megahirt <chris@hirtfamily.net>
2013-06-10 10:06:41 +0700

    got ride of base_page and created nested view templates

--- src/views/templates/header.html.php
+++ src/views/templates/header.html.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-GB">
-	<head>
-		<meta charset="utf-8" />
-		<title>Scripture Forge Homepage Template</title>
-		<link rel="stylesheet" href="/css/sf.css" />
-		<link rel="stylesheet" media="screen" href="/css/superfish.css" />
-		<link rel="stylesheet" media="screen" href="/css/slides.css" />
-		<link rel="icon" href="favicon.ico" type="image/x-icon" />
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-	</head>
-	<body>
-		
 		<div id="header" class="png_bg">
 			
 			<div class="container">
