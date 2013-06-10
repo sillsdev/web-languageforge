297febb9a60e6c0001c3ae6d8952da9af8f2f5a9
megahirt <chris@hirtfamily.net>
2013-06-10 10:06:41 +0700

    got ride of base_page and created nested view templates

--- src/controllers/pages.php
+++ src/controllers/pages.php
@@ -1,8 +1,28 @@
 <?php 
-require 'base_page.php';
 
-class Pages extends Base_Page {
+class Pages extends CI_Controller {
 	
+	public function header($data) {
+		// all pages have a header
+		$this->load->view('templates/header.html.php', $data);
+	}
+	
+	public function footer($data) {
+		// all pages have a header
+		$this->load->view('templates/footer.html.php', $data);
+	}
+	
+	public function view($page = 'frontpage') {
+		$data = array();
+		$data['page'] = $page;
+		$data['title'] = "Scripture Forge";
+		if ( ! file_exists('views/pages/'.$page.'.html.php'))
+		{
+			show_404();
+		} else {
+			$this->load->view('templates/container.html.php', $data);
+		}
+	}
 }
 
 
