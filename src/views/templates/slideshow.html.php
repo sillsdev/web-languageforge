2099d6722ebd65407d604fc0d8bd983b801b7a19
megahirt <chris@hirtfamily.net>
2013-06-10 16:26:07 +0700

    added marketing fluff
    Former-commit-id: 4f34777fb0a13b21785e3df25875b8a676aa0c83

--- src/views/templates/slideshow.html.php
+++ src/views/templates/slideshow.html.php
@@ -7,7 +7,7 @@
 					<?php foreach ($slides as $slide): ?>
 					
 					<div class="slides_content">
-						<img src="<?php echo "/images/" . $slide['image_url'] ?>" alt="<?php $slide['image_url'] ?>" width="300" height="349" class="left png_bg">
+						<img src="<?php echo "/images/" . $slide['image_url'] ?>" alt="<?php $slide['image_url'] ?>" width="300" height="349" style="margin-top:20px; border: 2px solid grey" class="left png_bg">
 						<h2 class="left" style="margin-top: 110px;"><?php echo $slide['title'] ?></h2>
 						<p class="left" style="margin-top: 10px;"><?php echo $slide['summary'] ?></p>
 						<input type="button" class="btn standard-btn standard-btn-big" value="Learn More" onclick="window.location='<?php echo $slide['target_url'] ?>';" />
