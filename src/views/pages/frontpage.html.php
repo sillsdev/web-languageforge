6bc86780fca33d697b6e797311aced921f1eef3f
megahirt <chris@hirtfamily.net>
2013-06-10 15:20:12 +0700

    break out slideshow and 3column display into separate templates
    Former-commit-id: 4b3633fd4f5c52bd7b07b236754b07b1d3ca3a2d

--- src/views/pages/frontpage.html.php
+++ src/views/pages/frontpage.html.php
@@ -1,70 +1,61 @@
-		<div id="promotion" class="container">
-			<div id="promotion-ribbon" class="png_bg"></div>
-			<div id="promotionCutout" class="png_bg"></div>
-			<div id="slides">
-				<div class="slides_container">
-					
-					<div class="slides_content">
-						<img src="/images/promotion-sf-logo.png" alt="promotion-sf-logo" width="299" height="439" class="left png_bg">
-						<h2 class="left" style="margin-top: 110px;">Collaborative Document Translation</h2>
-						<p class="left" style="margin-top: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-						<input type="button" class="btn standard-btn standard-btn-big" value="Learn More" />
-						<p style="margin-top: 50px;">{Social Media}</p>
-					</div>
-					
-					<div class="slides_content">
-						<img src="/images/promotion-sf-logo2.png" alt="promotion-sf-logo2" width="299" height="439" class="left png_bg">
-						<h2 class="left" style="margin-top: 110px;">Collaborative Document Translation 2</h2>
-						<p class="left" style="margin-top: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-						<input type="button" class="btn standard-btn standard-btn-big" value="Learn More" />
-						<p style="margin-top: 50px;">{Social Media}</p>
-					</div>
-					
-					<div class="slides_content">
-						<img src="/images/promotion-sf-logo3.png" alt="promotion-sf-logo3" width="299" height="439" class="left png_bg">
-						<h2 class="left" style="margin-top: 110px;">Collaborative Document Translation 3</h2>
-						<p class="left" style="margin-top: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-						<input type="button" class="btn standard-btn standard-btn-big" value="Learn More" />
-						<p style="margin-top: 50px;">{Social Media}</p>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-		
+			<?php 
+			// perhaps this data array should be put into the controller?
 			
-		<div class="container cf">
-			<div class="three-col">
-				<div class="three-col-1">
-					<h2>Scripture Forge</h2>
-					<p>Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue. Donec id elit non mi porta gravida at eget metus.</p>
-				</div>
-			</div>
+			$data['slides'] = array(
+				array(
+					"title" => "Community Checking",
+					"summary" => "Scripture Forge enables communities to participate in the Scripture checking process like never before",
+					"social_media" => "{Social Media}",
+					"image_url" => "community.jpg",
+					"target_url" => "/learn_scripture_forge"
+					),
+				array(
+					"title" => "Your Team, Expanded",
+					"summary" => "Improve your scripture checking project by adding more native speakers to your workflow",
+					"social_media" => "{Social Media}",
+					"image_url" => "team.jpg",
+					"target_url" => "/learn_expand_your_team"
+					),
+				array(
+					"title" => "Make a Difference Today",
+					"summary" => "Speak a minority language?  See if your language has a scripture checking project in progress today",
+					"social_media" => "{Social Media}",
+					"image_url" => "contribute.jpg",
+					"target_url" => "/learn_contribute"
+					)
+			);
+			$this->load->view("templates/slideshow.html.php", $data);
 			
-			<div class="three-col">
-				<div class="three-col-2">
-					<h2>Expand the Team</h2>
-					<p>Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet.</p>
-					<p><a href="#" class="arrowed">Read More</a></p>
-				</div>
-			</div>
+			$data['columns'] = array(
+				array(
+					"title" => "Scripture Forge",
+					"summary" => "summary",
+					"target_url" => ""
+					),
+				array(
+					"title" => "Expand Your Team",
+					"summary" => "summary",
+					"target_url" => "/learn_expand_your_team"
+					),
+				array(
+					"title" => "Contribute",
+					"summary" => "summary",
+					"target_url" => "/learn_contribute"
+					)
+			);
+			$this->load->view("templates/3column.html.php", $data);
 			
-			<div class="three-col">
-				<div class="three-col-3">
-					<h2>Contribute</h2>
-					<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-					<p><a href="#" class="arrowed">Read More</a></p>
-				</div>
-			</div>
-		</div>
-		
+			
+			?>
+			
 		<div class="sub-promotion container cf">
-			<img src="/images/chalkboard.jpg" alt="chalkboard" width="299" height="182" class="left">
+			<img src="/images/girlsmiling.jpg" alt="girl smiling" width="299" height="182" class="left">
 			<h2>The Power of Collaboration</h2>
 			<p>Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
 			<p><a href="#" class="arrowed">Get Involved Today</a></p>
 		</div>
 		
+		<!--
 		<div class="container cf">
 			<h2>Latest News</h2>
 			
@@ -96,3 +87,4 @@
 			</div>
 			
 		</div>
+		-->
