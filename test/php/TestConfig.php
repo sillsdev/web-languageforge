81ebab8da59238b318e1d0c1d6bf0ede800675a9
cambell <cambell_prince@sil.org>
2013-06-13 17:27:38 +0700

    wip: Mongo Former-commit-id: faea0dd1488df97682d398f96ef7722145807355

---
+++ test/php/TestConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+$rootPath = dirname(__FILE__) . '/';
+
+//define('TestMode', true);
+
+define('TestPath', $rootPath);
+define('SimpleTestPath', $rootPath . '../lib/simpletest/');
+define('SourcePath', $rootPath . '../../src/');
+
+
+?>
