46816f267261cefb91eb466083ffbff6b1374b61
megahirt <chris@hirtfamily.net>
2013-06-14 16:20:53 +0700

    wip temporary disable of Foundation library

--- src/views/templates/container.html.php
+++ src/views/templates/container.html.php
@@ -8,13 +8,7 @@ $this->load->view('templates/meta.html.php');
 <body>
 <?php
 $this->load->view('templates/header.html.php');
-?>
-<div class="container cf">
-	<div style="margin: 40px 0 0 140px">
-		<?php $this->load->view($page); ?>
-	</div>
-</div>
-<?php
+$this->load->view($page); 
 $this->load->view('templates/script.html.php');
 $this->load->view('templates/footer.html.php');
 ?>
