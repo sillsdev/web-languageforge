28c88824c43848ee2072cd9b4a55ea3a6326a68c
megahirt <chris@hirtfamily.net>
2013-06-14 11:21:26 +0700

    moved admin index to CI view

--- src/views/apps/admin.html.php
+++ src/views/apps/admin.html.php
@@ -18,7 +18,7 @@
   <!-- In production use:
   <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
   -->
-  <script src="lib/angular/angular.js"></script>
+  <script src="/lib/angular_stable_1.0.7/angular.js"></script>
   <script src="js/app.js"></script>
   <script src="js/services.js"></script>
   <script src="js/controllers.js"></script>
