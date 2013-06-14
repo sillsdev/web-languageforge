f6effbac941ef179f8a30259b4205cc6229c5313
cambell <cambell.prince@gmail.com>
2013-06-14 17:36:59 +0700

    Refactor ModelMapper and MongoMapper
    Former-commit-id: 8c182fe125441bd509785015f2fae64fc9300670

--- src/models/user_model.php
+++ src/models/user_model.php
@@ -1,108 +1,5 @@
 <?php
 
-class ModelMapper /*extends CI_Model*/
-{
-	protected $_mapper;
-	
-	function __construct($mapper)
-	{
-		$this->_mapper = $mapper;
-	}
-	
-}
-
-class MongoMapper
-{
-	/**
-	 * 
-	 * @var MongoDB
-	 */
-	protected $_db;
-	
-	/**
-	 * @var string
-	 */
-	private $_idKey;
-	
-	/**
-	 * @param string $database
-	 * @param string $collection
-	 * @param string $idKey defaults to id
-	 */
-	public function __construct($database, $idKey = 'id')
-	{
-		$this->_db = MongoStore::connect($database);
-		$this->_idKey = $idKey;
-	}
-	
-	/**
-	 * Sets the public properties of $model to values from $values[propertyName] 
-	 * @param object $model
-	 * @param array $values
-	 */
-	public function decode($model, $values)
-	{
-		$properties = get_object_vars($model);
-		$idKey = $this->_idKey;
-		// Map the Mongo _id to the property $idKey
-		if (array_key_exists($idKey, $properties))
-		{
-			$model->$idKey = $values['_id'];
-			unset($properties[$idKey]);
-		}
-		foreach ($properties as $key => $value)
-		{
-			if (!array_key_exists($key, $values))
-			{
-				// oops // TODO Add to list, throw at end CP 2013-06
-				continue;
-			}
-			$model->$key = $values[$key];
-		}
-	}
-
-	/**
-	 * Sets key/values in the array to the public properties of $model
-	 * @param object $model
-	 * @return array
-	 */
-	public function encode($model)
-	{
-		$data = array();
-		$properties = get_object_vars($model);
-		$idKey = $this->_idKey;
-		// We don't want the 'idKey' in the data so remove that from the properties 
-		if (array_key_exists($idKey, $properties))
-		{
-			unset($properties[$idKey]);
-		}
-		foreach ($properties as $key => $value)
-		{
-			$data[$key] = $value;
-		}
-		return $data;
-	}
-	
-	/**
-	 * 
-	 * @param MongoCollection $collection
-	 * @param array $data
-	 * @param MongoId $id
-	 * @return MongoId
-	 */
-	protected function update($collection, $data, $id)
-	{
-		assert($id === NULL || is_a($id, 'MongoId'));
-		$result = $collection->update(
-				array('_id' => $id),
-				$data,
-				array('upsert' => true, 'multiple' => false, 'safe' => true)
-		);
-		return isset($result['upserted']) ? $result['upserted'] : $id;
-	}
-	
-}
-
 class UserModel_MongoMapper extends MongoMapper
 {
 	function __construct()
@@ -146,21 +43,9 @@ class UserModel extends ModelMapper
 	
 	public function __construct($id = NULL)
 	{
-		parent::__construct(new UserModel_MongoMapper());
-		if (!empty($id))
-		{
-			$this->_mapper->read($this, $id);
-		}
+		parent::__construct(new UserModel_MongoMapper(), $id);
 	}
 	
-	/**
-	 * @return string The unique id of the object written
-	 */
-	function write()
-	{
-		return $this->_mapper->write($this);
-	}
-		
 }
 
 ?>
