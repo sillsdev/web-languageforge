1ed19c3afa337de90558b20b0847a16613275a01
cambell <cambell.prince@gmail.com>
2013-06-16 21:02:25 +0700

    MongoMapper refactoring

--- src/models/user_model.php
+++ src/models/user_model.php
@@ -6,32 +6,8 @@ class User_model_MongoMapper extends MongoMapper
 {
 	function __construct()
 	{ 
-		parent::__construct('scriptureforge');
+		parent::__construct('scriptureforge', 'users');
 	}
-	
-	/**
-	 * @param User_model $model
-	 * @param MongoId $id
-	 */
-	public function read($model, $id)
-	{
-		assert(is_string($id));
-		$collection = $this->_db->users;
-		$data = $collection->findOne(array("_id" => new MongoId($id)));
-		if ($data === NULL)
-		{
-			throw new Exception("Could not find id '$id'");
-		}	
-		$this->decode($model, $data);
-	}
-	
-	public function write($model)
-	{
-		$collection = $this->_db->users;
-		$data = $this->encode($model);
-		return $this->update($collection, $data, $model->id);
-	}
-	
 	
 }
 
@@ -43,11 +19,7 @@ class User_model extends MapperModel
 	
 	public $email;
 	
-	public function __construct($id = NULL)
-	{
-		parent::__construct(new User_model_MongoMapper(), $id);
-	}
-	
 }
+User_model::init(new User_model_MongoMapper());
 
 ?>
