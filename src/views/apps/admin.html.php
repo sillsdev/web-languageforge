6a948f50ed7c589c3c68ff3714a317c2fbef0623
Robin Munn <Robin_Munn@sil.org>
2013-06-18 13:24:25 +0700

    Update the sample admin app for the new JS paths

--- src/views/apps/admin.html.php
+++ src/views/apps/admin.html.php
@@ -11,12 +11,12 @@
 	<!-- In production use:
 	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
 	-->
-	<script src="/lib/angular_stable_1.0.7/angular.js"></script>
-	<script src="js/app.js"></script>
-	<script src="js/services.js"></script>
-	<script src="js/controllers.js"></script>
-	<script src="js/filters.js"></script>
-	<script src="js/directives.js"></script>
+	<script src="/js/lib/angular_stable_1.0.7/angular.js"></script>
+	<script src="/angular-app/admin/js/app.js"></script>
+	<script src="/angular-app/admin/js/services.js"></script>
+	<script src="/angular-app/admin/js/controllers.js"></script>
+	<script src="/angular-app/admin/js/filters.js"></script>
+	<script src="/angular-app/admin/js/directives.js"></script>
 	  
 	  
 </div>
