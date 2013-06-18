733844e790f136826bab967fb43c6f7c7b584f11
Robin Munn <Robin_Munn@sil.org>
2013-06-18 13:40:11 +0700

    Demo code for calling JSON-RPC API
    This demonstrates two ways to call the JSON-RPC API: first
    by hand, and then with the helper library I just wrote.
    
    
    Former-commit-id: cfbbd12db5cb5445bb1f462a0faee9b6a5265385

--- src/views/apps/admin.html.php
+++ src/views/apps/admin.html.php
@@ -1,12 +1,23 @@
 <div ng-app="myApp">
-	<ul class="menu">
-	<li><a href="#/view1">view1</a></li>
-	<li><a href="#/view2">view2</a></li>
-	</ul>
+	<div style="margin-top: 150px"><!-- Spacer --></div>
+	<div class="container">
+		<div class="application" style="padding: 10px" ng-controller="MyCtrl1">
+			
+		<ul class="menu">
+		<li><a href="#/view1">view1</a></li>
+		<li><a href="#/view2">view2</a></li>
+		</ul>
+		<br/>
+		Enter message: <input type="text" ng-model="data.message">
+		<h3>{{data.message}} world</h3>
+		Some JSON-RPC data: {{data}}
+			
+		</div>
+	</div>
 	
 	<div ng-view></div>
 	
-	<div>Angular seed app: v<span app-version></span></div>
+	<!-- Not yet implemented: <div>Angular seed app: v<span app-version></span></div> -->
 	
 	<!-- In production use:
 	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
@@ -17,6 +28,15 @@
 	<script src="/angular-app/admin/js/controllers.js"></script>
 	<script src="/angular-app/admin/js/filters.js"></script>
 	<script src="/angular-app/admin/js/directives.js"></script>
-	  
+
+
+	<script	src="/js/lib/angular_stable_1.0.7/angular.js"></script>
+	<script	src="/js/jsonrpc.js"></script>
+	<script	src="/angular-app/admin/js/controllers.js"></script>
+	<!-- <script	src="/angular-app/admin/js/app.js"></script>
+	<script	src="/angular-app/admin/js/services.js"></script> -->
+	<!-- <script	src="/angular-app/admin/js/filters.js"></script>
+	<script	src="/angular-app/admin/js/directives.js"></script> -->
+	
 	  
 </div>
