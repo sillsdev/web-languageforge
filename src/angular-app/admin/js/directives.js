7bcd4ea8bb97a94fbfb77807e66b5f750e890a7a
Robin Munn <Robin_Munn@sil.org>
2013-06-19 12:39:11 +0700

    Refactor to match style of Angular seed app
    The seed app has controllers, directives, filters, etc. all in their
    own files, which seems like good style. Let's match that.

--- src/angular-app/admin/js/directives.js
+++ src/angular-app/admin/js/directives.js
@@ -3,9 +3,33 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+angular.module('myApp.directives', ["jsonRpc"]).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }])
+  .directive('userData', ['jsonRpc', function(jsonRpc) {
+	  return {
+		  // templateUrl = "",  // Eventually we'll move this out to its own template file
+		  restrict: "E",
+		  link: function(scope, elem, attrs) {
+			  scope.$watch("vars.userid", function(newval, oldval) {
+			  //attrs.$observe("userid", function(newval, oldval) {
+				  console.log("Watch triggered with oldval '" + oldval + "' and newval '" + newval + "'");
+				  if (newval) {
+					  get_user_by_id(newval);
+				  }
+			  });
+			  
+			  function get_user_by_id(userid) {
+				  console.log("Fetching id: " + userid);
+				  jsonRpc.connect("/api/sf");
+				  jsonRpc.call("user_read", {"id": userid}, function(result) {
+					  scope.result = result.data.result;
+				  });
+			  }
+		  },
+		  template: '<div class="details">{{vars.userid}}: {{result}}</div>',
+	  };
   }]);
