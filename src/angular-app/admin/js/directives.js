61b5e68b38e60e5cf4fd962a5130987982aee597
Robin Munn <Robin_Munn@sil.org>
2013-06-21 16:37:10 +0700

    Focus correct input box when Add or Save clicked
    This will allow users entering multiple records at once to keep
    typing without interruption, and the Tab and Enter keys will do
    exactly what they expect.

--- src/angular-app/admin/js/directives.js
+++ src/angular-app/admin/js/directives.js
@@ -39,4 +39,71 @@ angular.module('myApp.directives', ["jsonRpc"]).
 	  return {
 		  restrict: "E",
 		  templateUrl: "/partials/userlist.html",
-  }});
+  }})
+.directive('ngFocus', function($parse, $timeout) {
+	return function(scope, elem, attrs) {
+		var ngFocusGet = $parse(attrs.ngFocus);
+		var ngFocusSet = ngFocusGet.assign;
+		if (!ngFocusSet) {
+			throw Error("Non assignable expression");
+		}
+		console.log("In ng-focus directive, trying to focus with var ", attrs.ngFocus);
+
+		var abortFocusing = false;
+		var unwatch = scope.$watch(attrs.ngFocus, function(newVal){
+			if(newVal){
+				$timeout(function(){
+					elem[0].focus();  
+				},0);
+			}
+			else {
+				$timeout(function(){
+					elem[0].blur();
+				},0);
+			}
+		});
+
+
+		elem.bind("blur", function(){
+
+			if(abortFocusing) return;
+
+			$timeout(function(){
+				ngFocusSet(scope,false);
+			},0);
+
+		});
+
+
+		var timerStarted = false;
+		var focusCount = 0;
+
+		function startTimer(){
+			$timeout(function(){
+				timerStarted = false;
+				if(focusCount > 3){
+					unwatch();
+					abortFocusing = true;
+					throw new Error("Aborting : ngFocus cannot be assigned to the same variable with multiple elements");
+				}
+			},200);
+		}
+
+		elem.bind("focus", function(){
+
+			if(abortFocusing) return;
+
+			if(!timerStarted){
+				timerStarted = true;
+				focusCount = 0;
+				startTimer();
+			}
+			focusCount++;
+
+			$timeout(function(){
+				ngFocusSet(scope,true);
+			},0);
+
+		});
+	};
+});
