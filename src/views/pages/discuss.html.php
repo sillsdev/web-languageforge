c3cdfd0e58900f80004cd9a314f98908abe5ae28
megahirt <chris@hirtfamily.net>
2013-06-24 14:24:10 +0700

    added beach promotional images for other static marketing pages

--- src/views/pages/discuss.html.php
+++ src/views/pages/discuss.html.php
@@ -1,4 +1,6 @@
-<div style="margin-top:200px"></div>
+<div class="container banner">
+	<img src="/images/beach2.jpg" width="965px" height="360px" alt="on the beach" />
+</div>
 <div class="container">
 			<h2>Discuss</h2>
 			<p>You can discuss Scripture Forge at our <a href="#">Google Group here.</a></p>
