6864a406c537b55552cc9994ecbf729bc9c5a822
megahirt <chris@hirtfamily.net>
2013-06-24 14:24:10 +0700

    added beach promotional images for other static marketing pages

--- src/views/pages/contribute.html.php
+++ src/views/pages/contribute.html.php
@@ -1,5 +1,6 @@
-<div style="margin-top:200px"></div>
+<div class="container banner">
+	<img src="/images/beach1.jpg" width="965px" height="360px" alt="on the beach" />
+</div>
 <div class="container">
 			<h2>Contribute Placeholder for App</h2>
-			
-		</div>
+</div>
