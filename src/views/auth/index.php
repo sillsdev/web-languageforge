9c8d36a8e2bab2aba6ab75928e9c66afced681ea
megahirt <chris@hirtfamily.net>
2013-06-25 14:27:26 +0700

    modified auth user create/update controllers/templates to just be name and email (removed phone and company)

--- src/views/auth/index.php
+++ src/views/auth/index.php
@@ -6,7 +6,6 @@
 <table cellpadding=0 cellspacing=10>
 	<tr>
 		<th><?php echo lang('index_fname_th');?></th>
-		<th><?php echo lang('index_lname_th');?></th>
 		<th><?php echo lang('index_email_th');?></th>
 		<th><?php echo lang('index_groups_th');?></th>
 		<th><?php echo lang('index_status_th');?></th>
@@ -14,8 +13,7 @@
 	</tr>
 	<?php foreach ($users as $user):?>
 		<tr>
-			<td><?php echo $user->first_name;?></td>
-			<td><?php echo $user->last_name;?></td>
+			<td><?php echo $user->name;?></td>
 			<td><?php echo $user->email;?></td>
 			<td>
 				<?php foreach ($user->groups as $group):?>
