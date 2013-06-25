90ffdf19464727c886e2dd9fad0d1069d97a07b5
Robin Munn <Robin_Munn@sil.org>
2013-06-25 13:37:55 +0700

    Fix Ion Auth having wrong default user group
    The group that all users should belong to is called "users", not
    "members". This was causing new user accounts to get assigned
    to incorrect groups when created via the Ion Auth create_user
    form.

--- src/config/ion_auth.php
+++ src/config/ion_auth.php
@@ -85,7 +85,7 @@ $config['max_rounds']     = 9;
  */
 $config['site_title']                 = "Example.com";       // Site Title, example.com
 $config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
-$config['default_group']              = 'members';           // Default group, use name
+$config['default_group']              = 'users';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             // A database column which is used to login with
 $config['min_password_length']        = 8;                   // Minimum Required Length of Password
