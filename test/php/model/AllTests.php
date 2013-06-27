be2994f67149882d2a35e7a4da04e5eb3c378765
Robin Munn <Robin_Munn@sil.org>
2013-06-27 19:11:46 +0700

    Add test for PHP object-orientation bugfix
    This test exercises the bug that I fixed a couple days ago with
    commit 968825b631796b4703610bc1263070bcc32963df [formerly e6385af5275e80b75089a492fba64e1e18422e25]. If you
    revert that commit in your local git repo, this test will start to
    fail as users get put into the projects collection (or vice versa
    if you just swap the order of the two require statements). But
    if that commit is applied in your local git repo, the test passes.
    
    
    Former-commit-id: 8e263c92bbc7b793f1edfa445fbb30adf4d3fa48

--- test/php/model/AllTests.php
+++ test/php/model/AllTests.php
@@ -8,6 +8,7 @@ class AllTests extends TestSuite {
         parent::__construct();
  		$this->addFile(TestPath . 'model/UserModel_Test.php');
  		$this->addFile(TestPath . 'model/ProjectModel_Test.php');
+ 		$this->addFile(TestPath . 'model/MultipleModel_Test.php');
     }
 
 }
