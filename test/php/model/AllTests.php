a4554c20fe50853d2163b5c6a79569b89aa58420
Robin Munn <Robin_Munn@sil.org>
2013-06-27 19:06:50 +0700

    Add some tests for project model code

--- test/php/model/AllTests.php
+++ test/php/model/AllTests.php
@@ -7,6 +7,7 @@ class AllTests extends TestSuite {
     function __construct() {
         parent::__construct();
  		$this->addFile(TestPath . 'model/UserModel_Test.php');
+ 		$this->addFile(TestPath . 'model/ProjectModel_Test.php');
     }
 
 }
