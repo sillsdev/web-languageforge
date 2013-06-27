8e263c92bbc7b793f1edfa445fbb30adf4d3fa48
Robin Munn <Robin_Munn@sil.org>
2013-06-27 19:11:46 +0700

    Add test for PHP object-orientation bugfix
    This test exercises the bug that I fixed a couple days ago with
    commit e6385af5275e80b75089a492fba64e1e18422e25. If you
    revert that commit in your local git repo, this test will start to
    fail as users get put into the projects collection (or vice versa
    if you just swap the order of the two require statements). But
    if that commit is applied in your local git repo, the test passes.

---
+++ test/php/model/MultipleModel_Test.php
@@ -0,0 +1,61 @@
+<?php
+require_once(dirname(__FILE__) . '/../TestConfig.php');
+require_once(SimpleTestPath . 'autorun.php');
+
+require_once(SourcePath . "models/project_model.php");
+require_once(SourcePath . "models/user_model.php");
+
+class TestMultipleModel extends UnitTestCase {
+
+	function __construct()
+	{
+	}
+
+	function testWrite_ReadBackSameMultiple()
+	{
+		$model = new User_model();
+		$model->email = "user@example.com";
+		$model->username = "SomeUser";
+		$id = $model->write();
+		$this->assertNotNull($id);
+		$this->assertIsA($id, 'string');
+		$otherModel = new User_model($id);
+		$this->assertEqual($id, $otherModel->id);
+		$this->assertEqual('user@example.com', $otherModel->email);
+		$this->assertEqual('SomeUser', $otherModel->username);
+
+		$model = new Project_model();
+		$model->language = "SomeLanguage";
+		$model->projectname = "SomeProject";
+		$id = $model->write();
+		$this->assertNotNull($id);
+		$this->assertIsA($id, 'string');
+		$otherModel = new Project_model($id);
+		$this->assertEqual($id, $otherModel->id);
+		$this->assertEqual('SomeLanguage', $otherModel->language);
+		$this->assertEqual('SomeProject', $otherModel->projectname);
+	}
+
+	function testUserList_HadOnlyUsers()
+	{
+		$model = new User_list_model();
+		$model->read();
+
+		foreach ($model->entries as $entry) {
+			$this->assertTrue(array_key_exists("username", $entry));
+		}
+	}
+
+	function testProjectList_HadOnlyProjects()
+	{
+		$model = new Project_list_model();
+		$model->read();
+		
+		foreach ($model->entries as $entry) {
+			$this->assertTrue(array_key_exists("projectname", $entry));
+		}
+	}
+	
+}
+
+?>
