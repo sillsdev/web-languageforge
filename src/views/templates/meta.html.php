706f20e070c65404680e3ae2ae63757ecd7aa080
cambell <cambell_prince@sil.org>
2013-07-01 15:44:39 +0700

    Load jQuery first so that it is included before angular, so that angular can use full jQuery rather than jQLite
    Former-commit-id: 97f62b0e71d567e22871070c0f360cf0ac32e294

--- src/views/templates/meta.html.php
+++ src/views/templates/meta.html.php
@@ -10,3 +10,4 @@
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 		<script src="/js/lib/foundation/custom.modernizr.js"></script>
+		<script src="/js/lib/jquery-1.8.0.min.js"></script>
