7e36262bbae104bdeb752318da86e1cd109e045a
cambell <cambell_prince@sil.org>
2013-07-01 16:48:16 +0700

    wip projectadmin: Addedl jsonrpc to user service for typeahead
    Former-commit-id: 3fa70f28fc7fc9438303b28d4149e675d2948b1c

--- src/angular-app/projectadmin/js/services.js
+++ src/angular-app/projectadmin/js/services.js
@@ -6,7 +6,9 @@
 // In this case it is a simple value service.
 angular.module('projectAdmin.services', [])
 	.value('version', '0.1')
-	.service('userService', ['$timeout', function($timeout) {
+	.service('userService', ['jsonRpc', function(jsonRpc) {
+		jsonRpc.connect('/api/sf'); // Note this doesn't actually 'connect', it simply sets the connection url.
+
 		this.read = function(id) {
 	
 		};
@@ -17,46 +19,7 @@ angular.module('projectAdmin.services', [])
 	
 		};
 		this.typeadhead = function(term, callback) {
-			var result = {};
-			result.ok = true;
-			result.data = [ {
-				'name' : 'Cambell',
-				'username' : 'cambell',
-				'email' : 'cambell@example.com'
-			}, {
-				'name' : 'John',
-				'username' : 'john',
-				'email' : 'john@example.com'
-			}, {
-				'name' : 'Bob',
-				'username' : 'bob',
-				'email' : 'bob@example.com'
-			}, {
-				'name' : 'Sam',
-				'username' : 'sam',
-				'email' : 'sam@example.com'
-			}, {
-				'name' : 'Sammy',
-				'username' : 'sammy',
-				'email' : 'sammy@example.com'
-			}, {
-				'name' : 'Samuel',
-				'username' : 'samuel',
-				'email' : 'samuel@example.com'
-			}, {
-				'name' : 'Samson',
-				'username' : 'samson',
-				'email' : 'samson@example.com'
-			}, {
-				'name' : 'Sonny',
-				'username' : 'sonny',
-				'email' : 'sonny@example.com'
-			}, {
-				'name' : 'Samantha',
-				'username' : 'samantha',
-				'email' : 'samantha@example.com'
-			} ];
-			$timeout(function() { callback(result); }, 300);
+			jsonRpc.call('user_typeahead', [term], callback);
 		};
 	}])
 	;
