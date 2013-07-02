8101d677d4ca9b5b2bf65f1bb4956e95f9e1c5a4
megahirt <chris@hirtfamily.net>
2013-07-02 17:02:32 +0700

    wip: user profile displays user

--- src/angular-app/common/js/services.js
+++ src/angular-app/common/js/services.js
@@ -6,17 +6,20 @@ angular.module('sf.services', [])
 	.service('userService', ['jsonRpc', function(jsonRpc) {
 		jsonRpc.connect('/api/sf'); // Note this doesn't actually 'connect', it simply sets the connection url.
 
-		this.read = function(id) {
-	
+		this.read = function(id, callback) {
+			jsonRpc.call('user_read', [id], callback);
 		};
-		this.update = function(model) {
-	
+		this.update = function(model, callback) {
+			jsonRpc.call('user_update', [model], callback);
 		};
-		this.remove = function(id) {
-	
+		this.remove = function(id, callback) {
+			jsonRpc.call('user_remove', [id], callback);
 		};
 		this.typeahead = function(term, callback) {
 			jsonRpc.call('user_typeahead', [term], callback);
 		};
+		this.updatePassword = function(password, callback) {
+			jsonRpc.call('user_updatePassword', [password], callback);
+		}
 	}])
 	;
--- src/angular-app/userprofile/js/app.js
+++ src/angular-app/userprofile/js/app.js
@@ -1,9 +1,22 @@
 'use strict';
 
-function userProfileCtrl($scope) {
+function userProfileCtrl($scope, userService) {
 	$scope.user = {'name' : 'Chris', 'id': 1};
+	
+	var loadUser = function() {
+		userService.read(window.session.userid, function(result) {
+			if (result.ok) {
+				$scope.user = result.data;
+			} else {
+				// TODO report the real error
+				$scope.error = true;
+				$scope.errorMessage = 'something went wrong';
+			}
+		});
+	};	
+	loadUser();
 }
 
-angular.module('userProfile', ['jsonRpc', 'ui.bootstrap']).
-controller('userProfileCtrl', ['$scope', userProfileCtrl])
+angular.module('userProfile', ['jsonRpc', 'ui.bootstrap', 'sf.services']).
+controller('userProfileCtrl', ['$scope', 'userService', userProfileCtrl])
 ;
