cafa62e89b76d26cfabe5c5b50d45a532c844280
cambell-prince <cambell.prince@gmail.com>
2013-07-02 17:16:40 +0700

    wip projectadmin: user list control
    Former-commit-id: 8628186c5905ec1492762a1e1bca68e44dccba91

--- src/angular-app/common/js/services.js
+++ src/angular-app/common/js/services.js
@@ -6,14 +6,17 @@ angular.module('sf.services', [])
 	.service('userService', ['jsonRpc', function(jsonRpc) {
 		jsonRpc.connect('/api/sf'); // Note this doesn't actually 'connect', it simply sets the connection url.
 
-		this.read = function(id) {
+		this.read = function(id, callback) {
 	
 		};
-		this.update = function(model) {
+		this.update = function(model, callback) {
 	
 		};
-		this.remove = function(id) {
+		this.remove = function(id, callback) {
 	
+		};
+		this.list = function(callback) {
+			jsonRpc.call('user_list', [], callback);
 		};
 		this.typeahead = function(term, callback) {
 			jsonRpc.call('user_typeahead', [term], callback);
