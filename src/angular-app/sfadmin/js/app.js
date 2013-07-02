0601ac1a7a576f2f58ab46751d81c176273ae6af
megahirt <chris@hirtfamily.net>
2013-07-02 14:29:47 +0700

    removed palaso.ui from sfadmin

--- src/angular-app/sfadmin/js/app.js
+++ src/angular-app/sfadmin/js/app.js
@@ -4,7 +4,7 @@
 // TODO This is currently not used.  The module is really defined in controller.js for now CP 2013-07
 
 // Declare app level module which depends on filters, and services
-angular.module('sfAdmin', ['sfAdmin.filters', 'sfAdmin.services', 'sfAdmin.directives', 'sfAdmin.controllers', 'ui.bootstrap', 'palaso.ui']);
+angular.module('sfAdmin', ['sfAdmin.filters', 'sfAdmin.services', 'sfAdmin.directives', 'sfAdmin.controllers', 'ui.bootstrap']);
 /*
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
