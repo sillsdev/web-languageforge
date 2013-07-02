eae5092a462e173e44972d61ba77002189c3b06c
cambell <cambell_prince@sil.org>
2013-07-02 14:09:18 +0700

    Moved user service to angular-app/common/js

--- src/angular-app/projectadmin/js/services.js
+++ src/angular-app/projectadmin/js/services.js
@@ -6,20 +6,4 @@
 // In this case it is a simple value service.
 angular.module('projectAdmin.services', [])
 	.value('version', '0.1')
-	.service('userService', ['jsonRpc', function(jsonRpc) {
-		jsonRpc.connect('/api/sf'); // Note this doesn't actually 'connect', it simply sets the connection url.
-
-		this.read = function(id) {
-	
-		};
-		this.update = function(model) {
-	
-		};
-		this.remove = function(id) {
-	
-		};
-		this.typeahead = function(term, callback) {
-			jsonRpc.call('user_typeahead', [term], callback);
-		};
-	}])
 	;
