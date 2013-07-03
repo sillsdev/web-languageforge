a3a8ea59f6cdc9bf6ad2b3d86edd7b268089b459
cambell-prince <cambell.prince@gmail.com>
2013-07-03 11:05:17 +0700

    wip: listview control Former-commit-id: a1cec15cb784a17e683814a5eca4070363304793

--- src/views/apps/projectadmin.html.php
+++ src/views/apps/projectadmin.html.php
@@ -1,8 +1,9 @@
 <div class="container" ng-app="projectAdmin">
 	<div style="margin-top: 50px"><h2>Project Administration</h2></div>
 
-	<tabset>
+	<tabset ng-cloak>
 	<tab heading="Users">
+	<!-- 
 		<div ng-controller="UserSearchCtrl" style="overflow:hidden">
 		<label>User</label>
 		<typeahead class="typeahead" items="user" term="term" search="searchUser(term)" select="selectUser(item)">
@@ -15,23 +16,27 @@
 			</ul>
 		</typeahead>
 		</div>
+		 -->
 		<legend>Users (total: {{data.entries.length}})</legend>
 		<div ng-controller="UserListCtrl">
+		<div listview items="user" search="queryUsers()" select="selectUser(item)">
 		<table class="table" style="width: 100%">
 			<thead>
 			<tr>
 				<th>Username</th>
 				<th>Full Name</th>
-			</tr>
+				<th>Role</th>
+				</tr>
 			</thead>
 			<tbody>
-			<tr ng-repeat="user in users" ng-class="{info: $index==selectedIndex}" ng-click="selectRow($index, user)">
+			<tr listview-item="user" ng-repeat="user in users"	>
 				<td>{{user.username}}</td>
 				<td>{{user.name}}</td>
+				<td>&nbsp;</td>
 			</tr>
 			</tbody>
-		</table>
-		<pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="'&lsaquo;'" next-text="'&rsaquo;'" first-text="'&laquo;'" last-text="'&raquo;'"></pagination>
+		</table>		
+		</div>
 		<div class="left"><a href="#" ng-click="addRecord()" class="btn btn-small"><i class="icon-plus"></i> Add New</a></div>
 		<div class="right"><a href="#" ng-click="deleteRecord(vars.record)" class="btn btn-small" ng-class="{disabled: vars.selectedIndex == -1}"><i class="icon-minus"></i> Delete</a></div>
 		</div>
@@ -50,6 +55,7 @@
 	<script	src="/js/lib/ng-ui-bootstrap-tpls-0.4.0.js"></script>
 	<script	src="/angular-app/common/js/jsonrpc.js"></script>
 	<script	src="/angular-app/common/js/typeahead.js"></script>
+	<script	src="/angular-app/common/js/listview.js"></script>
 	<script	src="/angular-app/common/js/services.js"></script>
 	<!-- 	<script	src="/angular-app/projectadmin/js/app.js"></script> -->
 	<script	src="/angular-app/projectadmin/js/controllers.js"></script>
