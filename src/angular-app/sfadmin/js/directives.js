efba9f7f890c28eea4447baba10e1ab8ebfbc805
Robin Munn <Robin_Munn@sil.org>
2013-07-04 15:01:58 +0700

    Add require-equal directive for validation
    The require-equal directive is designed to be used as an
    attribute on an input field, with the value of the attribute
    being the Angular expression whose value this input field
    should match. Most useful in password-changing inputs, e.g.:
    
    <input type="password" ng-model="record.password"/>
    <input type="password" ng-model="record.confirm" require-equal="record.password"/>

--- src/angular-app/sfadmin/js/directives.js
+++ src/angular-app/sfadmin/js/directives.js
@@ -71,6 +71,30 @@ angular.module('sfAdmin.directives', ["jsonRpc", "sfAdmin.filters"]).
 		  restrict: "E",
 		  templateUrl: "/angular-app/sfadmin/partials/projectlist.html",
   }})
+.directive("requireEqual", function() {
+	return {
+		restrict: "A",
+		require: "ngModel",
+		scope: {
+			requireEqual: "=",
+		},
+		// Basic idea is it's a directive to do validation, used like this:
+		// <input type="password" ng-model="record.password"/>
+		// <input type="password" ng-model="record.confirmPassword" require-equal="record.password"/>
+		link: function(scope, elem, attrs, ngModelCtrl) {
+			// If for some reason we're getting called early, when ngModelCtrl
+			// is not yet available, then do nothing.
+			if (!ngModelCtrl) return;			
+			ngModelCtrl.$parsers.unshift(function(newValue) {
+				if (newValue == scope.requireEqual) {
+					ngModelCtrl.$setValidity("match", true);
+				} else {
+					ngModelCtrl.$setValidity("match", false);
+				}
+			});
+		},
+	};
+})
 // This directive's code is from http://stackoverflow.com/q/16016570/
 .directive('ngFocus', function($parse, $timeout) {
 	return function(scope, elem, attrs) {
