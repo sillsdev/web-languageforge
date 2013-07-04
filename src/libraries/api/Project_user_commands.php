37bf7dd30154243a2b4862308ad2d8457d464500
cambell-prince <cambell.prince@gmail.com>
2013-07-04 17:27:56 +0700

    wip project user command
    Former-commit-id: ff33e4147e28eddc216eb6dea22ccc68f6e2b808

--- src/libraries/api/Project_user_commands.php
+++ src/libraries/api/Project_user_commands.php
@@ -10,12 +10,39 @@ class Project_user_commands {
 		$this->_projectModel = $projectModel;
 	}
 	
+	/**
+	 * Added the user represented by the json array equivalent.
+	 * { id: ...
+	 *   name: ...
+	 *   email: ...
+	 * }
+	 * Only one of the above is set.
+	 * If id is set, add the existing user to the project.
+	 * If name is set, create a new user and add to the project.
+	 * If email is set, create a user, add to the project, and email to invite user to complete sign up.
+	 * @param array $object
+	 * @return string
+	 */
 	public function addUser($object) {
-		// If we've got a key then try and read existing user.
-		
-		// No key, so create a new user.
-		
+		$userId = null;
+		if (array_key_exists('id', $object)) {
+			// TODO Check user exists? CP 2013-07
+			$userId = $object['id'];
+		} else if (array_key_exists('email', $object)) {
+			throw new Exception("Project_user_commands::addUser with email NYI");
+		} else if (array_key_exists('name', $object)) {
+			// No key, so create a new user.
+			$user = new User_model();
+			$user->name = $object['name'];
+			// TODO passwords, how to notify, email? CP 2013-07
+			$userId = $user->write();
+		} else {
+			throw new Exception("Project_user_commands::addUser with unsupported data");
+		}
 		// Add the user to the project.
+		assert($userId != null);
+		$this->_projectModel->addUser($userId);
+		return $userId;
 	}
 	
 	public function deleteProject() {
