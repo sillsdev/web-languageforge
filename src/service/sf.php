f3d5e604ad1914233140d8da26f90560a0e84fdc
cambell-prince <cambell.prince@gmail.com>
2013-07-05 15:08:40 +0700

    Add helpers/loader_helper to use php5 auto loading.
    * And subsequent rework.
    
    Former-commit-id: 86309bef105a910baa31b9072ce0d2f901ca196e

--- src/service/sf.php
+++ src/service/sf.php
@@ -2,28 +2,30 @@
 
 require_once(APPPATH . 'libraries/Bcrypt.php');
 
+require_once(APPPATH . 'models/UserModel.php');
+require_once(APPPATH . 'models/ProjectModel.php');
+
+use libraries\sf\JsonRpcServer;
+
 class Sf
 {
 	
 	public function __construct()
 	{
 		$CI =& get_instance();
-		$CI->load->model('User_model');
-		$CI->load->model('Password_model');
-		$CI->load->model('Project_model');
 		$CI->load->library('bcrypt',8); // Might increase this at some future date to increase PW hashing time
 		// TODO put in the LanguageForge style error handler for logging / jsonrpc return formatting etc. CP 2013-07
-		ini_set('display_errors', 0);
+//		ini_set('display_errors', 0);
 	}
 
 	/**
 	 * Create/Update a User
-	 * @param User_model $json
+	 * @param UserModel $json
 	 * @return string Id of written object
 	 */
 	public function user_update($params) {
-		$user = new User_model();
-		Jsonrpc_server::decode($user, $params);
+		$user = new \models\UserModel();
+		JsonRpcServer::decode($user, $params);
 		$result = $user->write();
 		return $result;
 	}
@@ -33,7 +35,7 @@ class Sf
 	 * @param string $id
 	 */
 	public function user_read($id) {
-		$user = new User_model($id);
+		$user = new \models\UserModel($id);
 		return $user;
 	}
 	
@@ -43,31 +45,31 @@ class Sf
 	 * @return string Id of deleted record
 	 */
  	public function user_delete($id) {
- 		User_model::remove($id);
+ 		\models\UserModel::remove($id);
 		return true;
  	}
 
 	// TODO Pretty sure this is going to want some paging params
 	public function user_list() {
-		$list = new User_list_model();
+		$list = new \models\UserListModel();
 		$list->read();
 		return $list;
 	}
 	
 	public function user_typeahead($term) {
-		$list = new User_typeahead_model($term);
+		$list = new \models\UserTypeaheadModel($term);
 		$list->read();
 		return $list;
 	}
 	
 	/**
 	 * Create/Update a Project
-	 * @param Project_model $json
+	 * @param ProjectModel $json
 	 * @return string Id of written object
 	 */
 	public function project_update($object) {
-		$project = new Project_model();
-		Jsonrpc_server::decode($project, $object);
+		$project = new \models\ProjectModel();
+		JsonRpcServer::decode($project, $object);
 		$result = $project->write();
 		return $result;
 	}
@@ -77,7 +79,7 @@ class Sf
 	 * @param string $id
 	 */
 	public function project_read($id) {
-		$project = new Project_model($id);
+		$project = new \models\ProjectModel($id);
 		return $project;
 	}
 	
@@ -87,19 +89,19 @@ class Sf
 	 * @return string Id of deleted record
 	 */
  	public function project_delete($id) {
- 		Project_model::remove($id);
+ 		\models\ProjectModel::remove($id);
 		return true;
  	}
 
 	// TODO Pretty sure this is going to want some paging params
 	public function project_list() {
-		$list = new Project_list_model();
+		$list = new \models\ProjectListModel();
 		$list->read();
 		return $list;
 	}
 	
 	public function change_password($userid, $newPassword) {
-		$user = new Password_model($userid);
+		$user = new \models\PasswordModel($userid);
 		$bcrypt = new Bcrypt();
 		$user->password = $bcrypt->hash($newPassword);
 		$user->remember_code = null;
@@ -107,24 +109,25 @@ class Sf
 	}
 	
 	public function project_readUser($projectId, $userId) {
-		throw new Exception("project_readUser NYI");
+		throw new \Exception("project_readUser NYI");
 	}
 	
 	public function project_updateUser($projectId, $object) {
-		$projectModel = new Project_model($projectId);
-		$command = new Project_user_commands($projectModel);
+		
+		$projectModel = new \models\ProjectModel($projectId);
+		$command = new \libraries\api\ProjectUserCommands($projectModel);
 		return $command->addUser($object);
 	}
 	
 	public function project_deleteUser($projectId, $userId) {
 		// This removes the user from the project.
-		$projectModel = new Project_model($projectId);
+		$projectModel = new \models\ProjectModel($projectId);
 		$projectModel->removeUser($userId);
 		$projectModel->write();
 	}
 	
 	public function project_listUsers($projectId) {
-		$projectModel = new Project_model($projectId);
+		$projectModel = new \models\ProjectModel($projectId);
 		return $projectModel->listUsers();
 	}
 	
