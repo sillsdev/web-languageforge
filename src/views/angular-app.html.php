8df021d88810d1b953ed3149ee068561413144e7
megahirt <chris@hirtfamily.net>
2013-07-16 14:25:26 +0700

    fixed changepassword app confirm_password input validation

--- src/views/angular-app.html.php
+++ src/views/angular-app.html.php
@@ -8,6 +8,7 @@ window.session = <?php echo $jsSessionVars; ?>
 	
 <script	src="/js/lib/angular_stable_1.0.7/angular.js"></script>
 <script	src="/js/lib/ng-ui-bootstrap-tpls-0.4.0.js"></script>
+<script src="/js/lib/ng-ui-utils-validate.js"></script>
 
 <?php foreach($jsCommonFiles as $filename): ?>
     <script	src="/angular-app/common/js/<?php echo $filename; ?>"></script>
