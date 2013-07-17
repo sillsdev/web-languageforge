12d7f973bc31dd001edc7389d613a1a7763a6489
Robin Munn <robin_munn@sil.org>
2013-07-17 18:02:08 +0700

    Add "items per page" selector to list view

--- src/angular-app/common/js/listview.js
+++ src/angular-app/common/js/listview.js
@@ -6,7 +6,7 @@ angular.module('palaso.ui.listview', [])
 			restrict : 'EA',
 			transclude : true,
 			replace : true,
-			template : '<div class="listview"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="\'&lsaquo;\'" next-text="\'&rsaquo;\'" first-text="\'&laquo;\'" last-text="\'&raquo;\'"></pagination></div></div>',
+			template : '<div class="listview"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="\'&lsaquo;\'" next-text="\'&rsaquo;\'" first-text="\'&laquo;\'" last-text="\'&raquo;\'"></pagination><div class="right">Items per page: <select ng-model="itemsPerPage"><option value="10" selected>10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></div></div></div>',
 			scope : {
 				search : "&",
 				select : "&",
@@ -15,10 +15,10 @@ angular.module('palaso.ui.listview', [])
 				item: "="
 			},
 			controller: ["$scope", function($scope) {
-				$scope.noOfPages = 3;
+				$scope.noOfPages = 3;  // TODO: calculate this automatically
 				$scope.currentPage = 1;
 				$scope.maxSize = 5;
-				$scope.itemsPerPage = 50;  // TODO: Add a control to change this RM 2013-07
+				$scope.itemsPerPage = 10;  // This should match the default value for the selector above
 				$scope.items = [];
 				
 				this.activate = function(item) {
@@ -49,8 +49,8 @@ angular.module('palaso.ui.listview', [])
 				};
 			}],
 			link : function(scope, element, attrs, controller) {
-				scope.$watch('currentPage', function(currentPage) {
-					controller.query(currentPage, scope.itemsPerPage);
+				scope.$watch('currentPage + itemsPerPage', function(currentPage, itemsPerPage) {
+					controller.query(currentPage, itemsPerPage);
 				});
 			}
 		};
