ca077170093f4f544e7831941f70592d2137d520
megahirt <chris@hirtfamily.net>
2013-07-18 14:40:21 +0700

    breadcrumbs now working in sfchecks #30

--- src/angular-app/sfchecks/js/services.js
+++ src/angular-app/sfchecks/js/services.js
@@ -3,37 +3,28 @@
 /* Services */
 
 angular.module('sfchecks.services', [])
-	.factory('breadcrumbs', ['$rootScope', '$routeParams', function($rootScope, $r) {
+	.factory('breadcrumbService', ['$rootScope', '$routeParams', function($rootScope, $r) {
 		
 		var breadcrumbService = {};
 		
-		breadcrumbService.getAll = function() {
-			var breadcrumb = {};
+		breadcrumbService.read = function() {
 			var crumbs = [];
-			var projectListLabel = "My Projects";
 			var url = "#/project";
-			breadcrumb.here = projectListLabel;
-			crumbs.push({"label": projectListLabel, "url": url});
 			if ($r.projectName) {
 				url = url + "/" + encodeURIComponent($r.projectName) + "/" + $r.projectId;
 				crumbs.push({"label": $r.projectName, "url": url});
-				breadcrumb.here = $r.projectName;
 			}
 			if ($r.textName) {
 				url = url + "/" + encodeURIComponent($r.textName) + "/" + $r.textId;
 				crumbs.push({"label": $r.textName, "url": url});
-				breadcrumb.here = $r.textName;
 			}
 			if ($r.questionName) {
 				url = url + "/" + encodeURIComponent($r.questionName) + "/" + $r.questionId;
 				crumbs.push({"label": $r.questionName, "url": url});
-				breadcrumb.here = $r.questionName;
 			}
-			breadcrumbs.crumbs = crumbs;
-			return breadcrumbs;
+			return crumbs;
 		}
 		
 		return breadcrumbService;
 		
-	}])
-	;
+	}]);
