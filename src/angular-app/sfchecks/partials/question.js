80b48ca26094f0675b95e0177bcdcaa881c10e11
cambell-prince <cambell.prince@gmail.com>
2013-07-18 15:35:36 +0700

    wip #28: Add rough cut for question.html with sample data.

--- src/angular-app/sfchecks/partials/question.js
+++ src/angular-app/sfchecks/partials/question.js
@@ -5,5 +5,33 @@ angular.module(
 		[ 'sf.services', 'palaso.ui.listview', 'palaso.ui.typeahead', 'ui.bootstrap' ]
 	)
 	.controller('QuestionCtrl', ['$scope', 'projectService', function($scope, projectService) {
+		$scope.question = {
+				'title': 'Who is Sherlock refering to in the text?',
+				'content': 'Some pertinent question'
+		};
+		$scope.answers = 
+			[{
+				'content': 'Some relevant answer',
+				'by': 'Robin',
+				'score': '6',
+				'comments':
+				[{
+					'content': 'Some engaging comment',
+					'by': 'Cambell'
+				}, {
+					'content': 'Some dispute',
+					'by': 'Chris'
+				}]
+			}, {
+				'content': 'Some relevant answer',
+				'by': 'Cambell',
+				'score': '3',
+				'comments':
+				[{
+					'content': 'Some engaging comment'
+				}, {
+					'content': 'Some dispute'
+				}]
+			}];
 	}])
 	;
