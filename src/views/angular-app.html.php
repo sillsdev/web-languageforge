dbf860d4a03c3f2e3bc95c245ad6a6a4790edab7
megahirt <chris@hirtfamily.net>
2013-07-18 15:15:11 +0700

    added jquery-te WYSIWYG editor as an include into the app space

--- src/views/angular-app.html.php
+++ src/views/angular-app.html.php
@@ -9,6 +9,7 @@ window.session = <?php echo $jsSessionVars; ?>
 <script	src="/js/lib/angular_stable_1.0.7/angular.js"></script>
 <script	src="/js/lib/ng-ui-bootstrap-tpls-0.4.0.js"></script>
 <script src="/js/lib/ng-ui-utils-validate.js"></script>
+<script src="/js/lib/jquery-te-1.4.0.min.js"></script>
 
 <?php foreach($jsCommonFiles as $filename): ?>
 <script src="/<?php echo $filename; ?>"></script>
