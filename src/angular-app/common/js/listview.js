8889108d5f77ae4cd4becc6b5f16429e0fd18bc7
Robin Munn <robin_munn@sil.org>
2013-07-18 11:41:54 +0700

    Align items-per-page control with paging controls
    Minor CSS tweaks here, no functionality change.

--- src/angular-app/common/js/listview.js
+++ src/angular-app/common/js/listview.js
@@ -6,7 +6,7 @@ angular.module('palaso.ui.listview', [])
 			restrict : 'EA',
 			transclude : true,
 			replace : true,
-			template : '<div class="listview"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="\'&lsaquo;\'" next-text="\'&rsaquo;\'" first-text="\'&laquo;\'" last-text="\'&raquo;\'"></pagination><div class="right">Items per page: <select ng-model="itemsPerPage"><option value="10" selected>10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></div></div></div>',
+			template : '<div class="listview"><div ng-transclude></div><div class="paginationblock"><pagination boundary-links="true" num-pages="noOfPages" current-page="currentPage" previous-text="\'&lsaquo;\'" next-text="\'&rsaquo;\'" first-text="\'&laquo;\'" last-text="\'&raquo;\'"></pagination><div class="right pagination">Items per page: <select ng-model="itemsPerPage"><option value="10" selected>10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></div></div></div>',
 			scope : {
 				search : "&",
 				select : "&",
