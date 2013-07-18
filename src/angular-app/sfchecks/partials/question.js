208a5edf08971b6c26b1438345701e562a467c40
megahirt <chris@hirtfamily.net>
2013-07-18 16:27:05 +0700

    integrated jquery-te into question.html

--- src/angular-app/sfchecks/partials/question.js
+++ src/angular-app/sfchecks/partials/question.js
@@ -5,6 +5,24 @@ angular.module(
 		[ 'sf.services', 'palaso.ui.listview', 'palaso.ui.typeahead', 'ui.bootstrap' ]
 	)
 	.controller('QuestionCtrl', ['$scope', 'projectService', function($scope, projectService) {
+		$(".jqte").jqte({
+			"placeholder": "Say what you think...",
+			"u": false,
+			"indent": false,
+			"outdent": false,
+			"left": false,
+			"center": false,
+			"right": false,
+			"rule": false,
+			"source": false,
+			"link": false,
+			"unlink": false,
+			"fsize": false,
+			"formats": [
+				["p", "Normal"],
+				["h4", "Large"]
+			]
+		});
 		$scope.question = {
 				'title': 'Who is Sherlock refering to in the text?',
 				'content': 'Some pertinent question'
