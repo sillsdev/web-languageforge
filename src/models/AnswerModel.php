5395c10eee27baf3e98d692db6131e0252b7287e
cambell-prince <cambell.prince@gmail.com>
2013-07-31 15:53:43 +0700

    Added comments array to AnswerModel.

--- src/models/AnswerModel.php
+++ src/models/AnswerModel.php
@@ -2,10 +2,18 @@
 
 namespace models;
 
+use models\mapper\ArrayOf;
+
 class AnswerModel extends CommentModel
 {
 	public function __construct() {
 		parent::__construct();
+		$this->comments = new ArrayOf(
+			ArrayOf::OBJECT,
+			function() {
+				return new CommentModel();
+			}
+		);
 	}
 	
 	/**
