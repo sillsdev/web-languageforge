53bb56daacd62f2b4134148a17e9cc3a725ecb8b
cambell-prince <cambell.prince@gmail.com>
2013-08-01 14:19:21 +0700

    Fix use of _decode in MongoDecoder

--- src/models/mapper/MongoDecoder.php
+++ src/models/mapper/MongoDecoder.php
@@ -53,7 +53,7 @@ class MongoDecoder extends JsonDecoder {
 		foreach ($data as $item) {
 			if ($model->getType() == ArrayOf::OBJECT) {
 				$object = $model->generate($item);
-				$this->decode($object, $item, false);
+				$this->_decode($object, $item, false);
 				$model->data[] = $object;
 			} else if ($model->getType() == ArrayOf::VALUE) {
 				if (is_array($item)) {
