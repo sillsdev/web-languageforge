b1bfffa650ecc0834c6466113a2a269a386756b9
megahirt <chris@hirtfamily.net>
2013-08-01 14:11:33 +0700

    added DtoEncoder class

--- src/models/dto/QuestionCommentDto.php
+++ src/models/dto/QuestionCommentDto.php
@@ -2,9 +2,32 @@
 
 namespace models\dto;
 
+use models\UserModel;
+
+use models\TextModel;
+
+use models\mapper\JsonEncoder;
+
 use models\ProjectModel;
 
 use models\QuestionModel;
+
+class DtoEncoder extends JsonEncoder {
+	
+	public function encodeIdReference($key, $model) {
+		if ($key == 'userRef') {
+			$user = new UserModel($model->id);
+			return array(
+					'userid' => $user->id->asString(),
+					'avatar_ref' => $user->avatar_ref,
+					'username' => $user->username);
+		} else {
+			$result = $model->id;
+			return $result;
+		}
+	}
+}
+
 
 class QuestionCommentDto
 {
@@ -12,47 +35,18 @@ class QuestionCommentDto
 	 * 
 	 * @param string $projectId
 	 * @param string $questionId
+	 * @returns array - the DTO array
 	 */
 	public static function encode($projectId, $questionId) {
+		$e = new DtoEncoder();
+		$question = $e->encode($questionModel);
+		$textId = $questionModel->textRef->asString();
+		$textModel = new TextModel($projectModel, $textId);
+		$text = JsonEncoder::encode($textModel);
 		$data = array();
-		
-		$projectModel = new ProjectModel($this->projectId);
-		$questionModel = new QuestionModel($projectModel, $this->questionId);
-		$data['projectid'] = $this->projectId;
-		$data['questionid'] = $this->questionId;
-		$data['title'] = $questionModel->title;
-		$data['description'] = $questionModel->description;
-		$data['answers'] = array();
-		
-		foreach ($questionModel->answers as $answerModel) {
-			$answer = array();
-			$answer['userid'] = $answerModel->userId;
-			$answer['date_created'] = $answerModel->dateCreated;
-			$answer['date_edited'] = $answerModel->dateEdited;
-			$answer['answer'] = $answerModel->content;
-			$answer['score'] = $answerModel->score;
-			
-			$userModel = new UserModel($answerModel->userId);
-			$answer['avatar_ref'] = $userModel->avatar_ref;
-			$answer['by'] = $userModel->username;
-		
-			$answer['comments'] = array();
-			foreach ($answerModel->comments as $commentModel) {
-				$comment = array();
-				$comment['userid'] = $commentModel->userId;
-				$comment['date_created'] = $commentModel->dateCreated;
-				$comment['date_edited'] = $commentModel->dateEdited;
-				$comment['content'] = $commentModel->content;
-				$comment['text_ref'] = $commentModel->textRef;
-				
-				$userModel = new UserModel($commentModel->userId);
-				$comment['by'] = $userModel->username;
-				
-				array_push($answer['comments'], $comment);
-			}
-			
-			array_push($data['answers'], $answer);
-		}
+		$data['question'] = $question;
+		$data['text'] = $text;
+		$data['projectid'] = $projectId;
 		
 		return $data;
 	}
