9f972ed53e872cb90677d1f8e8e02edd90a0a122
Robin Munn <robin_munn@sil.org>
2013-08-02 18:02:54 +0700

    Revert "Get text count for project"
    This reverts commit af66d968c4040d8e5601a0e0d1c2f7aa61915cbd [formerly 68bab5925ebf65466cdfe9d33175e47343968f07].
    
    
    Former-commit-id: 845f43be4075ba3be3875446d8df13672ce54713

--- src/angular-app/sfchecks/partials/projects.js
+++ src/angular-app/sfchecks/partials/projects.js
@@ -4,7 +4,7 @@ angular.module(
 		'sfchecks.projects',
 		[ 'sf.services', 'palaso.ui.listview', 'palaso.ui.typeahead', 'ui.bootstrap' ]
 	)
-	.controller('ProjectsCtrl', ['$scope', 'projectService', 'textService', function($scope, projectService, textService) {
+	.controller('ProjectsCtrl', ['$scope', 'projectService', function($scope, projectService) {
 		// Listview Selection
 		$scope.selected = [];
 		$scope.updateSelection = function(event, item) {
@@ -72,19 +72,8 @@ angular.module(
 		};
 
 		$scope.getTextCount = function(project) {
-			if (angular.isUndefined(project.textCount)) {
-				console.log('Trying to get text count for', project.id);
-				textService.list(project.id, function(result) {
-					if (result.ok) {
-						console.log('Result OK, got', result.data.count);
-						project.textCount = result.data.count;
-					} else {
-						console.log('Result failed:', result);
-						project.textCount = undefined;
-					};
-				});
-			}
-			return project.textCount;
+			// return projects.texts.count;
+			return fakeData.textCount;
 		}
 
 		$scope.getViewsCount = function(project) {
