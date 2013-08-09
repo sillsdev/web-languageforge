bcbebf84d78ac22a75b0b394b709798f782e8998
megahirt <chris@hirtfamily.net>
2013-08-09 16:39:59 +0700

    wip: ActivityDto
    Former-commit-id: d36e34ef9581a2a0ef25f83292b6f325ce2a16a5

--- src/models/dto/ActivityListDto.php
+++ src/models/dto/ActivityListDto.php
@@ -4,6 +4,12 @@ namespace models\dto;
 
 
 
+use models\ActivityListModel;
+
+use models\ProjectListModel;
+
+use models\UserModel;
+
 use models\ProjectModel;
 
 
@@ -14,10 +20,25 @@ class ActivityListDto
 	 * @param string $questionId
 	 * @return array - the DTO array
 	 */
-	public static function encode($projectId, $userId) {
+	public static function getActivityForProject($projectId) {
 		$projectModel = new ProjectModel($projectId);
+		$activityList = new ActivityListModel($projectModel);
+		return DtoEncoder::encode($activityList);
 	}
 	
+	public static function getActivityForUser($userId) {
+		/*
+		$user = new UserModel($userId);
+		$projectList = $user->listProjects();
+		*/
+		$projectList = new ProjectListModel();
+		$dto = array();
+		foreach ($projectList->entries as $project) {
+			$projectId = $project->id->asString();	
+			$dto = array_merge($dto, $this::getActivityForProject($projectId));
+		}
+		return $dto;
+	}
 }
 
 ?>
