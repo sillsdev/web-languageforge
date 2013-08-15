2115b41ee80c9e5cd0e22217dad3f4af19266d84
cambell-prince <cambell.prince@gmail.com>
2013-08-15 15:08:17 +0700

    Fix ng-jqte directive that was not binding in both directions.

--- src/angular-app/common/js/ng-jqte.js
+++ src/angular-app/common/js/ng-jqte.js
@@ -11,15 +11,19 @@ angular.module('palaso.ui.jqte', [])
 					return;
 				}
 				ngModelCtrl.$render = function() {
-					element.val(ngModelCtrl.$viewValue);
+					var phase = scope.$$phase;
+					console.log('render ', phase);
+					element.jqteVal(ngModelCtrl.$viewValue);
 				};
 				var options = scope[attrs.puiJqte] === undefined ? {} : scope[attrs.puiJqte];
 				options.change = function() {
-					var v = element.val();
-					console.log('change', v);
+					if (scope.$$phase == '$digest') {
+						return;
+					}
 					scope.$apply(read);
 				}; 
 				element.jqte(options);
+				read();
 				function read() {
 					ngModelCtrl.$setViewValue(element.val());
 				}
