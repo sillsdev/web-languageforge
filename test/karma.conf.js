315cd3a21835fd7d6aa8c45bf3377df75d56c5dc
cambell-prince <cambell.prince@gmail.com>
2013-08-16 14:39:41 +0700

    Updated test/karma.conf.js for karma v 0.10.1
    Former-commit-id: afb5b9e4108ebe7a861a3ca89dd77145c9e51eb1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,56 +1,58 @@
 // Karma configuration
+module.exports = function(config) {
+  config.set({
+    basePath: '..',
+    frameworks: ['jasmine'],
+    // list of files / patterns to load in the browser
+    files: [
+      'src/js/lib/angular_stable_1.0.7/angular.js',
+      'test/lib/angular/angular-mocks.js',
+      'src/js/lib/*ui-bootstrap*.js',
+      'src/angular-app/**/*.js',
+      'test/app/admin/unit/controllersSpec.js',
+      'test/spec/**/*.spec.js'
+    ],
 
-// base path, that will be used to resolve files and exclude
-basePath = '..';
+    // list of files to exclude
+    exclude: [],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'src/js/lib/angular_stable_1.0.7/angular.js',
-  'test/lib/angular/angular-mocks.js',
-  'src/js/lib/*ui-bootstrap*.js',
-  'src/angular-app/**/*.js',
-  'test/app/admin/unit/controllersSpec.js',
-  'test/spec/**/*.spec.js'
-];
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress'],
 
-// list of files to exclude
-exclude = [];
+    // web server port
+    port: 8080,
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // cli runner port
+    runnerPort: 9100,
 
-// web server port
-port = 8080;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// cli runner port
-runnerPort = 9100;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO ||
+	// LOG_DEBUG
+    logLevel: config.LOG_WARN,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable watching file and executing tests whenever any file
+	// changes
+    autoWatch: false,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_WARN;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 8000,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 8000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
