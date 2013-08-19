85d74c9b6281e86ba9bacd59580a4b7008bb9f01
cambell-prince <cambell.prince@gmail.com>
2013-08-19 14:25:05 +0700

    wip: session variables for rights.

--- src/controllers/secure_base.php
+++ src/controllers/secure_base.php
@@ -6,9 +6,8 @@ class Secure_base extends Base {
 	
 	function __construct() {
 		parent::__construct();
-		$this->load->library('ion_auth');
 		$this->load->helper('url');
-		if (!$this->ion_auth->logged_in())
+		if (!$this->_isLoggedIn)
 		{
 			//redirect them to the login page
 			redirect('auth/login', 'refresh');
