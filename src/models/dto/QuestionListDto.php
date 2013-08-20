0c82860f5e86c75858c2c9a348b74042e1473380
cambell-prince <cambell.prince@gmail.com>
2013-08-20 14:42:30 +0700

    rights: questions page Former-commit-id: 691690df31516929328e35eac0a79ef0b29df29d

--- src/models/dto/QuestionListDto.php
+++ src/models/dto/QuestionListDto.php
@@ -3,9 +3,8 @@
 namespace models\dto;
 
 use models\ProjectModel;
-
+use models\UserModel;
 use models\QuestionAnswersListModel;
-
 
 class QuestionListDto
 {
@@ -17,13 +16,14 @@ class QuestionListDto
 	 * @returns array - the DTO array
 	 */
 	public static function encode($projectId, $textId, $userId) {
+		$userModel = new UserModel($userId);
 		$projectModel = new ProjectModel($projectId);
 		//$textModel = new TextModel($projectModel, $textId);
 		$questionList = new QuestionAnswersListModel($projectModel, $textId);
 		$questionList->read();
 
 		$data = array();
-		$data['rights'] = $projectModel->getRightsArray($userId);
+		$data['rights'] = RightsHelper::encode($userModel, $projectModel);
 		$data['count'] = $questionList->count;
 		$data['entries'] = array();
 		foreach ($questionList->entries as $questionData) {
