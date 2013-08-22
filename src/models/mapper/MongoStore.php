986f4339c1f4d5453d4c5f48655d71be2a14795a
Robin Munn <robin_munn@sil.org>
2013-08-22 16:31:25 +0700

    Rename databases when renaming projects
    Note that since MongoDB doesn't implement a renameDatabase() command,
    you have to copy the database over and then delete the old one. See
    http://stackoverflow.com/questions/14701418/rename-mongo-database for
    one example of how to do this in PHP.

--- src/models/mapper/MongoStore.php
+++ src/models/mapper/MongoStore.php
@@ -42,7 +42,53 @@ class MongoStore
 		$result = array_filter($databases['databases'], function($item) use($databaseName) { return $item['name'] == $databaseName; } );
 		return count($result) != 0;
 	}
-	
+
+	/**
+	 * @param string $sourceName
+	 * @param string $destName
+	 * @return MongoDB
+	 */
+	public static function copyDB($sourceName, $destName) {
+		$response = static::$_mongo->admin->command(array(
+			'copydb' => 1,
+			'fromhost' => 'localhost',
+			'fromdb' => $sourceName,
+			'todb' => $destName,
+		));
+		return $response;
+	}
+
+	/**
+	 * @param string $databaseName
+	 * @return MongoDB
+	 */
+	public static function dropDB($databaseName) {
+		$oldDB = MongoStore::connectMongo($databaseName);
+		$response = $oldDB->drop();
+		return $response;
+	}
+
+	/**
+	 * @param string $oldName
+	 * @param string $newName
+	 * @return MongoDB
+	 */
+	public static function renameDB($oldName, $newName) {
+		// See http://stackoverflow.com/questions/14701418/rename-mongo-database
+		if (!MongoStore::hasDB($oldName)) {
+			throw new \Exception("Database " . $oldName . " does not exist; cannot rename it to " . $newName . ".");
+		}
+		if (MongoStore::hasDB($newName)) {
+			throw new \Exception("Database " . $newName . " already exists; not renaming " . $oldName . " to " . $newName . ".");
+		}
+		$copyResult = MongoStore::copyDB($oldName, $newName);
+		$dropResult = MongoStore::dropDB($oldName);
+		$result = array(
+			'copyResult' => $copyResult,
+			'dropResult' => $dropResult
+		);
+		return $result;
+	}
 }
 
 ?>
