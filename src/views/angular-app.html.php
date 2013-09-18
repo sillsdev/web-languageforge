734c705e6b749478ccf592b813e348ea1baa467e
Robin Munn <robin_munn@sil.org>
2013-09-18 16:57:16 +0700

    Use safe, sanitized HTML for answers, comments
    Fixes #53
    
    
    Former-commit-id: fade4aec7f590fc9216afd316b56b5a82d66f0ce

--- src/views/angular-app.html.php
+++ src/views/angular-app.html.php
@@ -7,6 +7,7 @@ window.session = <?php echo $jsonSession; ?>
 </script>
 	
 <script	src="/js/lib/angular_stable_1.0.7/angular.js"></script>
+<script	src="/js/lib/angular_stable_1.0.7/angular-sanitize.js"></script>
 <script	src="/js/lib/ng-ui-bootstrap-tpls-0.4.0.js"></script>
 <script src="/js/lib/ng-ui-utils-validate.js"></script>
 <script src="/js/lib/jquery-te-1.4.0.min.js"></script>
