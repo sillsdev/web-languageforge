fade4aec7f590fc9216afd316b56b5a82d66f0ce
Robin Munn <robin_munn@sil.org>
2013-09-18 16:57:16 +0700

    Use safe, sanitized HTML for answers, comments
    Fixes #53

--- src/angular-app/sfchecks/ng-app.js
+++ src/angular-app/sfchecks/ng-app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('sfchecks', 
 		[
+		 'ngSanitize',
 		 'sfchecks.projects',
 		 'sfchecks.project',
 		 'sfchecks.questions',
