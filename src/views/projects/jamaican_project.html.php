ecb566c5f7d669948ba4f59fa47f6148383416b2
megahirt <chris@hirtfamily.net>
2013-09-23 09:08:52 +0700

    project home page functionality

---
+++ src/views/projects/jamaican_project.html.php
@@ -0,0 +1,8 @@
+<div class="sfcontainer banner">
+	<img src="/images/beach1.jpg" width="965px" height="360px" alt="on the beach" />
+</div>
+<div class="sfcontainer">
+			<h2>Contribute</h2>
+			<p>Jamaican Project Home Page</p>
+			
+		</div>
