7dd4d4760bdcf1f509aec5d48b446a02a276f228
cambell-prince <cambell.prince@gmail.com>
2013-09-26 17:07:42 +0700

    email: wip add composer, mustache, swiftmailer.
    * Email class.
    * Email helper class.

---
+++ src/libraries/sfchecks/Email.php
@@ -0,0 +1,58 @@
+<?php
+namespace libraries\sfchecks;
+
+use models\UserModel;
+
+class EmailHelper
+{
+	/**
+	 * @param UserModel $userModel
+	 * @return string
+	 */
+	public static function addValidateTagToUser($userModel) {
+		$tag = sha1(microtime(true).mt_rand(10000,90000));
+	}
+	
+	public static function template($name) {
+		$m = self::templateEngine();
+		return $m->loadTemplate($name);
+	}
+	
+	/**
+	 * @return \Mustache_Engine
+	 */
+	public static function templateEngine() {
+		static $instance = null;
+		if (null === $instance) {
+			$instance = new Mustache_Engine(array(
+					'loader' => new Mustache_Loader_FilesystemLoader(APPPATH . 'views/email/en'),
+					'partials_loader' => new Mustache_Loader_FilesystemLoader(APPPATH . 'views/email/en/partials')
+			));
+		}
+		return $instance;
+	}
+	
+	public static function send() {
+	
+	}
+	
+}
+
+class Email
+{
+	public static function sendJoin($userId) {
+		$t = self::template('SignupValidate.tmpl');
+		$userModel = new UserModel($userId);
+		$validationKey = $userModel->setValidationKey();
+		$vars = array(
+			'user' =>$userModel,
+			
+		);
+		
+	}
+	
+	public static function sendJoinWithProject() {
+		
+	}
+	
+}
