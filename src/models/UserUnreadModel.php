3adc3a57034ac9d3eb3340ed578e73834dc7ea6a
megahirt <chris@hirtfamily.net>
2013-10-07 10:57:22 +0700

    userUnreadModel wip
    Former-commit-id: c7f22880eba175dda5337fe73fefe0241f8de139

--- src/models/UserUnreadModel.php
+++ src/models/UserUnreadModel.php
@@ -1,52 +1,89 @@
 <?php
 namespace models;
 
-use models\UnreadItem;
 use models\mapper\ArrayOf;
 use models\mapper\MongoMapper;
 use models\mapper\Id;
 use models\mapper\IdReference;
 
-class UnreadItem 
+class UnreadItem
 {
+	const ACTIVITY = 'activity';
+	const ANSWER = 'answer';
+	const COMMENT = 'comment';
+	
+	public function __construct() {
+		$this->itemRef = new IdReference();
+		$this->type = "";
+	}
+	
 	/**
+	 * @var IdReference
+	 */
+	public $itemRef;
+	
+	/**
+	 * 
 	 * @var string
 	 */
 	public $type;
-	
-	/**
-	 * @var IdReference
-	 */
-	public $questionRef;
-	
-	/**
-	 * @var IdReference
-	 */
-	public $answerRef;
-	
-	/**
-	 * @var IdReference
-	 */
-	public $commentRef;
 }
 
 class UserUnreadModel extends UserRelationModel
 {
-	public function __construct($id = '') {
+	public function __construct($userId = '') {
 		$this->unread = new ArrayOf(
 			ArrayOf::OBJECT, 
 			function($data) {
-				return new UnreadItem(); 
+				return new UnreadItem();
 			}
 		);
-		parent::__construct('unread', $id);
+		parent::__construct('unread_item', $userId);
+	}
+	
+	public function markUnread($itemType, $itemId) {
+		$item = new UnreadItem();
+		$item->itemRef->id = $itemId;
+		$item->type = $itemType;
+		if (in_array($item, $this->unread->data)) {
+			return;
+		}
+		$this->unread->data[] = $item;
+		
+	}
+	
+	public function markRead($itemType, $itemId) {
+		$item = new UnreadItem();
+		$item->itemRef->id = $itemId;
+		$item->type = $itemType;
+		foreach ($this->unread->data as $key => $value) {
+			if ($value == $item) {
+				unset($this->unread->data[$key]);
+			}
+		}
+	}
+	
+	public function isUnread($itemType, $itemId) {
+		$item = new UnreadItem();
+		$item->itemRef->id = $itemId;
+		$item->type = $itemType;
+		return in_array($item, $this->unread->data);
+	}
+	
+	public function unreadCount($itemType) {
+		$count = 0;
+		foreach ($this->unread->data as $key => $value) {
+			if ($value->type == $itemType) {
+				$count++;
+			}
+		}
+		return $count;
 	}
 	
 	/**
 	 * @var ArrayOf ArrayOf<UnreadItem>
 	 */
 	public $unread;
-	
 }
 
 ?>
