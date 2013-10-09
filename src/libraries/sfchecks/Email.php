5247cf654f0751a5f829dc0b433081176c60ed9e
irahopkinson <ira.hopkinson@gmail.com>
2013-10-09 16:46:16 +0700

    email: wip Twig templates and SwiftMailer
    * Removed mustache, no longer used.
    * Tests for signup content with validation key.
    * Mock swift mailer.

--- src/libraries/sfchecks/Email.php
+++ src/libraries/sfchecks/Email.php
@@ -3,55 +3,110 @@ namespace libraries\sfchecks;
 
 use models\UserModel;
 
+class EmailSwiftMailWrapper
+{
+
+	private $_to;
+	private $_from;
+	private $_body;
+	
+	public function setTo($to) {
+		$this->_to = $to;
+	}
+	
+	public function setFrom($from) {
+		$this->_from = $from;
+	}
+	
+	public function setBody($body) {
+		$this->_body = $body;
+	}
+	
+	/**
+	 * @param UserModel $userModel
+	 * @param string $content
+	 */
+	public function send() {
+		// Create the Transport
+		$transport = \Swift_SmtpTransport::newInstance('smtp.example.org', 25)
+			->setUsername('your username')
+			->setPassword('your password')
+			;
+		
+		// Create the Mailer using your created Transport
+		$mailer = \Swift_Mailer::newInstance($transport);
+		
+		// Create a message
+		$message = \Swift_Message::newInstance('Wonderful Subject');
+		$message->setFrom($this->_from);
+		$message->setTo($this->_to);
+		$message->setBody($this->_body);
+		
+		// Send the message
+		$result = $mailer->send($message);		
+		
+		
+	}
+}
+
 class EmailHelper
 {
 	/**
 	 * @param UserModel $userModel
 	 * @return string
 	 */
-	public static function addValidateTagToUser($userModel) {
-		$tag = sha1(microtime(true).mt_rand(10000,90000));
-	}
-	
-	public static function template($name) {
-		$m = self::templateEngine();
-		return $m->loadTemplate($name);
+	public static function addValidateKeyToUser($userModel) {
+		$key = sha1(microtime(true).mt_rand(10000,90000));
+		$userModel->validationKey = $key;
+		$userModel->validationDate = new \DateTime();
+		$userModel->write();
 	}
 	
 	/**
-	 * @return \Mustache_Engine
+	 * 
+	 * @param string $name
+	 * @return \Twig_Template
 	 */
-	public static function templateEngine() {
-		static $instance = null;
-		if (null === $instance) {
-			$instance = new Mustache_Engine(array(
-					'loader' => new Mustache_Loader_FilesystemLoader(APPPATH . 'views/email/en'),
-					'partials_loader' => new Mustache_Loader_FilesystemLoader(APPPATH . 'views/email/en/partials')
-			));
+	public static function template($name) {
+		$loader = new \Twig_Loader_Filesystem(APPPATH . '/views');
+		if (defined('TestMode')) {
+			$options = array();
+		} else {
+			$options = array(
+				'cache' => APPPATH . '/cache',
+			);
 		}
-		return $instance;
-	}
-	
-	public static function send() {
-	
+		$twig = new \Twig_Environment($loader, $options);
+		$template = $twig->loadTemplate($name);
+		return $template;
 	}
 	
 }
 
 class Email
 {
-	public static function sendJoin($userId) {
-		$t = self::template('SignupValidate.tmpl');
+	/**
+	 * Send an email to validate a user when they sign up.
+	 * @param string $userId
+	 * @param SwiftMailer $mailer
+	 */
+	public static function sendSignup($userId, $mailer = null) {
 		$userModel = new UserModel($userId);
-		$validationKey = $userModel->setValidationKey();
+		EmailHelper::addValidateKeyToUser($userModel);
 		$vars = array(
-			'user' =>$userModel,
-			
+			'user' => $userModel,
+			'link' => 'http://' . $_SERVER['SERVER_NAME'] . '/validate/' . $userModel->validationKey,
 		);
+		$t = EmailHelper::template('email/en/SignupValidate.html');
+		$html = $t->render($vars);
 		
+		$mailer->setFrom(array('no-reply@scriptureforge.org' => 'ScriptureForge')); // TODO put this in the sf config CP 2013-10
+		$mailer->setTo(array($userModel->email => $userModel->name));
+		$mailer->setBody($html);
+		$mailer->send();
 	}
 	
-	public static function sendJoinWithProject() {
+	public static function sendSignupWithProject() {
 		
 	}
 	
