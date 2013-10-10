74dbc3655640f09c089da93bdf2d2465c110e6fe
Robin Munn <rmunn@pobox.com>
2013-10-11 00:49:21 +0700

    WIP on tags: add/remove functions
    Tags now have add and remove functions. Remove function has been
    hooked up to the red close button. Still need to add the UI element
    for adding tags (a text-entry box that will eventually become a kind
    of specialised typeahead). Then once that's working, I'll turn this
    into a directive.

--- src/angular-app/sfchecks/partials/robintest.js
+++ src/angular-app/sfchecks/partials/robintest.js
@@ -8,5 +8,14 @@ angular.module(
 	                              function($scope, $routeParams, questionService, ss, breadcrumbService, notice) {
 		console.log("Testing tag layout");
 		$scope.tags = ["First", "Second", "Third", "Fourth"];
+
+		$scope.addTag = function(tag) {
+			$scope.tags.push(tag);
+		}
+		$scope.addTag("DEBUG: And another one");
+
+		$scope.removeTag = function(tagIndex) {
+			$scope.tags.splice(tagIndex, 1);
+		}
 	}])
 	;
