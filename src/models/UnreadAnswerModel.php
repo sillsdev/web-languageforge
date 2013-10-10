06ed43b80c9967d235448dc5ffa11ea254fd9d44
megahirt <chris@hirtfamily.net>
2013-10-10 15:21:50 +0700

    unread comments/answers implemented in question page
    Former-commit-id: 200123eb91668bb8a5dd12343c4d43057cfc75a5

--- src/models/UnreadAnswerModel.php
+++ src/models/UnreadAnswerModel.php
@@ -5,8 +5,8 @@ use models\UserUnreadModel;
 
 class UnreadAnswerModel extends UserUnreadModel
 {
-	public function __construct($userId, $projectId) {
-		parent::__construct('answer', $userId, $projectId);
+	public function __construct($userId, $projectId, $questionId) {
+		parent::__construct('answer', $userId, $projectId, $questionId);
 	}
 }
 
