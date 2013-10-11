c894c6f70a24c2144994d9d9cae661c4590c4241
cambell-prince <cambell.prince@gmail.com>
2013-10-11 16:41:32 +0700

    sms: wip

---
+++ src/models/sms/SmsModel.php
@@ -0,0 +1,101 @@
+<?php
+namespace models\sms;
+
+use models\mapper\IdReference;
+
+use models\mapper\MongoMapper;
+use models\mapper\MapperModel;
+use models\mapper\Id;
+
+class SmsModel extends MapperModel
+{
+	
+	public const SMS_NEW     = 'new';
+	public const SMS_SENDING = 'sending';
+	public const SMS_FAIL    = 'fail';
+	
+	public function __construct($id = '') {
+		$this->id = new Id();
+		$this->dateCreated = new \DateTime();
+		$this->dateSent = new \DateTime();
+		parent::__construct(self::mapper(), $id);
+	}
+	
+	/**
+	 * @return \models\mapper\MongoMapper>
+	 */
+	public static function mapper() {
+		static $instance = null;
+		if (null === $instance) {
+			$instance = new MongoMapper(SF_DATABASE, 'sms');
+		}
+		return $instance;
+	}
+	
+	public static function remove($id) {
+		self::mapper()->remove($id);
+	}
+
+	/**
+	 * @var Id
+	 */
+	public $id;
+
+	/**
+	 * @var IdReference
+	 */
+	public $userRef;
+
+	/**
+	 * @var string
+	 */
+	public $userName;
+	
+	/**
+	 * @var string
+	 */
+	public $from;
+
+	/**
+	 * @var string
+	 */
+	public $to;
+
+	/**
+	 * @var string
+	 */
+	public $message;
+	
+	/**
+	 * @var string
+	 */
+	public $state;
+	
+	/**
+	 * @var string
+	 */
+	public $error;
+	
+	/**
+	 * @var \DateTime
+	 */
+	public $dateCreated;
+	
+	/**
+	 * @var \DateTime
+	 */
+	public $dateSent;
+	
+	/**
+	 * @var string
+	 */
+	public $provider;
+	
+	/**
+	 * @var string
+	 */
+	public $providerInfo
+	
+}
+
+?>
