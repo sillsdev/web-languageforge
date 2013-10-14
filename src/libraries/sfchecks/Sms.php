b4641c87b60a13f38058da071c1a94539dd7e732
cambell-prince <cambell.prince@gmail.com>
2013-10-14 15:49:44 +0700

    sms: wip add SmsSettings

--- src/libraries/sfchecks/Sms.php
+++ src/libraries/sfchecks/Sms.php
@@ -19,19 +19,4 @@ class Sms
 		
 	}
 
-	/**
-	 * Attempts to deliver a single sms message. 
-	 * @param SmsModel $smsModel
-	 */
-	public static function deliver($smsModel) {
-		
-	}
-	
-	/**
-	 * Attempts to deliver any sms messages in the queue.
-	 */
-	public static function processQueue() {
-		
-	}
-	
 }
