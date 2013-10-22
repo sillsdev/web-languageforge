1a951d7d36117921e85f276320b2295089aa81aa
irahopkinson <ira.hopkinson@gmail.com>
2013-10-22 14:49:10 +0700

    wip: file upload works with drag & drop on small files

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -5,14 +5,18 @@ require_once 'base.php';
 class Upload extends Base {
 
 	public function receive($param) {
-		//$request = file_get_contents('php://input');
-		//var_dump($request);
-		echo "PHP says:\n";
-		var_dump($_FILES);
-		var_dump($_REQUEST);
-		var_dump($_POST);
+		// form submit
+		$file = $_FILES['file'];
+	
+		if ($file['error'] == UPLOAD_ERR_OK) {
+			$fileName = $file['name'];
+			move_uploaded_file(
+				$file['tmp_name'],
+				'audio/' . $fileName
+			);
+			echo "<p>File $fileName uploaded.</p>";
+		}
 	}
 }
-
 
 ?>
