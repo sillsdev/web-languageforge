e52586bd6a92c475a8528effbcda67aeebc968be
megahirt <chris@hirtfamily.net>
2013-10-22 13:51:59 +0700

    fixed up broken links on front of house
    Also added a "login / sign up button bar" template that can be called from any other page template

--- src/views/pages/learn_expand_your_team.html.php
+++ src/views/pages/learn_expand_your_team.html.php
@@ -4,5 +4,6 @@
 		<div class="sfcontainer cf">
 			<h2>Expand Your Team</h2>
 			<p>Give your Scripture project a boost during the checking phase. Sign-up is easy and free. As the project manager, you can control who joins your "extended team" of scripture checkers. Comments from checkers can flow right back in to your ParaTExt project and can be acted upon and resolved. Spread the word among the language community on Facebook, Twitter and Google+.</p>
+			<?php $this->load->view("templates/login_signup_buttonbar.html.php"); ?>
 			
 		</div>
