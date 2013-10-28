254318cc5f09eeee371517f800337ead23443e0e
cambell-prince <cambell.prince@gmail.com>
2013-10-28 17:19:50 +0700

    Allow custom templates per project for all pages.

--- src/config/routes.php
+++ src/config/routes.php
@@ -38,11 +38,12 @@
 |
 */
 
-if ($_SERVER['HTTP_HOST'] == 'jamaicanpsalms.scriptureforge.org') {
-	$route['default_controller'] = "/projects/view/jamaican_psalms";
-} else {
-	$route['default_controller'] = "/pages/view/frontpage";
-}
+// if ($_SERVER['HTTP_HOST'] == 'jamaicanpsalms.scriptureforge.org') {
+// 	$route['default_controller'] = "/projects/view/jamaicanpsalms";
+// } else {
+// 	$route['default_controller'] = "/pages/view/frontpage";
+// }
+$route['default_controller'] = "/pages/view/frontpage";
 
 $route['404_override'] = '';
 $route['login'] = 'auth/login';
