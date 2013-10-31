f012dd00c0b427bf3ad33379f746585e9ff24ec5
Robin Munn <robin_munn@sil.org>
2013-10-31 11:27:52 +0700

    Improve CSS for chapter/verse selection forms
    Former-commit-id: fa3dde89bd9a02885f97756a827598e7158818a9

--- src/views/templates/meta.html.php
+++ src/views/templates/meta.html.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" media="screen" href="/css/slides.css" />
 		<link rel="stylesheet" href="/css/font-awesome.css">
 		<link rel="stylesheet" href="/css/sf.css" />
+		<link rel="stylesheet" href="/css/sf-ui.css" />
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 		<script src="/js/lib/jquery-1.8.0.min.js"></script>
