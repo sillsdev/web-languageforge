484bc2fe11ff1d143e9d8c48d4156bfd1d1fcd73
irahopkinson <ira.hopkinson@gmail.com>
2013-11-06 14:35:07 +0700

    WIP admin invite: remove ProjectUserCommands and LinkCommands, incorporate tests from these into UserCommands and ProjectCommands

--- test/php/commands/ProjectCommands_Test.php
+++ test/php/commands/ProjectCommands_Test.php
@@ -22,37 +22,84 @@ class TestProjectCommands extends UnitTestCase {
 		ProjectCommands::deleteProjects(array($projectId));
 	}
 
-	function testUpdateUserRole_ExistingUserAndProject_UserJoinedProject() {
+	function testUpdateUserRole_UpdateUserInProject_UserJoinedProject() {
 		$e = new MongoTestEnvironment();
 		$e->clean();
-		
+	
+		// setup parameters: user, project and params
 		$userId = $e->createUser("existinguser", "Existing Name", "existing@example.com");
 		$user = new UserModel($userId);
 		$project = $e->createProject(SF_TESTPROJECT);
 		$projectId = $project->id->asString();
 		$params = array(
 				'id' => $user->id->asString(),
+				'role' => Roles::PROJECT_ADMIN,
+		);
+	
+		// update user role in project
+		$updatedUserId = ProjectCommands::updateUserRole($projectId, $params);
+	
+		// read from disk
+		$updatedUser = new UserModel($updatedUserId);
+		$sameProject = new ProjectModel($projectId);
+		
+		// user updated and joined to project
+		$this->assertEqual($updatedUser->id, $userId);
+		$this->assertNotEqual($updatedUser->role, Roles::PROJECT_ADMIN);
+		$projectUser = $sameProject->listUsers()->entries[0];
+		$this->assertEqual($projectUser['name'], "Existing Name");
+		$userProject = $updatedUser->listProjects()->entries[0];
+		$this->assertEqual($userProject['projectname'], SF_TESTPROJECT);
+	}
+	
+	function testUpdateUserRole_JoinTwice_JoinedOnce() {
+		$e = new MongoTestEnvironment();
+		$e->clean();
+		
+		// setup user and project
+		$project = $e->createProject(SF_TESTPROJECT);
+		$projectId = $project->id->asString();
+		$userId = $e->createUser("existinguser", "Existing Name", "existing@example.com");
+		$params = array(
+				'id' => $userId,
 		);
 		
-		$updatedUserID = ProjectCommands::updateUserRole($projectId, $params);
+		// update user role in project once
+		$updatedUserId = ProjectCommands::updateUserRole($projectId, $params);
 		
-		$updatedUser = new UserModel($updatedUserID);
-		$this->assertEqual($updatedUser->id, $userId);
-		$this->assertEqual($updatedUser->name, "Existing Name");
-		$this->assertEqual($updatedUser->email, "existing@example.com");
-		$this->assertEqual($project->listUsers()->count, 1);
-		$this->assertEqual($updatedUser->listProjects()->count, 1);
+		// read from disk
+		$sameUser = new UserModel($updatedUserId);
+		$sameProject = new ProjectModel($projectId);
+		
+		// user in project once and project has one user
+		$this->assertEqual($sameProject->listUsers()->count, 1);
+		$this->assertEqual($sameUser->listProjects()->count, 1);
+		
+		// update user role in project again
+		$updatedUserId = ProjectCommands::updateUserRole($projectId, $params);
+		
+		// read from disk again
+		$sameProject->read($projectId);
+		$sameUser->read($updatedUserId);
+		
+		// user still in project once and project still has one user
+		$this->assertEqual($sameProject->listUsers()->count, 1);
+		$this->assertEqual($sameUser->listProjects()->count, 1);
 	}
 	
 	function testRemoveUsers_NoUsers_NoThrow() {
 		$e = new MongoTestEnvironment();
 		$e->clean();
 		
+		// setup parameters: project and users
 		$project = $e->createProject(SF_TESTPROJECT);
 		$projectId = $project->id->asString();
 		$userIds = array();
 		
+		// there are no users in project
 		$this->assertEqual($project->listUsers()->count, 0);
+		
+		// remove users from project with no users - no throw expected
 		ProjectCommands::removeUsers($projectId, $userIds);
 	}
 
@@ -60,6 +107,7 @@ class TestProjectCommands extends UnitTestCase {
 		$e = new MongoTestEnvironment();
 		$e->clean();
 
+		// setup project and users
 		$project = $e->createProject(SF_TESTPROJECT);
 		$projectId = $project->id->asString();
 		$user1Id = $e->createUser("user1name", "User1 Name", "user1@example.com");
@@ -78,19 +126,42 @@ class TestProjectCommands extends UnitTestCase {
 		$user2->write();
 		$user3->addProject($project->id->asString());
 		$user3->write();
+		
+		// read from disk
+		$otherProject = new ProjectModel($projectId);
+		$otherUser1 = new UserModel($user1Id);
+		$otherUser2 = new UserModel($user2Id);
+		$otherUser3 = new UserModel($user3Id);
+		
+		// each user in project, project has each user
+		$user1Project = $otherUser1->listProjects()->entries[0];
+		$this->assertEqual($user1Project['projectname'], SF_TESTPROJECT);
+		$user2Project = $otherUser1->listProjects()->entries[0];
+		$this->assertEqual($user2Project['projectname'], SF_TESTPROJECT);
+		$user3Project = $otherUser1->listProjects()->entries[0];
+		$this->assertEqual($user3Project['projectname'], SF_TESTPROJECT);
+		$projectUser1 = $otherProject->listUsers()->entries[0];
+		$this->assertEqual($projectUser1['username'], "user1name");
+		$projectUser2 = $otherProject->listUsers()->entries[1];
+		$this->assertEqual($projectUser2['username'], "user2name");
+		$projectUser3 = $otherProject->listUsers()->entries[2];
+		$this->assertEqual($projectUser3['username'], "user3name");
+		
+		// remove users from project
 		$userIds = array($user1->id->asString(), $user2->id->asString(), $user3->id->asString());
-		
-		$this->assertEqual($project->listUsers()->count, 3);
-		$this->assertEqual($user1->listProjects()->count, 1);
-		$this->assertEqual($user2->listProjects()->count, 1);
-		$this->assertEqual($user3->listProjects()->count, 1);
-		
 		ProjectCommands::removeUsers($projectId, $userIds);
 		
-		$this->assertEqual($project->listUsers()->count, 0);
-		$this->assertEqual($user1->listProjects()->count, 0);
-		$this->assertEqual($user2->listProjects()->count, 0);
-		$this->assertEqual($user3->listProjects()->count, 0);
+		// read from disk
+		$sameProject = new ProjectModel($projectId);
+		$sameUser1 = new UserModel($user1Id);
+		$sameUser2 = new UserModel($user2Id);
+		$sameUser3 = new UserModel($user3Id);
+		
+		// project has no users, each user not in project
+		$this->assertEqual($sameProject->listUsers()->count, 0);
+		$this->assertEqual($sameUser1->listProjects()->count, 0);
+		$this->assertEqual($sameUser2->listProjects()->count, 0);
+		$this->assertEqual($sameUser3->listProjects()->count, 0);
 	}
 	
 }
