e3567faa40910d4f9b59d3d453d896171353ab93
megahirt <chris@hirtfamily.net>
2014-01-08 14:37:48 +0700

    updated email validation success template to look like registration success template
    Former-commit-id: 5d1b99c5876ca7c7437b5097bda4e7d6d159467f

--- src/views/validate/validate.html.php
+++ src/views/validate/validate.html.php
@@ -1,6 +1,18 @@
-<div class="container banner">
-	<img src="/images/beach3.jpg" width="965px" height="360px" alt="on the beach" />
-	<h2>New User Account Validaton</h2>
-	<p><?php echo $message;?></p>
+<div class="content container">
+    <div class="row span9" style="font-size:16px" ng-show="submissionComplete">
+		<h1>You're Almost There...</h1>
+        <div class="row">
+            <div class="span2"><i style="font-size:100px; color: green" class="icon-check-sign"></i></div>
+            <div class="span7"><h2>Register</h2><div>Great!  You're already registered!</div></div>
+        </div>
+        <div class="row">
+            <div class="span2"><i style="font-size:100px; color: green" class="icon-check-sign"></i></div>
+            <div class="span7"><h2>Validate Email</h2><div>Great!  Your account is now validated!</div></div>
+        </div>
+        <div class="row">
+            <div class="span2"><i style="font-size:110px;" class="icon-check-empty"></i></div>
+            <div class="span7"><h2><a href="/login">Login and Get Started</a></h2><div><a href="/login">Login</a> now and get started working on your project.</div></div>
+        </div>
+    </div>
 </div>
 		
