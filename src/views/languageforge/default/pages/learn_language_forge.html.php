d99d434d7f2811d98733aaeabf7d395a6f5531f2
megahirt <chris@hirtfamily.net>
2014-02-12 17:02:16 +0700

    added languageforge images, css and js and changed urls to suite

---
+++ src/views/languageforge/default/pages/learn_language_forge.html.php
@@ -0,0 +1,10 @@
+<div class="sfcontainer banner">
+	<img class="left" src="/images/languageforge/default/communityteam.jpg" width="500px" height="375px" alt="Community Engagement" />
+		<div style="margin-left:410pt; sfcontainer">
+			<h2>About Language Forge</h2>
+			<p>Take your dictonary project to the next level by involving tens, hundreds or even more contributors in the collection and refinement of lexical data upon which your dictionary will be based.  Engage your language's diaspora speakers on the web or their phone, wherever they are.  Bring new insights and a richer corpus into the lexical development process by engaging remote participants.  While your lexical development team continues to work in <a href="http://wesay.palaso.org/" target="_blank">WeSay</a>, and/or <a href="http://fieldworks.sil.org/flex/" target="_blank">FieldWorks Language Explorer</a>, Language Forge provides a community contribution and feedback mechanisms to help your team grow and evaluate your lexicon as it is under development. Finally, the community engagement results in buyin and ownership of the resultant dictionaries which will increas community awareness and use.
+			</p>
+			<p>When everyone plays their part in developing dictionaries and getting them into the right hands, everyone benefits.  It's the power of collaboration that produces richer and more standardized dictionaries.  Our technology brings lexical development to the masses, in a positive environment that focuses participants on the task and works wherever they are.  On the tablet, phone or web, Language Forge is there.</p>
+			
+		</div>
+</div>
