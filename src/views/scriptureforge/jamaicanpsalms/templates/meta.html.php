ab0ee13358b0301d91e087ff072fbdc46f8cd8f2
megahirt <chris@hirtfamily.net>
2014-02-12 17:01:19 +0700

    upgrade to jquery 1.8.3

--- src/views/scriptureforge/jamaicanpsalms/templates/meta.html.php
+++ src/views/scriptureforge/jamaicanpsalms/templates/meta.html.php
@@ -9,10 +9,10 @@
 		<link rel="stylesheet" media="screen" href="/css/scriptureforge/jamaicanpsalms/slides-jamaica.css" />
 		<link rel="stylesheet" href="/css/shared/font-awesome.css">
 		<link rel="stylesheet" href="/css/scriptureforge/jamaicanpsalms/sf-jamaica.css" />
-		<link rel="stylesheet" href="/css/shared/sf-ui.css" />
+		<link rel="stylesheet" href="/css/scriptureforge/jamaicanpsalms/sf-ui.css" />
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Dosis:300' rel='stylesheet' type='text/css'>
 
-		<script src="/js/lib/jquery-1.8.0.min.js"></script>
+		<script src="/js/lib/jquery-1.8.3.min.js"></script>
 		<!-- jquery *must* be the first js to load -->
