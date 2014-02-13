63106371a4a5860a717b726c74f8bbeffe4e2497
megahirt <chris@hirtfamily.net>
2014-02-13 11:14:58 +0700

    made 404 error page site specific
    Former-commit-id: 395761e3ab73c3a8c7a3827e4da1521cde6afad2

--- src/controllers/download.php
+++ src/controllers/download.php
@@ -2,12 +2,12 @@
 
 require_once 'base.php';
 
-class Download extends CI_Controller {
+class Download extends Base {
 	
 	public function assets($id, $file) {
 		$filePath = APPPATH .  "assets/$id/" . urldecode($file);
 		if (!file_exists($filePath)) {
-			show_404();
+			show_404($this->site);
 			return;
 		}
 		header("Content-type: octet/stream");
