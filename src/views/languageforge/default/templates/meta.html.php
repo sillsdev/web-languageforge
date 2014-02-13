21772cf0da295463cb73ca8898543e57d64a6214
megahirt <chris@hirtfamily.net>
2014-02-13 10:11:19 +0700

    moved superfish.css to project folder

--- src/views/languageforge/default/templates/meta.html.php
+++ src/views/languageforge/default/templates/meta.html.php
@@ -9,8 +9,8 @@
 		<link rel=stylesheet href="/<?php echo $filename; ?>" />
 		<?php endforeach; ?>
 		<?php endif; ?>
-		<link rel="stylesheet" media="screen" href="/css/shared/superfish.css" />
-		<link rel="stylesheet" media="screen" href="/css/shared/slides.css" />
+		<link rel="stylesheet" media="screen" href="/css/languageforge/default/superfish.css" />
+		<link rel="stylesheet" media="screen" href="/css/languageforge/default/slides.css" />
 		<link rel="stylesheet" media="screen" href="/css/shared/jquery.fileupload-ui.css" />
 		<link rel="stylesheet" href="/css/languageforge/default/lf-dictionary.css" />
 		<link rel="stylesheet" href="/css/languageforge/default/lf.css" />
@@ -21,3 +21,4 @@
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
   -->
 		<script src="/js/lib/jquery-1.8.3.min.js"></script>
+		<!-- jquery must be the first js to be loaded -->
