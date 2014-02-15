2f7a42ff0d2f2880fca422731b1cd2a96aa8f840
irahopkinson <ira.hopkinson@gmail.com>
2014-02-15 15:38:38 +0700

    add writing systems tag

--- src/angular-app/languageforge/lexicon/views/settings.js
+++ src/angular-app/languageforge/lexicon/views/settings.js
@@ -7,14 +7,23 @@ angular.module(
 	.controller('SettingsCtrl', ['$scope', 'userService', 'sessionService', 'lexEntryService', '$window', '$timeout', 
 	                                 function($scope, userService, ss, lexService, $window, $timeout) {
 
-		$scope.currentWritingSystemId = '';
+		$scope.currentWritingSystemTag = '';
+		$scope.currentWritingSystem = {};
+		$scope.currentWritingSystem.id = '';
+		$scope.currentWritingSystem.special = '';
+		$scope.currentWritingSystem.purpose = '';
+		$scope.currentWritingSystem.script = '';
+		$scope.currentWritingSystem.region = '';
+		$scope.currentWritingSystem.variant = '';
 		$scope.selectWritingSystem = function(writingSystemId) {
-			$scope.currentWritingSystemId = writingSystemId;
+			$scope.currentWritingSystem.id = writingSystemId;
 		};
 		
+		$scope.currentField = {};
+		$scope.currentField.hidden = '';
 		$scope.currentFieldId = '';
-		$scope.selectField = function(fielId) {
-			$scope.currentFieldId = fielId;
+		$scope.selectField = function(fieldId) {
+			$scope.currentFieldId = fieldId;
 		};
 		
 		$scope.querySettings = function() {
@@ -29,11 +38,38 @@ angular.module(
 		};
 		
 		$scope.$watch('config.writingsystems', function(newValue) {
-//			console.log("writing systems watch ", newValue);
+//			console.log("config writing systems watch: ", newValue);
 			if (newValue != undefined) {
 				for (var key in newValue.map) {
-					$scope.currentWritingSystemId = key;
+					$scope.currentWritingSystem.id = key;
 					break;
+				}
+			}
+		});
+		$scope.$watchCollection('currentWritingSystem', function(newValue) {
+//			console.log("current writing system watch: ", newValue);
+			if (newValue != undefined) {
+				$scope.currentWritingSystemTag = $scope.currentWritingSystem.id;
+				switch(newValue.special) {
+					case 'IPA transcription':
+						$scope.currentWritingSystemTag += '-fonipa';
+						switch(newValue.purpose) {
+							case 'Etic (raw phonetic transcription)':
+								$scope.currentWritingSystemTag += '-x-etic';
+								break;
+							case 'Emic (uses the phonology of the language)':
+								$scope.currentWritingSystemTag += '-x-emic';
+								break;
+						}
+						break;
+					case 'Voice':
+						$scope.currentWritingSystemTag += '-Zxxx-x-audio';
+						break;
+					case 'Script / Region / Variant':
+						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.script) ? '-' + $scope.currentWritingSystem.script : '';
+						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.region) ? '-' + $scope.currentWritingSystem.region : '';
+						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.variant) ? '-x-' + $scope.currentWritingSystem.variant : '';
+						break;
 				}
 			}
 		});
