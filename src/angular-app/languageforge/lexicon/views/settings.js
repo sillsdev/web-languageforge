c3b17408b0a476e8e40e51298a62e9e1b561136c
irahopkinson <ira.hopkinson@gmail.com>
2014-02-17 13:09:27 +0700

    wip: change to Input Systems (from Writing Systems) with new config structure
    * implement service function with callback
    * inputSystems helper module

--- src/angular-app/languageforge/lexicon/views/settings.js
+++ src/angular-app/languageforge/lexicon/views/settings.js
@@ -7,18 +7,21 @@ angular.module(
 	.controller('SettingsCtrl', ['$scope', 'userService', 'sessionService', 'lexEntryService', '$window', '$timeout', 
 	                                 function($scope, userService, ss, lexService, $window, $timeout) {
 
-		$scope.currentWritingSystemTag = '';
-		$scope.currentWritingSystem = {};
-		$scope.currentWritingSystem.id = '';
-		$scope.currentWritingSystem.abbreviation = '';
-		$scope.currentWritingSystem.special = '';
-		$scope.currentWritingSystem.purpose = '';
-		$scope.currentWritingSystem.script = '';
-		$scope.currentWritingSystem.region = '';
-		$scope.currentWritingSystem.variant = '';
-		$scope.selectWritingSystem = function(writingSystemId) {
-			$scope.currentWritingSystem.abbreviation = writingSystemId;		// TODO add. fiddle until load current writing system is done IJH 2014-02
-			$scope.currentWritingSystem.id = writingSystemId;
+		$scope.config = {};
+		$scope.languageCodes = {};
+		
+		$scope.currentInputSystemTag = '';
+		$scope.currentInputSystem = {};
+		$scope.currentInputSystem.code = '';
+		$scope.currentInputSystem.abbreviation = '';
+		$scope.currentInputSystem.special = '';
+		$scope.currentInputSystem.purpose = '';
+		$scope.currentInputSystem.script = '';
+		$scope.currentInputSystem.region = '';
+		$scope.currentInputSystem.variant = '';
+		$scope.selectInputSystem = function(inputSystemTag) {
+			$scope.currentInputSystem.abbreviation = inputSystemTag;		// TODO add. fiddle until load current input system is done IJH 2014-02
+			$scope.currentInputSystem.code = inputSystemTag;
 		};
 		
 		$scope.currentField = {};
@@ -28,50 +31,56 @@ angular.module(
 			$scope.currentFieldId = fieldId;
 		};
 		
-		$scope.querySettings = function() {
-			$scope.config = lexService.projectSettings();
+		$scope.queryProjectSettings = function() {
+			lexService.projectSettings('', function(result) {	// TODO Add. $scope.project.id in place of '' when part of project IJH 2014-02
+				if (result.ok) {
+					$scope.config = result.data.config;
+					$scope.languageCodes = inputSystems.langaugeCodes();
+				}
+			});
 		};
 		
-		$scope.querySettings();
-		$scope.editWritingSystems = {};
-		$scope.editWritingSystems.collapsed = true;
 		
-		$scope.saveWritingSystems = function() {
-			$scope.editWritingSystems.collapsed = true;
+		$scope.queryProjectSettings();
+		$scope.editInputSystems = {};
+		$scope.editInputSystems.collapsed = true;
+		
+		$scope.saveInputSystems = function() {
+			$scope.editInputSystems.collapsed = true;
 		};
 		
-		$scope.$watch('config.writingsystems', function(newValue) {
-//			console.log("config writing systems watch: ", newValue);
+		$scope.$watch('config.inputSystems', function(newValue) {
+//			console.log("config input systems watch: ", newValue);
 			if (newValue != undefined) {
 				for (var key in newValue.map) {
-					$scope.currentWritingSystem.id = key;
+					$scope.currentInputSystem.code = key;
 					break;
 				}
 			}
 		});
-		$scope.$watchCollection('currentWritingSystem', function(newValue) {
-//			console.log("current writing system watch: ", newValue);
+		$scope.$watchCollection('currentInputSystem', function(newValue) {
+//			console.log("current input system watch: ", newValue);
 			if (newValue != undefined) {
-				$scope.currentWritingSystemTag = $scope.currentWritingSystem.id;
+				$scope.currentInputSystemTag = $scope.currentInputSystem.code;
 				switch(newValue.special) {
 					case 'IPA transcription':
-						$scope.currentWritingSystemTag += '-fonipa';
+						$scope.currentInputSystemTag += '-fonipa';
 						switch(newValue.purpose) {
 							case 'Etic (raw phonetic transcription)':
-								$scope.currentWritingSystemTag += '-x-etic';
+								$scope.currentInputSystemTag += '-x-etic';
 								break;
 							case 'Emic (uses the phonology of the language)':
-								$scope.currentWritingSystemTag += '-x-emic';
+								$scope.currentInputSystemTag += '-x-emic';
 								break;
 						}
 						break;
 					case 'Voice':
-						$scope.currentWritingSystemTag += '-Zxxx-x-audio';
+						$scope.currentInputSystemTag += '-Zxxx-x-audio';
 						break;
 					case 'Script / Region / Variant':
-						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.script) ? '-' + $scope.currentWritingSystem.script : '';
-						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.region) ? '-' + $scope.currentWritingSystem.region : '';
-						$scope.currentWritingSystemTag += ($scope.currentWritingSystem.variant) ? '-x-' + $scope.currentWritingSystem.variant : '';
+						$scope.currentInputSystemTag += ($scope.currentInputSystem.script) ? '-' + $scope.currentInputSystem.script : '';
+						$scope.currentInputSystemTag += ($scope.currentInputSystem.region) ? '-' + $scope.currentInputSystem.region : '';
+						$scope.currentInputSystemTag += ($scope.currentInputSystem.variant) ? '-x-' + $scope.currentInputSystem.variant : '';
 						break;
 				}
 			}
