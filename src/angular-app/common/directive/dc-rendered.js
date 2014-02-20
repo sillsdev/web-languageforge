8bb3f343a9ef1cedf60ac5c8747cbad733ca1733
Robin Munn <robin_munn@sil.org>
2014-02-20 15:12:29 +0700

    WIP: Rendered dictionary definition
    Remove quite a bit of debugging code

--- src/angular-app/common/directive/dc-rendered.js
+++ src/angular-app/common/directive/dc-rendered.js
@@ -33,36 +33,28 @@ angular.module('palaso.ui.dc.rendered', [])
 				};
 
 				$scope.render = function(entry) {
-					if (entry == "Lorem ipsum") {
-						$scope.definition.rendered = (
-							"1) Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-							"2) Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-							"3) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-							"4) Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-					} else {
-						$scope.definition.label = $scope.getLexemeForm($scope.model);
-						var defParts = [];
-						var useNumbers = (entry.senses.length > 1);
-						var nextNum = 1;
-						angular.forEach(entry.senses, function(sense) {
-							if (useNumbers) {
-								defParts.push(nextNum.toString() + ") ");
-								nextNum++;
-							};
-							if (sense.partOfSpeech) {
-								var abbrev = $scope.posAbbrevs[sense.partOfSpeech];
-								defParts.push(abbrev ? (abbrev + ' ') : '');
-							};
-							// Might be nice to order the definitions in some way, like primary analysis
-							// language first, then other languages after in a consistent order. But until
-							// there's a way to specify primary analysis languages in config, we'll just
-							// go through the definitions in whatever order forEach() produces them.
-							angular.forEach(sense.definition, function(def, wsid) {
-								defParts.push(def + ' ');
-							});
+					$scope.definition.label = $scope.getLexemeForm($scope.model);
+					var defParts = [];
+					var useNumbers = (entry.senses.length > 1);
+					var nextNum = 1;
+					angular.forEach(entry.senses, function(sense) {
+						if (useNumbers) {
+							defParts.push(nextNum.toString() + ") ");
+							nextNum++;
+						};
+						if (sense.partOfSpeech) {
+							var abbrev = $scope.posAbbrevs[sense.partOfSpeech];
+							defParts.push(abbrev ? (abbrev + ' ') : '');
+						};
+						// Might be nice to order the definitions in some way, like primary analysis
+						// language first, then other languages after in a consistent order. But until
+						// there's a way to specify primary analysis languages in config, we'll just
+						// go through the definitions in whatever order forEach() produces them.
+						angular.forEach(sense.definition, function(def, wsid) {
+							defParts.push(def + ' ');
 						});
-						$scope.definition.rendered = defParts.join("") || "[No definition exists yet: add one!]";
-					}
+					});
+					$scope.definition.rendered = defParts.join("") || "[No definition exists yet: add one!]";
 				};
 
 				$scope.makeValidModel = function() {
@@ -75,15 +67,12 @@ angular.module('palaso.ui.dc.rendered', [])
 							}
 						}
 					}
-					console.log('model is', $scope.model);
 				};
 			}],
 			link : function(scope, element, attrs, controller) {
 				scope.$watch('model', function(model) {
-					console.log('model watch was triggered');
 					scope.makeValidModel();
-					// scope.render("Lorem ipsum"); // For testing
-					scope.render(model); // For production
+					scope.render(model);
 				});
 				scope.$watch('definition', function(definition) {
 				})
