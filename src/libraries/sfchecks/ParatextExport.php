6728aa8d942181c13a074fba3df2332b9f61f8d9
megahirt <chris@hirtfamily.net>
2014-02-24 16:34:02 +0700

    improvements to paratext export; added UsxHelper->getMetadata() and tests

--- src/libraries/sfchecks/ParatextExport.php
+++ src/libraries/sfchecks/ParatextExport.php
@@ -2,6 +2,8 @@
 namespace libraries\sfchecks;
 
 
+use models\dto\UsxHelper;
+
 use models\UserModel;
 use models\ProjectModel;
 use models\TextModel;
@@ -14,6 +16,8 @@ class ParatextExport
 	public static function exportCommentsForText($projectId, $textId, $params) {
 		$project = new ProjectModel($projectId);
 		$text = new TextModel($project, $textId);
+		$usxHelper = new UsxHelper($text->content);
+		$textInfo = $usxHelper->getMetadata();
 		$questionlist = new QuestionAnswersListModel($project, $textId);
 		$questionlist->read();
 		$dl = array(
@@ -21,20 +25,24 @@ class ParatextExport
 			'inprogress' => false,
 			'answerCount' => 0,
 			'commentCount' => 0,
+			'totalCount' => 0,
 			'xml' => "<CommentList>\n"
 		);
 		foreach ($questionlist->entries as $question) {
 			foreach ($question['answers'] as $answerId => $answer) {
 				if (count($params['tags']) == 0 || count(array_intersect($params['tags'], $answer['tags'])) > 0) { // if the answer is tagged with an export tag
 					$dl['answerCount']++;
-					$dl['xml'] .= self::makeCommentXml($params['username'], $answer['tags'], $answer['score'], $text, $answerId, $answer);
-					foreach ($answer['comments'] as $commentId => $comment) {
-						$dl['xml'] .= self::makeCommentXml($params['username'], array(), 0, $text, $commentId, $comment);
-						$dl['commentCount']++;
+					$dl['xml'] .= self::makeCommentXml($params['username'], $answer['tags'], $answer['score'], $textInfo, $answerId, $answer);
+					if ($params['exportComments']) {
+						foreach ($answer['comments'] as $commentId => $comment) {
+							$dl['xml'] .= self::makeCommentXml($params['username'], array(), 0, $textInfo, $commentId, $comment);
+							$dl['commentCount']++;
+						}
 					}
 				}
 			}
 		}
+		$dl['totalCount'] = $dl['answerCount'] + $dl['commentCount'];
 		$dl['xml'] .= "</CommentList>";
 		return $dl;
 	}
@@ -49,13 +57,12 @@ class ParatextExport
 	 * @param string $username
 	 * @param array $tags
 	 * @param int $votes
-	 * @param TextModel $text
+	 * @param array $textInfo
 	 * @param string $commentId
 	 * @param array $comment
 	 */
-	private static function makeCommentXml($username, $tags, $votes, $text, $commentId, $comment) {
+	private static function makeCommentXml($username, $tags, $votes, $textInfo, $commentId, $comment) {
 		$user = new UserModel((string)$comment['userRef']);
-		; // 'c' is ISO 8601 date format
 		
 		$content = $comment['content'] . " (by " . $user->username . " on " . date(\DateTime::RFC822, $comment['dateEdited']->sec) . ")";
 		if (count($tags) > 0) {
@@ -73,15 +80,15 @@ class ParatextExport
 		<Thread>" . $commentId . "</Thread>
 		<User>$username</User>
 		<Date>" . date(\DateTime::ISO8601, $comment['dateEdited']->sec) . "</Date>
-		<VerseRef>unknown</VerseRef>
+		<VerseRef>" . $textInfo['bookCode'] . " " . $textInfo['startChapter'] . ":" . $textInfo['startVerse'] . "</VerseRef>
 		<SelectedText />
 		<StartPosition></StartPosition>
-		<ContextBefore>\v unknown</ContextBefore>
+		<ContextBefore>\\v " . $textInfo['startVerse'] . "</ContextBefore>
 		<ContextAfter/>
 		<Status>todo</Status>
 		<Type/>
 		<Language/>
-		<Verse>\v unknown</Verse>
+		<Verse>\\v " . $textInfo['startVerse'] . "</Verse>
 		<Field Name='assigned'/>
 		<Contents>$content</Contents>
 	</Comment>\n";
