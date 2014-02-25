f981c2bf7aa0507cb133b0756319abce9dda1bf2
Robin Munn <robin_munn@sil.org>
2014-02-25 11:33:43 +0700

    Update Angular UI Bootstrap to 0.8.0 (from 0.4.0)

--- src/views/shared/angular-app.html.php
+++ src/views/shared/angular-app.html.php
@@ -18,7 +18,7 @@ window.session = <?php echo $jsonSession; ?>;
 	<script src="/js/lib/angular_stable_1.2.4/angular-route.js"></script>
 	<script src="/js/lib/angular_stable_1.2.4/angular-sanitize.js"></script>
 	<script src="/js/lib/angular-file-upload.js"></script>
-	<script src="/js/lib/ng-ui-bootstrap-tpls-0.4.0.js"></script>
+	<script src="/js/lib/ng-ui-bootstrap-tpls-0.8.0.js"></script>
 	<script src="/js/lib/ng-ui-utils-validate.js"></script>
 <?php endif; ?>
 	
