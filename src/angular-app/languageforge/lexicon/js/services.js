7fe1492b4c2968064ebdddecb66399709e36d827
megahirt <chris@hirtfamily.net>
2014-03-03 16:41:53 +0700

    menu no longer fires separate setting api call

--- src/angular-app/languageforge/lexicon/js/services.js
+++ src/angular-app/languageforge/lexicon/js/services.js
@@ -1,143 +1,23 @@
 angular.module('lexicon.services', ['jsonRpc'])
 	.service('lexProjectService', ['jsonRpc', '$location', function(jsonRpc, $location) {
 		jsonRpc.connect('/api/sf');
-		/*
-		var _config = {
-			'inputSystems': {
-				'en': {
-					'languageName': 'English',
-					'abbreviation': 'en',
-					'fieldUseCount': 11
-				},
-				'qaa': {
-					'languageName': 'Unlisted Language',
-					'abbreviation': 'qaa',
-					'fieldUseCount': 0
-				},
-				'th': {
-					'languageName': 'Thai',
-					'abbreviation': 'th',
-					'fieldUseCount': 11
-				},
-				'th-fonipa-x-etic': {
-					'languageName': 'Thai',
-					'abbreviation': 'thipa',
-					'fieldUseCount': 11
-				},
-				'mi-Zxxx-x-audio': {
-					'languageName': 'Maori',
-					'abbreviation': 'mi',
-					'fieldUseCount': 0
-				},
-				'mi-Latn-NZ-x-Ngati': {
-					'languageName': 'Maori',
-					'abbreviation': 'miNgati',
-					'fieldUseCount': 0
-				}
-			},
-			'entry': {
-				'type': 'fields',
-				'fieldOrder': ['lexeme', 'senses'],
-				'fields': {
-					'lexeme': {
-						'type': 'multitext',
-						'label': 'Word',
-						'visible': true,
-						'inputSystems': ['th-fonipa-x-etic'],
-						'width': 20
-					},
-					'senses': {
-						'type': 'fields',
-						'fieldOrder': ['definition', 'partOfSpeech', 'semanticDomainValue', 'examples'],
-						'fields': {
-							'definition': {
-								'type': 'multitext',
-								'label': 'Meaning',
-								'visible': true,
-								'inputSystems': ['th', 'en'],
-								'width': 20
-							},
-							'partOfSpeech': {
-								'type': 'optionlist',
-								'label': 'Part of Speech',
-								'visible': true,
-								'values': {
-									'noun': 'Noun',
-									'verb': 'Verb',
-									'adjective': 'Adjective'
-								},
-								'width': 20
-							},
-							'semanticDomainValue': {
-								'type': 'optionlist',
-								'label': 'Semantic Domain',
-								'visible': true,
-								'values': {
-									'2.1': '2.1 Body',
-									'2.2': '2.2 Head and Shoulders',
-									'2.3': '2.3 Feet'
-								},
-								'width': 20
-							},
-							'examples': {
-								'type': 'fields',
-								'visible': true,
-								'fieldOrder': ['example', 'translation'],
-								'fields': {
-									'example': {
-										'type': 'multitext',
-										'label': 'Example Sentence',
-										'visible': true,
-										'inputSystems': ['th'],
-										'width': 20
-									},
-									'translation': {
-										'type': 'multitext',
-										'label': 'Example Translation',
-										'visible': true,
-										'inputSystems': ['en'],
-										'width': 20
-									}
-								}
-							}
-						}
-					}
-				}
-			},
-			'tasks': {
-				'view': {'visible': true},
-				'dashboard': {
-					'visible': true,
-					'timeRange': '30days',
-					'targetWordCount': 0
-				},
-				'gather-texts': {'visible': true},
-				'semdom': {
-					'visible': true,
-					'language': 'en',
-					'visibleFields': {
-						'definition': true,
-						'partOfSpeech': true,
-						'example': true,
-						'translation': true
-					}
-				},
-				'wordlist': {'visible': true},
-				'dbe': {'visible': true},
-				'add-meanings': {'visible': true},
-				'add-grammar': {'visible': true},
-				'add-examples': {'visible': true},
-				'settings': {'visible': true},
-				'review': {'visible': true}
-			}
-		};
-		*/
-
 		this.configInputSystems = function() {
 			return _config.inputSystems;
 		};
 		
 		var _config = {};
+		
+		var settingsChangeCallbacks = [];
+		
+		this.settingsChangeNotify = function(callback) {
+			settingsChangeCallbacks.push(callback);
+		};
+		
+		function _notify() {
+			angular.forEach(settingsChangeCallbacks, function(callback) {
+				callback();
+			});
+		}
 		
 		this.getSettings = function(callback) {
 			// TODO this could potentially get called more than once when a page is directly loaded such as /p/{projectId}/settings
@@ -149,6 +29,7 @@ angular.module('lexicon.services', ['jsonRpc'])
 					if (result.ok) {
 						_config = result.data;
 						callback(result);
+						_notify();
 					}
 				});
 			} else {
@@ -161,6 +42,7 @@ angular.module('lexicon.services', ['jsonRpc'])
 				if (result.ok) {
 					_config = angular.copy(settings);
 					callback(result);
+					_notify();
 				}
 			});
 		};
@@ -527,3 +409,137 @@ angular.module('lexicon.services', ['jsonRpc'])
 		// --- END TEST CODE ---
 	}])
 	;
+
+
+		/*
+		var _config = {
+			'inputSystems': {
+				'en': {
+					'languageName': 'English',
+					'abbreviation': 'en',
+					'fieldUseCount': 11
+				},
+				'qaa': {
+					'languageName': 'Unlisted Language',
+					'abbreviation': 'qaa',
+					'fieldUseCount': 0
+				},
+				'th': {
+					'languageName': 'Thai',
+					'abbreviation': 'th',
+					'fieldUseCount': 11
+				},
+				'th-fonipa-x-etic': {
+					'languageName': 'Thai',
+					'abbreviation': 'thipa',
+					'fieldUseCount': 11
+				},
+				'mi-Zxxx-x-audio': {
+					'languageName': 'Maori',
+					'abbreviation': 'mi',
+					'fieldUseCount': 0
+				},
+				'mi-Latn-NZ-x-Ngati': {
+					'languageName': 'Maori',
+					'abbreviation': 'miNgati',
+					'fieldUseCount': 0
+				}
+			},
+			'entry': {
+				'type': 'fields',
+				'fieldOrder': ['lexeme', 'senses'],
+				'fields': {
+					'lexeme': {
+						'type': 'multitext',
+						'label': 'Word',
+						'visible': true,
+						'inputSystems': ['th-fonipa-x-etic'],
+						'width': 20
+					},
+					'senses': {
+						'type': 'fields',
+						'fieldOrder': ['definition', 'partOfSpeech', 'semanticDomainValue', 'examples'],
+						'fields': {
+							'definition': {
+								'type': 'multitext',
+								'label': 'Meaning',
+								'visible': true,
+								'inputSystems': ['th', 'en'],
+								'width': 20
+							},
+							'partOfSpeech': {
+								'type': 'optionlist',
+								'label': 'Part of Speech',
+								'visible': true,
+								'values': {
+									'noun': 'Noun',
+									'verb': 'Verb',
+									'adjective': 'Adjective'
+								},
+								'width': 20
+							},
+							'semanticDomainValue': {
+								'type': 'optionlist',
+								'label': 'Semantic Domain',
+								'visible': true,
+								'values': {
+									'2.1': '2.1 Body',
+									'2.2': '2.2 Head and Shoulders',
+									'2.3': '2.3 Feet'
+								},
+								'width': 20
+							},
+							'examples': {
+								'type': 'fields',
+								'visible': true,
+								'fieldOrder': ['example', 'translation'],
+								'fields': {
+									'example': {
+										'type': 'multitext',
+										'label': 'Example Sentence',
+										'visible': true,
+										'inputSystems': ['th'],
+										'width': 20
+									},
+									'translation': {
+										'type': 'multitext',
+										'label': 'Example Translation',
+										'visible': true,
+										'inputSystems': ['en'],
+										'width': 20
+									}
+								}
+							}
+						}
+					}
+				}
+			},
+			'tasks': {
+				'view': {'visible': true},
+				'dashboard': {
+					'visible': true,
+					'timeRange': '30days',
+					'targetWordCount': 0
+				},
+				'gather-texts': {'visible': true},
+				'semdom': {
+					'visible': true,
+					'language': 'en',
+					'visibleFields': {
+						'definition': true,
+						'partOfSpeech': true,
+						'example': true,
+						'translation': true
+					}
+				},
+				'wordlist': {'visible': true},
+				'dbe': {'visible': true},
+				'add-meanings': {'visible': true},
+				'add-grammar': {'visible': true},
+				'add-examples': {'visible': true},
+				'settings': {'visible': true},
+				'review': {'visible': true}
+			}
+		};
+		*/
+
