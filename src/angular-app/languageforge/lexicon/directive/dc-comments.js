d06c9e4117422b74716e7d429aae83687b6b08bb
Robin Munn <robin_munn@sil.org>
2014-03-04 09:43:52 +0700

    WIP: Comments tab on DBE - can add subcomments
    "Reply" link next to comments now allows you to add subcomments.
    (Called "responses" in the requirements document, but "subcomments" in
    the source code.)
    
    
    Former-commit-id: 1f4f09148b89d12a4a3653916e723ae91ec7b3e4

--- src/angular-app/languageforge/lexicon/directive/dc-comments.js
+++ src/angular-app/languageforge/lexicon/directive/dc-comments.js
@@ -24,6 +24,7 @@ angular.module('palaso.ui.dc.comments', ['palaso.ui.dc.entry', 'angularjs-gravat
 				// Create and return an empty comment object
 				return {
 					userRef: {username: "Robin M.", email: "Robin_Munn@sil.org"}, // Sample data. If email provided, will be used in fetching Gravatar
+					// TODO: Get actual username & email from session service
 					dateModified: new Date(), // Default to today's date, can modify this elsewhere if needed
 					regarding: "",
 					content: "This is the sample comment",
@@ -38,9 +39,18 @@ angular.module('palaso.ui.dc.comments', ['palaso.ui.dc.entry', 'angularjs-gravat
 				comment.dateModified = new Date(); // Duplicate of code in makeValidComment(); decide later which one to use
 				comment.regarding = $scope.dcModel.value;
 				comment.content = newCommentContent;
-				comment.subcomments.push($scope.makeValidComment());
+				// comment.subcomments.push($scope.makeValidComment());
 				$scope.dcModel.comments.push(comment);
 			};
+
+			$scope.submitSubcomment = function(newSubcommentContent, parentComment) {
+				var subcomment = $scope.makeValidComment();
+				subcomment.dateModified = new Date(); // Duplicate of code in makeValidComment(); decide later which one to use
+				subcomment.regarding = parentComment.content; // We're inside the ng-repeat="comment in ngModel.comments" scope here
+				subcomment.content = newSubcommentContent;
+				subcomment.subcomments.push($scope.makeValidComment());
+				parentComment.subcomments.push(subcomment);
+			}
 		}],
 		link: function(scope, element, attrs, controller) {
 			scope.$watch('visibility', function() { // Or some other variable instead of visibility... this is just an example
