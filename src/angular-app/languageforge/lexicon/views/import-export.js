114ebe15f8ec2573be8ffe2a2e46a98415a0df22
irahopkinson <ira.hopkinson@gmail.com>
2014-03-04 19:53:59 +0700

    LIFT Import layout

--- src/angular-app/languageforge/lexicon/views/import-export.js
+++ src/angular-app/languageforge/lexicon/views/import-export.js
@@ -8,41 +8,14 @@ angular.module('lexicon.importExport', ['ui.bootstrap', 'bellows.services', 'pal
 .controller('LiftImportCtrl', ['$scope', 'userService', 'sessionService', 'silNoticeService', '$http',
                                  function($scope, userService, ss, notice, $http) {
 	$scope.mergeWithExisting = true;
-	
-	$scope.progress = 0;
-	$scope.uploadResult = '';
+
 	$scope.onFileSelect = function($files) {
-		var file = $files[0];	// take the first file only
-		$scope.file = file;
-		if (file['size'] <= ss.fileSizeMax()) {
-			$http.uploadFile({
-			    url: '/upload',	// upload.php script
-//				headers: {'myHeaderKey': 'myHeaderVal'},
-				data: {
-					projectId: projectId,
-					textId: textId,
-				},
-				file: file
-			}).progress(function(evt) {
-				$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-				if (!$scope.$$phase) {
-					$scope.$apply();
-				}
-			}).success(function(data, status, headers, config) {
-				$scope.uploadResult = data.toString();
-				$scope.progress = 100.0;
-				// to fix IE not updating the dom
-				if (!$scope.$$phase) {
-					$scope.$apply();
-				}
-			});
-		} else {
-			$scope.uploadResult = file['name'] + " is too large.";
-		}
+		$scope.file = $files[0];	// take the first file only
+		
 	};
 	
-	$scope.updateImport = function() {
-		console.log("updateImport");
+	$scope.importLift = function() {
+		console.log("importLift");
 	};
 
 }])
