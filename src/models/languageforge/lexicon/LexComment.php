f983061edb2d5ec90bbdaec662a4e46fe0679768
megahirt <chris@hirtfamily.net>
2014-03-06 16:33:09 +0700

    LexEntryCommands tests passing for readEntry and updateEntry
    Former-commit-id: 418bacd47689d05deec3e1a6f502951e77f6f3f9

--- src/models/languageforge/lexicon/LexComment.php
+++ src/models/languageforge/lexicon/LexComment.php
@@ -6,15 +6,32 @@ use models\mapper\ArrayOf;
 
 class LexComment extends LexCommentReply {
 	
+	/**
+	 * 
+	 * @var string - value of the LexiconField under comment at the time the comment was written
+	 */
 	public $regarding;
 	
+	/**
+	 * 
+	 * @var int
+	 */
 	public $score;
 	
+	/**
+	 * 
+	 * @var ArrayOf<LexCommentReply>
+	 */
 	public $subcomments;
 	
+	/**
+	 * 
+	 * @var string - comment status e.g. open, todo, reviewed, resolved
+	 */
 	public $status;
 	
-	public function __construct() {
+	public function __construct($content = '') {
+		$this->content = $content;
 		$this->subcomments = new ArrayOf(
 			function($data) {
 				return new LexCommentReply();
--- test/php/languageforge/lexicon/LexEntryCommands_Test.php
+++ test/php/languageforge/lexicon/LexEntryCommands_Test.php
@@ -1,5 +1,13 @@
 <?php
 
+use models\languageforge\lexicon\commands\LexEntryCommands;
+
+use models\languageforge\lexicon\LexCommentReply;
+
+use models\languageforge\lexicon\LexComment;
+
+use models\languageforge\lexicon\Example;
+
 use models\languageforge\lexicon\Sense;
 
 use models\languageforge\lexicon\LexiconFieldWithComments;
@@ -23,72 +31,112 @@ class TestLexEntryCommands extends UnitTestCase {
 		$projectId = $project->id->asString();
 		
 		$entry = new LexEntryModel($project);
-		$entry->lexeme['en'] = new LexiconFieldWithComments('apple');
+		$entry->lexeme['th'] = new LexiconFieldWithComments('apple');
+
 		$sense = new Sense();
-		$sense->definition['th'] = new LexiconFieldWithComments('red fruit');
+		$sense->definition['en'] = new LexiconFieldWithComments('red fruit');
 		$sense->partOfSpeech = new LexiconFieldWithComments('noun');
-		$entry->senses[] = new Sense();
-		$entry->senses[]
+		
+		$example = new Example();
+		$example->sentence['th'] = new LexiconFieldWithComments('example1');
+		$example->translation['en'] = new LexiconFieldWithComments('trans1');
+		
+		$sense->examples[] = $example;
+		
+		$entry->senses[] = $sense;
+		
+		$entryId = $entry->write();
+		
+		$newEntry = LexEntryCommands::readEntry($projectId, $entryId);
+		
+		$this->assertEqual($newEntry['lexeme']['th']['value'], 'apple');
+		$this->assertEqual($newEntry['senses'][0]['definition']['en']['value'], 'red fruit');
+		$this->assertEqual($newEntry['senses'][0]['partOfSpeech']['value'], 'noun');
+		$this->assertEqual($newEntry['senses'][0]['examples'][0]['sentence']['th']['value'], 'example1');
+		$this->assertEqual($newEntry['senses'][0]['examples'][0]['translation']['en']['value'], 'trans1');
+
 		
 	}
 
 	function testReadEntry_HasComments_ReadBackOk() {
+		$e = new LexiconMongoTestEnvironment();
+		$e->clean();
+		
+		$project = $e->createProject(SF_TESTPROJECT);
+		$projectId = $project->id->asString();
+		
+		$entry = new LexEntryModel($project);
+		$entry->lexeme['th'] = new LexiconFieldWithComments('apple');
+		
+		$reply = new LexCommentReply('reply1');
+		$comment = new LexComment('this is a comment');
+		$comment->score = 5;
+		$comment->regarding = "apple";
+		$comment->subcomments[] = $reply;
+		
+		$entry->lexeme['th']->comments[] = $comment;
+		
+
+		$sense = new Sense();
+		$sense->definition['en'] = new LexiconFieldWithComments('red fruit');
+		$sense->partOfSpeech = new LexiconFieldWithComments('noun');
+		
+		$entry->senses[] = $sense;
+		
+		$entryId = $entry->write();
+		
+		$newEntry = LexEntryCommands::readEntry($projectId, $entryId);
+		
+		$this->assertEqual($newEntry['lexeme']['th']['value'], 'apple');
+		$this->assertEqual($newEntry['senses'][0]['definition']['en']['value'], 'red fruit');
+		$this->assertEqual($newEntry['senses'][0]['partOfSpeech']['value'], 'noun');
+		$this->assertEqual($newEntry['lexeme']['th']['comments'][0]['content'], 'this is a comment');
+		$this->assertEqual($newEntry['lexeme']['th']['comments'][0]['score'], 5);
+		$this->assertEqual($newEntry['lexeme']['th']['comments'][0]['regarding'], 'apple');
+		$this->assertEqual($newEntry['lexeme']['th']['comments'][0]['subcomments'][0]['content'], 'reply1');
+
 
 	}
 	
 	function testUpdateEntry_DataPersists() {
-		
-	}
-	
-	function testRemoveEntries_EntriesRemoved() {
-		
-	}
-
-	function testReadProjectSettings_DefaultsOk() {
 		$e = new LexiconMongoTestEnvironment();
 		$e->clean();
 		
 		$project = $e->createProject(SF_TESTPROJECT);
 		$projectId = $project->id->asString();
 		
-		$dto = LexProjectCommands::readSettings($projectId);
-		
-		// test for a few default values
-		$this->assertEqual($dto['inputSystems']['en']['tag'], 'en');
-		$this->assertTrue($dto['tasks']['dbe']['visible']);
-		$this->assertEqual($dto['entry']['fields']['lexeme']['label'], 'Word');
-		$this->assertEqual($dto['entry']['fields']['lexeme']['label'], 'Word');
-		$this->assertEqual($dto['entry']['fields']['senses']['fields']['partOfSpeech']['label'], 'Part of Speech');
-	}
+		$entry = new LexEntryModel($project);
+		$entry->lexeme['th'] = new LexiconFieldWithComments('apple');
 
-	function testUpdateProjectSettings_SettingsPersist() {
-		$e = new LexiconMongoTestEnvironment();
-		$e->clean();
+		$sense = new Sense();
+		$sense->definition['en'] = new LexiconFieldWithComments('red fruit');
+		$sense->partOfSpeech = new LexiconFieldWithComments('noun');
 		
-		$project = $e->createProject(SF_TESTPROJECT);
-		$projectId = $project->id->asString();
+		$example = new Example();
+		$example->sentence['th'] = new LexiconFieldWithComments('example1');
+		$example->translation['en'] = new LexiconFieldWithComments('trans1');
 		
-		$settingsDto = LexProjectCommands::readSettings($projectId);
+		$sense->examples[] = $example;
 		
-		$this->assertTrue($settingsDto['tasks']['addMeanings']['visible']);
-		$this->assertEqual($settingsDto['entry']['fields']['lexeme']['inputSystems'][0], 'en');
+		$entry->senses[] = $sense;
+		
+		$entryId = $entry->write();
 
-		$settingsDto['tasks']['addMeanings']['visible'] = false;
-		$settingsDto['entry']['fields']['lexeme']['inputSystems'] = array('my', 'th');
+		$params = LexEntryCommands::readEntry($projectId, $entryId);
+		$params['lexeme']['th']['value'] = 'rose apple';
+		$params['senses'][0]['partOfSpeech']['comments'] = array(array('content' => 'i vote for adj'));
 		
-		LexProjectCommands::updateSettings($projectId, $settingsDto);
+		LexEntryCommands::updateEntry($projectId, $entryId, $params);
 		
-		$project2 = new LexiconProjectModel($projectId);
+		$newEntry = LexEntryCommands::readEntry($projectId, $entryId);
+
+		$this->assertEqual($newEntry['lexeme']['th']['value'], 'rose apple');
+		$this->assertEqual($newEntry['senses'][0]['definition']['en']['value'], 'red fruit');
+		$this->assertEqual($newEntry['senses'][0]['partOfSpeech']['value'], 'noun');
+		$this->assertEqual($newEntry['senses'][0]['examples'][0]['sentence']['th']['value'], 'example1');
+		$this->assertEqual($newEntry['senses'][0]['examples'][0]['translation']['en']['value'], 'trans1');
+		$this->assertEqual($newEntry['senses'][0]['partOfSpeech']['comments'][0]['content'], 'i vote for adj');
 		
-		// test for a few default values
-		$this->assertEqual($project2->inputSystems['en']->tag, 'en');
-		$this->assertTrue($project2->settings->tasks['dbe']->visible);
-		$this->assertEqual($project2->settings->entry->fields['lexeme']->label, 'Word');
-		
-		// test for updated values
-		$this->assertFalse($project2->settings->tasks['addMeanings']->visible);
-		$this->assertEqual($project2->settings->entry->fields['lexeme']->inputSystems[0], 'my');
-		$this->assertEqual($project2->settings->entry->fields['lexeme']->inputSystems[1], 'th');
 	}
 }
 
