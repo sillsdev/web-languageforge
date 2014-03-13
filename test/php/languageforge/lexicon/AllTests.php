9f6ceae9936e42902ba0a3123640b6bc62ba1cf5
megahirt <chris@hirtfamily.net>
2014-03-13 16:50:24 +0700

    fixed up lexicon tests
    Former-commit-id: 85865e3da0f8ac7ba784f1639be96fc1c2d5fa77

--- test/php/languageforge/lexicon/AllTests.php
+++ test/php/languageforge/lexicon/AllTests.php
@@ -7,6 +7,7 @@ class AllLexiconAppTests extends TestSuite {
     function __construct() {
         parent::__construct();
         $this->addFile(TestPath . 'languageforge/lexicon/LexEntryCommands_Test.php');
+        $this->addFile(TestPath . 'languageforge/lexicon/LexDbeDto_Test.php');
         $this->addFile(TestPath . 'languageforge/lexicon/LexProjectCommands_Test.php');
         $this->addFile(TestPath . 'languageforge/lexicon/LiftImport_Test.php');
     }
