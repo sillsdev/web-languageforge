bc122d3e871b5706b97a97299e98b66fee67657f
megahirt <chris@hirtfamily.net>
2014-03-17 16:45:07 +0700

    got part of speech dropdown working

--- src/models/languageforge/lexicon/settings/LexiconOptionlistConfigObj.php
+++ src/models/languageforge/lexicon/settings/LexiconOptionlistConfigObj.php
@@ -7,6 +7,7 @@ use models\mapper\MapOf;
 class LexiconOptionlistConfigObj extends LexiconConfigObj {
 	public function __construct() {
 		$this->type = LexiconConfigObj::OPTIONLIST;
+		$this->values = new MapOf();
 		
 		// default values
 		$this->label = '';
@@ -17,6 +18,12 @@ class LexiconOptionlistConfigObj extends LexiconConfigObj {
 	 * @var string
 	 */
 	public $label;
+	
+	/**
+	 * 
+	 * @var MapOf
+	 */
+	public $values;
 	
 }
 
