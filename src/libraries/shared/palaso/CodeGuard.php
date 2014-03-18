d485acc4f71be460628d1fcfff2e5f599351f1a8
megahirt <chris@hirtfamily.net>
2014-03-18 12:02:17 +0700

    code guard improvements

--- src/libraries/shared/palaso/CodeGuard.php
+++ src/libraries/shared/palaso/CodeGuard.php
@@ -21,30 +21,43 @@ class CodeGuard {
 		if ($type == 'object') {
 			$type = get_class($var);
 		}
-		var_dump(debug_backtrace());
+		self::printStackTrace();
 		throw new \Exception("Type Exception: Expected '" . $expectedType . "' given '" . $type . "'");
 	}
 	
 	public static function checkNullAndThrow($var, $name) {
 		if ($var == null) {
-			debug_print_backtrace();
+			self::printStackTrace();
 			throw new \Exception("'$name' should not be null");
 		}
 	}
 	
 	public static function checkEmptyAndThrow($var, $name) {
 		if (empty($var)) {
-			debug_print_backtrace();
+			self::printStackTrace();
 			throw new \Exception("'$name' should not be empty");
 		}
 	}
 	
 	public static function checkNotFalseAndThrow($var, $name) {
 		if ($var == null || !$var) {
-			debug_print_backtrace();
+			self::printStackTrace();
 			throw new \Exception("'$name' should not evaluate to false");
 		}
 	}
+	
+	private static function printStackTrace() {
+		$stacktrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 7);
+		foreach ($stacktrace as $item) {
+			$file = substr($item['file'], strrpos($item['file'], '/')+1);
+			$line = $item['line'];
+			$function = $item['function'];
+			$type = $item['type'];
+			$class = substr($item['class'], strrpos($item['class'], '\\')+1);
+			$args = implode(', ', array_map(function($val) { return (is_array($val)) ? 'Array' : $val; }, $item['args']));
+			print "<pre>Line $line in $file, $class$type$function($args)</pre>";
+		}
+	} 
 	
 }
 
