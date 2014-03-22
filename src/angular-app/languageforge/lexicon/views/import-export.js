eb56e7647f7660f93dfc10c133e113b520b25987
irahopkinson <ira.hopkinson@gmail.com>
2014-03-22 11:38:43 +0700

    Import/Export - refactor to use LexBaseViewDto
    Former-commit-id: f463a8502c7bf7294d27b692fef8e4ea82de979d

--- src/angular-app/languageforge/lexicon/views/import-export.js
+++ src/angular-app/languageforge/lexicon/views/import-export.js
@@ -1,22 +1,18 @@
 'use strict';
 
 angular.module('lexicon.importExport', ['ui.bootstrap', 'bellows.services', 'palaso.ui.notice', 'palaso.ui.language', 'ngAnimate', 'angularFileUpload', 'lexicon.upload'])
-.controller('LiftImportCtrl', ['$scope', 'breadcrumbService', 'lexLinkService', 'userService', 'sessionService', 'silNoticeService', 'fileReader', 'lexProjectService', 
-                               function($scope, breadcrumbService, linkService, userService, ss, notice, fileReader, lexProjectService) {
-	$scope.project = {};
-	$scope.project.projectname = breadcrumbService.get('top')[1]['label'];	// TODO Fix. doesn't work on a page refresh, very page needs to pull a minimal dto for config ad projectname. IJH 2014-03
-	breadcrumbService.set('top',
-		[
-		 {href: '/app/projects', label: 'My Projects'},
-		 {href: linkService.project(), label: $scope.project.projectname},
-		 {href: linkService.projectView('importExport'), label: 'Import/Export'},
-		]
-	);
+.controller('LiftImportCtrl', ['$scope', 'silNoticeService', 'fileReader', 'lexProjectService', 'lexConfigService', 
+                               function($scope, notice, fileReader, lexProjectService, configService) {
+	lexProjectService.baseViewDto('importExport', 'Import/export', function(result) {
+		if (result.ok) {
+			configService.setConfig(result.data.config);
+		}
+	});
 	
 	$scope.mergeRule = 'createDuplicates';
 	$scope.skipSameModTime = true;
 	$scope.deleteMatchingEntry = false;
-	
+
 	$scope.onFileSelect = function($files) {
 		$scope.file = $files[0];	// take the first file only
 		fileReader.readAsDataUrl($scope.file, $scope)
