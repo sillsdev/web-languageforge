9630ef2b1349a247ba7d370e1da5ae80572f2d06
irahopkinson <ira.hopkinson@gmail.com>
2014-03-31 17:21:03 +0700

    refactor UserProfileModel to have projectsProperties which includes interfaceLanguageCode
    Former-commit-id: 885a6ded9123e5b25bd27aeedb342daa4be6ce5b

--- src/models/languageforge/lexicon/dto/LexBaseViewDto.php
+++ src/models/languageforge/lexicon/dto/LexBaseViewDto.php
@@ -5,8 +5,8 @@ namespace models\languageforge\lexicon\dto;
 use models\languageforge\lexicon\LexiconProjectModel;
 use models\mapper\JsonEncoder;
 use models\shared\dto\RightsHelper;
-use models\UserModel;
 use models\ProjectModel;
+use models\UserProfileModel;
 
 class LexBaseViewDto {
 	
@@ -15,14 +15,20 @@ class LexBaseViewDto {
 	 * @returns array - the DTO array
 	 */
 	public static function encode($projectId, $userId) {
-		$user = new UserModel($userId);
+		$user = new UserProfileModel($userId);
 		$project = new LexiconProjectModel($projectId);
-		
+
 		$config = JsonEncoder::encode($project->config);
 		$config['inputSystems'] = JsonEncoder::encode($project->inputSystems);
 		
+		$interfaceLanguageCode = $project->interfaceLanguageCode;
+		if (key_exists($projectId, $user->projectsProperties) && $user->projectsProperties[$projectId]->interfaceLanguageCode) {
+			$interfaceLanguageCode = $user->projectsProperties[$projectId]->interfaceLanguageCode;
+		}
+		
 		$data = array();
 		$data['config'] = $config;
+		$data['user'] = array('interfaceLanguageCode' => $interfaceLanguageCode);
 		$data['project'] = array('projectname' => $project->projectname);
 		$data['rights'] = RightsHelper::encode($user, $project);
 		
