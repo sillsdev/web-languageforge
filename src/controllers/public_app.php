ad6868c5780f6e99c7e91d1d81da886d2a3c5fed
megahirt <chris@hirtfamily.net>
2014-04-22 16:51:24 +0700

    fixed up public_app CI controller to be consistent with app CI controller

--- src/controllers/public_app.php
+++ src/controllers/public_app.php
@@ -10,7 +10,7 @@ class Public_app extends Base {
 	public function view($app = 'main') {
 		$appFolder = "angular-app/" . $this->site . "/public/$app";
 		if (!file_exists($appFolder)) {
-			$appFolder = "angular-app/bellows/app/public/$app";
+			$appFolder = "angular-app/bellows/apps/public/$app";
 			if (!file_exists($appFolder)) {
 				show_404($this->site); // this terminates PHP
 			}
@@ -22,9 +22,16 @@ class Public_app extends Base {
 		$data['appFolder'] = $appFolder;
 		
 		$data['jsFiles'] = array();
-		self::addJavascriptFiles("angular-app/bellows/js", $data['jsFiles']);
+		self::addJavascriptFiles("angular-app/bellows/js", $data['jsFiles'], array('vendor/', 'assets/'));
 		self::addJavascriptFiles ( "angular-app/bellows/directive", $data ['jsFiles'] );
-		self::addJavascriptFiles($appFolder, $data['jsFiles']);
+		self::addJavascriptFiles($appFolder, $data['jsFiles'], array('vendor/', 'assets/'));
+		
+		// remove asset js files
+		$data['jsNotMinifiedFiles'] = array();
+		self::addJavascriptFiles("angular-app/bellows/js/vendor", $data['jsNotMinifiedFiles']);
+		self::addJavascriptFiles("angular-app/bellows/js/assets", $data['jsNotMinifiedFiles']);
+		self::addJavascriptFiles($appFolder . "/js/vendor", $data['jsNotMinifiedFiles']);
+		self::addJavascriptFiles($appFolder . "/js/assets", $data['jsNotMinifiedFiles']);
 			
 		$data['cssFiles'] = array();
 		self::addCssFiles("angular-app/bellows/css", $data['cssFiles']);
@@ -44,18 +51,39 @@ class Public_app extends Base {
 		return pathinfo($filename, PATHINFO_BASENAME);
 	}
 	
-	private static function addJavascriptFiles($dir, &$result) {
-		if (($handle = opendir($dir))) {
+	private static function addJavascriptFiles($dir, &$result, $exclude = array()) {
+		self::addFiles('js', $dir, $result, $exclude);
+	}
+	
+	private static function addCssFiles($dir, &$result) {
+		self::addFiles('css', $dir, $result, array());
+	}
+	
+	private static function addFiles($ext, $dir, &$result, $exclude) {
+		if (is_dir($dir) && ($handle = opendir($dir))) {
 			while ($file = readdir($handle)) {
-				if (is_file($dir . '/' . $file)) {
-					$base = self::basename($file);
-					//$isMin = (strpos($base, '-min') !== false) || (strpos($base, '.min') !== false);
-					$isMin = FALSE;
-					if (!$isMin && self::ext($file) == 'js') {
-						$result[] = $dir . '/' . $file;
+				$filepath = $dir . '/' . $file;
+				foreach ($exclude as $ex) {
+					if (strpos($filepath, $ex)) {
+						continue 2;
+					}
+				}
+				if (is_file($filepath)) {
+					if ($ext == 'js') {
+						/* For Javascript, check that file is not minified */
+						$base = self::basename($file);
+						//$isMin = (strpos($base, '-min') !== false) || (strpos($base, '.min') !== false);
+						$isMin = FALSE;
+						if (!$isMin && self::ext($file) == $ext) {
+							$result[] = $filepath;
+						}
+					} else {
+						if (self::ext($file) == $ext) {
+							$result[] = $filepath;
+						}
 					}
 				} elseif ($file != '..' && $file != '.') {
-					self::addJavascriptFiles($dir . '/' . $file, $result);
+					self::addFiles($ext, $filepath, $result, $exclude);
 				}
 			}
 			closedir($handle);
