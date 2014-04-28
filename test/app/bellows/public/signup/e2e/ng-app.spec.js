240570a80f958fb4dcf459b4f57436be5fe29ebb
irahopkinson <ira.hopkinson@gmail.com>
2014-04-28 17:06:37 +0700

    wip: signup e2e tests
    Former-commit-id: eee5c30b51dfe63e5413cdf36134a8f8320f84e2

--- test/app/bellows/public/signup/e2e/ng-app.spec.js
+++ test/app/bellows/public/signup/e2e/ng-app.spec.js
@@ -1,38 +1,51 @@
 'use strict';
 
+var SfSignupPage = function() {
+	this.get = function() {
+		browser.get('http://jamaicanpsalms.scriptureforge.local/signup');
+	};
+	
+	this.userForm = element('form#userForm');
+	this.userNameExists = element(by.id('userNameExists'));
+	this.userNameOk = element(by.id('userNameOk'));
+	this.usernameInput = element(by.model('record.username'));
+	this.nameInput = element(by.model('record.name'));
+	this.emailInput = element(by.model('record.email'));
+	this.passwordInput = element(by.model('record.password'));
+	this.captchaInput = element(by.model('record.captcha'));
+	this.signupButton = element(by.id('submit'));
+	this.warnNotices = $('.notices').element.all(by.css('.alert div span'));
+}; 
+
 describe('E2E testing: Signup app', function() {
+	var sfSignupPage = new SfSignupPage();
 	
 	beforeEach(function() {
-	    browser.get('http://jamaicanpsalms.scriptureforge.local/signup');
+		sfSignupPage.get();
 	});
 	
 	it('contains a user form', function() {
-		expect(element("form#userForm")).toBeDefined();
+		expect(sfSignupPage.userForm).toBeDefined();
 	});
 	
 	it('finds the admin user already exists', function() {
-		element(by.model('record.username')).sendKeys('admin');
+		sfSignupPage.usernameInput.sendKeys('admin');
 		
 		// trigger the username lookup
-//		element("input#username").query(function (element, done) {
-//			element.blur();
-//			done();
-//		});
-		element(by.model('record.username')).sendKeys(protractor.Key.ENTER);
+		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
 		
-		expect(element('#userNameExists:visible')).toBe(true);
+		expect(sfSignupPage.userNameExists.isDisplayed()).toBeTruthy();
+		expect(sfSignupPage.userNameOk.isDisplayed()).toBeFalsy();
 	});
-/*	
+	
 	it("can verify that 'newuser' is an available username", function() {
-		input("record.username").enter("newuser");
+		sfSignupPage.usernameInput.sendKeys('newuser');
 		
 		// trigger the username lookup
-		element("input#username").query(function (element, done) {
-			element.blur();
-			done();
-		});
-		input("record.name").enter("New User");
-		expect(element("#userNameExists:visible").count()).toBe(0);
+		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
+
+		expect(sfSignupPage.userNameExists.isDisplayed()).toBeFalsy();
+		expect(sfSignupPage.userNameOk.isDisplayed()).toBeTruthy();
 	});
 	
 	/* I failed to figure out how to make this test pass.  For some reason the viewCaptcha PHP api is not returning an image in the test environment when it does return an image in a normal browser environment
@@ -41,23 +54,23 @@ describe('E2E testing: Signup app', function() {
 		expect(element("img#captcha").prop("naturalWidth")).toBeGreaterThan(0);
 	});
 	*/
-/*	
-	it("can submit a user registration request and captcha is invalid", function() {
-		input("record.username").enter("newuser");
+	
+	it('can submit a user registration request and captcha is invalid', function() {
+		sfSignupPage.usernameInput.sendKeys('newuser');
 		
 		// trigger the username lookup
-		element("input#username").query(function (element, done) {
-			element.blur();
-			done();
+		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
+
+		sfSignupPage.nameInput.sendKeys('New User');
+		sfSignupPage.emailInput.sendKeys('email@example.com');
+		sfSignupPage.passwordInput.sendKeys('12345');
+		sfSignupPage.captchaInput.sendKeys('whatever');
+
+		expect(sfSignupPage.warnNotices.count()).toBe(0);
+		sfSignupPage.signupButton.click().then(function() {
+			expect(sfSignupPage.warnNotices.count()).toBe(1);
+//			expect(sfSignupPage.warnNotices[0].getText()).toContain('image verification failed');
 		});
-		
-		input("record.name").enter("New User");
-		input("record.email").enter("email@address.com");
-		input("record.password").enter("12345");
-		input("record.captcha").enter("whatever");
-		expect(element("div#captchaError:visible").count()).toBe(0);
-		element("button#submit").click();
-		expect(element("div#captchaError:visible").count()).toBe(1);
 	});
-*/	
+	
 });
