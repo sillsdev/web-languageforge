89487f62a1bacfef68c3cdfce8426df3b3da023b
darcywong00 <darcy_wong@sil.org>
2014-05-06 16:40:47 +0700

    Start protractor test for verifying User account page

--- test/protractorConf.js
+++ test/protractorConf.js
@@ -18,7 +18,8 @@ exports.config = {
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['app/setupTestEnvironment.spec.js', 'app/**/e2e/*.spec.js', 'app/teardownTestEnvironment.spec.js'],
+  // Put this back: specs: ['app/setupTestEnvironment.spec.js', 'app/**/e2e/*.spec.js', 'app/teardownTestEnvironment.spec.js'],
+  specs: ['app/bellows/public/userProfile/e2e/*.spec.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
