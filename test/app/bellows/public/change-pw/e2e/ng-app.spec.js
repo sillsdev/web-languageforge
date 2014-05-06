80519eaa80c64948cee931476822efc2f04aaa80
Robin Munn <robin_munn@sil.org>
2014-05-06 16:39:15 +0700

    WIP: E2E test for changing password
    Currently does NOT work, as apparently there is no isDisabled()
    function on elements. I'll need to find the correct way to check
    for an element being disabled, but it should be straightforward.

---
+++ test/app/bellows/public/change-pw/e2e/ng-app.spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var SfChangePasswordPage = function() {
+	this.get = function() {
+		browser.get('http://jamaicanpsalms.scriptureforge.local/app/changepassword');
+	};
+	// Do the 
+	this.passwordForm = element('form#passwordForm');
+	this.passwordInput = element(by.model('vars.password'));
+	this.confirmInput = element(by.model('vars.confirm_password'));
+	this.signupButton = element(by.tagName('button')); // Might need to change this...?
+}; 
+
+describe('E2E testing: Change password', function() {
+	var sfChangePasswordPage = new SfChangePasswordPage();
+	
+	beforeEach(function() {
+		sfChangePasswordPage.get();
+	});
+	
+	it('contains a password form', function() {
+		expect(sfChangePasswordPage.passwordForm).toBeDefined();
+	});
+	
+	it('refuses to allow form submission if the confirm input does not match', function() {
+		sfChangePasswordPage.passwordInput.sendKeys('abc123').then(function() {
+			sfChangePasswordPage.confirmInput.sendKeys('abcd1234').then(function() {
+				expect(sfChangePasswordPage.signupButton.isDisabled()).toBeTruthy();
+			});
+		});
+	});
+	
+});
