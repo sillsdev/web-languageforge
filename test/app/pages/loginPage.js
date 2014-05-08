7feaadc81fb21e3cf7623ef0d31e1d0bf6866d68
Robin Munn <robin_munn@sil.org>
2014-05-08 16:34:34 +0700

    WIP: setupTestEnvironment.spec.js (now working)
    Still incomplete, but there are some features that need to be added to
    our siteadmin app (like a user search feature) before this can truly do
    all the setup required. For now, it just prints some console messages
    detailing what manual steps are needed for the setup.
    
    
    Former-commit-id: 7960bfc90a6ac26d34bca815cb2f627739fa0af5

---
+++ test/app/pages/loginPage.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var SfLoginPageWithoutAstrolabe = function() {
+	var page = this; // For use inside our methods. Necessary when passing anonymous functions around, which lose access to "this".
+
+	this.adminUsername   = 'test_runner_admin';
+	this.adminPassword   = 'hammertime';
+	this.managerUsername = 'test_runner_manager';
+	this.managerPassword = 'blacksmith571';
+	this.memberUsername  = 'testuser';
+	this.memberPassword  = 'test1234';
+
+	this.baseUrl = browser.baseUrl || 'http://jamaicanpsalms.scriptureforge.local';
+	this.get = function() {
+		return browser.driver.get(this.baseUrl + '/auth/login');
+	};
+
+	// Note that we can't use browser.driver.findElement() yet, as that doesn't return a promise
+	// but tries to find the element *immediately*. We have to use findElement() later, in the login() function
+	this.username = by.id('identity');
+	this.password = by.id('password');
+	this.submit   = by.xpath('//input[@type="submit"]');
+
+	this.login = function(username, password) {
+		page.get();
+
+		// Now we need to wait for the page to load
+		browser.driver.wait(function() {
+			// wait() wants a function that will return true when we should finish waiting.
+			// In other words, we should return a promise that, when it is fulfilled, means "You can stop waiting now".
+			// Here, we use "Is the username field present?" as the promise that fulfills the wait condition.
+			return browser.driver.isElementPresent(page.username); // NOTE: using "this.username" would have failed, because "this" is undefined right now.
+		}, 8000); // Timeout if still not loaded after 8 seconds
+
+		browser.driver.findElement(page.username).sendKeys(username);
+		browser.driver.findElement(page.password).sendKeys(password);
+		browser.driver.findElement(page.submit).click();
+	};
+	this.loginAsAdmin = function() {
+		this.login(this.adminUsername, this.adminPassword);
+	};
+	this.loginAsManager = function() {
+		this.login(this.managerUsername, this.managerPassword);
+	};
+	this.loginAsUser = this.loginAsMember = function() {
+		this.login(this.memberUsername, this.memberPassword);
+	};
+};
+
+// WARNING: SfLoginPageWithAstrolabe is NOT working yet. Do NOT use it while this comment is still in the code.
+var astrolabe = require('astrolabe');
+var SfLoginPageWithAstrolabe = astrolabe.Page.create({
+	adminUsername:   { value: 'test_runner_admin' },
+	adminPassword:   { value: 'hammertime' },
+	managerUsername: { value: 'test_runner_manager' },
+	managerPassword: { value: 'blacksmith571' },
+	memberUsername:  { value: 'testuser' },
+	memberPassword:  { value: 'test1234' },
+	baseUrl: { value: browser.baseUrl || 'http://jamaicanpsalms.scriptureforge.local' },
+	url:     { value: this.baseUrl + "/app/siteadmin" },
+	username: { get: function() { return this.findElement(this.by.id('identity')); }},
+	password: { get: function() { return this.findElement(this.by.id('password')); }},
+	submit:   { get: function() { return this.findElement(this.by.xpath('//input[@type="submit"]')); }},
+
+	login: { value: function(username, password) {
+		var page = this;
+		page.go();
+		browser.driver.wait(function() {
+			// wait() wants a function that will return true when we should finish waiting.
+			// In other words, we should return a promise that, when it is fulfilled, means "You can stop waiting now".
+			// Here, we use "Is the username field present?" as the promise that fulfills the wait condition.
+			return browser.driver.isElementPresent(by.id('identity')); // NOTE: using "this.username" would have failed, because "this" is undefined right now.
+		}, 8000); // Timeout if still not loaded after 8 seconds
+
+		page.username.sendKeys(username);
+		page.password.sendKeys(password);
+		page.submit.click();
+	}},
+	loginAsAdmin: { value: function() {
+		this.login(this.adminUsername, this.adminPassword);
+	}},
+	loginAsManager: { value: function() {
+		this.login(this.managerUsername, this.managerPassword);
+	}},
+	loginAsUser: { value: function() {
+		this.login(this.memberUsername, this.memberPassword);
+	}},
+	loginAsMember: { value: function() {
+		this.login(this.memberUsername, this.memberPassword);
+	}},
+});
+
+module.exports = SfLoginPageWithoutAstrolabe;
+// This makes the result of calling require('./pages/loginPage') to be the SfLoginPage constructor function.
+// So you'd use this as "var LoginPage = require('./pages/loginPage'); var loginPage = new LoginPage();"
