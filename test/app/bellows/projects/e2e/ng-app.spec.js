a0346e0440007be5bbaa4b4ed418a244b02f2872
megahirt <chris@hirtfamily.net>
2014-05-13 16:31:28 +0700

    projects list e2e wip

---
+++ test/app/bellows/projects/e2e/ng-app.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+describe('E2E Projects List App - Normal User', function() {
+	
+	// environment for test
+	/*
+	var testEnvironment = require();
+	
+	testEnvironment.ensureNormalUserExists();
+	testEnvironment.makeProjectIfNecessary('project1');
+	testEnvironment.makeProjectIfNecessary('project2');
+	testEnvironment.ensureNormalUserIsProjectMember('project1');
+	*/
+	
+	
+	it('should list the project of which the user is a member', function() {
+		
+	});
+	
+	it('should not list projects the user is not a member of', function() {
+		
+	});
+	
+	it('can list two projects of which the user is a member', function() {
+		
+	});
+});
+
+describe('E2E Projects List App - Site Admin User', function() {
+	
+	it('should list all projects for ScriptureForge', function() {
+		
+	});
+	
+	it('should link to only projects of which the admin user is a member', function() {
+		
+	});
+	
+	it('should allow the admin to add themselves to the project as member or manager', function() {
+		
+	});
+});
