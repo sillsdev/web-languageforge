6e0405c890407db563f99f4b55df2410a589ab14
darcywong00 <darcy_wong@sil.org>
2014-05-14 16:21:51 +0700

    Updates to Activity E2E test: Add own answer.  WIP finding where to add comment to an answer.

--- test/app/bellows/activity/e2e/ng-app.spec.js
+++ test/app/bellows/activity/e2e/ng-app.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
+var constants   = require('../../../../testConstants');
+
 var SfActivity = function() {
-	// Get Questions page to add/edit/delete answers and comments
-	this.getQuestion = function() {
-		this.questionURL = browser.baseUrl ;
-		
-		browser.driver.get(this.questionURL);
-	}
+	this.devElement = by.id('comments'); // Using ID "Comments" for Questions
+	//this.newAnswer  = by.model('newAnswer.content');
+	this.newComment = by.model('newComment.content');
+	this.done       = by.id('doneBtn'); 
 	
 	// Select a specific object from the row of selections [texts / questions]
 	this.selectObj = function(type) {
@@ -14,14 +14,25 @@ var SfActivity = function() {
 		var expectedValue;
 		var repeaterKey;
 		
-		if (type == 'text') {
+		switch(type) {
+		case 'text':
 			columnParam   = '{{text.title}}';
 			expectedValue = 'Chapter 3';
 			repeaterKey   = 'text in visibleTexts';
-		} else if (type == 'question') {
+			break;
+		case 'question':
 			columnParam   = '{{question.calculatedTitle}}';
 			expectedValue = 'Who is speaking?';
-			repeaterKey   = 'question in visibleQuestions'
+			repeaterKey   = 'question in visibleQuestions';
+			break;
+		case 'comments':
+			columnParam   = ''; //'{{comment.content}} - {{comment.userRef.username}} - {{comment.dateCreated | date:mediumdate}}';
+			expectedValue = 'I dont know';
+			repeaterKey   = 'comment in answer.comments';
+			break;
+		default :
+			console.log('activity E2E: Unexpectedly looking for type ' + type);
+			return;
 		};
 		
 		// Choose the object from the list
@@ -29,6 +40,11 @@ var SfActivity = function() {
 		var foundLink;
 		rows.map(function(column) {
 			column.getText().then(function(value) {
+				if (type == 'comments') {
+					console.log('Comment loop: ' + column.getText());
+					
+				};
+				
 				if (value === expectedValue) {
 					//textColumn.getOuterHtml().then(console.log);
 					foundLink = column.findElement(by.xpath('ancestor::a'));
@@ -38,8 +54,11 @@ var SfActivity = function() {
 			// expect a value to be found
 			//	expect(foundLink.isPresent()).toBeTruthy();
 			foundLink.getAttribute('href').then(function(url) {
+				//console.log('Going to ' + url);
 				browser.get(url);
 			});
+//		}).then(function() {
+//			browser.sleep(8000);
 		});
 	}
 	
@@ -53,6 +72,7 @@ var SfActivity = function() {
 
 describe('E2E testing: User Activity page', function() {
 	var sfUserActivity = new SfActivity();
+	var newAnswerText  = 'Beethoven was the speaker.';
 	
 	var SfLoginPage = require('../../../pages/loginPage');
 	var loginPage   = new SfLoginPage();
@@ -60,11 +80,10 @@ describe('E2E testing: User Activity page', function() {
 	var SfProjPage  = require('../../../pages/projectsPage');
 	var projPage    = new SfProjPage();
 	
-	var constants   = require('../../../../testConstants');
-
 	loginPage.loginAsUser();
 	
 	it('should display user\'s activity', function() {
+		
 		projPage.get();
 		projPage.findProject('test_project').then(function(projectRow) {
 			var link = projectRow.$('a');
@@ -75,10 +94,29 @@ describe('E2E testing: User Activity page', function() {
 			// Select the text and question according to role
 			sfUserActivity.selectObj('text');
 			sfUserActivity.selectObj('question');
-			
 		});
 		
+		// Add your own answer
+		// TODO: Currently Chrome browser has issues and separates the string.
+		// Firefox 28.0 correctly sends the string, but Firefox 29.0.1 does not
+		var devElement = browser.findElement(sfUserActivity.devElement);
+		devElement.$(".jqte_editor").sendKeys(newAnswerText);
 
+		// TODO: Currently sending this extra "TAB" key appears to help sendKeys send the entire Answer
+		devElement.$(".jqte_editor").sendKeys(protractor.Key.TAB);
+		devElement.findElement(sfUserActivity.done).click();
+		
+		// Add your own comment
+		//sfUserActivity.selectObj('comments');
+		//devElement = browser.findElement(sfUserActivity.newComment);
+		//devElement.$("input[type='submit']").click();
 	});
 
 });
+
+
+
+
+
+
+
