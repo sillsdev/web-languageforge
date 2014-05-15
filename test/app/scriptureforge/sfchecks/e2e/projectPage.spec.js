78188c08fa9f7e2e33c263f0cce917c942699bf1
megahirt <chris@hirtfamily.net>
2014-05-15 15:51:08 +0700

    basic projectPage object
    Former-commit-id: 8130c08028b98c59cad43feb6ee67c4867d7a661

---
+++ test/app/scriptureforge/sfchecks/e2e/projectPage.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('exercise the projectPage object', function() {
+	var projectPage = require('../../../pages/projectPage.js');
+	var loginPage = require('../../../pages/loginPage.js');
+	loginPage = new loginPage();
+
+    it('can click on a text', function() {
+    	loginPage.login('chris', '0JvbuHNYQrvib3n0iNls');
+    	projectPage.get('51fa387956dd85d1714e0ff8');
+    	projectPage.clickOnText('Saamz 3');
+    });
+
+});
