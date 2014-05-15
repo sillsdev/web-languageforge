ca6c1ac9460efb7ecfef777914bb83e6a863972e
megahirt <chris@hirtfamily.net>
2014-05-15 15:05:38 +0700

    updated runE2e.sh

--- test/app/bellows/public/signup/runE2e.sh
+++ test/app/bellows/public/signup/runE2e.sh
@@ -2,5 +2,5 @@
 
 TESTDIR=`pwd|perl -pi -e 's@/test/.*@/test@'`
 
-node $TESTDIR/node_modules/protractor/bin/protractor $TESTDIR/protractorConf.js --verbose --specs "`find . -wholename "*e2e*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
+node $TESTDIR/node_modules/protractor/bin/protractor $1 $TESTDIR/protractorConf.js --verbose --specs "`find . -wholename "*e2e*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
 
