fd3d63cc1ab07a24a68a07d11d3ef489e6a13142
darcywong00 <darcy_wong@sil.org>
2014-05-16 16:10:31 +0700

    Architecture updates to activity E2E
    Update activity E2E to add answer to the end of the answers list, and then add comment to that answer
    Created textPage to view/select questions on the Text page
    Consolidated answerPage and commentPage into questionPage.

--- test/app/bellows/activity/e2e/ng-app.spec.js
+++ test/app/bellows/activity/e2e/ng-app.spec.js
@@ -3,65 +3,6 @@
 var constants   = require('../../../../testConstants');
 
 var SfActivity = function() {
-	this.devElement = by.id('comments'); // Using ID "Comments" for Questions
-	//this.newAnswer  = by.model('newAnswer.content');
-	this.newComment = by.model('newComment.content');
-	this.done       = by.id('doneBtn'); 
-	
-	// Select a specific object from the row of selections [texts / questions]
-	this.selectObj = function(type) {
-		var columnParam;
-		var expectedValue;
-		var repeaterKey;
-		
-		switch(type) {
-		case 'text':
-			columnParam   = '{{text.title}}';
-			expectedValue = 'Chapter 3';
-			repeaterKey   = 'text in visibleTexts';
-			break;
-		case 'question':
-			columnParam   = '{{question.calculatedTitle}}';
-			expectedValue = 'Who is speaking?';
-			repeaterKey   = 'question in visibleQuestions';
-			break;
-		case 'comments':
-			columnParam   = ''; //'{{comment.content}} - {{comment.userRef.username}} - {{comment.dateCreated | date:mediumdate}}';
-			expectedValue = 'I dont know';
-			repeaterKey   = 'comment in answer.comments';
-			break;
-		default :
-			console.log('activity E2E: Unexpectedly looking for type ' + type);
-			return;
-		};
-		
-		// Choose the object from the list
-		var rows = element.all(by.repeater(repeaterKey).column(columnParam));
-		var foundLink;
-		rows.map(function(column) {
-			column.getText().then(function(value) {
-				if (type == 'comments') {
-					console.log('Comment loop: ' + column.getText());
-					
-				};
-				
-				if (value === expectedValue) {
-					//textColumn.getOuterHtml().then(console.log);
-					foundLink = column.findElement(by.xpath('ancestor::a'));
-				};
-			});
-		}).then(function() {
-			// expect a value to be found
-			//	expect(foundLink.isPresent()).toBeTruthy();
-			foundLink.getAttribute('href').then(function(url) {
-				//console.log('Going to ' + url);
-				browser.get(url);
-			});
-//		}).then(function() {
-//			browser.sleep(8000);
-		});
-	}
-	
 	// Get Activity feed
 	this.getActivity = function() {
 		this.activityURL = browser.baseUrl + '/app/activity';
@@ -72,47 +13,41 @@ var SfActivity = function() {
 
 describe('E2E testing: User Activity page', function() {
 	var sfUserActivity = new SfActivity();
-	var newAnswerText  = 'Beethoven was the speaker.  ' + new Date();
-	var newCommentText = 'This comment is added by test_runner_normal_user';
+	var testText       = 'Chapter 3';
+	var testQuestion   = 'Who is speaking?';
+	var timestamp      = new Date();
+	var newAnswerText  = 'Beethoven was the speaker.  ' + timestamp;
+	var newCommentText = 'This comment is added in an E2E test';
 	
-	var SfLoginPage = require('../../../pages/loginPage');
-	var loginPage   = new SfLoginPage();
+	var SfLoginPage     = require('../../../pages/loginPage');
+	var SfProjListPage  = require('../../../pages/projectsPage');
+	var projectPage     = require('../../../pages/projectPage');
+	var questionPage    = require('../../../pages/questionPage');
+	var textPage        = require('../../../pages/textPage');
+
+	var projectListPage = new SfProjListPage();
 	
-	var SfProjPage  = require('../../../pages/projectsPage');
-	var projPage    = new SfProjPage();
-	
-	var answerPage  = require('../../../pages/answerPage');
-	var commentPage = require('../../../pages/commentPage');
-	
+	var loginPage       = new SfLoginPage();
 	loginPage.loginAsUser();
 	
 	it('should display user\'s activity', function() {
+
 		// Perform some actions to populate the activity feed
-		
+		projectListPage.get();
+		projectListPage.clickOnProject('test_project');
+		projectPage.clickOnText(testText);
+		textPage.clickOnQuestion(testQuestion);
 
-		// TODO: Goal is to follow this flow
-		projPage.get();
-		projPage.clickOnProject('test_project');
-//		textPage = new require('textPage');
-//		textPage.clickOnText('text1')l
-//		questionListPage = require('questionListPage');
-//		questionListPage.clickOnQuestion('question1');
-//		questionPage = require('questionPage');
-//		answers = questionPage.answers;
+		// Add your own answer to the end of the answers list
+		questionPage.answers.add(newAnswerText);
 		
-		// deprecate these lines
-		sfUserActivity.selectObj('text');
-		sfUserActivity.selectObj('question');
+		// Add your own comment to the end of the last answer
+		questionPage.comments.addToLastAnswer(newCommentText);
 
-		// Add your own answer (Put this back in once addComment works)
-		answerPage.addAnswer(newAnswerText);
+		// Now check the activity feed
+		sfUserActivity.getActivity();
 		
-		// Add your own comment
-		commentPage.addComment(newCommentText);
-		
-		//sfUserActivity.selectObj('comments');
-		//devElement = browser.findElement(sfUserActivity.newComment);
-		//devElement.$("input[type='submit']").click();
+		browser.debugger();
 	});
 
 });
