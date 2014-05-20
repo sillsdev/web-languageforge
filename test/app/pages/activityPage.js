8822b8c1fbb363687a02bb94baeed166f04d5bcc
darcywong00 <darcy_wong@sil.org>
2014-05-20 14:29:24 +0700

    Implement verification in activity E2E test
    Verified user actions to add answers and comments appear in the activity feed
    Tested buttons to filter activity feed for all actions and user actions

--- test/app/pages/activityPage.js
+++ test/app/pages/activityPage.js
@@ -1,43 +1,43 @@
 'use strict';
 
+/*
+// This object handles the activity page and provides methods to access items in the activity list
+ */
 var SfActivityPage = function() {
 	var page = this; // For use inside our methods. Necessary when passing anonymous functions around, which lose access to "this".
 
 	this.activityURL = '/app/activity';
 	this.activitiesList = element.all(by.repeater('item in filteredActivities'));
 
+	// Navigate to the Activity page
 	this.get = function() {
 		browser.get(browser.baseUrl + this.activityURL);
 		
 	};
-		
+
+	this.clickOnAllActivity = function() {
+		element(by.partialButtonText('All Activity')).click();
+	};
+
+	this.clickOnShowOnlyMyActivity = function() {
+		element(by.partialButtonText('Show Only My Activity')).click();
+	};
+
+	// Returns the number of items in the activity list
 	this.getLength = function() {
 		return page.activitiesList.count();
 	}
 
-	this.getActivity = function(index) {
-		console.log('Inside getActivity for index', index);
-		this.getLength().then(function(len) {
-			if ((index >= 0) && (index < len)) {
-				console.log('1 >= 0 and length is '+ len);
-				return (page.activitiesList).get(index).getText();//.then(console.log);
-			} else {
-				console.log('Error: activity list does not have index ' + index);
-			}
-		return;
-
-		});
+	// Returns the text in the activity list for a specified index
+	this.getActivityText = function(index) {
+		return page.activitiesList.get(index).getText();
 	}
 	
 	
 	// Prints the entire activity list
 	this.printActivitiesNames = function() {
-		var i=0;
 		(page.activitiesList).each(function(names) {
-			if (i < 5) {
-				names.getText().then(console.log);
-			}
-			i++;
+			names.getText().then(console.log);
 		});
 	};
 	
