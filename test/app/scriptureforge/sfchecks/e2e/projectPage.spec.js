339ca91320a12edf7fcdf98e6dda0e37487c9279
megahirt <chris@hirtfamily.net>
2014-05-20 15:24:12 +0700

    projects page wip

--- test/app/scriptureforge/sfchecks/e2e/projectPage.spec.js
+++ test/app/scriptureforge/sfchecks/e2e/projectPage.spec.js
@@ -1,10 +1,23 @@
 'use strict';
 
-describe('exercise the projectPage object', function() {
+describe('the project dashboard AKA text list page', function() {
 	var projectListPage = require('../../../pages/projectsPage.js');
 	var projectPage = require('../../../pages/projectPage.js');
 	var loginPage = require('../../../pages/loginPage.js');
+	
+	projectListPage.setup.makeSampleProject();
 
+	it('can create a new text', function() {});
+	
+	it('lists existing texts', function() {});
+	
+	it('can click through to the questions page', function() {});
+	
+	it('can click through to the settings page', function() {});
+	
+	// TODO: how do we clean up the test data??? - cjh
+	
+	
     it('can click on a text', function() {
     	loginPage.loginAsUser();
     	//loginPage.login('chris', '0JvbuHNYQrvib3n0iNls');
