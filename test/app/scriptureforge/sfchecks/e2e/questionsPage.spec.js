7f6efc4bf5f16567ac0690f43e2fde18a01a0307
Robin Munn <robin_munn@sil.org>
2014-05-28 10:08:14 +0700

    Tweak text page test to use nested describe() fns
    Former-commit-id: 55b837139ded9d226c739520ef4a679478fe6c97

--- test/app/scriptureforge/sfchecks/e2e/questionsPage.spec.js
+++ test/app/scriptureforge/sfchecks/e2e/questionsPage.spec.js
@@ -8,95 +8,105 @@ describe('the questions list page AKA the text page', function() {
 	var util = require('../../../pages/util.js');
 	var constants = require('../../../../testConstants.json');
 
-	it('setup: login as normal user', function() {
-		loginPage.loginAsMember();
-		projectListPage.get();
-		projectListPage.clickOnProject(constants.testProjectName);
-		projectPage.textLink(constants.testText1Title).click();
-	});
-
-	it('should have some questions added by the setup script', function() {
-		expect(textPage.questionNames.first().getText()).toBe(constants.testText1Question1Title);
-		expect(textPage.questionNames.last() .getText()).toBe(constants.testText1Question2Title);
-		util.findRowByText(textPage.questionRows, constants.testText1Question1Title).then(function(row) {
-			var answerCount = row.findElement(by.binding('{{getAnswerCount(question)}}'));
-			var responseCount = row.findElement(by.binding('{{getResponses(question)}}'));
-			expect(answerCount.getText()).toBe('1');
-			expect(responseCount.getText()).toBe('2 responses');
+	describe('normal user', function() {
+		it('setup: login as normal user', function() {
+			loginPage.loginAsMember();
+			projectListPage.get();
+			projectListPage.clickOnProject(constants.testProjectName);
+			projectPage.textLink(constants.testText1Title).click();
 		});
-		util.findRowByText(textPage.questionRows, constants.testText1Question2Title).then(function(row) {
-			var answerCount = row.findElement(by.binding('{{getAnswerCount(question)}}'));
-			var responseCount = row.findElement(by.binding('{{getResponses(question)}}'));
-			expect(answerCount.getText()).toBe('1');
-			expect(responseCount.getText()).toBe('2 responses');
+
+		it('should list the questions in the text', function() {
+			// Setup script creates two questions
+			expect(textPage.questionNames.first().getText()).toBe(constants.testText1Question1Title);
+			expect(textPage.questionNames.last() .getText()).toBe(constants.testText1Question2Title);
+		});
+
+		it('should list answer counts and responses for each question', function() {
+			util.findRowByText(textPage.questionRows, constants.testText1Question1Title).then(function(row) {
+				var answerCount = row.findElement(by.binding('{{getAnswerCount(question)}}'));
+				var responseCount = row.findElement(by.binding('{{getResponses(question)}}'));
+				expect(answerCount.getText()).toBe('1');
+				expect(responseCount.getText()).toBe('2 responses');
+			});
+			util.findRowByText(textPage.questionRows, constants.testText1Question2Title).then(function(row) {
+				var answerCount = row.findElement(By.binding('{{getAnswerCount(question)}}'));
+				var responseCount = row.findElement(By.binding('{{getResponses(question)}}'));
+				expect(answerCount.getText()).toBe('1');
+				expect(responseCount.getText()).toBe('2 responses');
+			});
+		});
+
+		it('a normal user cannot add new questions', function() {
+			expect(textPage.addNewBtn.isDisplayed()).toBeFalsy();
+		});
+
+		it('a normal user cannot delete questions', function() {
+			expect(textPage.deleteBtn.isDisplayed()).toBeFalsy();
+		});
+
+		it('a normal user cannot create templates', function() {
+			expect(textPage.makeTemplateBtn.isDisplayed()).toBeFalsy();
+		});
+
+		it('a normal user cannot edit text settings', function() {
+			// The text settings button should not even exist on the page for a normal user
+			expect(textPage.textSettingsBtn.isPresent()).toBeFalsy();
+			//expect(textPage.textSettingsBtn.isDisplayed()).toBeFalsy();
 		});
 	});
 
-	it('a normal user cannot add new questions', function() {
-		expect(textPage.addNewBtn.isDisplayed()).toBeFalsy();
+	describe('project manager', function() {
+		it('setup: login as manager', function() {
+			loginPage.loginAsManager();
+			projectListPage.get();
+			projectListPage.clickOnProject(constants.testProjectName);
+			projectPage.textLink(constants.testText1Title).click();
+		});
+
+		it('a project manager can add new questions', function() {
+			expect(textPage.addNewBtn.isDisplayed()).toBeTruthy();
+		});
+
+		it('a project manager can delete questions', function() {
+			expect(textPage.deleteBtn.isDisplayed()).toBeTruthy();
+		});
+
+		it('a project manager can create templates', function() {
+			expect(textPage.makeTemplateBtn.isDisplayed()).toBeTruthy();
+		});
+
+		it('a project manager can edit text settings', function() {
+			// The text settings button should both exist and be displayed for a manager
+			expect(textPage.textSettingsBtn.isPresent()).toBeTruthy(); // Why falsy? Shouldn't it be truthy?
+			expect(textPage.textSettingsBtn.isDisplayed()).toBeTruthy();
+		});
 	});
 
-	it('a normal user cannot delete questions', function() {
-		expect(textPage.deleteBtn.isDisplayed()).toBeFalsy();
-	});
+	describe('site admin', function() {
+		it('setup: login as admin', function() {
+			loginPage.loginAsAdmin();
+			projectListPage.get();
+			projectListPage.clickOnProject(constants.testProjectName);
+			projectPage.textLink(constants.testText1Title).click();
+		});
 
-	it('a normal user cannot create templates', function() {
-		expect(textPage.makeTemplateBtn.isDisplayed()).toBeFalsy();
-	});
+		it('a site admin can add new questions', function() {
+			expect(textPage.addNewBtn.isDisplayed()).toBeTruthy();
+		});
 
-	it('a normal user cannot edit text settings', function() {
-		// The text settings button should not even exist on the page for a normal user
-		expect(textPage.textSettingsBtn.isPresent()).toBeFalsy();
-		//expect(textPage.textSettingsBtn.isDisplayed()).toBeFalsy();
-	});
+		it('a site admin can delete questions', function() {
+			expect(textPage.deleteBtn.isDisplayed()).toBeTruthy();
+		});
 
-	it('setup: login as manager', function() {
-		loginPage.loginAsManager();
-		projectListPage.get();
-		projectListPage.clickOnProject(constants.testProjectName);
-		projectPage.textLink(constants.testText1Title).click();
-	});
+		it('a site admin can create templates', function() {
+			expect(textPage.makeTemplateBtn.isDisplayed()).toBeTruthy();
+		});
 
-	it('a project manager can add new questions', function() {
-		expect(textPage.addNewBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a project manager can delete questions', function() {
-		expect(textPage.deleteBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a project manager can create templates', function() {
-		expect(textPage.makeTemplateBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a project manager can edit text settings', function() {
-		// The text settings button should both exist and be displayed for a manager
-		expect(textPage.textSettingsBtn.isPresent()).toBeTruthy(); // Why falsy? Shouldn't it be truthy?
-		expect(textPage.textSettingsBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('setup: login as admin', function() {
-		loginPage.loginAsAdmin();
-		projectListPage.get();
-		projectListPage.clickOnProject(constants.testProjectName);
-		projectPage.textLink(constants.testText1Title).click();
-	});
-
-	it('a site admin can add new questions', function() {
-		expect(textPage.addNewBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a site admin can delete questions', function() {
-		expect(textPage.deleteBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a site admin can create templates', function() {
-		expect(textPage.makeTemplateBtn.isDisplayed()).toBeTruthy();
-	});
-
-	it('a site admin can edit text settings', function() {
-		// The text settings button should both exist and be displayed for a site admin
-		expect(textPage.textSettingsBtn.isPresent()).toBeTruthy();
-		expect(textPage.textSettingsBtn.isDisplayed()).toBeTruthy();
+		it('a site admin can edit text settings', function() {
+			// The text settings button should both exist and be displayed for a site admin
+			expect(textPage.textSettingsBtn.isPresent()).toBeTruthy();
+			expect(textPage.textSettingsBtn.isDisplayed()).toBeTruthy();
+		});
 	});
 });
