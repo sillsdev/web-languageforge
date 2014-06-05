5d571f6a1444b11753559e0e64749f2a365c1355
irahopkinson <ira.hopkinson@gmail.com>
2014-06-05 14:47:29 +0700

    wip: signup E2E tests failing
    Former-commit-id: 3e2323983d52589295858cad92e2f228b85e8a6c

--- test/app/bellows/public/signup/e2e/ng-app.spec.js
+++ test/app/bellows/public/signup/e2e/ng-app.spec.js
@@ -13,7 +13,9 @@ var SfSignupPage = function() {
 	this.usernameInput = element(by.model('record.username'));
 	this.nameInput = element(by.model('record.name'));
 	this.emailInput = element(by.model('record.email'));
-	this.passwordInput = element(by.model('record.password'));
+//	this.passwordInput = element(by.model('record.password'));
+	this.passwordInput = element(by.id('visiblePassword'));
+	this.showPassword = element(by.model('showPassword'));
 	this.captchaInput = element(by.model('record.captcha'));
 	this.signupButton = element(by.id('submit'));
 	//this.warnNotices = $('.notices').element.all(by.css('.alert div span'));
@@ -21,34 +23,32 @@ var SfSignupPage = function() {
 }; 
 
 describe('E2E testing: Signup app', function() {
-	var sfSignupPage = new SfSignupPage();
-	
-	beforeEach(function() {
-		sfSignupPage.get();
-	});
+	var page = new SfSignupPage();
 	
 	it('contains a user form', function() {
-		expect(sfSignupPage.userForm).toBeDefined();
+		page.get();
+		expect(page.userForm).toBeDefined();
 	});
 	
 	it('finds the admin user already exists', function() {
-		sfSignupPage.usernameInput.sendKeys(constants.adminUsername);
+		page.usernameInput.sendKeys(constants.adminUsername);
 		
 		// trigger the username lookup
-		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
+		page.usernameInput.sendKeys(protractor.Key.ENTER);
 		
-		expect(sfSignupPage.userNameExists.isDisplayed()).toBeTruthy();
-		expect(sfSignupPage.userNameOk.isDisplayed()).toBeFalsy();
+		expect(page.userNameExists.isDisplayed()).toBeTruthy();
+		expect(page.userNameOk.isDisplayed()).toBeFalsy();
 	});
 	
 	it("can verify that 'newuser' is an available username", function() {
-		sfSignupPage.usernameInput.sendKeys('newuser');
+		page.get();
+		page.usernameInput.sendKeys('newuser');
 		
 		// trigger the username lookup
-		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
+		page.usernameInput.sendKeys(protractor.Key.ENTER);
 
-		expect(sfSignupPage.userNameExists.isDisplayed()).toBeFalsy();
-		expect(sfSignupPage.userNameOk.isDisplayed()).toBeTruthy();
+		expect(page.userNameExists.isDisplayed()).toBeFalsy();
+		expect(page.userNameOk.isDisplayed()).toBeTruthy();
 	});
 	
 	it("has a captcha image", function() {
@@ -57,20 +57,22 @@ describe('E2E testing: Signup app', function() {
 	
 	
 	it('can submit a user registration request and captcha is invalid', function() {
-		sfSignupPage.usernameInput.sendKeys('newuser');
+		page.get();
+		page.usernameInput.sendKeys('newuser');
 		
 		// trigger the username lookup
-		sfSignupPage.usernameInput.sendKeys(protractor.Key.ENTER);
+		page.usernameInput.sendKeys(protractor.Key.ENTER);
 
-		sfSignupPage.nameInput.sendKeys('New User');
-		sfSignupPage.emailInput.sendKeys('email@example.com');
-		sfSignupPage.passwordInput.sendKeys('12345');
-		sfSignupPage.captchaInput.sendKeys('whatever');
+		page.nameInput.sendKeys('New User');
+		page.emailInput.sendKeys('email@example.com');
+		page.showPassword.click();
+		page.passwordInput.sendKeys('12345');
+		page.captchaInput.sendKeys('whatever');
 
-		expect(sfSignupPage.noticeList.count()).toBe(0);
-		sfSignupPage.signupButton.click().then(function() {
-			expect(sfSignupPage.noticeList.count()).toBe(1);
-			expect(sfSignupPage.noticeList.get(0).getText()).toContain('image verification failed');
+		expect(page.noticeList.count()).toBe(0);
+		page.signupButton.click().then(function() {
+			expect(page.noticeList.count()).toBe(1);
+			expect(page.noticeList.get(0).getText()).toContain('image verification failed');
 		});
 	});
 	
