77d4450f1b942785427872cbfc29efa70ee68443
irahopkinson <ira.hopkinson@gmail.com>
2014-06-09 12:58:26 +0700

    moved shared commands tests
    Former-commit-id: e9d90135fbe2bc13f032c32636a59da26cbefbba

---
+++ test/php/shared/commands/AllTests.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__FILE__) . '/../../TestConfig.php');
+require_once(SimpleTestPath . 'autorun.php');
+
+class AllCommandsTests extends TestSuite {
+	
+    function __construct() {
+        parent::__construct();
+ 		$this->addFile(TestPath . 'shared/commands/ProjectCommands_Test.php');
+ 		$this->addFile(TestPath . 'shared/commands/UserCommands_Test.php');
+    }
+
+}
+
+?>
