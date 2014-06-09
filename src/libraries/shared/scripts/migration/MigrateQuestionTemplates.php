ff16be3112397acc5d45d2dbfb2c7110fa6f4455
megahirt <chris@hirtfamily.net>
2014-06-09 13:55:03 +0700

    migrateQuestionTemplates migration script

--- src/libraries/shared/scripts/migration/MigrateQuestionTemplates.php
+++ src/libraries/shared/scripts/migration/MigrateQuestionTemplates.php
@@ -1,6 +1,10 @@
 <?php
 namespace libraries\shared\scripts\migration;
 
+use models\GlobalQuestionTemplateModel;
+
+use models\scriptureforge\sfchecks\QuestionTemplateModel;
+
 use models\GlobalQuestionTemplateListModel;
 
 use models\shared\rights\ProjectRoleModel;
@@ -11,7 +15,9 @@ use models\ProjectModel;
 use models\UserListModel;
 use models\UserModel;
 
-class FixProjectRoles {
+require_once 'models/GlobalQuestionTemplateModel.php';
+
+class MigrateQuestionTemplates {
 	
 	public function run($mode = 'test') {
 		$testMode = ($mode != 'run');
@@ -23,50 +29,46 @@ class FixProjectRoles {
 		if (count($templateList->entries) > 0) {
 			
 		} else {
-			$message .= "There are no global templates to migrate";
+			$message .= "There are no global templates to migrate\n\n";
 			
 		}
 
-		$contribRoleUpdated = 0;
-		$managerRoleUpdated = 0;
+		$templatesMoved = 0;
 
 		$projectlist = new ProjectListModel();
 		$projectlist->read();
 		
+
+		$project = null;
 		foreach ($projectlist->entries as $projectParams) { // foreach existing project
 			$projectId = $projectParams['id'];
-			$project = new SfchecksProjectModel($projectId);
-			if ($project->databaseName() != 'sf_jamaican_psalms') {
-				continue;
+			$p = new SfchecksProjectModel($projectId);
+			if ($p->databaseName() == 'sf_jamaican_psalms') {
+				$project = $p;
 			}
-			$message .= "Migrating global templates to the '" . $project->projectname . "' project";
-			$users = $project->users;
-			foreach ($users as $userId => $rm) {
-				$role = $rm->role;
-				if ($role == 'user') {
-					$roleModel = new ProjectRoleModel();
-					$roleModel->role = 'contributor';
-					$project->users[$userId] = $roleModel;
-					$contribRoleUpdated++;
-					$message .= "Updated user role for user $userId\n";
-				} else if ($role == 'project_admin') {
-					$roleModel = new ProjectRoleModel();
-					$roleModel->role = 'project_manager';
-					$project->users[$userId] = $roleModel;
-					$message .= "Updated manager role for user $userId\n";
-					$managerRoleUpdated++;
+		}
+		if (!is_null($project)) {
+			
+			$message .= "Migrating global templates to the '" . $project->projectname . "' project\n\n";
+			
+			foreach ($templateList->entries as $globalTemplate) {
+				$template = new QuestionTemplateModel($project);
+				$template->title = $globalTemplate['title'];
+				$template->description = $globalTemplate['description'];
+				$message .= "Moving '" . $template->title . "'\n";
+				if (!$testMode) {
+					$template->write();
+					$gt = new GlobalQuestionTemplateModel($globalTemplate['id']);
+					$gt->remove();
 				}
+				$templatesMoved++;
 			}
-			if (!$testMode) {
-				$message .= "saving project $projectId\n";
-				$project->write();
-			}
-		}
-		if ($contribRoleUpdated > 0 || $managerRoleUpdated) {
-			$message .= "\n\nChanged $contribRoleUpdated user roles to be 'contributor' and $managerRoleUpdated project_admin roles to be 'project_manager'\n\n";
+			$message .= "\n\nMoved $templatesMoved global templates to the specified project\n\n";
+			
 		} else {
-			$message .= "\n\nNo old roles were found/changed \n\n";
+			$message .= "Error: cound not find the Jamaican Psalms project on this server\n\n";
 		}
+
 		return $message;
 	}
 }
