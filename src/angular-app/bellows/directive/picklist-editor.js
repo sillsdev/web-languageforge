4663c778b1d8ac68158cedb218fa7bd10df0f2e5
Robin Munn <robin_munn@sil.org>
2014-06-11 12:14:35 +0700

    Picklist WIP: Add Save and Reset buttons
    Rather than update the original list every time something changes,
    we'll update it only when the user presses the "Save" button. We'll also
    provide a "Reset" button to revert to the original values (or at least
    the last time the user pressed "Save").

--- src/angular-app/bellows/directive/picklist-editor.js
+++ src/angular-app/bellows/directive/picklist-editor.js
@@ -10,5 +10,48 @@ angular.module('palaso.ui.picklistEditor', ['ngRepeatReorder'])
 			values: '=',
 			picklistName: '@name',
 		},
+		controller: ['$scope', function($scope) {
+			$scope.getValuesFromClient = function() {
+				$scope.items = [];
+				for (var i = 0, l = $scope.values.length; i < l; i++) {
+					$scope.items.push({value: $scope.values[i]});
+				}
+				$scope.dataChanged = false;
+				$scope.startWatchingItems();
+			};
+			$scope.returnValuesToClient = function() {
+				// Build a new array, then copy it all at once with Array.prototype.slice()
+				var newValues = new Array();
+				for (var i = 0, l = $scope.items.length; i < l; i++) {
+					newValues.push($scope.items[i].value);
+				}
+				$scope.values = newValues.slice();
+				$scope.dataChanged = false;
+				$scope.startWatchingItems();
+			};
+
+			// Activate Save and Reset buttons only when values have changed
+			$scope.itemWatcher = function(newval, oldval) {
+				if (newval && newval != oldval) {
+					$scope.dataChanged = true;
+					// Since a values watch is expensive, stop watching after first time data changes
+					$scope.stopWatchingItems();
+				}
+			};
+			$scope.stopWatchingItems = function() {
+				if ($scope.deregisterItemWatcher) {
+					$scope.deregisterItemWatcher();
+					$scope.deregisterItemWatcher = undefined;
+				}
+			};
+			$scope.startWatchingItems = function() {
+				if ($scope.deregisterItemWatcher) {
+					$scope.deregisterItemWatcher();
+				}
+				$scope.deregisterItemWatcher = $scope.$watch('items', $scope.itemWatcher, true);
+			};
+
+			$scope.getValuesFromClient();
+		}],
 	};
 });
