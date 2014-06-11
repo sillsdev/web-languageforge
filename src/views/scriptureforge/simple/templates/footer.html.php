0108353f3d1aef388521ab7ef947e718744caaed
irahopkinson <ira.hopkinson@gmail.com>
2014-06-11 07:33:41 +0700

    wip: add simple template, without logos or sensitive wording

---
+++ src/views/scriptureforge/simple/templates/footer.html.php
@@ -0,0 +1,33 @@
+		<div id="footercontainer">
+			
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+										
+						<div class="span2">
+						</div>
+						
+						<div class="span2">
+							<h4>Navigate</h4>
+							<small>
+								<a href="/">Home</a><br /><a href="/explore">Explore</a><br /><a href="/learn_scripture_forge">Learn</a><br /><a href="/learn_contribute">Contribute</a><br /><a href="/discuss">Discuss</a>
+							</small>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		
+			<div id="footerBaseline">
+				<div class="container">
+					<div class="row">
+						<div class="darkTextLogo span2"></div>
+						<div class="span1" style="color: #404040"><small>v <?php echo VERSION;?></small></div>
+						<div class="span8">
+							<small>Use of this site is governed by our <a href="/terms_and_conditions">terms and conditions</a> of use.</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+		</div>
