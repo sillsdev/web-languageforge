68f3fb2d737547d88698fc29ed27c1a2058efcb3
megahirt <chris@hirtfamily.net>
2014-06-12 10:45:28 +0700

    remove theme specific links in project list and project menu - fix #104

--- test/rune2eSF.sh
+++ test/rune2eSF.sh
@@ -3,6 +3,6 @@ if [ $# -eq 0 ]
   then
     node ./node_modules/protractor/bin/protractor protractorConf.js
   else
-    node ./node_modules/protractor/bin/protractor protractorConf.js --specs "`find . -wholename "*$1*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
+    node ./node_modules/protractor/bin/protractor protractorConf.js --specs "`find . -wholename "*e2e*$1*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
 fi
 php app/teardownTestEnvironment.php
