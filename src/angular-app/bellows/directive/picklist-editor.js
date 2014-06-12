9ca861faff51652c3e8f578cfa7b52662396d2b6
Robin Munn <robin_munn@sil.org>
2014-06-12 15:21:29 +0700

    Picklist: Make client responsible for save/reset
    After looking at how the picklist interacts with an actual page like the
    project settings page, I determined that the "Save List" and "Reset List"
    buttons would have to go. The interaction between them and the "Save"
    button at the bottom of the page was just too confusing and had too much
    potential for users clicking "Save List" and thinking their edits had
    been persisted, when in fact the edits would not actually be persisted
    until they clicked "Save" at the bottom of the form. So now the picklist
    will update the client-supplied data "live" as users make edits, and the
    client code will be responsible for doing a Save, and/or supplying a
    reset button if desired.
    
    This simplifies the picklist code IMMENSELY, though the client code will
    necessarily become more complex. But the picklist code is now concerned
    only with operations that are properly within its scope.

--- src/angular-app/bellows/directive/picklist-editor.js
+++ src/angular-app/bellows/directive/picklist-editor.js
@@ -18,74 +18,10 @@ angular.module('palaso.ui.picklistEditor', ['ngRepeatReorder'])
 		restrict: 'AE',
 		templateUrl: '/angular-app/bellows/directive/picklist-editor.html',
 		scope: {
-			clientItems: '=items',
+			items: '=',
 			defaultKey: '=?',
 		},
 		controller: ['$scope', function($scope) {
-			$scope.noteDataChange = function() {
-				$scope.dataChanged = true;
-			}
-			$scope.isKeyValueObject = function(item) {
-				return (item.hasOwnProperty("value"))
-			};
-			$scope.getValuesFromClient = function() {
-				$scope.stopWatchingItems();
-				if (angular.isDefined($scope.clientItems)) { // Might be called during setup
-					$scope.items = [];
-					$scope.clientWantsObjects = true;
-					for (var i = 0, l = $scope.clientItems.length; i < l; i++) {
-						var clientItem = $scope.clientItems[i];
-						var item = angular.copy(clientItem);
-						if (!item.hasOwnProperty("key")) {
-							// If client didn't supply keys, construct some
-							item.key = item.value.replace(/ /gi, '_');
-						}
-						if ($scope.defaultKey && $scope.defaultKey === item.key) {
-							$scope.defaultItem = item;
-						}
-						$scope.items.push(item);
-					}
-					$scope.dataChanged = false;
-				}
-				$scope.startWatchingItems();
-			};
-			$scope.returnValuesToClient = function() {
-				$scope.stopWatchingItems();
-				$scope.clientItems = $scope.items.slice();
-				if ($scope.defaultItem) {
-					$scope.defaultKey = $scope.defaultItem.key;
-				} else {
-					$scope.defaultKey = null;
-				}
-				$scope.dataChanged = false;
-				$scope.startWatchingItems();
-			};
-
-			// Activate Save and Reset buttons only when values have changed
-			$scope.itemWatcher = function(newval, oldval) {
-				if (angular.isDefined(newval) && newval != oldval) {
-					$scope.noteDataChange();
-				}
-			};
-			$scope.clientItemWatcher = function(newval, oldval) {
-				if (angular.isDefined(newval) && newval != oldval) {
-					$scope.getValuesFromClient(); // Might end up triggering after returnValuesToClient; figure out how to prevent that.
-				}
-			};
-			$scope.startWatchingItems = function() {
-				$scope.stopWatchingItems();
-				$scope.disableItemWatcher = $scope.$watchCollection('items', $scope.itemWatcher);
-				$scope.disableClientItemWatcher = $scope.$watch('clientItems', $scope.clientItemWatcher, true);
-			};
-			$scope.stopWatchingItems = function() {
-				if ($scope.disableItemWatcher) {
-					$scope.disableItemWatcher();
-					$scope.disableClientItemWatcher();
-					$scope.disableItemWatcher = undefined;
-					$scope.disableClientItemWatcher = undefined;
-				}
-			};
-
 			$scope.pickAddItem = function() {
 				if ($scope.newValue) {
 					$scope.items.push({value: $scope.newValue});
@@ -95,8 +31,6 @@ angular.module('palaso.ui.picklistEditor', ['ngRepeatReorder'])
 			$scope.pickRemoveItem = function(index) {
 				$scope.items.splice(index, 1);
 			};
-
-			$scope.getValuesFromClient();
 		}],
 	};
 });
