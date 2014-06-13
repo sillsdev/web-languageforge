918a5865dfe1b0bf9768bba94f64e14546a96ad5
Robin Munn <robin_munn@sil.org>
2014-06-13 15:55:53 +0700

    Picklist WIP: ngRepeatReorder demo nigh-verbatim
    The only difference is that I deleted the ng-app="..." attr and turned
    the demo module into a controller. Apart from that, it's the same demo
    as the one on Codepen.
    
    This is for testing purposes only and should not end up in production.

--- src/angular-app/scriptureforge/sfchecks/partials/projectSettings.js
+++ src/angular-app/scriptureforge/sfchecks/partials/projectSettings.js
@@ -310,6 +310,20 @@ angular.module('sfchecks.projectSettings', ['bellows.services', 'sfchecks.servic
 	});
 
 }])
+.controller("dragndropdemo", ['$scope', function($scope) {
+			$scope.names = [{val:'bob'},{val:'lucy'},{val:'john'},{val:'luke'},{val:'han'}];
+			$scope.tempplayer = '';
+			$scope.updateNames = function (){
+				if($scope.tempplayer === "") return
+				$scope.names.push({val: $scope.tempplayer});
+				$scope.tempplayer = "";
+			};
+			$scope.checkForNameDelete = function($index){
+				if($scope.names[$index].val === ''){
+					$scope.names.splice($index, 1);
+				}
+			};
+		}])
 .controller('ProjectSettingsUsersCtrl', ['$scope', '$location', '$routeParams', 'breadcrumbService', 'userService', 'projectService', 'sessionService', 'silNoticeService', 'messageService',
                                  function($scope, $location, $routeParams, breadcrumbService, userService, projectService, ss, notice, messageService) {
 	$scope.userFilter = '';
