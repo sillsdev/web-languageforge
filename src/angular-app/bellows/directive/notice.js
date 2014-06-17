7597675afb163cb60e1f479b7cfd4a11569e5691
megahirt <chris@hirtfamily.net>
2014-06-17 16:21:18 +0700

    enhance error messages
    includes link to github issue tracker and outputs more details (including html formatting!) in a collapsible details div

--- src/angular-app/bellows/directive/notice.js
+++ src/angular-app/bellows/directive/notice.js
@@ -1,4 +1,4 @@
-angular.module('palaso.ui.notice', ['ui.bootstrap', 'bellows.services', 'ngAnimate'])
+angular.module('palaso.ui.notice', ['ui.bootstrap', 'bellows.services', 'ngAnimate', 'ngSanitize'])
 .factory('silNoticeService', ['$interval', 'utilService', function($interval, util) {
 	var notices = [];
 	var timers = {};
@@ -11,14 +11,14 @@ angular.module('palaso.ui.notice', ['ui.bootstrap', 'bellows.services', 'ngAnima
 		}
 	};
 	return {
-		push: function(type, message) {
+		push: function(type, message, details) {
 			var id = util.uuid();
 			if (type() == this.SUCCESS()) {
 				// success alert messages will auto-close after 10 seconds
 				var localFactory = this;
 				timers[id] = $interval(function() {localFactory.removeById(id); }, 10 * 1000, 1);
 			}
-			notices.push({type: type(), message: message, id: id});
+			notices.push({type: type(), message: message, id: id, details: details, showDetails: false, toggleDetails: function() {this.showDetails = !this.showDetails;}});
 		},
 		removeById: function(id) {
 			this.remove(getIndexById(id));
@@ -41,7 +41,7 @@ angular.module('palaso.ui.notice', ['ui.bootstrap', 'bellows.services', 'ngAnima
 .directive('silNotices', ['silNoticeService', function(noticeService) {
 	return {
 		restrict : 'EA',
-		template : '<div class="notices"><alert class="animate-repeat" ng-repeat="notice in notices()" type="notice.type" close="closeNotice(notice.id)">{{notice.message}}</alert></div>',
+		templateUrl : '/angular-app/bellows/directive/notice.html',
 		replace : true,
 		compile : function(tElement, tAttrs) {
 			return function($scope, $elem, $attr) {
