784ea80a48fb16c71d1dc5a6cf1e7ce7b5350413
irahopkinson <ira.hopkinson@gmail.com>
2014-06-20 14:15:29 +0700

    update Export test for archived questions

--- test/php/libraries/ParatextExport_Test.php
+++ test/php/libraries/ParatextExport_Test.php
@@ -74,7 +74,7 @@ class TestParatextExport extends UnitTestCase {
 		$this->assertPattern('/\(Tags: export, to review\) \(10 Votes\)<\/Contents>/', $download['xml']);
 	}
 	
-	function testExportCommentsForText_OnlyExportFlagged_OnlyFlaggedExported() {
+	function testExportCommentsForText_OnlyExportFlagged_OnlyFlaggedExportedExceptArchived() {
 		$e = new MongoTestEnvironment();
 		
 		$project = $e->createProject(SF_TESTPROJECT);
@@ -134,6 +134,69 @@ class TestParatextExport extends UnitTestCase {
 		$this->assertPattern('/<Contents>third answer - very very very very long \(by user3/', $download['xml']);
 		$this->assertNoPattern('/<Contents>second answer/', $download['xml']);
 		$this->assertPattern('/<Contents>first answer/', $download['xml']);
+	}
+	
+	function testExportCommentsForText_QuestionArchived_NoneExported() {
+		$e = new MongoTestEnvironment();
+		
+		$project = $e->createProject(SF_TESTPROJECT);
+		
+		$text = new TextModel($project);
+		$text->title = "Text 1";
+		$usx = MongoTestEnvironment::usxSample();
+		$text->content = $usx;
+		$textId = $text->write();
+		
+		$user1Id = $e->createUser("user1", "user1", "user1@email.com");
+		$user2Id = $e->createUser("user2", "user2", "user2@email.com");
+		$user3Id = $e->createUser("user3", "user3", "user3@email.com");
+		
+		// Workflow is first to create a question
+		$question = new QuestionModel($project);
+		$question->title = "the question";
+		$question->description = "question description";
+		$question->textRef->id = $textId;
+		$question->isArchived = true;
+		$questionId = $question->write();
+		
+		// Then to add an answer to a question
+		$answer = new AnswerModel();
+		$answer->content = "first answer";
+		$answer->score = 10;
+		$answer->userRef->id = $user1Id;
+		$answer->tags->exchangeArray(array('export', 'to review'));
+		$answer->isToBeExported = true;
+		$answerId = $question->writeAnswer($answer);
+		
+		// Then to add an answer to a question
+		$answer = new AnswerModel();
+		$answer->content = "second answer - very long";
+		$answer->score = 2;
+		$answer->userRef->id = $user2Id;
+		$answer->tags->exchangeArray(array('to review'));
+		$answer->isToBeExported = false;
+		$answerId = $question->writeAnswer($answer);
+		
+		// Then to add an answer to a question
+		$answer = new AnswerModel();
+		$answer->content = "third answer - very very very very long";
+		$answer->userRef->id = $user3Id;
+		$answer->tags->exchangeArray(array('export'));
+		$answer->isToBeExported = true;
+		$answerId = $question->writeAnswer($answer);
+		
+		$params = array(
+			'textId' => $textId,
+			'exportComments' => true,
+			'exportFlagged' => true,
+			'tags' => array()
+		);
+		$download = ParatextExport::exportCommentsForText($project->id->asString(), $textId, $params);
+		//echo "<pre>" . print_r($download) . "</pre>";
+		
+		$this->assertNoPattern('/<Contents>third answer - very very very very long \(by user3/', $download['xml']);
+		$this->assertNoPattern('/<Contents>second answer/', $download['xml']);
+		$this->assertNoPattern('/<Contents>first answer/', $download['xml']);
 	}
 	
 }
