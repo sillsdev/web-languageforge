74f477f0f506c9e00374a6e44c47ad4a7f99e2fb
megahirt <chris@hirtfamily.net>
2014-06-23 15:22:44 +0700

    fixed bad find/replace in templates

--- src/views/scriptureforge/default/pages/frontpage.html.php
+++ src/views/scriptureforge/default/pages/frontpage.html.php
@@ -24,7 +24,7 @@
 					"target_url" => "/learn_contribute"
 					)
 			);
-			$this->load->view("RightsHelper::hasSiteRight/templates/slideshow.html.php", $data);
+			$this->load->view("$themePath/templates/slideshow.html.php", $data);
 			
 			$data['columns'] = array(
 				array(
@@ -43,7 +43,7 @@
 					"target_url" => "/learn_contribute"
 					)
 			);
-			$this->load->view("RightsHelper::hasSiteRight/templates/3column.html.php", $data);
+			$this->load->view("$themePath/templates/3column.html.php", $data);
 			?>
 		
 			<div class="container">
