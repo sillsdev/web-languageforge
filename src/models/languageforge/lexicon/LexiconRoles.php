b05cd9061136346565671c212a7618f666ced113
irahopkinson <ira.hopkinson@gmail.com>
2014-07-03 16:48:43 +0700

    fix LexiconRoles permissions and use bellows project service updateUserRole

--- src/models/languageforge/lexicon/LexiconRoles.php
+++ src/models/languageforge/lexicon/LexiconRoles.php
@@ -1,17 +1,16 @@
 <?php
+
 namespace models\languageforge\lexicon;
 
-use models\shared\rights\ProjectRoles;
-
-use models\shared\rights\Operation;
 use models\shared\rights\Domain;
+use models\shared\rights\Operation;
+use models\shared\rights\ProjectRoles;
 use models\shared\rights\RolesBase;
 
 class LexiconRoles extends ProjectRoles {
 	
 	const OBSERVER = 'observer';
 	const OBSERVER_WITH_COMMENT = 'observer_with_comment';
-	const CONTRIBUTOR = 'contributor';
 	
 	public static function init() {
 		
@@ -36,11 +35,16 @@ class LexiconRoles extends ProjectRoles {
 		$rights[] = Domain::ENTRIES + Operation::DELETE;
 		self::$_rights[self::CONTRIBUTOR] = $rights;
 		
-		// Manager
-		$rights = array();
-		self::grantAllOnDomain($rights, Domain::PROJECTS);
+		// Manager (everything a Contributor has... plus the following)
+		$rights = self::$_rights[self::CONTRIBUTOR];
+		$rights[] = Domain::COMMENTS + Operation::EDIT;
+		$rights[] = Domain::COMMENTS + Operation::DELETE;
+		$rights[] = Domain::PROJECTS + Operation::EDIT;
+		$rights[] = Domain::USERS + Operation::CREATE;
+		$rights[] = Domain::USERS + Operation::EDIT;
+		$rights[] = Domain::USERS + Operation::DELETE;
+		$rights[] = Domain::USERS + Operation::VIEW;
 		self::grantAllOnDomain($rights, Domain::ENTRIES);
-		self::grantAllOnDomain($rights, Domain::COMMENTS);
 		self::$_rights[self::MANAGER] = $rights;
 	}
 
