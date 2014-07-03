9d8dc2c495136853cb818d18daba77049474de1a
irahopkinson <ira.hopkinson@gmail.com>
2014-07-03 16:45:48 +0700

    move updateUserRole to bellows project services

--- src/angular-app/bellows/js/services.js
+++ src/angular-app/bellows/js/services.js
@@ -84,13 +84,15 @@ angular.module('bellows.services', ['jsonRpc'])
 	this.readUser = function(userId, callback) {
 		jsonRpc.call('project_readUser', [userId], callback);
 	};
+	this.updateUserRole = function(userId, role, callback) {
+		jsonRpc.call('project_updateUserRole', [userId, role], callback);
+	};
 	this.removeUsers = function(users, callback) {
 		jsonRpc.call('project_removeUsers', [users], callback);
 	};
 	this.projectCodeExists = function(projectCode, callback) {
 		jsonRpc.call('projectcode_exists', [projectCode], callback);
 	};
-	
 	this.joinProject = function(projectId, role, callback) {
 		jsonRpc.call('project_joinProject', [projectId, role], callback);
 	};
