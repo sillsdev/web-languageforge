7fefaca277386a0c10ef873758cbdd88167bac7d
megahirt <chris@hirtfamily.net>
2014-07-08 14:29:31 +0700

    fix google group link for LF

--- src/views/languageforge/default/pages/contribute.html.php
+++
@@ -1,6 +0,0 @@
-<div class="sfcontainer banner">
-	<img src="/images/languageforge/default/youngleaders.jpg" width="400px" height="268px" alt="on the beach" />
-</div>
-<div class="sfcontainer">
-			<h2>Contribute Placeholder for App</h2>
-</div>
