128902d8f3e2d0a98919410b5c6144c4b7af693e
irahopkinson <ira.hopkinson@gmail.com>
2014-07-09 14:05:33 +0700

    connect roleView fields settings to enable fields

--- src/angular-app/languageforge/lexicon/directive/dc-example.js
+++ src/angular-app/languageforge/lexicon/directive/dc-example.js
@@ -1,37 +1,39 @@
 angular.module('palaso.ui.dc.example', ['palaso.ui.dc.multitext'])
-  // Palaso UI Dictionary Control: Example Sentence
-  .directive('dcExample', [function() {
-		return {
-			restrict : 'E',
-			templateUrl : '/angular-app/languageforge/lexicon/directive/dc-example.html',
-			scope : {
-				config : "=",
-				model : "=",
-				index : "=",
-				remove : "=",
-				comment : "&",
-				control : "="
-			},
-			controller: ['$scope', function($scope) {
-				$scope.makeValidModel = function() {
-					if (!$scope.model) {
-						$scope.model = {};
-					}
-				};
-				
-				$scope.submitComment = function(comment, field) {
-					if (angular.isDefined(field)) {
-						comment.field = field;
-					}
-					comment.exampleId = $scope.model.id;
-					$scope.comment({comment: comment});
-				};
-			}],
-			link : function(scope, element, attrs, controller) {
-				scope.$watch('model', function() {
-					scope.makeValidModel();
-				});
-			}
-		};
-  }])
-  ;
+// Palaso UI Dictionary Control: Example Sentence
+.directive('dcExample', [function() {
+	return {
+		restrict : 'E',
+		templateUrl : '/angular-app/languageforge/lexicon/directive/dc-example.html',
+		scope : {
+			config : "=",
+			model : "=",
+			index : "=",
+			remove : "=",
+			comment : "&",
+			control : "="
+		},
+		controller: ['$scope', 'lexEntryService', function($scope, lexEntryService) {
+			$scope.isFieldEnabled = lexEntryService.isFieldEnabled;
+			
+			$scope.makeValidModel = function() {
+				if (!$scope.model) {
+					$scope.model = {};
+				}
+			};
+			
+			$scope.submitComment = function(comment, field) {
+				if (angular.isDefined(field)) {
+					comment.field = field;
+				}
+				comment.exampleId = $scope.model.id;
+				$scope.comment({comment: comment});
+			};
+		}],
+		link : function(scope, element, attrs, controller) {
+			scope.$watch('model', function() {
+				scope.makeValidModel();
+			});
+		}
+	};
+}])
+;
