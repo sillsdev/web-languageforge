f39174d7f4b929c8aa69ba6a301a49cf60c4599f
megahirt <chris@hirtfamily.net>
2014-07-11 16:52:28 +0700

    removed old lf cruft
    Former-commit-id: b99f51dbb0229eb5b2e0453512328eec81b2e3a4

--- src/angular-app/languageforge/entryblock/js/directives.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-/* Directives */
-
-angular.module('entryblock.directives', ['entryblock.services']);
-
--- src/angular-app/languageforge/entryblock/ng-app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-// TODO This is currently not used.  The module is really defined in controller.js for now CP 2013-07
-
-// Declare app level module which depends on filters, and services
-angular.module('entryblock', [ 'entryblock.services', 'entryblock.directives', 'entryblock.controllers', 'ui.bootstrap']);
