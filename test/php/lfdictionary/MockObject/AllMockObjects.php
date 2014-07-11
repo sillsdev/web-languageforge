98b803fda72102ae53ff5e9b522466a03c663d41
megahirt <chris@hirtfamily.net>
2014-07-11 13:34:14 +0700

    remove old lfdictionary files and cruft

--- test/php/lfdictionary/MockObject/AllMockObjects.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('ProjectAccessMockEnvironment.php');
-require_once('TestEnvironment.php');
-require_once('UserModelMockObject.php');
-require_once('ProjectModelMockObject.php');
-?>
