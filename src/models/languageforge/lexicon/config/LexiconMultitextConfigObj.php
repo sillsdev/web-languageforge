e4d9c17aa8c2a9049271115ebb620c7ce0e55538
irahopkinson <ira.hopkinson@gmail.com>
2014-07-22 15:48:57 +0700

    add display mode to MultiText config

--- src/models/languageforge/lexicon/config/LexiconMultitextConfigObj.php
+++ src/models/languageforge/lexicon/config/LexiconMultitextConfigObj.php
@@ -7,10 +7,15 @@ use models\mapper\MapOf;
 
 class LexiconMultitextConfigObj extends LexiconConfigObj {
 	
-	public function __construct() {
+	// display mode
+	const SINGLE_LINE = 'singleline';
+	const MULTI_LINE = 'multiline';
+	
+	public function __construct($displayMode = self::SINGLE_LINE) {
 		$this->type = LexiconConfigObj::MULTITEXT;
 		
 		// default values
+		$this->displayMode = $displayMode;
 		$this->label = '';
 		$this->width = 20;
 		$this->inputSystems = new ArrayOf();
@@ -30,6 +35,11 @@ class LexiconMultitextConfigObj extends LexiconConfigObj {
 	 * @var ArrayOf
 	 */
 	public $inputSystems;
+	
+	/**
+	 * @var string SINGLE_LINE or MULTI_LINE
+	 */
+	public $displayMode;
 	
 }
 
