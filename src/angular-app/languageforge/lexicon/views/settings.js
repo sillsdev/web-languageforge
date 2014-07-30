9a8ee9eae655f6b76e6059f12c89f96d55fd5117
irahopkinson <ira.hopkinson@gmail.com>
2014-07-30 10:56:15 +0700

    fix User Management over writing View Settings breadcrumbs; tidy up Project Settings breadcrumbs also

--- src/angular-app/languageforge/lexicon/views/settings.js
+++ src/angular-app/languageforge/lexicon/views/settings.js
@@ -1,49 +1,51 @@
 'use strict';
 
 angular.module('lexicon.settings', ['bellows.services', 'palaso.ui.listview', 'palaso.ui.typeahead', 'ui.bootstrap', 'palaso.ui.notice', 'palaso.ui.textdrop'])
-.controller('SettingsCtrl', ['$scope', '$filter', 'userService', 'sessionService', 'silNoticeService', 'lexProjectService',  
-                             function($scope, $filter, userService, ss, notice, lexProjectService) {
-	$scope.rights.canEditCommunicationSettings = ss.hasSiteRight(ss.domain.PROJECTS, ss.operation.EDIT);
-	
-	$scope.readProject = function() {
-		lexProjectService.readProject(function(result) {
-			if (result.ok) {
-				$.extend($scope.project, result.data.project);
-			}
-		});
-	};
-	
-	$scope.readProject();
-	
-	$scope.updateProject = function() {
-		lexProjectService.updateProject($scope.project, function(result) {
-			if (result.ok) {
-				notice.push(notice.SUCCESS, $filter('translate')("{projectName} settings updated successfully", {projectName: $scope.project.projectName}));
-			}
-		});
-	};
-	
-	$scope.settings = {
-		'sms': {},
-		'email': {}
-	};
-		
-	$scope.readCommunicationSettings = function() {
-		lexProjectService.readSettings(function(result) {
-			if (result.ok) {
-				$scope.settings.sms = result.data.sms;
-				$scope.settings.email = result.data.email;
-			}
-		});
-	};
-
-	$scope.updateCommunicationSettings = function() {
-		lexProjectService.updateSettings($scope.settings.sms, $scope.settings.email, function(result) {
-			if (result.ok) {
-				notice.push(notice.SUCCESS, $filter('translate')("{projectName} SMS settings updated successfully", {projectName: $scope.project.projectName}));
-			}
-		});
-	};
-	
-}])
-;
+  .controller('SettingsCtrl', ['$scope', '$filter', 'userService', 'sessionService', 'silNoticeService', 'lexProjectService',  
+  function($scope, $filter, userService, ss, notice, lexProjectService) {
+    lexProjectService.setBreadcrumbs('settings', $filter('translate')('Project Settings'));
+    
+    $scope.rights.canEditCommunicationSettings = ss.hasSiteRight(ss.domain.PROJECTS, ss.operation.EDIT);
+    
+    $scope.readProject = function() {
+      lexProjectService.readProject(function(result) {
+        if (result.ok) {
+          $.extend($scope.project, result.data.project);
+        }
+      });
+    };
+    
+    $scope.readProject();
+    
+    $scope.updateProject = function() {
+      lexProjectService.updateProject($scope.project, function(result) {
+        if (result.ok) {
+          notice.push(notice.SUCCESS, $filter('translate')("{projectName} settings updated successfully", {projectName: $scope.project.projectName}));
+        }
+      });
+    };
+    
+    $scope.settings = {
+      'sms': {},
+      'email': {}
+    };
+      
+    $scope.readCommunicationSettings = function() {
+      lexProjectService.readSettings(function(result) {
+        if (result.ok) {
+          $scope.settings.sms = result.data.sms;
+          $scope.settings.email = result.data.email;
+        }
+      });
+    };
+    
+    $scope.updateCommunicationSettings = function() {
+      lexProjectService.updateSettings($scope.settings.sms, $scope.settings.email, function(result) {
+        if (result.ok) {
+          notice.push(notice.SUCCESS, $filter('translate')("{projectName} SMS settings updated successfully", {projectName: $scope.project.projectName}));
+        }
+      });
+    };
+    
+  }])
+  ;
