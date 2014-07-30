12a66ff53016d036320aa0a90bf6efde93394b67
irahopkinson <ira.hopkinson@gmail.com>
2014-07-30 13:53:34 +0700

    Select Language - UI for suggesting languages from the lexical data

--- src/angular-app/bellows/directive/pui-language.js
+++ src/angular-app/bellows/directive/pui-language.js
@@ -1,43 +1,57 @@
+'use strict';
 
 angular.module('palaso.ui.language', [])
- // Palaso UI Select Language 
-.directive('puiSelectLanguage', [function() {
-	return {
-		restrict : 'E',
-		transclude: true,
-		templateUrl : '/angular-app/bellows/directive/pui-language.html',
-		scope : {
-			puiCode : "=",
-			puiLanguage : "=",
-			puiAddDisabled : "=",
-			puiShowLinks : "=",
-		},
-		controller: ['$scope', '$filter', function($scope, $filter) {
-			// TODO Enhance. Could use infinite scrolling since search can return large results. See example here http://jsfiddle.net/W6wJ2/. IJH 2014-02
-			
-			$scope.languages = InputSystems.languages();
-			
-			$scope.filterText = 'xXxXxXxXxXxDoesntExistxXxXxXxXxXx';
-			$scope.search = function() {
-				$scope.filterText = $scope.searchText;
-				if ($scope.searchText == '*') {
-					$scope.filterText = '';
-				}
-			};
-			$scope.clearSearch = function() {
-				$scope.searchText = '';
-				$scope.filterText = 'xXxXxXxXxXxDoesntExistxXxXxXxXxXx';
-			};
-
-			$scope.currentCode = '';
-			$scope.puiAddDisabled = true;
-			$scope.selectLanguage = function(language) {
-				$scope.currentCode = language.code.three;
-				$scope.puiCode = (language.code.two) ? language.code.two : language.code.three;
-				$scope.puiLanguage = language;
-				$scope.puiAddDisabled = false;
-			};
-		}]
-	};
-}])
-;
+  
+  // Palaso UI Select Language 
+  .directive('puiSelectLanguage', [function() {
+    return {
+      restrict : 'E',
+      transclude: true,
+      templateUrl : '/angular-app/bellows/directive/pui-language.html',
+      scope : {
+        puiCode : "=",
+        puiLanguage : "=",
+        puiAddDisabled : "=",
+        puiSuggestedLanguages : "=",
+        puiShowLinks : "="
+      },
+      controller: ['$scope', '$filter', function($scope, $filter) {
+        
+        // TODO Enhance. Could use infinite scrolling since search can return large results. See example here http://jsfiddle.net/W6wJ2/. IJH 2014-02
+        $scope.languages = InputSystems.languages();
+        $scope.currentCode = '';
+        $scope.puiAddDisabled = true;
+        $scope.filterText = 'xXxXxXxXxXxDoesntExistxXxXxXxXxXx';
+        
+        $scope.search = function search() {
+          $scope.filterText = $scope.searchText;
+          if ($scope.searchText == '*') {
+            $scope.filterText = '';
+          }
+        };
+        $scope.clearSearch = function clearSearch() {
+          $scope.searchText = '';
+          $scope.filterText = 'xXxXxXxXxXxDoesntExistxXxXxXxXxXx';
+          delete $scope.languages;
+          $scope.languages = InputSystems.languages();
+          $scope.showSuggestions = false;
+        };
+        
+        $scope.selectLanguage = function(language) {
+          $scope.currentCode = language.code.three;
+          $scope.puiCode = (language.code.two) ? language.code.two : language.code.three;
+          $scope.puiLanguage = language;
+          $scope.puiAddDisabled = false;
+        };
+        
+        $scope.suggest = function suggest() {
+          delete $scope.languages;
+          $scope.languages = $scope.puiSuggestedLanguages;
+          $scope.filterText = '';
+          $scope.showSuggestions = true;
+        };
+        
+      }]
+    };
+  }])
+  ;
