9b81c5f19fbe8a33761d82bfeb00ac48c46334c0
darcywong00 <darcy_wong@sil.org>
2014-08-07 11:26:22 +0700

    Cleaning up front of house for beta preview phase
    Remove "FORGING!" ribbon from slideshow
    Remove LF news from frontpage
    Add list of features to About page

--- src/views/languageforge/default/pages/learn_language_forge.html.php
+++ src/views/languageforge/default/pages/learn_language_forge.html.php
@@ -5,6 +5,20 @@
 			<p>Take your dictonary project to the next level by involving tens, hundreds or even more contributors in the collection and refinement of lexical data upon which your dictionary will be based.  Engage your language's diaspora speakers on the web or their phone, wherever they are.  Bring new insights and a richer corpus into the lexical development process by engaging remote participants.  While your lexical development team continues to work in <a href="http://wesay.palaso.org/" target="_blank">WeSay</a>, and/or <a href="http://fieldworks.sil.org/flex/" target="_blank">FieldWorks Language Explorer</a>, Language Forge provides a community contribution and feedback mechanisms to help your team grow and evaluate your lexicon as it is under development. Finally, the community engagement results in buyin and ownership of the resultant dictionaries which will increas community awareness and use.
 			</p>
 			<p>When everyone plays their part in developing dictionaries and getting them into the right hands, everyone benefits.  It's the power of collaboration that produces richer and more standardized dictionaries.  Our technology brings lexical development to the masses, in a positive environment that focuses participants on the task and works wherever they are.  On the tablet, phone or web, Language Forge is there.</p>
-			
+			<h2>Beta Preview Features</h2>
+			<ul>
+				<li>Import your WeSay or FLEx data (LIFT format)</li>
+				<li>View or Edit the dictionary online</li>
+				<li>Invite others to view, collaborate, edit and comment on your lexicon data</li>
+				<li>Dictionary configuration editor: customize input systems, lexicon fields and tasks</li>
+				<li>Supports most FLEx fields</li>
+				<li>Assign project members one of several roles: Observer, Commenter, Collaborator and Manager</li>
+				<li>Customize what each member sees down to the field level</li>
+				<li>Custom field support</li>
+				<li>Right-to-left script support</li>
+				<li>Integrated commenting system allows team members to have discussions about any piece of data in the lexicon</li>
+				<li>Editors receive near real-time updates to the lexicon when there are multiple editors active online.</li>
+				<li>Data protection.  Only those you give access to can view or edit the lexicon.</li>
+			</ul>
 		</div>
 </div>
