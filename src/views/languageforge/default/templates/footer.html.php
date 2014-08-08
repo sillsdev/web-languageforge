5c9a94afbd15339bbfa00d035671249b9137fbf0
darcywong00 <darcy_wong@sil.org>
2014-08-08 14:44:50 +0700

    Update SIL International logo and copyright to conform
    The new July 2014 SIL International logo has specific requirements on blue border.  png file has been updated accordingly (for future usage).  Due to the contrast with the LF blue footer, I've removed the logo image.

--- src/views/languageforge/default/templates/footer.html.php
+++ src/views/languageforge/default/templates/footer.html.php
@@ -9,24 +9,23 @@
 							<p class="small">Language Forge<br />SIL International<br />7500 W. Camp Wisdom Road<br />Dallas, TX 75236-5629 USA</p>
 						</div>
 						<div class="left footer-nav">
-							<h4>Navigate</h4>
-							<p class="small"><a href="/">Home</a><br /><a href="/learn_language_forge">Learn</a><br /><a href="/discuss">Discuss</a></p>
+							<h4 style="margin-left: 20px;">Navigate</h4>
+							<p class="small" style="margin-left: 20px;"><a href="/">Home</a><br /><a href="/learn_language_forge">Learn</a><br /><a href="/discuss">Discuss</a></p>
 						</div>
 					</div>
 				</div>
 				
 				<div class="three-col">
 					<div class="three-col-2">
-						<h4>Credits</h4>
-						<p class="small">Copyright <?php echo strftime("%Y"); ?> Language Forge. An SIL International project. All rights reserved.</p>
-						<p class="small">Use of this site is governed by our <a href="/terms_and_conditions">terms and conditions</a> of use.</p>
+						<h4 style="margin-left: 80px;">Credits</h4>
+						<p class="small" style="margin-left: 80px;">Copyright <?php echo strftime("%Y"); ?> SIL International. All rights reserved.</p>
+						<p class="small" style="margin-left: 80px;">Use of this site is governed by our <a href="/terms_and_conditions">terms and conditions</a> of use.</p>
 					</div>
 				</div>
 				
 				<div class="three-col">
 					<div class="three-col-3">
 						<a href="http://ic.payap.ac.th/"><img src="/images/shared/PAYAP_logo.png" alt="PAYAP" width="170" height="50" class="right png_bg"></a>
-						<a href="http://www.sil.org"><img src="/images/shared/2014_sil_logo.png" alt="SIL" width="110" height="50" class="right png_bg" style="margin-right: 30px;"></a>
 					</div>
 				</div>
 			
