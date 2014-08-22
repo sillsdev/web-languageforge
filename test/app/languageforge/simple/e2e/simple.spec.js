3b126c281616dc6fb0732224f4d197352a1621d3
Robin Munn <robin_munn@sil.org>
2014-08-22 14:34:19 +0700

    Better E2E test for DBE view
    Also started on creating a better page object for the DBE page.

--- test/app/languageforge/simple/e2e/simple.spec.js
+++ test/app/languageforge/simple/e2e/simple.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('E2E testing: pause for manual checking', function() {
+describe('E2E testing: sample LF test', function() {
 	var constants    = require('../../../testConstants');
 	var loginPage    = require('../../../bellows/pages/loginPage.js');
 	var projectsPage = require('../../../bellows/pages/projectsPage.js');
@@ -9,11 +9,47 @@ describe('E2E testing: pause for manual checking', function() {
 	it('setup: login', function() {
 		loginPage.loginAsManager();
 	});
-	it('pause for manual testing', function() {
+	it('clicking on browse page takes you to edit page for that word', function() {
 		projectsPage.get();
 		projectsPage.clickOnProject(constants.testProjectName);
-		dbePage.clickEntryByLexeme(constants.testEntry1.lexeme.th.value);
+
+		// Word 1
+		dbePage.browse.clickEntryByLexeme(constants.testEntry1.lexeme.th.value);
+		dbePage.edit.getFirstLexeme().then(function(word) {
+			expect(word).toBe(constants.testEntry1.lexeme.th.value);
+		});
+		dbePage.edit.getLexemeDivByWsid('th').then(function(elems) {
+			if (elems.length > 0) { elems[0].$('input').getAttribute('value').then(console.log); }
+		});
+		dbePage.edit.getLexemeByWsid('thipa').then(function(lexeme) {
+			console.log(lexeme);
+		});
+		expect(dbePage.edit.getLexemes()).toEqual([
+			{ wsid: 'th'   , value: constants.testEntry1.lexeme.th.value },
+			{ wsid: 'thipa', value: constants.testEntry1.lexeme['th-fonipa'].value },
+		]);
+		dbePage.edit.getLexemesAsObject().then(console.log);
+		dbePage.edit.getFirstLexeme().then(console.log);
 		
-		browser.sleep(5000); // TODO: Do some verifying of the contents of this page
+		// Back to list to check word 2
+		dbePage.edit.toListLink.click();
+		
+		// Word 2
+		dbePage.browse.clickEntryByLexeme(constants.testEntry2.lexeme.th.value);
+		dbePage.edit.getFirstLexeme().then(function(word) {
+			expect(word).toBe(constants.testEntry2.lexeme.th.value);
+		});
+		dbePage.edit.getLexemeDivByWsid('th').then(function(elems) {
+			if (elems.length > 0) { elems[0].$('input').getAttribute('value').then(console.log); }
+		});
+		dbePage.edit.getLexemeByWsid('thipa').then(function(lexeme) {
+			console.log(lexeme);
+		});
+		expect(dbePage.edit.getLexemes()).toEqual([
+			{ wsid: 'th'   , value: constants.testEntry2.lexeme.th.value },
+			{ wsid: 'thipa', value: constants.testEntry2.lexeme['th-fonipa'].value },
+		]);
+		dbePage.edit.getLexemesAsObject().then(console.log);
+		dbePage.edit.getFirstLexeme().then(console.log);
 	});
 });
