c8edbfa72cfebf24c9c4ffe6984a48d677ce8051
Robin Munn <robin_munn@sil.org>
2014-08-28 15:13:27 +0700

    WIP on DBE page object: slow way to parse dc-entry
    This approach works, but is too slow, has an inconvenient API, and is
    sometimes proving brittle: it's so slow that the page sometimes goes
    away before the test has finished retrieving all the data from it, and
    Protractor throws "TypeError: Cannot call method 'then' of undefined".
    But re-run the exact same test code a second time with no changes, and
    it works.
    
    However, this code is worth checking in even if it won't be finally
    used, because it's at least working even if it's slow.

--- test/app/languageforge/simple/e2e/simple.spec.js
+++ test/app/languageforge/simple/e2e/simple.spec.js
@@ -5,6 +5,7 @@ describe('E2E testing: sample LF test', function() {
 	var loginPage    = require('../../../bellows/pages/loginPage.js');
 	var projectsPage = require('../../../bellows/pages/projectsPage.js');
 	var dbePage      = require('../../pages/dbePage.js');
+	var dbeUtil      = require('../../pages/dbeUtil.js');
 	
 	it('setup: login', function() {
 		loginPage.loginAsManager();
@@ -14,7 +15,7 @@ describe('E2E testing: sample LF test', function() {
 		projectsPage.clickOnProject(constants.testProjectName);
 
 		expect(dbePage.browse.entriesList.count()).toEqual(dbePage.browse.getEntryCount());
-		expect(dbePage.browse.getEntryCount()).toBe(2);
+		expect(dbePage.browse.getEntryCount()).toBe(3);
 
 		dbePage.browse.search.input.sendKeys('asparagus');
 		expect(dbePage.browse.search.getMatchCount()).toBe(1);
@@ -52,6 +53,34 @@ describe('E2E testing: sample LF test', function() {
 		});
 		dbePage.edit.renderedDiv.getText().then(function(text) {
 			console.log('Rendered:', text);
+		});
+		
+		// Back to list to check word 3
+		dbePage.edit.toListLink.click();
+
+		// Word 3 (multiple meaning entry 1)
+		dbePage.browse.clickEntryByLexeme(constants.testMultipleMeaningEntry1.lexeme.th.value);
+
+		var elems = dbeUtil.parseDcEntry(element(by.css("dc-entry")));
+		elems.then(function(data) {
+			data.senses.then(function(sensesData) {
+				sensesData.forEach(function(sense) {
+					sense.Meaning.then(function(meaning) {
+						console.log('Meaning:', meaning);
+					});
+					sense['Part of Speech'].then(function(pos) {
+						console.log('Part of Speech:', pos);
+					});
+					sense.examples.forEach(function(example) {
+						example.Example.then(function(exampleText) {
+							console.log('Example:', exampleText);
+						});
+						example.Translation.then(function(translationText) {
+							console.log('Translation:', translationText);
+						});
+					});
+				});
+			});
 		});
 	});
 });
