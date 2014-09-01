3d89ece39332a60395b52f401f482b1cc6ae7716
Robin Munn <robin_munn@sil.org>
2014-09-01 13:54:35 +0700

    DBE E2E test: page object & tests for comments

--- test/app/languageforge/dbe/e2e/dbe.spec.js
+++ test/app/languageforge/dbe/e2e/dbe.spec.js
@@ -84,4 +84,84 @@ describe('Browse and edit page (DBE)', function() {
 			{'en': constants.testMultipleMeaningEntry1.senses[1].examples[1].translation.en.value},
 		]);
 	});
+
+	it('while Show All Fields has not been clicked, uncommon fields are hidden', function() {
+		expect(dbePage.edit.getOneField('General Note').isPresent()).toBe(false);
+		dbePage.edit.showUncommonFields();
+		expect(dbePage.edit.getOneField('General Note').isPresent()).toBe(true);
+	});
+
+	it('word with multiple meanings: edit page has correct general notes, sources', function() {
+		expect(dbePage.edit.getFieldValues('General Note')).toEqual([
+			{'en': constants.testMultipleMeaningEntry1.senses[0].generalNote.en.value},
+			{'en': constants.testMultipleMeaningEntry1.senses[1].generalNote.en.value},
+		]);
+		expect(dbePage.edit.getFieldValues('Source')).toEqual([
+			{'en': ''}, // Searching for "Source" also matches "Etymology Source", which is empty
+			{'en': constants.testMultipleMeaningEntry1.senses[0].source.en.value},
+			{'en': constants.testMultipleMeaningEntry1.senses[1].source.en.value},
+		]);
+	});
+
+	it('switch to comments page, add one comment', function() {
+		dbePage.edit.toCommentsLink.click();
+		dbePage.comment.newComment.textarea.sendKeys('First comment on this word.');
+		dbePage.comment.newComment.postBtn.click();
+	});
+
+	it('comments page: check that comment shows up', function() {
+		var comment = dbePage.comment.getComment(0);
+		expect(comment.wholeComment.isPresent()).toBe(true);
+		expect(comment.avatar.getAttribute('src')).toContain(constants.avatar);
+		expect(comment.author.getText()).toEqual(constants.managerName);
+		expect(comment.date.getText()).toContain('ago');
+		expect(comment.score.getText()).toEqual('0');
+		expect(comment.plusOne.isPresent()).toBe(true);
+		expect(comment.content.getText()).toEqual('First comment on this word.');
+		// This comment should have no "regarding" section
+		expect(comment.regarding.fieldLabel.isDisplayed()).toBe(false);
+	});
+
+	it('comments page: add comment about a specific part of the entry', function() {
+		dbePage.comment.newComment.textarea.clear();
+		dbePage.comment.newComment.textarea.sendKeys('Second comment.');
+		dbePage.comment.entry.getOneField('Word').then(function(elem) {
+			elem.$$('span.wsid').first().click();
+		});
+		dbePage.comment.newComment.postBtn.click();
+	});
+
+	it('comments page: check that second comment shows up', function() {
+		var comment = dbePage.comment.getComment(-1);
+		expect(comment.wholeComment.isPresent()).toBe(true);
+		expect(comment.avatar.getAttribute('src')).toContain(constants.avatar);
+		expect(comment.author.getText()).toEqual(constants.managerName);
+		expect(comment.date.getText()).toContain('ago');
+		expect(comment.score.getText()).toEqual('0');
+		expect(comment.plusOne.isPresent()).toBe(true);
+		expect(comment.content.getText()).toEqual('Second comment.');
+		// This comment should have a "regarding" section
+		expect(comment.regarding.fieldLabel.isDisplayed()).toBe(true);
+		var word    = constants.testMultipleMeaningEntry1.lexeme.th.value;
+		var meaning = constants.testMultipleMeaningEntry1.senses[0].definition.en.value;
+		expect(comment.regarding.word.getText()).toEqual(word);
+		expect(comment.regarding.meaning.getText()).toEqual(meaning);
+		expect(comment.regarding.fieldLabel.getText()).toEqual('Word');
+		expect(comment.regarding.fieldWsid .getText()).toEqual('th');
+		expect(comment.regarding.fieldValue.getText()).toEqual(word);
+	});
+
+	it('comments page: click +1 button on first comment', function() {
+		var comment = dbePage.comment.getComment(0);
+		expect(comment.plusOne.getAttribute('ng-click')).not.toBe(null); // Should be clickable
+		comment.plusOne.click();
+		expect(comment.score.getText()).toEqual('1');
+	});
+
+	it('comments page: +1 button disabled after clicking', function() {
+		var comment = dbePage.comment.getComment(0);
+		expect(comment.plusOne.getAttribute('ng-click')).toBe(null); // Should NOT be clickable
+		comment.plusOne.click();
+		expect(comment.score.getText()).toEqual('1'); // Should not change from previous test
+	});
 });
