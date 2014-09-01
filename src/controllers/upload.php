d0b8e3985d30e9beed4e6ccd11820adbd2a50474
irahopkinson <ira.hopkinson@gmail.com>
2014-09-01 17:09:21 +0700

    wip: make upload generic and call API
    Former-commit-id: aa0ebafd99de253acf16785b913a417b09c3e7dd

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -1,80 +1,45 @@
 <?php
 
-use models\shared\rights\Operation;
-
-use models\shared\rights\Domain;
-
-use models\scriptureforge\SfchecksProjectModel;
-
 use models\shared\dto\RightsHelper;
-
-use models\TextModel;
+use models\shared\rights\Domain;
+use models\shared\rights\Operation;
+use models\scriptureforge\SfchecksProjectModel;
 use models\ProjectModel;
+use models\TextModel;
 
 require_once 'secure_base.php';
 
 class Upload extends Secure_base {
 
-	public function receive() {
-		$projectModel = new SfchecksProjectModel($this->_projectId);
-		if (!$projectModel->hasRight($this->_userId, Domain::TEXTS + Operation::EDIT)) {
-			throw new \Exception("Insufficient privilege to upload");
-		}
-		
-		// Note: ideally in the future this would have been implemented as an API method to take advantage of the security measures in place - cjh
-		$allowedTypes = array("audio/mpeg", "audio/mp3");	// type: documented, observed
-		$allowedExtensions = array(".mp3");
-		
+	public function receive($app, $fileType) { // e.g. 'lf', 'entry-audio'
 		$file = $_FILES['file'];
-		$fileType = $file['type'];
-		$fileName = $file['name'];
-		$fileName = str_replace(array('/', '\\', '?', '%', '*', ':', '|', '"', '<', '>'), '_', $fileName);	// replace special characters with _
-		$fileExt = (false === $pos = strrpos($fileName, '.')) ? '' : substr($fileName, $pos);
-
-		$projectId = $this->_projectId;
-		$textId = $_POST['textId'];
 		
 		if ($file['error'] == UPLOAD_ERR_OK) {
-			if (in_array($fileType, $allowedTypes) && in_array($fileExt, $allowedExtensions)) {
-				// make the folder if it doesn't exist
-				$folderPath = 'assets/' . $projectId;
-				if (!file_exists($folderPath) and !is_dir($folderPath)) {
-					mkdir($folderPath);
-				};
+			
+			// Call the api here
+			if ($app == 'sf-checks') {
+				$api = new Sf($this);
 				
-				// cleanup previous files of any allowed extension
-				$cleanupFiles = glob($folderPath . '/' . $textId . '*[' . implode(', ', $allowedExtensions) . ']');
-				foreach ($cleanupFiles as $cleanupFile) {
-					@unlink($cleanupFile);
-				}
+				// Do whatever permissions / rights check that should be done.
 				
-				// move uploaded file from tmp location to assets
-				$filePath =  $folderPath . '/' . $textId . '_' . $fileName;
-				$moveOk = move_uploaded_file($file['tmp_name'], $filePath);
-
-				// update database with file location
-				$project = new ProjectModel($projectId);
-				$text = new TextModel($project, $textId);
-				if ($moveOk) {
-					$text->audioUrl = $filePath;
-				} else {
-					$text->audioUrl = '';
-				}
-				$text->write();
-
-				echo "File uploaded successfully.";
+				$result = $api->sfChecks_uploadFile($fileType);
+				
+			} else if ($app == 'lf-lexicon') {
+				$api = new Sf($this);
+				
+				// Do whatever permissions / rights check that should be done.
+				
+				$result = $api->lex_uploadFile($fileType);
+				
 			} else {
-				$allowedExtensionsStr = implode(", ", $allowedExtensions);
-				if (count($allowedExtensions) < 1) {
-					echo "$fileName is not an allowed audio file. No audio file formats are currently enabled.";
-				} else if (count($allowedExtensions) == 1) {
-					echo "$fileName is not an allowed audio file. Ensure the file is an $allowedExtensionsStr.";
-				} else {
-					echo "$fileName is not an allowed audio file. Ensure the file is one of the following types: $allowedExtensionsStr.";
-				}
+				// Return some kind of programmer isn't that clever error.
 			}
+
 		}
+//  		header("Content-Type", "text/javascript");
+		return json_encode($result); // Might need to set the header to get the content type ?
 	}
+	
 }
 
 ?>
