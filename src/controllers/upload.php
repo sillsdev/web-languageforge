b9d1db46606719619290834f17ac062df06cb59b
irahopkinson <ira.hopkinson@gmail.com>
2014-09-02 16:36:30 +0700

    fix PHP formating to conform to PSR-2
    Former-commit-id: b9d19e918ae595a655059fce414121ec56c8e834

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -1,44 +1,42 @@
 <?php
-
 require_once 'secure_base.php';
 
-class Upload extends Secure_base {
+class Upload extends Secure_base
+{
 
-	public function receive($app, $uploadType) { // e.g. 'lf', 'entry-audio'
-		
-		// Need to require this after the autoloader is loaded, hence it is here.
-		require_once 'service/sf.php';
+    public function receive($app, $uploadType)
+    { // e.g. 'lf', 'entry-audio'
 
-		$result = array();
-		
-		$file = $_FILES['file'];
-		
-		if ($file['error'] == UPLOAD_ERR_OK) {
-			
-			// Call the api here
-			if ($app == 'sf-checks') {
-				$api = new Sf($this);
-				
-				// Do whatever permissions / rights check that should be done.
-				
-				$result = $api->sfChecks_uploadFile($uploadType);
-				
-			} else if ($app == 'lf-lexicon') {
-				$api = new Sf($this);
-				
-				// Do whatever permissions / rights check that should be done.
-				
-				$result = $api->lex_uploadFile($uploadType);
-				
-			} else {
-				// Return some kind of programmer isn't that clever error.
-			}
+        // Need to require this after the autoloader is loaded, hence it is here.
+        require_once 'service/sf.php';
 
-		}
-// 		header("Content-type: text/javascript");
-		return json_encode($result); // Might need to set the header to get the content type ?
-	}
-	
+        $result = array();
+
+        $file = $_FILES['file'];
+
+        if ($file['error'] == UPLOAD_ERR_OK) {
+
+            // Call the api here
+            if ($app == 'sf-checks') {
+                $api = new Sf($this);
+
+                // Do whatever permissions / rights check that should be done.
+
+                $result = $api->sfChecks_uploadFile($uploadType);
+            } else
+                if ($app == 'lf-lexicon') {
+                    $api = new Sf($this);
+
+                    // Do whatever permissions / rights check that should be done.
+
+                    $result = $api->lex_uploadFile($uploadType);
+                } else {
+                    // Return some kind of programmer isn't that clever error.
+                }
+        }
+        // header("Content-type: text/javascript");
+        return json_encode($result); // Might need to set the header to get the content type ?
+    }
 }
 
 ?>
