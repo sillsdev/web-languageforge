4d9b0475d7bbf5ba9bf87505de1a78a1b47dcf34
irahopkinson <ira.hopkinson@gmail.com>
2014-09-03 16:22:47 +0700

    add test for mp3 upload allowed and mp4 upload disallowed

--- test/php/scriptureforge/commands/SfchecksUploadCommands_Test.php
+++ test/php/scriptureforge/commands/SfchecksUploadCommands_Test.php
@@ -22,7 +22,8 @@ class TestSfchecksUploadCommands extends UnitTestCase
 
         $fileType = 'audio';
         $file = array();
-        $file['name'] = 'fileName.mp3';
+        $fileName = 'fileName.mp3';
+        $file['name'] = $fileName;
         $file['type'] = 'audio/mp3';
         $file['tmp_name'] = '';
         $_FILES['file'] = $file;
@@ -31,6 +32,50 @@ class TestSfchecksUploadCommands extends UnitTestCase
         $response = SfchecksUploadCommands::uploadFile($projectId, $fileType);
 
         $this->assertEqual($response->result, true);
+
+        $file['type'] = 'audio/mpeg';
+
+        $response = SfchecksUploadCommands::uploadFile($projectId, $fileType);
+
+        $this->assertEqual($response->result, true);
+        $this->assertPattern("/$projectId/", $response->data->path);
+        $this->assertPattern("/$textId/", $response->data->fileName);
+        $this->assertPattern("/$fileName/", $response->data->fileName);
+    }
+
+    function testUploadAudio_notMp3File_uploadDisallowed()
+    {
+        $e = new MongoTestEnvironment();
+        $e->clean();
+
+        $project = $e->createProject(SF_TESTPROJECT, SF_TESTPROJECTCODE);
+        $projectId = $project->id->asString();
+        $text = new TextModel($project);
+        $text->title = "Some Title";
+        $textId = $text->write();
+
+        $fileType = 'audio';
+        $file = array();
+        $file['name'] = 'fileName.mp3';
+        $file['type'] = 'audio/mp4';
+        $file['tmp_name'] = '';
+        $_FILES['file'] = $file;
+        $_POST['textId'] = $textId;
+
+        $response = SfchecksUploadCommands::uploadFile($projectId, $fileType);
+
+        $this->assertEqual($response->result, false);
+        $this->assertEqual($response->data->errorType, 'UserMessage');
+        $this->assertPattern('/Ensure the file is an .mp3/', $response->data->errorMessage);
+
+        $file['type'] = 'audio/mp3';
+        $file['name'] = 'fileName.mp4';
+
+        $response = SfchecksUploadCommands::uploadFile($projectId, $fileType);
+
+        $this->assertEqual($response->result, false);
+        $this->assertEqual($response->data->errorType, 'UserMessage');
+        $this->assertPattern('/Ensure the file is an .mp3/', $response->data->errorMessage);
     }
 }
 
