7edfa23cb382c128667ae7fa08456dcd34f6bc67
irahopkinson <ira.hopkinson@gmail.com>
2014-09-03 16:35:40 +0700

    Move UploadResponse and sub-classes to their own files

---
+++ src/models/scriptureforge/sfchecks/commands/MediaResult.php
@@ -0,0 +1,26 @@
+<?php
+namespace models\scriptureforge\sfchecks\commands;
+
+class MediaResult
+{
+
+    /**
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     *
+     * @var string
+     */
+    public $path;
+
+    /**
+     *
+     * @var string
+     */
+    public $fileName;
+}
+
+?>
