59bf6c78dbe4dd0292f512905b9518bed8851370
irahopkinson <ira.hopkinson@gmail.com>
2014-09-03 21:52:04 +0700

    fix migration script permissions; wip: auto load available scripts
    Former-commit-id: c05cc69fb610efdd6254e3c21931f90f2e1fa3bb

--- src/libraries/shared/scripts/control/Panel.php
+++ src/libraries/shared/scripts/control/Panel.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace libraries\shared\scripts\control;
 
 class Panel
 {
+
     public function run()
     {
         $message = '<div ng-app>';
@@ -12,14 +12,42 @@ class Panel
         $message .= '<h3>Migration Scripts</h3>';
         $message .= '<select ng-model="run"><option value="">Test only</option><option value="/run">Run</option></select>';
         $message .= '<ul>';
-        $message .= '<li><a href="/script/migration/FixAnswerCommentUserRefs{{run}}">FixAnswerCommentUserRefs </a></li>';
-        $message .= '<li><a href="/script/migration/FixProjectUserRefs{{run}}">FixProjectUserRefs </a></li>';
-        $message .= '<li><a href="/script/migration/FixUserRoles{{run}}">FixUserRoles </a></li>';
-        $message .= '<li><a href="/script/migration/FixAvatarRefs{{run}}">FixAvatarRefs </a></li>';
-        $message .= '<li><a href="/script/migration/MakeAllSfchecksProjects{{run}}">MakeAllSfchecksProjects </a></li>';
+        $message .= '<li><a href="/script/migration/FixProjectRoles{{run}}">FixProjectRoles.php </a></li>';
+        $message .= '<li><a href="/script/migration/MigrateQuestionTemplates{{run}}">MigrateQuestionTemplates.php </a></li>';
         $message .= '</ul>';
         $message .= '</div>';
-
+/*        
+        $_message = <<<EOT
+<div ng-app>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.min.js"></script>
+    <h2>Scripts Control Panel</h2>
+    <h3>Migration Scripts</h3>
+    <select ng-model="run"><option value="">Test only</option><option value="/run">Run</option></select>
+    <ul>
+        <li><a href="/script/migration/FixProjectRoles{{run}}">FixProjectRoles.php </a></li>
+        <li><a href="/script/migration/MigrateQuestionTemplates{{run}}">MigrateQuestionTemplates.php </a></li>
+    </ul>
+</div>
+EOT;
+        $_message = <<<EOD
+<!doctype html>
+<html ng-app='ControlPanel'>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.min.js"></script>
+        <script src="/libraries/shared/scripts/control/script.js"></script>
+    </head>
+    <body>
+        <h2>Scripts Control Panel</h2>
+        <h3>Migration Scripts</h3>
+        <select ng-model="run"><option value="">Test only</option><option value="/run">Run</option></select>
+        <ul>
+            <li><a href="/script/migration/FixProjectRoles{{run}}">FixProjectRoles.php </a></li>
+            <li><a href="/script/migration/MigrateQuestionTemplates{{run}}">MigrateQuestionTemplates.php </a></li>
+        </ul>
+    </body>
+</html>
+EOD;
+*/        
         return $message;
     }
 }
---
+++ src/libraries/shared/scripts/control/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ControlPanel', [])
+.controller('PanelCtrl', ['$scope', function($scope) {
+  
+}]);
