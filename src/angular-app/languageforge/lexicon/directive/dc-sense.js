4631fabf96d23a509ff338c246286a91dab2dbb7
irahopkinson <ira.hopkinson@gmail.com>
2014-09-10 14:40:47 +0700

    re-layout AirBNB style guide
    Former-commit-id: 33eff82073ad8ab6d9b079375c75c175355b9f23

--- src/angular-app/languageforge/lexicon/directive/dc-sense.js
+++ src/angular-app/languageforge/lexicon/directive/dc-sense.js
@@ -1,37 +1,37 @@
 "use strict";
+
 angular.module('palaso.ui.dc.sense', ['palaso.ui.dc.multitext', 'palaso.ui.dc.optionlist', 'palaso.ui.dc.multioptionlist', 'palaso.ui.dc.example', 'ngAnimate', 'bellows.services', 'lexicon.services'])
 // Palaso UI Dictionary Control: Sense
 .directive('dcSense', ['lexUtils', 'modalService', function(utils, modal) {
-	return {
-		restrict : 'E',
-		templateUrl : '/angular-app/languageforge/lexicon/directive/dc-sense.html',
-		scope : {
-			config : "=",
-			model : "=",
-			index : "=",
-			remove : "=",
-			control : "="
-		},
-		controller: ['$scope', 'lexConfigService', function($scope, lexConfigService) {
-			$scope.addExample = function() {
-                var newExample = {};
-                $scope.control.makeValidModelRecursive($scope.config.fields.examples, newExample);
-                $scope.model.examples.push(newExample);
-			};
+  return {
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/directive/dc-sense.html',
+    scope: {
+      config: "=",
+      model: "=",
+      index: "=",
+      remove: "=",
+      control: "="
+    },
+    controller: ['$scope', 'lexConfigService', function($scope, lexConfigService) {
+      $scope.addExample = function() {
+        var newExample = {};
+        $scope.control.makeValidModelRecursive($scope.config.fields.examples, newExample);
+        $scope.model.examples.push(newExample);
+      };
 
-			$scope.deleteExample = function(index) {
-                var deletemsg = "Are you sure you want to delete the example <b>' " + utils.getExampleSentence($scope.config.fields.examples, $scope.model.examples[index])  + " '</b>";
-                modal.showModalSimple('Delete Example', deletemsg, 'Cancel', 'Delete Example').then(function() {
-                    $scope.model.examples.splice(index, 1);
-                });
-			};
+      $scope.deleteExample = function(index) {
+        var deletemsg = "Are you sure you want to delete the example <b>' " + utils.getExampleSentence($scope.config.fields.examples, $scope.model.examples[index]) + " '</b>";
+        modal.showModalSimple('Delete Example', deletemsg, 'Cancel', 'Delete Example').then(function() {
+          $scope.model.examples.splice(index, 1);
+        });
+      };
 
-            $scope.fieldContainsData = lexConfigService.fieldContainsData;
-		}],
-		link : function(scope, element, attrs, controller) {
-            scope.optionlists = scope.control.config.optionlists;
+      $scope.fieldContainsData = lexConfigService.fieldContainsData;
+    }],
+    link: function(scope, element, attrs, controller) {
+      scope.optionlists = scope.control.config.optionlists;
 
-		}
-	};
-}])
-;
+    }
+  };
+}]);
