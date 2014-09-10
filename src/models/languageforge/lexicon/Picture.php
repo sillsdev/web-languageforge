47bda7f71de08071247c59f955ac4183d882d127
irahopkinson <ira.hopkinson@gmail.com>
2014-09-10 14:37:17 +0700

    simplify entry pictures model

--- src/models/languageforge/lexicon/Picture.php
+++ src/models/languageforge/lexicon/Picture.php
@@ -1,22 +1,6 @@
 <?php
 namespace models\languageforge\lexicon;
 
-use models\mapper\ArrayOf;
-
-function _createPicture($data)
-{
-    return new Picture();
-}
-
-class Pictures extends ArrayOf
-{
-    public function __construct()
-    {
-        parent::__construct('\models\languageforge\lexicon\_createPicture');
-    }
-
-}
-
 class Picture
 {
     public function __construct($fileName = '')
