eb339bd6c61d95e7b8956d3dc8586fdd524f9344
irahopkinson <ira.hopkinson@gmail.com>
2014-09-10 16:41:01 +0700

    wip: add sense image widget
    Former-commit-id: 5a0f5f850b873187b4b50013c84aa1f06a07a79c

--- src/angular-app/languageforge/lexicon/directive/dc-sense.js
+++ src/angular-app/languageforge/lexicon/directive/dc-sense.js
@@ -14,13 +14,23 @@ angular.module('palaso.ui.dc.sense', ['palaso.ui.dc.multitext', 'palaso.ui.dc.op
       control: "="
     },
     controller: ['$scope', 'lexConfigService', function($scope, lexConfigService) {
-      $scope.addExample = function() {
+      $scope.addExample = function addExample() {
         var newExample = {};
         $scope.control.makeValidModelRecursive($scope.config.fields.examples, newExample);
         $scope.model.examples.push(newExample);
       };
+      
+      $scope.addPicture = function addPicture() {
+        var newPicture = {};
+        $scope.control.makeValidModelRecursive($scope.config.fields.pictures, newPicture);
+        $scope.model.pictures.push(newPicture);
+      };
+      
+      $scope.pictureUrl = function pictureUrl(fileName) {
+        return '/assets/lexicon/' + $scope.control.project.slug + '/' + fileName;
+      };
 
-      $scope.deleteExample = function(index) {
+      $scope.deleteExample = function deleteExample(index) {
         var deletemsg = "Are you sure you want to delete the example <b>' " + utils.getExampleSentence($scope.config.fields.examples, $scope.model.examples[index]) + " '</b>";
         modal.showModalSimple('Delete Example', deletemsg, 'Cancel', 'Delete Example').then(function() {
           $scope.model.examples.splice(index, 1);
