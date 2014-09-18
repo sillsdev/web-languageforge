bf1b6903de9448b0560da34a76f9bc1214bf084f
irahopkinson <ira.hopkinson@gmail.com>
2014-09-18 21:58:50 +0700

    wip: re-organise picture config structure
    Former-commit-id: 250951454aa92e7c5ee81fae9b4800cb24dd73c1

---
+++ src/models/languageforge/lexicon/config/LexCaptionsConfigObj.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace models\languageforge\lexicon\config;
+
+use models\mapper\ArrayOf;
+
+class LexCaptionsConfigObj extends LexiconMultitextConfigObj
+{
+    public function __construct()
+    {
+        parent::__construct();
+//         $this->type = LexiconConfigObj::CAPTIONS;
+        $this->label = 'Captions';
+    }
+
+}
