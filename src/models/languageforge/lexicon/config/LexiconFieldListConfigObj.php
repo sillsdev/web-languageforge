5cb6bc436acac40d5502e62602af90c46798ff21
irahopkinson <ira.hopkinson@gmail.com>
2014-09-19 14:37:20 +0700

    re-organise picture config structure

--- src/models/languageforge/lexicon/config/LexiconFieldListConfigObj.php
+++ src/models/languageforge/lexicon/config/LexiconFieldListConfigObj.php
@@ -24,8 +24,6 @@ class LexiconFieldListConfigObj extends LexiconConfigObj
                         return new LexiconMultiOptionlistConfigObj();
                     case LexiconConfigObj::PICTURES:
                         return new LexPicturesConfigObj();
-                    case LexiconConfigObj::CAPTIONS:
-                        return new LexCaptionsConfigObj();
                     default:
                         $type = $data['type'];
                         throw new \Exception("Unknown field list config type: $type");
