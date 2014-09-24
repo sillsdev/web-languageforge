71824af3159b128af81fba71700e84f0ca8970b3
irahopkinson <ira.hopkinson@gmail.com>
2014-09-24 11:57:23 +0700

    cleanup test assets folder
    Former-commit-id: 49f4111687da2a6ce976e48d18a87f57a71e236e

--- test/app/teardownTestEnvironment.php
+++ test/app/teardownTestEnvironment.php
@@ -1,11 +1,33 @@
 <?php
+require_once ('e2eTestConfig.php');
 
-require_once('e2eTestConfig.php');
+use models\languageforge\lexicon\LexiconProjectModel;
+use models\languageforge\lexicon\commands\LexUploadCommands;
 
+$constants = json_decode(file_get_contents(TestPath . '/testConstants.json'), true);
+
+if ($constants['siteType'] == 'languageforge') {
+
+    // cleanup test assets folder
+    $testProject = new LexiconProjectModel();
+    $testProject->readByProperties(array(
+        'projectCode' => $constants['testProjectCode']
+    ));
+    $assetsFolderPath = $testProject->getAssetsFolderPath();
+    recursiveRemoveFolder($assetsFolderPath);
+}
+
+// put the live config back into place
 system(TestPath . '/useLiveConfig.sh');
 
-// use commands
-use models\commands\ProjectCommands;
-
-
-?>
+function recursiveRemoveFolder($folder)
+{
+    foreach (glob("{$folder}/*") as $file) {
+        if (is_dir($file)) {
+            recursiveRemoveFolder($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($folder);
+}
