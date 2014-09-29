c3342f5f1737f795f43038d51807c2e18eb36a46
irahopkinson <ira.hopkinson@gmail.com>
2014-09-29 13:10:48 +0700

    re-layout to style guides
    Former-commit-id: 6840a270ad5358084bc06e78c4286d3fea3ee25a

--- src/angular-app/languageforge/lexicon/views/import-export.js
+++ src/angular-app/languageforge/lexicon/views/import-export.js
@@ -1,53 +1,52 @@
 'use strict';
 
 angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'palaso.ui.notice', 'palaso.ui.language', 'ngAnimate', 'angularFileUpload', 'lexicon.upload'])
-.controller('LiftImportCtrl', ['$scope', 'silNoticeService', 'fileReader', 'lexProjectService', '$filter', '$location', 'sessionService',
-                               function($scope, notice, fileReader, lexProjectService, $filter, $location, ss) {
-	lexProjectService.setBreadcrumbs('importExport', 'Import/export');
-	
-	$scope.mergeRule = 'createDuplicates';
-	$scope.skipSameModTime = true;
-	$scope.deleteMatchingEntry = false;
+// Lift Import Controller
+.controller('LiftImportCtrl', ['$scope', 'silNoticeService', 'fileReader', 'lexProjectService', '$filter', '$location', 'sessionService', 
+function($scope, notice, fileReader, lexProjectService, $filter, $location, ss) {
+  lexProjectService.setBreadcrumbs('importExport', 'Import/export');
 
-	$scope.onFileSelect = function($files) {
-		$scope.file = $files[0];	// take the first file only
-		fileReader.readAsDataUrl($scope.file, $scope)
-		.then(function(result) {
-			$scope.file.data = result;
-		});
-	};
-	
-	$scope.importLift = function() {
-		var importData = {
-			file: $scope.file,
-			settings: {
-				mergeRule: $scope.mergeRule,
-				skipSameModTime: $scope.skipSameModTime,
-				deleteMatchingEntry: $scope.deleteMatchingEntry
-			}
-		};
-        notice.setLoading('Importing LIFT file...');
-        $scope.importStarted = true;
-		lexProjectService.importLift(importData, function(result) {
-			if (result.ok) {
+  $scope.mergeRule = 'createDuplicates';
+  $scope.skipSameModTime = true;
+  $scope.deleteMatchingEntry = false;
 
-                // reload the config after the import is complete
-                ss.refresh(function() {
-                    notice.cancelLoading();
-                    notice.push(notice.SUCCESS, $filter('translate')("LIFT import completed successfully"));
+  $scope.onFileSelect = function($files) {
+    
+    // take the first file only
+    $scope.file = $files[0];
+    fileReader.readAsDataUrl($scope.file, $scope).then(function(result) {
+      $scope.file.data = result;
+    });
+  };
 
-                    notice.push(notice.INFO, $filter('translate')('Your project was successfully imported.  Carefully review the dictionary configuration below before continuing, especially the input systems and fields tabs'));
-                    $location.path('/configuration');
-                });
-			} else {
-                notice.cancelLoading();
-            }
-		});
-	};
+  $scope.importLift = function() {
+    var importData = {
+      file: $scope.file,
+      settings: {
+        mergeRule: $scope.mergeRule,
+        skipSameModTime: $scope.skipSameModTime,
+        deleteMatchingEntry: $scope.deleteMatchingEntry
+      }
+    };
+    notice.setLoading('Importing LIFT file...');
+    $scope.importStarted = true;
+    lexProjectService.importLift(importData, function(result) {
+      if (result.ok) {
 
-}])
-.controller('LiftExportCtrl', ['$scope', 'userService', 'sessionService', 'silNoticeService', 
-                               function($scope, userService, ss, notice) {
-	
-}])
-;
+        // reload the config after the import is complete
+        ss.refresh(function() {
+          notice.cancelLoading();
+          notice.push(notice.SUCCESS, $filter('translate')("LIFT import completed successfully"));
+          notice.push(notice.INFO, $filter('translate')('Your project was successfully imported.  Carefully review the dictionary configuration below before continuing, especially the input systems and fields tabs'));
+          $location.path('/configuration');
+        });
+      } else {
+        notice.cancelLoading();
+      }
+    });
+  };
+
+}]).controller('LiftExportCtrl', ['$scope', 'userService', 'sessionService', 'silNoticeService', 
+function($scope, userService, ss, notice) {
+
+}]);
