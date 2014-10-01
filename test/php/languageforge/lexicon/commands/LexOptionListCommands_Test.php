caf6e9151284893979af5228019c264f24c3ab7a
irahopkinson <ira.hopkinson@gmail.com>
2014-10-01 10:53:23 +0700

    move command tests into commands folder
    Former-commit-id: cc2644b79fc3e351891cfe6060dc13bf918d93db

--- test/php/languageforge/lexicon/commands/LexOptionListCommands_Test.php
+++ test/php/languageforge/lexicon/commands/LexOptionListCommands_Test.php
@@ -3,7 +3,7 @@
 use models\languageforge\lexicon\commands\LexOptionListCommands;
 use models\languageforge\lexicon\LexOptionListListModel;
 
-require_once dirname(__FILE__) . '/../../TestConfig.php';
+require_once dirname(__FILE__) . '/../../../TestConfig.php';
 require_once SimpleTestPath . 'autorun.php';
 require_once TestPath . 'common/MongoTestEnvironment.php';
 require_once dirname(__FILE__) . '/LexTestData.php';
