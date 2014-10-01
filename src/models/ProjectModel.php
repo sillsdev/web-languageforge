2846511e6091eb7f9429754f291dfc6b6cff4c5d
Robin Munn <robin_munn@sil.org>
2014-10-01 17:16:11 +0700

    Fix bug where removing projects with assets fails
    rrmdir() function needed to specify its own location when calling itself
    recursively (i.e., $this->rrmdir() instead of rrmdir()). This would only
    happen if the project's assets directory contained other directories, so
    it never triggered before.

--- src/models/ProjectModel.php
+++ src/models/ProjectModel.php
@@ -335,7 +335,7 @@ class ProjectModel extends \models\mapper\MapperModel
             $objects = scandir($dir);
             foreach ($objects as $object) {
                 if ($object != "." && $object != "..") {
-                    if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                    if (filetype($dir."/".$object) == "dir") $this->rrmdir($dir."/".$object); else unlink($dir."/".$object);
                 }
             }
             reset($objects);
