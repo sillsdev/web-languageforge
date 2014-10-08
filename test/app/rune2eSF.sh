0061c7d625f3af46fdf284a6b4ffbb3bee84c5e1
irahopkinson <ira.hopkinson@gmail.com>
2014-10-08 10:56:54 +0700

    fix local E2E run scripts
     - useTestConfig.sh and useLiveConfig.sh have been pulled out setupTestEnvironment.php and teardownTestEnvironment.php so that build tests work properly.
    
    
    Former-commit-id: 66d55e9e4e85d7ca555814812f586c5e53c0c3e5

--- test/app/rune2eSF.sh
+++ test/app/rune2eSF.sh
@@ -1,4 +1,5 @@
-php setupTestEnvironment.php
+sh useTestConfig.sh
+sudo -u www-data php setupTestEnvironment.php
 if [ $# -eq 0 ]
   then
     node ./node_modules/protractor/bin/protractor protractorConf.js
@@ -6,3 +7,4 @@ if [ $# -eq 0 ]
     node ./node_modules/protractor/bin/protractor protractorConf.js --specs "`find . -wholename "*e2e*$1*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
 fi
 php teardownTestEnvironment.php
+sh useLiveConfig.sh
