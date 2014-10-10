27521112741bfc90238fda3495d318e58f86a4fa
irahopkinson <ira.hopkinson@gmail.com>
2014-10-10 09:24:20 +0700

    reformat to style guides
    Former-commit-id: 7e6814c13c57e77ad370a0b155d3d4d75f1e701c

--- src/angular-app/languageforge/lexicon/directive/dc-multitext.js
+++ src/angular-app/languageforge/lexicon/directive/dc-multitext.js
@@ -1,33 +1,35 @@
 'use strict';
 
 angular.module('palaso.ui.dc.multitext', ['bellows.services'])
-  .directive('dcMultitext', [function() {
-		return {
-			restrict : 'E',
-			templateUrl : '/angular-app/languageforge/lexicon/directive/dc-multitext.html',
-			scope : {
-				config : "=",
-				model : "=",
-				control : "=",
-                selectField : "&"
-			},
-			controller: ['$scope', 'sessionService', function($scope, ss) {
-                $scope.inputSystems = ss.session.projectSettings.config.inputSystems;
+// Dictionary Control Multitext
+.directive('dcMultitext', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/directive/dc-multitext.html',
+    scope: {
+      config: "=",
+      model: "=",
+      control: "=",
+      selectField: "&"
+    },
+    controller: ['$scope', 'sessionService', function($scope, ss) {
+      $scope.inputSystems = ss.session.projectSettings.config.inputSystems;
 
-				$scope.inputSystemDirection = function inputSystemDirection(tag) {
-					if (! (tag in $scope.inputSystems)) {
-						return 'ltr';
-					}
-                    return ($scope.inputSystems[tag].isRightToLeft) ? 'rtl' : 'ltr';
-				};
+      $scope.inputSystemDirection = function inputSystemDirection(tag) {
+        if (!(tag in $scope.inputSystems)) {
+          return 'ltr';
+        }
+        return ($scope.inputSystems[tag].isRightToLeft) ? 'rtl' : 'ltr';
+      };
 
-                $scope.selectInputSystem = function selectInputSystem(tag) {
-                    $scope.selectField({inputSystem: tag});
-                };
-				
-			}],
-			link : function(scope, element, attrs, controller) {
-			}
-		};
-  }])
-  ;
+      $scope.selectInputSystem = function selectInputSystem(tag) {
+        $scope.selectField({
+          inputSystem: tag
+        });
+      };
+
+    }],
+    link: function(scope, element, attrs, controller) {
+    }
+  };
+}]);
