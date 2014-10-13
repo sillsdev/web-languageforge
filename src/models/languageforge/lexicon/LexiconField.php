ac3b06c1acbdb5d0ba95936e559748436902eade
Cambell <cambell.prince@gmail.com>
2014-10-13 17:07:30 +0700

    Added more FLEx field support with tests.
    Former-commit-id: 9d0d72c650f15bf61059251819e12f74b5350d6f

--- src/models/languageforge/lexicon/LexiconField.php
+++ src/models/languageforge/lexicon/LexiconField.php
@@ -9,7 +9,20 @@ class LexiconField
         $this->value = $value;
     }
 
+    /**
+     * @var string
+     */
     public $value;
+
+    /**
+     * Ensures that the value $value is set
+     * This method is primarily to give an api consistent with LexiconMultiValueField
+     * @param string $value
+     */
+    public function value($value)
+    {
+        $this->value = $value;
+    }
 
     public function __toString() {
         return $this->value;
