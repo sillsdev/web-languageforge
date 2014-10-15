6c925aec00f6771a51c986b4a709235d9ed00dc6
irahopkinson <ira.hopkinson@gmail.com>
2014-10-15 09:54:08 +0700

    Can now insert language span; can't pick language yet
     - add "insert link" setup so it can be modified
     - remove undo and redo as they are just the browser ones
    
    
    Former-commit-id: 618fdea0a7dfadf15da94250d71903306f97f708

--- src/angular-app/languageforge/lexicon/directive/dc-formattedtext.js
+++ src/angular-app/languageforge/lexicon/directive/dc-formattedtext.js
@@ -8,13 +8,124 @@ angular.module('palaso.ui.dc.formattedtext', ['textAngular'])
 
     // $delegate is the taOptions we are decorating
     // register the tool with textAngular
-    taRegisterTool('languageSpan', {
-      iconclass: 'fa fa-language fa-lg',
-      tooltiptext: 'Language span',
-      action: function(){
-        return this.$editor().wrapSelection("formatBlock", "<SPAN>");
+    taRegisterTool('insert_link', {
+//      tooltiptext: taTranslations.insertLink.tooltip,
+      tooltiptext: 'Insert/edit link',
+      iconclass: 'fa fa-link',
+      action: function() {
+        var urlLink;
+        urlLink = $window.prompt(taTranslations.insertLink.dialogPrompt, 'http://');
+        if (urlLink && urlLink !== '' && urlLink !== 'http://') {
+          return this.$editor().wrapSelection('createLink', urlLink, true);
+        }
       },
-      activeState: function(){ return this.$editor().queryFormatBlockState('span'); }
+      activeState: function(commonElement) {
+        if (commonElement)
+          return commonElement[0].tagName === 'A';
+        return false;
+      },
+      onElementSelect: {
+        element: 'a',
+        action: function(event, $element, editorScope) {
+          // setup the editor toolbar
+          // Credit to the work at http://hackerwins.github.io/summernote/ for
+          // this editbar logic
+          event.preventDefault();
+          editorScope.displayElements.popover.css('width', '435px');
+          var container = editorScope.displayElements.popoverContainer;
+          container.empty();
+          container.css('line-height', '28px');
+          var link = angular.element('<a href="' + $element.attr('href') + '" target="_blank">' + $element.attr('href') + '</a>');
+          link.css({
+            'display': 'inline-block',
+            'max-width': '200px',
+            'overflow': 'hidden',
+            'text-overflow': 'ellipsis',
+            'white-space': 'nowrap',
+            'vertical-align': 'middle'
+          });
+          container.append(link);
+          var buttonGroup = angular.element('<div class="btn-group pull-right">');
+          var reLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-edit icon-edit"></i></button>');
+          reLinkButton.on('click', function(event) {
+            event.preventDefault();
+            var urlLink = $window.prompt(taTranslations.insertLink.dialogPrompt, $element.attr('href'));
+            if (urlLink && urlLink !== '' && urlLink !== 'http://') {
+              $element.attr('href', urlLink);
+              editorScope.updateTaBindtaTextElement();
+            }
+            editorScope.hidePopover();
+          });
+          buttonGroup.append(reLinkButton);
+          var unLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-unlink icon-unlink"></i></button>');
+          // directly before this click event is fired a digest is fired off
+          // whereby the reference to $element is orphaned off
+          unLinkButton.on('click', function(event) {
+            event.preventDefault();
+            $element.replaceWith($element.contents());
+            editorScope.updateTaBindtaTextElement();
+            editorScope.hidePopover();
+          });
+          buttonGroup.append(unLinkButton);
+          var targetToggle = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">Open in New Window</button>');
+          if ($element.attr('target') === '_blank') {
+            targetToggle.addClass('active');
+          }
+          targetToggle.on('click', function(event) {
+            event.preventDefault();
+            $element.attr('target', ($element.attr('target') === '_blank') ? '' : '_blank');
+            targetToggle.toggleClass('active');
+            editorScope.updateTaBindtaTextElement();
+          });
+          buttonGroup.append(targetToggle);
+          container.append(buttonGroup);
+          editorScope.showPopover($element);
+        }
+      }
+    });
+
+    // Written by the author of Rangy, see http://stackoverflow.com/questions/4652734/return-html-from-a-user-selected-text
+    function getSelectionHtml() {
+      var html = "";
+      if (typeof window.getSelection != "undefined") {
+          var sel = window.getSelection();
+          if (sel.rangeCount) {
+              var container = document.createElement("div");
+              for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                  container.appendChild(sel.getRangeAt(i).cloneContents());
+              }
+              html = container.innerHTML;
+          }
+      } else if (typeof document.selection != "undefined") {
+          if (document.selection.type == "Text") {
+              html = document.selection.createRange().htmlText;
+          }
+      }
+      return html;
+  }
+
+    taRegisterTool('languageSpan', {
+      tooltiptext: 'Language span',
+      iconclass: 'fa fa-language fa-lg',
+      action: function() {
+        var langSpan, 
+          languageTag = 'test',
+          selectedHtml = getSelectionHtml();
+        if (languageTag && languageTag !== '' && selectedHtml && selectedHtml !== '') {
+          langSpan = "<span lang='" + languageTag + "'>" + selectedHtml + "</span>";
+          return this.$editor().wrapSelection("insertHTML", langSpan, false);
+       }
+      },
+//      activeState: function() {
+//        return this.$editor().queryCommandState('insertHTML');
+//      },
+      onElementSelect: {
+        element: 'span',
+//        onlyWithAttrs: ['lang'],
+        action: function(event, $element, editorScope) {
+          console.log('select lang span');
+        }
+      }
     });
 
     // add the button to the default toolbar definition
