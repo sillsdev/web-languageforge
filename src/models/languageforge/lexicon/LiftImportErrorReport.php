9d3e07043917e108b7f5bf51c3a99316deca8585
irahopkinson <ira.hopkinson@gmail.com>
2014-11-03 17:08:09 +0700

    wip: move error reporting up to LiftImport, add error reporting to Example.

--- src/models/languageforge/lexicon/LiftImportErrorReport.php
+++ src/models/languageforge/lexicon/LiftImportErrorReport.php
@@ -4,103 +4,31 @@ namespace models\languageforge\lexicon;
 class LiftImportErrorReport
 {
 
-    const ENTRY = 'entry';
-    const SENSE = 'sense';
-    const EXAMPLE = 'example';
-
-    /**
-     *
-     * @var string guid of lift entry
-     */
-    public $guid;
-
-    /**
-     *
-     * @var array of errors
-     */
-    private $errors;
-
-    /**
-     * @var string
-     */
-    private $type;
-
-    public function __construct($type, $guid)
+    public function __construct()
     {
-        $this->type = $type;
-        $this->guid = $guid;
-        $this->errors = array();
+        $this->nodeErrors = array();
     }
+
+    /**
+     *
+     * @var array <LiftImportNodeError>
+     */
+    public $nodeErrors;
 
     public function hasError()
     {
-        return count($this->errors) > 0;
-    }
-
-    public function addUnhandledElement($elementName)
-    {
-        $this->errors[] = array(
-            'error' => 'UnhandledElement',
-            'element' => $elementName
-        );
-    }
-
-    public function addUnhandledField($typeName)
-    {
-        $this->errors[] = array(
-            'error' => 'UnhandledField',
-            'type' => $typeName
-        );
-    }
-
-    public function addUnhandledTrait($traitName)
-    {
-        $this->errors[] = array(
-            'error' => 'UnhandledTrait',
-            'name' => $traitName
-        );
-    }
-
-    public function addUnhandledMedia($url, $context)
-    {
-        $this->errors[] = array(
-            'error' => 'UnhandledMedia',
-            'url' => $url,
-            'context' => $context
-        );
-    }
-
-    public function addUnhandledNote($noteType)
-    {
-        $this->errors[] = array(
-            'error' => 'UnhandledNote',
-            'type' => $noteType
-        );
+        $hasError = false;
+        foreach ($this->nodeErrors as $nodeError) {
+            $hasError |= $nodeError->hasError();
+        }
+        return $hasError;
     }
 
     public function toString()
     {
-        $msg = "While processing $this->type '$this->guid'";
-        foreach ($this->errors as $error) {
-            switch ($error['error']) {
-                case 'UnhandledElement':
-                    $msg .= ", unhandled element '" . $error['element'] . "'";
-                    break;
-                case 'UnhandledField':
-                    $msg .= ", unhandled field '" . $error['type'] . "'";
-                    break;
-                case 'UnhandledTrait':
-                    $msg .= ", unhandled trait '" . $error['name'] . "'";
-                    break;
-                case 'UnhandledNote':
-                    $msg .= ", unhandled note '" . $error['type'] . "'";
-                    break;
-                case 'UnhandledMedia':
-                    $msg .= ", unhandled media '" . $error['url'] . "' in " . $error['context'];
-                    break;
-                default:
-                    throw new \Exception("Unknown error type '" . $error['error'] . "' while processing guid '" . $this->guid . "'");
-            }
+        $msg = '';
+        foreach ($this->nodeErrors as $nodeError) {
+            $msg .= $nodeError->toString();
         }
         return $msg;
     }
