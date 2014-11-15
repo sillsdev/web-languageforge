f9cff56975664d126d698c82cd88c62c208e7208
irahopkinson <ira.hopkinson@gmail.com>
2014-11-15 12:21:31 +0700

    LexUploadCommans::importProjectZip now has proper response; LiftImport::importZip changed to instance

---
+++ src/models/shared/commands/ImportResult.php
@@ -0,0 +1,33 @@
+<?php
+namespace models\shared\commands;
+
+class ImportResult
+{
+
+    /**
+     *
+     * @var string
+     */
+    public $path;
+
+    /**
+     *
+     * @var string
+     */
+    public $fileName;
+
+    /**
+     *
+     * @var integer
+     */
+    public $entriesImported;
+
+    /**
+     *
+     * @var string
+     */
+    public $importErrors;
+
+}
+
+?>
