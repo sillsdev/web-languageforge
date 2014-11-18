8ce21b828affd00ec0494d9667f9474e01cbe407
Patrick Hess <tuxyrulez@gmail.com>
2014-11-18 18:50:14 -0500

    Mark which subdomains have been chosen on setup config page

--- src/angular-app/languageforge/semdomtrans/views/setupConfig.js
+++ src/angular-app/languageforge/semdomtrans/views/setupConfig.js
@@ -5,29 +5,29 @@ angular.module('semdomtrans.setupConfig', ['jsonRpc', 'ui.bootstrap', 'bellows.s
 .controller('setupConfigCtrl', ['$scope', '$location', 'semdomtransEditService' ,'semdomtransConfigService', 'sessionService', 'modalService', 'silNoticeService',
 function($scope, $location, semdomEditApi, semdomConfigApi, sessionService, modal, notice) {
 	
-	semdomEditApi.editorDto(function(result) {
-		if (result.ok) {
-			$scope.terms = []
-			for (var i = 0; i < result.data.terms.length; i++) {
-				$scope.terms.push(
-					{
-						'key':  result.data.terms[i].key, 
-						'name':  result.data.terms[i].name,
-						'description': result.data.terms[i].description, 
-						'nameTrans': result.data.terms[i].nameTrans, 
-						'descriptionTrans': result.data.terms[i].descriptionTrans, 
-						'comments': result.data.terms[i].comments, 
-					'selected': false
-					});
-			}
-		}
-	});
+	
 	
 	
 	semdomConfigApi.getConfigurationData(function(result) { 
 		if(result.ok) {
 			$scope.config = result.data;
-			$scope.config.terms = []
+			semdomEditApi.editorDto(function(result) {
+				if (result.ok) {
+					$scope.terms = []
+					for (var i = 0; i < result.data.terms.length; i++) {
+						$scope.terms.push(
+							{
+								'key':  result.data.terms[i].key, 
+								'name':  result.data.terms[i].name,
+								'description': result.data.terms[i].description, 
+								'nameTrans': result.data.terms[i].nameTrans, 
+								'descriptionTrans': result.data.terms[i].descriptionTrans, 
+								'comments': result.data.terms[i].comments, 
+							'selected': (result.data.terms[i].key in $scope.config.terms)
+							});
+					}
+				}
+			});
 		}
 	});
 	
