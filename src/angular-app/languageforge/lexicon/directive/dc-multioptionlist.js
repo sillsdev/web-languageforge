9787e859b4f3b59d0b958b28486453d60451141d
irahopkinson <ira.hopkinson@gmail.com>
2014-11-24 17:04:06 +0700

    fix multioptionlist when adding new items. Add delete items.

--- src/angular-app/languageforge/lexicon/directive/dc-multioptionlist.js
+++ src/angular-app/languageforge/lexicon/directive/dc-multioptionlist.js
@@ -2,7 +2,7 @@
 
 angular.module('palaso.ui.dc.multioptionlist', [])
 
-// Palaso UI Optionlist
+// Palaso UI Multioptionlist
 .directive('dcMultioptionlist', [function() {
   return {
     restrict: 'E',
@@ -14,6 +14,9 @@ angular.module('palaso.ui.dc.multioptionlist', [])
       items: "="
     },
     controller: ['$scope', function($scope) {
+      $scope.isAdding = false;
+      $scope.valueToBeDeleted = '';
+
       $scope.getDisplayName = function getDisplayName(value) {
         var displayName = value;
         if (angular.isDefined($scope.items)) {
@@ -27,14 +30,44 @@ angular.module('palaso.ui.dc.multioptionlist', [])
         return displayName;
       };
 
+      $scope.orderItemsByListOrder = function orderItemsByListOrder(value) {
+        if (angular.isDefined($scope.items)) {
+          return $scope.items.map(function(i) {return i.value;}).indexOf(value);
+        }
+        return -1;
+      };
+
+      $scope.filterSelectedItems = function filterSelectedItems(item) {
+        return $scope.model.values.indexOf(item.value) == -1;
+      };
+
+      $scope.showAddButton = function showAddButton() {
+        if (angular.isDefined($scope.items) && !$scope.isAdding && $scope.model.values.length < $scope.items.length) {
+          return true;
+        }
+        return false;
+      };
+
       $scope.addValue = function addValue() {
         if (angular.isDefined($scope.newValue)) {
           $scope.model.values.push($scope.newValue);
         }
+        $scope.newValue = "";
+        $scope.isAdding = false;
       };
 
-    }],
-    link: function(scope, element, attrs, controller) {
-    }
+      $scope.showDeleteButton = function showDeleteButton(valueToBeDeleted, value) {
+        if (angular.isDefined($scope.items) && $scope.control.state == 'edit' && $scope.control.rights.canEditEntry()) {
+          return valueToBeDeleted == value;
+        }
+        return false;
+      };
+
+      $scope.deleteValue = function deleteValue(value) {
+        var index = $scope.model.values.indexOf(value);
+        $scope.model.values.splice(index, 1);
+      };
+
+    }]
   };
 }]);
