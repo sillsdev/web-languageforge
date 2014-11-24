e220f5c37db37fd69f32761ab46fd548cde65b41
irahopkinson <ira.hopkinson@gmail.com>
2014-11-24 13:58:19 +0700

    fix accessing item when undefined, reformat to style guides

--- src/angular-app/languageforge/lexicon/directive/dc-multioptionlist.js
+++ src/angular-app/languageforge/lexicon/directive/dc-multioptionlist.js
@@ -1,36 +1,40 @@
-angular.module('palaso.ui.dc.multioptionlist', [])
-  // Palaso UI Optionlist
-  .directive('dcMultioptionlist', [function() {
-		return {
-			restrict : 'E',
-			templateUrl : '/angular-app/languageforge/lexicon/directive/dc-multioptionlist.html',
-			scope : {
-				config : "=",
-				model : "=",
-				control: "=",
-                items: "="
-			},
-			controller: ['$scope', function($scope) {
-				$scope.getDisplayName = function(value) {
-                    var displayName = value;
-                    for (var i=0; i< $scope.items.length; i++) {
-                        if ($scope.items[i].key == value) {
-                            displayName = $scope.items[i].value;
-                            break;
-                        }
-                    }
-					return displayName;
-				};
-				
-				$scope.addValue = function() {
-					if (angular.isDefined($scope.newValue)) {
-						$scope.model.values.push($scope.newValue);
-					}
-				};
+'use strict';
 
-			}],
-			link : function(scope, element, attrs, controller) {
-			}
-		};
-  }])
-  ;
+angular.module('palaso.ui.dc.multioptionlist', [])
+
+// Palaso UI Optionlist
+.directive('dcMultioptionlist', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/directive/dc-multioptionlist.html',
+    scope: {
+      config: "=",
+      model: "=",
+      control: "=",
+      items: "="
+    },
+    controller: ['$scope', function($scope) {
+      $scope.getDisplayName = function getDisplayName(value) {
+        var displayName = value;
+        if (angular.isDefined($scope.items)) {
+          for (var i = 0; i < $scope.items.length; i++) {
+            if ($scope.items[i].key == value) {
+              displayName = $scope.items[i].value;
+              break;
+            }
+          }
+        }
+        return displayName;
+      };
+
+      $scope.addValue = function addValue() {
+        if (angular.isDefined($scope.newValue)) {
+          $scope.model.values.push($scope.newValue);
+        }
+      };
+
+    }],
+    link: function(scope, element, attrs, controller) {
+    }
+  };
+}]);
