c24152294d49345807f2c4a5fbc9a69db1f0d155
Patrick Hess <tuxyrulez@gmail.com>
2014-12-01 20:50:59 -0500

    Temporary hacky solution to modals on semdom setup page
    Former-commit-id: 7245cba724fdfa24650b3936b7d75afe59691aa3

--- src/angular-app/languageforge/semdomtrans/views/projectSetup.js
+++ src/angular-app/languageforge/semdomtrans/views/projectSetup.js
@@ -6,6 +6,10 @@ angular.module('semdomtrans.projectSetup', ['jsonRpc', 'ui.bootstrap', 'bellows.
 function($scope, $state, semdomSetupApi, sessionService, modal, notice) {
 	$scope.canJoin = false;
 	$scope.canCreate = false;
+	$scope.modalShowing = 0;
+	$scope.selectedID = -1;
+	$scope.modalTitle = "";
+	$scope.modalBody = "";
 	semdomSetupApi.getStartedProjects(function(result) {
 		if (result.ok) {
 			$scope.startedProjects = result.data;
@@ -18,30 +22,51 @@ function($scope, $state, semdomSetupApi, sessionService, modal, notice) {
 		}
 	});
 		
-	$scope.clickCreate =  function(project) {
-			
-	    var deletemsg = "Are you sure you want to create a semdom translation project into " + project.language;
-	    modal.showModalSimple('Create Project', deletemsg, 'Cancel', 'Create Project').then(function() {
+	$scope.showModal = function(project, option) {
+		$scope.selectedProject = project;
+		$scope.modalShowing = option;
+		
+		if (option == 1) {
+			$scope.modalTitle = "Join Project";
+			$scope.modalBody = "Do you want to submit a request to join the " + project.language + " project?"
+		} else if (option == 2) {
+			$scope.modalTitle = "Create Project";
+			$scope.modalBody = "Do you want to create the " + project.language + " project?"
+		}
+	}
+	
+	$scope.modalHide = function() {
+		$scope.modalShowing = 0;
+	}
+	
+	$scope.modalAccept = function() {
+		if ($scope.modalShowing == 1) {
+			$scope.submitJoinRequest();
+		}
+		else if ($scope.modalShowing == 2) {
+			$scope.createProject();
+		}
+		
+		$scope.modalShowing = 0;
+	}
+	
+	$scope.createProject =  function() {
+		var id = $scope.selectedProject.id;
+		semdomSetupApi.createProject(0, id, 1, function(result) {
+			if (result.ok && result.success) {
+	 	 		$state.go("edit", {'source' : 0, 'target': id});
+		 	}
+		});
 
-			semdomSetupApi.createProject(0, project.id, 1, function(result) {
-				if (result.ok && result.success) {
-		 	 		$state.go("edit", {'source' : 0, 'target': project.id});
-			 	}
-			});
-	    });
 	};
 	
-	$scope.clickJoin =  function(project) {
-		
-	    var deletemsg = "Do you want to submit a request to join semdom translation project into " + project.language;
-	    modal.showModalSimple('Join Project', deletemsg, 'Cancel', 'Submit Join Request').then(function() {
-
-			semdomSetupApi.createProject(0, project.id, 1, function(result) {
-				if (result.ok && result.success) {
-		 	 		$state.go("edit", {'source' : 0, 'target': project.id});
-			 	}
-			});
-	    });
+	$scope.submitJoinRequest = function() {
+		var id = $scope.selectedProject.id;
+		semdomSetupApi.createProject(0, id, 1, function(result) {
+			if (result.ok && result.success) {
+	 	 		$state.go("edit", {'source' : 0, 'target': id});
+		 	}
+		});
 	};
 	
 }]);
