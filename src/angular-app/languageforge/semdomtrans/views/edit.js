84a224d088571986e939ca1795423c8216fd3031
Patrick Hess <tuxyrulez@gmail.com>
2014-12-01 23:11:31 -0500

    Have one object store the semantic term and question (they are tighly coupled and separating them makes some logic more complicated)

--- src/angular-app/languageforge/semdomtrans/views/edit.js
+++ src/angular-app/languageforge/semdomtrans/views/edit.js
@@ -4,30 +4,21 @@ angular.module('semdomtrans.edit', ['jsonRpc', 'ui.bootstrap', 'bellows.services
 // DBE controller
 .controller('editCtrl', ['$scope', '$stateParams', 'semdomtransEditService',  'sessionService', 'modalService', 'silNoticeService',
 function($scope, $stateParams, semdomEditApi, sessionService, modal, notice) {
-	$scope.terms = [];
-	$scope.questions = [];
+	$scope.items = [];
 	$scope.selectedTab = 0;
 	$scope.tabDisplay = {"val": '0'};
 	$scope.domainsFiltered = [];
 	
 	semdomEditApi.editorDto($stateParams.source, $stateParams.target, function(result) {
 		if (result.ok) {
-			$scope.terms = result.data.terms;
-			for (var i = 0; i < $scope.terms.length; i++) {
-				if ($scope.terms[i].key.length == 1) {
-					$scope.domainsFiltered.push($scope.terms[i]);
+			$scope.items = result.data.items;
+			for (var i = 0; i < $scope.items.length; i++) {
+				if ($scope.items[i].key.length == 1) {
+					$scope.domainsFiltered.push($scope.items[i]);
 				}
 			}
 			
-			$scope.currentTerm = $scope.terms[0];
-			$scope.allQuestions = result.data.questions;
-			for (var i = 0; i < $scope.allQuestions.length; i++) {
-				var question = $scope.allQuestions[i];
-			    if (question.key == $scope.currentTerm.key) {
-			    	$scope.currentTermQuestions = question;
-			    	break;
-			    }
-			}
+			$scope.currentItem = $scope.items[0];			
 			
 		}
 	});
@@ -38,39 +29,20 @@ function($scope, $stateParams, semdomEditApi, sessionService, modal, notice) {
 	}
 	
 	$scope.getPreviousQuestion = function() {
-		$scope.currentTermQuestions.position--;
+		$scope.currentItem.questions.position--;
 	}
 	
 	$scope.getNextQuestion = function() {
-		$scope.currentTermQuestions.position++;
-	}
-		
-	$scope.updateDisplay = function(domainKey) {
-		var termChanged = $scope.terms[domainKey];
-		var display = termChanged.display;
-		
-		for (var i = 0; i < $scope.terms.length; i++) {
-			if ($scope.terms[i].key.substring(0, domainKey.length) == domainKey) {
-				$scope.terms[i].display = false;
-			}
-		}
-	}
+		$scope.currentItem.questions.position++;
+	}		
 	
 	$scope.changeTerm = function(key) {
-			for (var i = 0; i < $scope.terms.length; i++) {
-				if ($scope.terms[i].key == key) {
-					$scope.currentTerm = $scope.terms[i];
+			for (var i = 0; i < $scope.items.length; i++) {
+				if ($scope.items[i].key == key) {
+					$scope.currentItem = $scope.items[i];
 					break;
 				}
-			}
-			
-			for (var i = 0; i < $scope.allQuestions.length; i++) {
-				var question = $scope.allQuestions[i];
-			    if (question.key == $scope.currentTerm.key) {
-			    	$scope.currentTermQuestions = question;
-			    	break;
-			    }
-			}
+			}			
     }
 	
 }]);
