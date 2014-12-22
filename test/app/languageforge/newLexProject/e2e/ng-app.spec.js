af10a088f0d87924f539bf4f8fc13d19b9c751f7
irahopkinson <ira.hopkinson@gmail.com>
2014-12-22 12:15:16 +0700

    attempt to fix dev box E2E timing error
    Element is not clickable at point (755, 77). Other element would receive the click: <div class="animate-repeat alert ng-isolate-scope alert-error" ng-class="type &amp;&amp; &quot;alert-&quot; + type" type="notice.type" close="closeNotice(notice.id)">...</div>
    
    
    Former-commit-id: 25d2a06c984cacf2e6b492917c0ba73406bbfe41

--- test/app/languageforge/newLexProject/e2e/ng-app.spec.js
+++ test/app/languageforge/newLexProject/e2e/ng-app.spec.js
@@ -248,7 +248,7 @@ describe('E2E testing: New Lex Project wizard app', function() {
     it('can go to lexicon', function() {
       
       // wait for animation to finish
-      browser.sleep(2000);
+      browser.sleep(3000);
       expect(page.nextButton.isDisplayed()).toBe(true);
       expect(page.nextButton.isEnabled()).toBe(true);
       page.nextButton.expectFormIsValid();
