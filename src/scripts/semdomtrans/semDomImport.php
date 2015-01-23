1f9e29b114b3f52f1d2a30cde9406f52b0a5ac1f
Patrick Hess <tuxyrulez@gmail.com>
2015-01-23 14:55:06 +0700

    Initial attempts to parse xml file for semdom project

--- src/scripts/semdomtrans/semDomImport.php
+++ src/scripts/semdomtrans/semDomImport.php
@@ -4,6 +4,9 @@ require_once('../scriptConfig.php');
 use models\ProjectListModel;
 
 
+$index = 0;
+
+
 $projectList = new ProjectListModel();
 $projectList->read();
 
@@ -12,7 +15,23 @@ $projectList->read();
 $changeDatabase = false;
 
 // process xml into a php data structure, organized by language
+$xml = simplexml_load_file($argv[1]);
+$possibilities = $xml->SemanticDomainList->CmPossibilityList->Possibilities->children();
+//$echo $possibilities;
+$xmlElements = [];
+foreach($possibilities as $possibility) {
+	echo $possibility->Abbreviation->AUni . ": " . $possibility->Name->AUni;
+	echo "\n";
+	$children = $possibility->SubPossibilities->children();
+	
+	foreach($children as $p) {
+		array_push($possibilities, $p);
+	}
+}
 
+
+
+echo "\n";
 // loop over the set of languages to import
 
 // verify that no project for that language exists
@@ -27,11 +46,11 @@ $changeDatabase = false;
 
 
 
-foreach ($projectList->entries as $p) {
+/*foreach ($projectList->entries as $p) {
 	print $p['projectName'] . "\n";
 }
 
 print "\n";
-
+*/
 
 ?>
