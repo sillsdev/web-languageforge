8c6d2b47e99bf172503c728e3e208de3ccbdaa77
Patrick Hess <tuxyrulez@gmail.com>
2015-01-31 12:50:23 -0500

    Add questions to model for edit page dto test
    Former-commit-id: 0565eecb913b25a62d7fd1c2e6d2f815e6601de6

--- test/php/languageforge/semdomtrans/dto/SemDomTransEditDto_Test.php
+++ test/php/languageforge/semdomtrans/dto/SemDomTransEditDto_Test.php
@@ -5,6 +5,8 @@ use models\languageforge\SemDomTransProjectModel;
 use models\languageforge\semdomtrans\SemDomTransTranslatedForm;
 use models\languageforge\semdomtrans\dto\SemDomTransEditDto;
 use models\languageforge\semdomtrans\SemDomTransItemListModel;
+use models\languageforge\semdomtrans\SemDomTransQuestion;
+use models\mapper\ArrayOf;
 
 require_once dirname(__FILE__) . '/../../../TestConfig.php';
 require_once SimpleTestPath . 'autorun.php';
@@ -42,11 +44,23 @@ class TestSemDomTransEditDto extends UnitTestCase
         $sourceItemModel = new SemDomTransItemModel($sourceProject);
         $sourceItemModel->key = "1";
         $sourceItemModel->name = new SemDomTransTranslatedForm("universe");
-        $sourceItemModel->write();
+        $sourceItemModel->description = new SemDomTransTranslatedForm("Universe description");
+        $sq = new SemDomTransQuestion("A universe question", "A universe question term");
+        $sourceItemModel->questions = new ArrayOf(function ($data) {
+        	return new SemDomTransQuestion();
+        });      
+        $sourceItemModel->questions[] = $sq;
+        $sourceItemModel->write();       
         
         $targetItemModel = new SemDomTransItemModel($targetProject);
         $targetItemModel->key = "1";
         $targetItemModel->name = new SemDomTransTranslatedForm("wszechswiat");
+        $targetItemModel->description = new SemDomTransTranslatedForm("Opis wszechswiata");
+        $tq = new SemDomTransQuestion("Pytanie wszechswiata", "Termin zwiazany z wszechswiatem");
+        $targetItemModel->questions = new ArrayOf(function ($data) {
+        	return new SemDomTransQuestion();
+        });
+        $targetItemModel->questions[] = $tq;
         $targetItemModel->write();           
         
         // call dto
@@ -55,17 +69,37 @@ class TestSemDomTransEditDto extends UnitTestCase
         $loadSourceProject = new SemDomTransProjectModel($loadTargetProject->sourceLanguageProjectId);
         $result = SemDomTransEditDto::encode($prId->asString(), null);
         
+        //print_r($result);
         // check dto returns expected results
          $items = $result["items"];
          $this->assertTrue($items != null); 
          $this->assertTrue(count($items) > 0);
+         
          $firstObject = $items[0];
-         $this->assertTrue($firstObject["key"] != null);
-         $this->assertTrue($firstObject["key"] == "1");
-         $this->assertTrue($firstObject["name"] != null);
-         $this->assertTrue($firstObject["name"]["source"] == "universe");
-         $this->assertTrue($firstObject["name"]["translation"] == "wszechswiat");
-         $this->assertTrue($firstObject["name"]["status"] == 0);
+         
+         $this->assertNotEqual($firstObject["key"], null);
+         $this->assertEqual($firstObject["key"], "1");
+         
+         
+         $this->assertNotEqual($firstObject["name"], null);
+         $this->assertEqual($firstObject["name"]["source"], "universe");
+         $this->assertEqual($firstObject["name"]["translation"], "wszechswiat");
+         $this->assertEqual($firstObject["name"]["status"], 0);
+         
+         $this->assertNotEqual($firstObject["description"], null);
+         $this->assertEqual($firstObject["description"]["source"], "Universe description");
+         $this->assertEqual($firstObject["description"]["translation"], "Opis wszechswiata");
+         $this->assertEqual($firstObject["description"]["status"], 0);
+         
+         
+         $this->assertNotEqual($firstObject["questions"], null);
+         $this->assertNotEqual($firstObject["questions"][0], null);
+         $this->assertNotEqual($firstObject["questions"][0]["question"], null);
+         $this->assertNotEqual($firstObject["questions"][0]["terms"], null);
+         $this->assertEqual($firstObject["questions"][0]["question"]["source"], "A universe question");
+         $this->assertEqual($firstObject["questions"][0]["question"]["translation"], "Pytanie wszechswiata");
+         $this->assertEqual($firstObject["questions"][0]["terms"]["source"], "A universe question term");
+         $this->assertEqual($firstObject["questions"][0]["terms"]["translation"], "Termin zwiazany z wszechswiatem");
          
          // clean-up
          // TODO: create a custom semdomtrans mongo environment
