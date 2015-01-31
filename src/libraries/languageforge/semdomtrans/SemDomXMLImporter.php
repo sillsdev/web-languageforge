a8d863c42ef4d876bf9c35e8988aea9b31eee6af
Patrick Hess <tuxyrulez@gmail.com>
2015-01-31 08:10:56 -0500

    Add parsing of description, questions and search keys to SemDomXMLImporter

--- src/libraries/languageforge/semdomtrans/SemDomXMLImporter.php
+++ src/libraries/languageforge/semdomtrans/SemDomXMLImporter.php
@@ -6,6 +6,7 @@ use models\languageforge\semdomtrans\SemDomTransTranslatedForm;
 use models\languageforge\SemDomTransProjectModel;
 
 use models\languageforge\semdomtrans\SemDomTransItemModel;
+use models\languageforge\semdomtrans\SemDomTransQuestion;
 
 class SemDomXMLImporter {
 	
@@ -37,29 +38,52 @@ class SemDomXMLImporter {
 		}
 	}
 
+	public function _getPathVal($xmlPath) {
+		if ($xmlPath) {
+			$val = (string)$xmlPath[0];
+		} else {
+			$val= "";
+		}
+		return $val;
+	}
+	
 	private function _processDomainNode($domainNode) {
 		$guid = (string)$domainNode['guid'];
-		$xpathResult = $domainNode->xpath("Name/AUni[@ws='{$this->_lang}']");
-		if ($xpathResult) {
-			$name = (string)$xpathResult[0];
-		} else {
-			$name = "";
-			print_r($xpathResult);
-		}
+		$name = $this->_getPathVal($domainNode->xpath("Name/AUni[@ws='{$this->_lang}']"));
+		
 		$abbreviation = (string)$domainNode->xpath("Abbreviation/AUni[@ws='{$this->_lang}']")[0];
 
+		$description = (string) $domainNode->xpath("Description/AStr[@ws='{$this->_lang}']")[0]->xpath("Run[@ws='{$this->_lang}']")[0];
+		
+		$questionsXML = $domainNode->Questions->children();
+		
+		$questions = [];
+		$searchKeys = [];
+	
+		// parse nested questions
+		foreach($questionsXML as $questionXML) {
+			$question = $this->_getPathVal($questionXML->xpath("Question/AUni[@ws='{$this->_lang}']"));
+			$terms = $this->_getPathVal($questionXML->xpath("ExampleWords/AUni[@ws='{$this->_lang}']"));
+			array_push($questions, new SemDomTransQuestion($question, $terms));
+			array_push($searchKeys, new SemDomTransTranslatedForm($terms));
+		}				
+		
 		// assume that we are not matching up with guids
 		$itemModel = new SemDomTransItemModel($this->_projectModel);
 		$itemModel->readByProperty('xmlGuid', $guid);
 		
 		$itemModel->name = new SemDomTransTranslatedForm($name);
 		$itemModel->key = new SemDomTransTranslatedForm($abbreviation);
+		$itemModel->description = new SemDomTransTranslatedForm($description);
+		$itemModel->questions = $questions;
+		$itemModel->searchKeys = $searchKeys;
 		
 		if ($this->_runForReal) {
 			$itemModel->write();
 		}
 		print "Processed $abbreviation $name\n";
 		
+		// recurse on sub-domains
 		if (property_exists($domainNode, 'SubPossibilities')) {
 			foreach ($domainNode->SubPossibilities->children() as $subDomainNode) {
 				$this->_processDomainNode($subDomainNode);
