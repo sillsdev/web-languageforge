112c514eebab27ddce770d60fb4adda8b0c7cf20
irahopkinson <ira.hopkinson@gmail.com>
2015-02-13 14:58:04 +0700

    fix feature/customFieldImport: status multi option list label restored

--- src/models/languageforge/lexicon/config/LexConfiguration.php
+++ src/models/languageforge/lexicon/config/LexConfiguration.php
@@ -353,8 +353,8 @@ class LexConfiguration
         $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::SENSEIMPORTRESIDUE]->inputSystems[] = 'en';
 
         $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::STATUS] = new LexiconMultiOptionlistConfigObj();
-        $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::STATUS]->label = LexiconConfigObj::flexOptionlistName($listCode);
         $listCode = LexiconConfigObj::flexOptionlistCode(LexiconConfigObj::STATUS);
+        $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::STATUS]->label = LexiconConfigObj::flexOptionlistName($listCode);
         $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::STATUS]->listCode = $listCode;
         $this->entry->fields[LexiconConfigObj::SENSES_LIST]->fields[LexiconConfigObj::STATUS]->hideIfEmpty = true;
 
