2e48b6051363cf59e7c045a12712c567556b1886
irahopkinson <ira.hopkinson@gmail.com>
2015-02-19 16:06:08 +0700

    feature/textEditor: update production comments for Text Angular and Angular File Upload
    Former-commit-id: 4aa7c291aa073d84e67e5c073b6b3280902211f4

--- src/views/shared/angular-app.html.php
+++ src/views/shared/angular-app.html.php
@@ -7,14 +7,16 @@ window.session = <?php echo $jsonSession; ?>;
 </script>
 
 <?php if (SF_USE_MINIFIED_JS): ?>
+    <!-- Angular File Upload only has v1.6.1, v2.2.2, v3.0+ on CDN. Since we use v1.6.7, keep it in the repo. IJH 2015-02 -->
     <script src="/vendor_bower/angularjs-file-upload/angular-file-upload-html5-shim.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-animate.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-route.min.js"></script>
-<!--     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-sanitize.min.js"></script> -->
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-animate.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-route.min.js"></script>
+    <!-- Text Angular has its own version of sanitize, see https://github.com/fraywing/textAngular#where-to-get-it
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-sanitize.min.js"></script> -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.10/angular-ui-router.min.js"></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular-sanitize.min.js'></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular.min.js'></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular-sanitize.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/textAngular/1.2.2/textAngular.min.js"></script>
     <script src="/vendor_bower/angularjs-file-upload/angular-file-upload.min.js"></script>
     <script src="/js/lib/angular-translate_2.2.0/angular-translate.min.js"></script>
     <script src="/js/lib/angular-translate_2.2.0/angular-translate-loader-static-files.min.js"></script>
@@ -27,11 +29,12 @@ window.session = <?php echo $jsonSession; ?>;
     <script src="/js/lib/angular_stable_1.2.15/angular.js"></script>
     <script src="/js/lib/angular_stable_1.2.15/angular-animate.js"></script>
     <script src="/js/lib/angular_stable_1.2.15/angular-route.js"></script>
-<!--     <script src="/js/lib/angular_stable_1.2.15/angular-sanitize.js"></script> -->
+    <!-- Text Angular has its own version of sanitize, see https://github.com/fraywing/textAngular#where-to-get-it
+    <script src="/js/lib/angular_stable_1.2.15/angular-sanitize.js"></script> -->
     <script src="/js/lib/angular-ui-router_0.2.10/angular-ui-router.js"></script>
-    <script src='/vendor_bower/textAngular/src/textAngular-sanitize.js'></script>
-    <script src='/vendor_bower/textAngular/src/textAngularSetup.js'></script>
-    <script src='/vendor_bower/textAngular/src/textAngular.js'></script>
+    <script src="/vendor_bower/textAngular/src/textAngular-sanitize.js"></script>
+    <script src="/vendor_bower/textAngular/src/textAngularSetup.js"></script>
+    <script src="/vendor_bower/textAngular/src/textAngular.js"></script>
     <script src="/vendor_bower/angularjs-file-upload/angular-file-upload.js"></script>
     <script src="/js/lib/angular-translate_2.2.0/angular-translate.js"></script>
     <script src="/js/lib/angular-translate_2.2.0/angular-translate-loader-static-files.js"></script>
