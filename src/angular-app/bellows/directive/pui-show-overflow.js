82a5afb163bb2552d1a3591f7a49e0c6ea78530e
irahopkinson <ira.hopkinson@gmail.com>
2015-02-20 11:04:33 +0700

    fix overflow not showing in some multitexts

--- src/angular-app/bellows/directive/pui-show-overflow.js
+++ src/angular-app/bellows/directive/pui-show-overflow.js
@@ -10,20 +10,23 @@ angular.module('palaso.ui.showOverflow', [])
         ngBindHtml: '='
       },
       link: function (scope, element, attrs) {
-        
         var updateTitle = function updateTitle(newValue) {
-          if (element[0].offsetHeight < element[0].scrollHeight ||
-              element[0].offsetWidth < element[0].scrollWidth) {
-            // make multiline and strip html tags
-            var multiline = newValue.replace("</p><p>", "\r\n");
-            attrs.$set('title', multiline.replace(/<[^>]+>/gm, ''));
-          } else {
-            attrs.$set('title', '');
+          if (angular.isDefined(newValue) && 
+              (element[0].offsetHeight > 0 || element[0].offsetWidth > 0)) {
+            if (element[0].offsetHeight < element[0].scrollHeight ||
+                element[0].offsetWidth < element[0].scrollWidth) {
+              // make multiline and strip html tags
+              var multiline = newValue.replace("</p><p>", "\r\n");
+              attrs.$set('title', multiline.replace(/<[^>]+>/gm, ''));
+            } else {
+              attrs.$set('title', '');
+            }
           }
         };
+        
         if (angular.isDefined(scope.ngBindHtml)) {
           scope.$watch('ngBindHtml', updateTitle);
-        } else {
+        } else if (angular.isDefined(scope.ngModel)) {
           scope.$watch('ngModel', updateTitle);
         }
       } 
