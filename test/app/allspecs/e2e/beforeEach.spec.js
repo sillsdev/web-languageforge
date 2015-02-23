b5228dd87ec6804513ea8f7307a9e60f213cb9d2
Chris Hirt <chris_hirt@sil.org>
2015-02-23 17:05:46 +0700

    added afterEach expectation in protractor tests to check for JS console errors

--- test/app/allspecs/e2e/beforeEach.spec.js
+++ test/app/allspecs/e2e/beforeEach.spec.js
@@ -6,5 +6,20 @@ afterEach(function() {
   //browser.ignoreSyncronization = true;
   expect(appFrame.errorMessage.isPresent()).toBe(false);
   expect(body.phpError.isPresent()).toBe(false);
+
+  // output JS console errors and fail tests
+  browser.manage().logs().get('browser').then(function(browserLog) {
+    if (browserLog.length > 0) {
+      expect(browserLog.length).toBe(0); // throw an exception
+      for (var i=0;i<browserLog.length;i++) {
+        var message = browserLog[i].message;
+        if (message.indexOf('\n') != -1) {
+          message = message.substring(0, message.indexOf('\n'));
+        }
+        console.log(message);
+        console.log('');
+      }
+    }
+  });
   //browser.ignoreSyncronization = false;
 });
