5923bd19d15b0ac3aab378809976475b808fc0fe
Chris Hirt <chris_hirt@sil.org>
2015-02-26 10:56:53 +0700

    javascript console error testing improvements (attempting to get rid of false positives)

--- test/app/allspecs/e2e/beforeEach.spec.js
+++ test/app/allspecs/e2e/beforeEach.spec.js
@@ -8,25 +8,37 @@ afterEach(function() {
     if (isPresent) {
       appFrame.errorMessage.getText().then(function(message) {
         if (message.indexOf('Oh. Exception') != -1) {
-          console.log("Browser Console JS Error: " + message);
+          console.log("PHP API error on this page");
+          console.log(message);
           expect(true).toBe(false); // fail the test
         }
       });
     }
   });
-  expect(body.phpError.isPresent()).toBe(false);
+  body.phpError.isPresent().then(function(isPresent) {
+    if (isPresent) {
+      console.log("PHP Error present on this page");
+      expect(true).toBe(false); // fail the test
+    }
+  });
 
   // output JS console errors and fail tests
   browser.manage().logs().get('browser').then(function(browserLog) {
     if (browserLog.length > 0) {
-      expect(true).toBe(false); // fail the test
       for (var i=0;i<browserLog.length;i++) {
         var message = browserLog[i].message;
         if (message.indexOf('\n') != -1) {
           message = message.substring(0, message.indexOf('\n'));
         }
-        console.log(message);
-        console.log('');
+        if (/angular\.js .* TypeError: undefined is not a function/.test(message)) {
+          // we ignore errors of this type caused by Angular being unloaded prematurely on page refreshes (since it's not a real error)
+
+        } else {
+          console.log("Browser Console JS Error: " + message);
+          console.log('');
+          expect(true).toBe(false); // fail the test
+        }
+
       }
     }
   });
