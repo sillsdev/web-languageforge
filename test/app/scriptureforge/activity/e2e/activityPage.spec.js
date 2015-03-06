94facfe45ffcfaac1db8fd6a8ed86b917d178919
irahopkinson <ira.hopkinson@gmail.com>
2015-03-06 13:42:42 +0700

    refactor activity E2E test to show each action verified
    Former-commit-id: 44a515fd789a0e7046e98a834cb8de0c9170ee5a

--- test/app/scriptureforge/activity/e2e/activityPage.spec.js
+++ test/app/scriptureforge/activity/e2e/activityPage.spec.js
@@ -9,17 +9,16 @@ var projectSettingsPage = require('../../sfchecks/pages/projectSettingsPage');
 var questionPage        = require('../../sfchecks/pages/questionPage');
 var textPage            = require('../../sfchecks/pages/textPage');
 
-// Script of actions to perform which will then be verified on the activity feed.
-// Currently, this list assumes test normal user has the role permissions for the actions
-// scope options:  {'answers', 'comments'}
-// action options: {'add', 'addToLastAnswer', 'edit', 'upvote', 'archive'}
-// value: normally free text.  For adding questions, the value is the question summary.
-//        Value can also be an integer when used as 0-based index into the answers.list
-//        If value is left blank, then perform the action on the last item
+/* Script of actions to perform which will then be verified on the activity feed.
+ * These actions are performed by both normal user and manager user.
+ * Currently, this list assumes test normal user has the role permissions for the actions.
+ * scope options:  {'answers', 'comments'}
+ * action options: {'add', 'addToLastAnswer', 'edit', 'upvote', 'archive'}
+ * value: normally free text.  For adding questions, the value is the question summary.
+ *        Value can also be an integer when used as 0-based index into the answers.list
+ *        If value is left blank, then perform the action on the last item
+ */
 var script = [
-  
-  // These actions are performed by both normal user and manager user
-
   {scope: 'answers',   action: 'add',              value: 'Beethoven was the speaker.'},
   {scope: 'comments',  action: 'addToLastAnswer',  value: 'This is an original comment.'},
   {scope: 'comments',  action: 'edit',             value: 'This is an edited comment for the E2E test.'},
@@ -27,24 +26,24 @@ var script = [
   {scope: 'answers',   action: 'upvote',           value: 1},
   {scope: 'answers',   action: 'downvote',         value: 1},
   /* 
-  // archiving a comment/answer at a specific answer works, but the verification process doesn't
-  // know which indices were removed.
-  // Also, normal user isn't guaranteed to have permissions to archive answer at index 1
-  //{scope: 'comments',  action: 'archive',          value: 1},
-  //{scope: 'answers',   action: 'archive',          value: 1},
-  */
+   * archiving a comment/answer at a specific answer works, but the verification process doesn't
+   * know which indices were removed.
+   * Also, normal user isn't guaranteed to have permissions to archive answer at index 1
+   * {scope: 'comments',  action: 'archive',          value: 1},
+   * {scope: 'answers',   action: 'archive',          value: 1},
+   */
   {scope: 'comments',  action: 'archive',          value: ''},
   {scope: 'answers',   action: 'archive',          value: ''},
   {scope: 'texts',     action: 'add',              value: constants.testText3Title},
   {scope: 'questions', action: 'add',              value: constants.testText1Question3Summary},
-  // Adding a new username should be lower-case
-  {scope: 'users',     action: 'add',              value: 'jimmycricket'},
+  {scope: 'users',     action: 'add',              value: 'jimmycricket'}
 ];
 
 // Array of test usernames to test Activity page with different roles
-var usernames = [constants.memberUsername,
-                 constants.managerUsername
-    ];
+var usernames = [
+    constants.memberUsername,
+    constants.managerUsername
+  ];
 
 // Utility function to determine if the current user will have the role permissions to do the scope/action
 var isAllowed = function(scope, username) {
@@ -114,8 +113,7 @@ describe('Activity Page E2E Test', function() {
                 break;
               case 'questions' :
                 browser.navigate().back();
-                textPage.addNewQuestion(constants.testText1Question3Title,
-                                script[i].value);
+                textPage.addNewQuestion(constants.testText1Question3Title, script[i].value);
                 browser.navigate().forward();
                 break;
               case 'users' :
@@ -141,77 +139,84 @@ describe('Activity Page E2E Test', function() {
         });
       });
       
-      it ('Verify actions appear on the activity feed', function() {
+      it('goto Activity Page to verfy actions', function() {
         
-        // Now check the activity feed.  Current items are at the head
-        // of the activity feed so traverse the script in reverse order
         activityPage.get();
-        var scriptIndex = script.length - 1;
-        var activityIndex = 0;
         
         // Print everything in the activity list for debugging purposes
-        //activityPage.printActivitiesNames();
-        
-        while (scriptIndex >= 0) {
-          
-          // Skip verifying the following actions/scope because they don't appear in the
-          // activity feed or because normal user doesn't have role permissions to do them
-          if ( (script[scriptIndex].action == 'archive') || 
-               (script[scriptIndex].action == 'downvote') || 
-             (!isAllowed(script[scriptIndex].scope, expectedUsername)) ) {
-            scriptIndex--;
-            continue;
-          }
-          
-          // Expect some combinations of username, script scope, action, and value
-          // to appear in the activity feed
-          
-          // Expectation for the subject in the activity page text
-          var activityText = activityPage.getActivityText(activityIndex);
-          switch (script[scriptIndex].scope) {
-            case 'texts' :
-              expect(activityText).toContain(constants.testProjectName);
-              break;
-            case 'questions' :
-              expect(activityText).toContain(constants.testText1Title);
-              break;
-            case 'users' :
-              expect(activityText).toContain(script[scriptIndex].value);
-              break;
-            default :
-              expect(activityText).toContain(expectedUsername);
-          };
-          
-          // Truncate the ending 's' of the Scope string to partially 
-          // match strings with different scope tenses
-          var expectedString = script[scriptIndex].scope;
-          expectedString = expectedString.replace(/s$/gi, '');
-          if ((expectedString != 'text') && (expectedString != 'user')) {
-            expect(activityText).toContain(expectedString);
-          };
-          
-          // Verify actions
-          if (script[scriptIndex].action == 'edit') {
-            expect(activityText).toContain('updated');
-          } else if (script[scriptIndex].action == 'upvote') {
-            expect(activityText).toContain('+1\'d');
-          };
-          
-          // Verify values  
-          if (typeof script[scriptIndex].value == 'string') {
-            
-            // 'User' value already checked above, so check for project membership string
-            if (script[scriptIndex].scope == 'users') {
-              expect(activityText).toContain('is now a member of ' + constants.testProjectName);
-            } else {
-              expect(activityText).toContain(script[scriptIndex].value);
-            };
-          };
-          
-          scriptIndex--;
-          activityIndex++;
-        };
+        // activityPage.printActivitiesNames();
       });
+      
+      /* Now check the activity feed.  Current items are at the head
+       * of the activity feed so traverse the script in reverse order
+       */
+      var scriptIndex = script.length - 1;
+      var activityIndex = 0;
+      while (scriptIndex >= 0) {
+        
+        /* Skip verifying the following actions/scope because they don't appear in the
+         * activity feed or because normal user doesn't have role permissions to do them
+         */
+        if ( (script[scriptIndex].action == 'archive') || 
+             (script[scriptIndex].action == 'downvote') || 
+           (!isAllowed(script[scriptIndex].scope, expectedUsername)) ) {
+          scriptIndex--;
+          continue;
+        }
+
+        // see http://stackoverflow.com/questions/21634558/looping-on-a-protractor-test-with-parameters
+        (function(currentScript, activityIndex) {
+          
+          // Expect some combinations of username, script scope, action, and value to appear in the activity feed
+          it ("Verify action '" + currentScript.action + "' on '" + currentScript.scope + "' appears on the activity feed", function() {
+            
+            // Expectation for the subject in the activity page text
+            var activityText = activityPage.getActivityText(activityIndex);
+            switch (currentScript.scope) {
+              case 'texts' :
+                expect(activityText).toContain(constants.testProjectName);
+                break;
+              case 'questions' :
+                expect(activityText).toContain(constants.testText1Title);
+                break;
+              case 'users' :
+                expect(activityText).toContain(currentScript.value);
+                break;
+              default :
+                expect(activityText).toContain(expectedUsername);
+            };
+            
+            // Truncate the ending 's' of the Scope string to partially match strings with different scope tenses
+            var expectedString = currentScript.scope;
+            expectedString = expectedString.replace(/s$/gi, '');
+            if ((expectedString != 'text') && (expectedString != 'user')) {
+              expect(activityText).toContain(expectedString);
+            };
+            
+            // Verify actions
+            if (currentScript.action == 'edit') {
+              expect(activityText).toContain('updated');
+            } else if (currentScript.action == 'upvote') {
+              expect(activityText).toContain('+1\'d');
+            };
+            
+            // Verify values  
+            if (typeof currentScript.value == 'string') {
+              
+              // 'User' value already checked above, so check for project membership string
+              if (currentScript.scope == 'users') {
+                expect(activityText).toContain('is now a member of ' + constants.testProjectName);
+              } else {
+                expect(activityText).toContain(currentScript.value);
+              };
+            };
+  
+          });
+        })(script[scriptIndex], activityIndex);
+
+        scriptIndex--;
+        activityIndex++;
+      };
       
       it ('Verify filters work on the activity page', function() {
         
@@ -225,6 +230,7 @@ describe('Activity Page E2E Test', function() {
             return text.indexOf(expectedUsername) == -1;
           }));
         }).then(function(activityItems) {
+          
           // Currently in "All Activity" mode, so should see items without our username
           expect(activityItems.length).toBeGreaterThan(0);
         });
@@ -259,8 +265,6 @@ describe('Activity Page E2E Test', function() {
           expect(activityItems.length).toBeGreaterThan(0);
         });
         
-        //browser.debugger();
-      
       });
     });
   });
