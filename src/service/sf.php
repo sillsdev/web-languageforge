9abcc8f5151930c90b9195e792ed3bc663ce2027
Chris Hirt <chris_hirt@sil.org>
2015-04-16 10:23:15 +0700

    wip ImportEnglishSemDomProject

--- src/service/sf.php
+++ src/service/sf.php
@@ -726,15 +726,18 @@ class sf
      * @param string $appName
      * @return string | boolean - $projectId on success, false if project code is not unique
      */
+
+    // 2015-04 CJH REVIEW: this method should be moved to the semdom project commands (and a test should be written around it).  This method should also assert that a project with that code does not already exist
     public function semdom_create_project($languageIsoCode)
-    {        
-        $projectName = "Semdom $languageIsoCode Project";
-        $projectCode = "semdom-$languageIsoCode-20";
+    {
+        $semdomVersion = 4;
+        $projectName = "($languageIsoCode) Semantic Domain Translation";
+        $projectCode = "semdom-$languageIsoCode-$semdomVersion";
         $projectID = ProjectCommands::createProject($projectName, $projectCode, LfProjectModel::SEMDOMTRANS_APP, $this->_userId, $this->_website);    
         
         $project = new SemDomTransProjectModel($projectID);
         $project->languageIsoCode = $languageIsoCode;
-        $project->semdomVersion = 20;
+        $project->semdomVersion = $semdomVersion;
         $project->write();
         
         SemDomTransProjectCommands::preFillProject($projectID);
