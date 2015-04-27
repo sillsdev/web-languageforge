be3c9f79e2751008b6f8e14d9b8f886f5efbfc55
Chris Hirt <chris_hirt@sil.org>
2015-04-27 14:12:10 +0700

    Revert "* Account for weird whitespace character at the end of source xml files but cutting it out"
    This reverts commit 721e98f55fa82edac28f238d6691818fafca72aa [formerly 4a9ec708d66de98209cd6df86c249964a5a9d8fd].
    
    
    Former-commit-id: 814253ef7ca5f605d68d79baf115985021ca41a3

--- test/php/languageforge/semdomtrans/commands/SemDomTransProjectCommands_Test.php
+++ test/php/languageforge/semdomtrans/commands/SemDomTransProjectCommands_Test.php
@@ -45,8 +45,8 @@ class SemDomTransProjectCommands_Test extends UnitTestCase {
         $destArr = explode("\n", $destStr);
         for ($i = 0; $i < count($destArr); $i++) {
             if ($i % 500 == 0) {
-                //$this->assertEqual($sourceArr[$i], $destArr[$i]);
-                $this->assertEqual(substr($sourceArr[$i], 0, -1), $destArr[$i]);
+                $this->assertEqual($sourceArr[$i], $destArr[$i]);
+                //$this->assertEqual(substr($sourceArr[$i], 0, -1), $destArr[$i]);
             }
         }
 
