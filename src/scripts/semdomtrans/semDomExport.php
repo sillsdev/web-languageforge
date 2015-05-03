4e4e9b5a2e44cee765350e4f73f3fefff86471ec
Patrick Hess <tuxyrulez@gmail.com>
2015-05-02 22:04:41 -0400

    * In SemDomXMLExporter, only export fields that are approved

--- src/scripts/semdomtrans/semDomExport.php
+++ src/scripts/semdomtrans/semDomExport.php
@@ -4,13 +4,14 @@ use libraries\languageforge\semdomtrans\SemDomXMLExporter;
 use models\ProjectListModel;
 use libraries\languageforge\semdomtrans;
 use models\ProjectModel;
+use models\languageforge\semdomtrans\SemDomTransStatus;
 
 require_once('../scriptConfig.php');
 
 
 
 $lang = $argv[1];
-$version = (int) $argv[2];
+$version = SemDomTransProjectModel::SEMDOM_VERSION;
 $testMode = false;
 
 
@@ -18,7 +19,7 @@ $projectModel = new SemDomTransProjectModel();
 $projectModel->readByProperties(array("languageIsoCode" => $lang, "semdomVersion" => $version));
 
 $xml = simplexml_load_file($projectModel->xmlFilePath);
-$exporter = new SemDomXMLExporter($projectModel, $testMode,  ($argv[3] == "1"), true);
+$exporter = new SemDomXMLExporter($projectModel, $testMode,  ($argv[2] == "1"), ($argv[3] == "1"));
 
 $exporter->run(); 
 ?>
