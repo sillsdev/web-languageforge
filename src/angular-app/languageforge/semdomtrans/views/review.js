46bcd7c940163e5b50d5c0027f6f99ee76dfe315
Patrick Hess <tuxyrulez@gmail.com>
2015-05-05 19:49:09 -0400

    * Re-layout review task to 4 columns: field name, source, translation, buttons

--- src/angular-app/languageforge/semdomtrans/views/review.js
+++ src/angular-app/languageforge/semdomtrans/views/review.js
@@ -65,36 +65,66 @@ function($scope, $state, $stateParams, editorService, semdomEditApi, sessionServ
     
     $scope.$watch("currentEntry", function (newVal, oldVal) {
       if (newVal != oldVal) {
-        var fieldsForReview = {};
-        if (!angular.isUndefined($scope.currentEntry)) {
-          if (doesFieldNeedReview($scope.currentEntry.name)) {
-            fieldsForReview["Name"] = $scope.currentEntry.name;
-          };
-          
-          if (doesFieldNeedReview($scope.currentEntry.description)) {
-            fieldsForReview["Description"] = $scope.currentEntry.description;
-          }
-          
-          for (var i = 0; i < $scope.currentEntry.searchKeys.length; i++) {
-            if (doesFieldNeedReview($scope.currentEntry.searchKeys[i])) {
-              fieldsForReview["Search Key " + i] = $scope.currentEntry.searchKeys[i];
-            }
-          }
-          
-          for (var i = 0; i < $scope.currentEntry.questions.length; i++) {
-            if (doesFieldNeedReview($scope.currentEntry.questions[i].question)) {
-              fieldsForReview["Question " + i] = $scope.currentEntry.questions[i].question;
-            }
-            if (doesFieldNeedReview($scope.currentEntry.questions[i].terms)) {
-              fieldsForReview["Question Terms " + i] = $scope.currentEntry.questions[i].terms;
-            }
+        calculateFieldsForReview();
+      }
+    });
+    
+    function calculateFieldsForReview() {
+      var fieldsForReview = {};
+      if (!angular.isUndefined($scope.currentEntry)) {
+        if (doesFieldNeedReview($scope.currentEntry.name)) {
+          fieldsForReview["Name"] = $scope.currentEntry.name;
+        };
+        
+        if (doesFieldNeedReview($scope.currentEntry.description)) {
+          fieldsForReview["Description"] = $scope.currentEntry.description;
+        }
+        
+        for (var i = 0; i < $scope.currentEntry.searchKeys.length; i++) {
+          if (doesFieldNeedReview($scope.currentEntry.searchKeys[i])) {
+            fieldsForReview["Search Key " + i] = $scope.currentEntry.searchKeys[i];
           }
         }
         
-        $scope.fieldsForReview = fieldsForReview;
+        for (var i = 0; i < $scope.currentEntry.questions.length; i++) {
+          if (doesFieldNeedReview($scope.currentEntry.questions[i].question)) {
+            fieldsForReview["Question " + i] = $scope.currentEntry.questions[i].question;
+          }
+          if (doesFieldNeedReview($scope.currentEntry.questions[i].terms)) {
+            fieldsForReview["Question Terms " + i] = $scope.currentEntry.questions[i].terms;
+          }
+        }
       }
-    });
+      
+      $scope.fieldsForReview = fieldsForReview;
+    }
+    
     function doesFieldNeedReview(field) {
       return field.translation != '' && (field.status == 1 || field.status == 2);
     }
+    
+    
+     $scope.markAsApproved = function markAsApproved(field) {
+       field.status = 4;
+       updateParentItem();
+     }
+     
+     $scope.markAsNeedsRevision = function markAsNeedsRevision(field) {
+       field.status = 3;
+       updateParentItem();
+     }
+     
+     $scope.getAllFieldsForRevison = function getAllFieldsForRevision() {
+       
+     }
+     
+     function updateParentItem() {
+       calculateFieldsForReview();
+       semdomEditApi.updateTerm($scope.currentEntry, function(result) {
+         if (result.ok) {
+            $scope.refreshDbeData();
+         }
+       });
+     }
+
 }]);
