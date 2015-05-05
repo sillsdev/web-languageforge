f659cf9efbb4a765c283a4982eb34aa01a9616f8
Patrick Hess <tuxyrulez@gmail.com>
2015-05-05 17:12:06 -0400

    * Check that $stateParams.position is set before we use it

--- src/angular-app/languageforge/semdomtrans/views/edit.js
+++ src/angular-app/languageforge/semdomtrans/views/edit.js
@@ -195,7 +195,7 @@ function($scope, $state, $stateParams, semdomEditApi, editorDataService, session
       $scope.reloadItems(1);
       
       // reload current entry if it is included in lsit
-      if ($scope.includedItems[$scope.items[$stateParams.position].key]) {      
+      if (angular.isUndefined($stateParams.position) != "" && $scope.includedItems[$scope.items[$stateParams.position].key]) {      
         $scope.currentEntry = $scope.items[$stateParams.position];
         $scope.currentEntryIndex = angular.isUndefined($stateParams.position) ? 0 : $stateParams.position;
         $scope.changeTerm($scope.currentEntry.key);
