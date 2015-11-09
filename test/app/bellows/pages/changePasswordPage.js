89219d44b035ea9f474fde0453d899369ebd2a9b
irahopkinson <ira_hopkinson@sil.org>
2015-11-09 15:43:32 +0700

    build: fix build server E2E - fix project list app "adding member" test, fix changepassword
    Former-commit-id: c09c6ff67aedaa664d25ce69ba5acbfd256d27b1

--- test/app/bellows/pages/changePasswordPage.js
+++ test/app/bellows/pages/changePasswordPage.js
@@ -3,7 +3,7 @@
 var BellowsChangePasswordPage = function() {
   var _this = this;
   var expectedCondition = protractor.ExpectedConditions;
-  var CONDITION_TIMEOUT = 3000;
+  var CONDITION_TIMEOUT = 5000;
 
   // TODO: this will likely change when we refactor the display of notifications - cjh 2014-06
   this.get = function() {
@@ -14,7 +14,9 @@ var BellowsChangePasswordPage = function() {
   this.form = element(by.tagName('form'));
   this.password = element(by.model('vars.password'));
   this.confirm = element(by.model('vars.confirm_password'));
+  this.passwordMatchImage = element(by.id('passwordMatch'));
   this.submitButton = element(by.partialButtonText('Change Password'));
+  this.noticeList = element.all(by.repeater('notice in notices()'));
 };
 
 module.exports = new BellowsChangePasswordPage();
