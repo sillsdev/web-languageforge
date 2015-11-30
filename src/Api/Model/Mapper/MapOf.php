8d34e4e0120f496ecc1d75c474ebc00bbfcc8478
irahopkinson <ira_hopkinson@sil.org>
2015-11-30 09:22:03 +0700

    feature/sendReceive: Clean up unused items in SendReceiveCommands
    Former-commit-id: f47eef8522d55198eb44baca4f1edd2d0623ad20

--- src/Api/Model/Mapper/MapOf.php
+++ src/Api/Model/Mapper/MapOf.php
@@ -7,14 +7,14 @@ use Palaso\Utilities\CodeGuard;
 class MapOf extends \ArrayObject
 {
     /**
-     * @var function The function <object> function($data = null) returns an instance of the object.
+     * @var callable The function <object> function($data = null) returns an instance of the object.
      */
     private $_generator;
 
     private $data; // This is here to force client code using the older implementation to have a fatal error allowing us to identify code that needs upgrading. CP 2013-12
 
     /**
-     * @param function The function <object> function($data = null) returns an instance of the object.
+     * @param callable $generator The function <object> function($data = null) returns an instance of the object.
      */
     public function __construct($generator = null)
     {
