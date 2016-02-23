3e4e1c301bb982a9eeebe27e27a4be1394f017d6
Chris Hirt <chris@hirtfamily.net>
2016-02-23 16:05:40 +0700

    Fixed script controller and accompanying twig template
    Renamed textoutput to scriptoutput

--- src/Site/Controller/Script.php
+++ src/Site/Controller/Script.php
@@ -10,9 +10,14 @@ use Silex\Application;
 class Script extends Base
 {
     public function view(Application $app, $folder = '', $scriptName = '', $runType = 'test') {
+        $this->data['controlpanel'] = false;
         if (! file_exists("Api/Library/Shared/Script/$folder/$scriptName.php")) {
-            $app->abort(404, $this->website->base); // this terminates PHP
+            // show list of scripts
+            $this->data['scriptnames'] = $this->scriptBaseNames();
+            $this->data['controlpanel'] = true;
+            return $this->renderPage($app, 'scriptoutput');
         } else {
+            // run script and render output
             $userId = (string) $app['session']->get('user_id');
             if (! RightsHelper::hasSiteRight($userId, Domain::PROJECTS + Operation::DELETE)) {
                 $app->abort(403, 'You have insufficient privileges to run scripts'); // this terminates PHP
@@ -22,22 +27,29 @@ class Script extends Base
                     $script = new $className();
 
                     $this->data['scriptname'] = $className.'->run()';
-                    $this->data['insert'] = '';
                     $this->data['output'] = '';
-                    if (strtolower($folder) == 'control' and strtolower($scriptName) == 'panel') {
-                        $this->data['insert'] .= $script->run($userId, $runType);
-                    } else {
-                        if ($runType != 'run') {
-                            $this->data['output'] .= "--------------- THIS IS A TEST RUN - The database should not be modified ----------------\n\n";
-                        }
-                        $this->data['output'] .= $script->run($userId, $runType);
+                    if ($runType != 'run') {
+                        $this->data['scriptname'] = "[TEST RUN] " . $this->data['scriptname'];
                     }
+                    $this->data['output'] .= $script->run($userId, $runType);
 
-                    return $this->renderPage($app, 'textoutput');
+                    return $this->renderPage($app, 'scriptoutput');
                 } catch (\Exception $e) {
+                    var_dump($e);
                     $app->abort(500, "Looks like there was a problem with the script $className"); // this terminates PHP
                 }
             }
         }
+    }
+
+    private function scriptBaseNames() {
+        $folderPath = APPPATH.'Api/Library/Shared/Script/Migration';
+        $baseNames = glob($folderPath . '/*.php');
+        $file_count = count($baseNames);
+        for ($i = 0; $i < $file_count; $i++) {
+            $baseNames[$i] = basename($baseNames[$i], '.php');
+        }
+
+        return $baseNames;
     }
 }
