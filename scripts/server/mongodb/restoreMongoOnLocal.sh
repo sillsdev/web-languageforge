8585c04bdae8d29ec02db0d7bde3e83a3948c1b9
megahirt <chris@hirtfamily.net>
2016-03-11 11:39:10 +0700

    UpdateMongoForDevServer admin script to automate migrating the sitename in the dabase for project and user records
    This is useful when copying the production database to the dev server or to your local machine for testing purposes

--- scripts/server/mongodb/restoreMongoOnLocal.sh
+++ scripts/server/mongodb/restoreMongoOnLocal.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-TODAY=`date +%F`
-SRCDIR=${1:-/var/www/host/sil}
-BACKUPNAME=mongodb_backup_$TODAY
-ARCHIVEFILE=$BACKUPNAME.tgz
+ARCHIVEFILE=$1
 
-if [ -f $SRCDIR/$ARCHIVEFILE ]; then
-	cd ~
-	cp $SRCDIR/$ARCHIVEFILE .
+if [ -f $ARCHIVEFILE ]; then
 	tar -xvzf $ARCHIVEFILE
 	mongo --quiet --eval 'db.getMongo().getDBNames().forEach(function(i){  if (i.indexOf("sf_") == 0 || i.indexOf("scriptureforge") == 0) { print("Dropping " + i); db.getSiblingDB(i).dropDatabase()}})'
 	mongorestore mongo_backup
 
 	# clean up
-	rm $ARCHIVEFILE
 	if [ -d "mongo_backup" ]; then
 		echo Removing $BACKUPNAME
 		rm -r mongo_backup
 	fi
-	
+
 
 fi
