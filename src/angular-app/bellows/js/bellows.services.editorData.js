d2b66229572ced2650129ec4b44d0865010507ef
Ira Hopkinson <ira_hopkinson@sil.org>
2016-06-03 14:13:54 +0700

    Fix JSCS
    Former-commit-id: 9f15c28d4d416094b39b1e56666445ee1d122fc2

--- src/angular-app/bellows/js/bellows.services.editorData.js
+++ src/angular-app/bellows/js/bellows.services.editorData.js
@@ -3,10 +3,12 @@
 angular.module('bellows.services')
 
 // Lexicon Entry Service
-.factory('editorDataService', ['$q', 'sessionService', 'editorOfflineCache', 'commentsOfflineCache', 'silNoticeService', 'lexCommentService',
-function($q, ss, cache, commentsCache, notice, commentService) {
+.factory('editorDataService', ['$q', 'sessionService', 'editorOfflineCache', 'commentsOfflineCache',
+  'silNoticeService', 'lexCommentService',
+function ($q, sessionService, cache, commentsCache,
+          notice, commentService) {
 
-  var config = ss.session.projectSettings.config;
+  var config = sessionService.session.projectSettings.config;
   var entries = [];
   var visibleEntries = [];
   var browserInstanceId = Math.floor(Math.random() * 1000);
@@ -40,13 +42,13 @@ function($q, ss, cache, commentsCache, notice, commentService) {
     if (entries.length == 0) { // first page load
       if (cache.canCache()) {
         notice.setLoading('Loading Dictionary');
-        loadDataFromOfflineCache().then(function(projectObj) {
+        loadDataFromOfflineCache().then(function (projectObj) {
           if (projectObj.isComplete) {
             // data found in cache
             console.log('data successfully loaded from the cache.  Downloading updates...');
             notice.setLoading('Downloading Updates to Dictionary.');
             showInitialEntries();
-            refreshEditorData(projectObj.timestamp).then(function(result) {
+            refreshEditorData(projectObj.timestamp).then(function (result) {
               deferred.resolve(result);
               notice.cancelLoading();
             });
@@ -56,19 +58,19 @@ function($q, ss, cache, commentsCache, notice, commentService) {
             console.log('cached data was found to be incomplete. Full download started...');
             notice.setLoading('Downloading Full Dictionary.');
             notice.setPercentComplete(0);
-            doFullRefresh().then(function(result) {
+            doFullRefresh().then(function (result) {
               deferred.resolve(result);
               notice.setPercentComplete(100);
               notice.cancelLoading();
             });
           }
 
-        }, function() {
+        }, function () {
           // no data found in cache
           console.log('no data found in cache. Full download started...');
           notice.setLoading('Downloading Full Dictionary.');
           notice.setPercentComplete(0);
-          doFullRefresh().then(function(result) {
+          doFullRefresh().then(function (result) {
             deferred.resolve(result);
             notice.setPercentComplete(100);
             notice.cancelLoading();
@@ -78,7 +80,7 @@ function($q, ss, cache, commentsCache, notice, commentService) {
         console.log('caching not enabled. Full download started...');
         notice.setLoading('Downloading Full Dictionary.');
         notice.setPercentComplete(0);
-        doFullRefresh().then(function(result) {
+        doFullRefresh().then(function (result) {
           deferred.resolve(result);
           notice.setPercentComplete(100);
           notice.cancelLoading();
@@ -86,7 +88,7 @@ function($q, ss, cache, commentsCache, notice, commentService) {
       }
     } else {
       // intentionally not showing any loading message here
-      refreshEditorData().then(function(result) {
+      refreshEditorData().then(function (result) {
         deferred.resolve(result);
       });
     }
@@ -97,7 +99,7 @@ function($q, ss, cache, commentsCache, notice, commentService) {
   function doFullRefresh(offset) {
     offset = offset || 0;
     var deferred = $q.defer();
-    api.dbeDtoFull(browserInstanceId, offset, function(result) {
+    api.dbeDtoFull(browserInstanceId, offset, function (result) {
       if (!result.ok) {
         notice.cancelLoading();
         deferred.error(result);
@@ -107,13 +109,13 @@ function($q, ss, cache, commentsCache, notice, commentService) {
       var newOffset = offset + result.data.itemCount;
       var totalCount = result.data.itemTotalCount;
       notice.setPercentComplete(parseInt(newOffset * 100 / totalCount));
-      processEditorDto(result, false).then(function() {
+      processEditorDto(result, false).then(function () {
         if (offset == 0) {
           showInitialEntries();
         }
 
         if (newOffset < totalCount) {
-          doFullRefresh(newOffset).then(function() {
+          doFullRefresh(newOffset).then(function () {
             deferred.resolve(result);
           });
         } else {
@@ -134,8 +136,8 @@ function($q, ss, cache, commentsCache, notice, commentService) {
     var deferred = $q.defer();
 
     // get data from the server
-    api.dbeDtoUpdatesOnly(browserInstanceId, timestamp, function(result) {
-      processEditorDto(result, true).then(function(result) {
+    api.dbeDtoUpdatesOnly(browserInstanceId, timestamp, function (result) {
+      processEditorDto(result, true).then(function (result) {
         deferred.resolve(result);
       });
     });
@@ -150,7 +152,7 @@ function($q, ss, cache, commentsCache, notice, commentService) {
   var removeEntryFromLists = function removeEntryFromLists(id) {
     // todo: make this method async, returning a promise
 
-    cache.deleteEntry(id).then(function() {
+    cache.deleteEntry(id).then(function () {
       var iFullList = getIndexInList(id, entries);
       if (angular.isDefined(iFullList)) {
         entries.splice(iFullList, 1);
@@ -169,13 +171,14 @@ function($q, ss, cache, commentsCache, notice, commentService) {
   function storeDataInOfflineCache(data, isComplete) {
     var deferred = $q.defer();
     if (data.timeOnServer && cache.canCache()) {
-      cache.updateProjectData(data.timeOnServer, data.commentsUserPlusOne, isComplete).then(function() {
-        cache.updateEntries(data.entries).then(function() {
-          commentsCache.updateComments(data.comments).then(function() {
-            deferred.resolve();
+      cache.updateProjectData(data.timeOnServer, data.commentsUserPlusOne, isComplete)
+        .then(function () {
+          cache.updateEntries(data.entries).then(function () {
+            commentsCache.updateComments(data.comments).then(function () {
+              deferred.resolve();
+            });
           });
         });
-      });
     } else {
       deferred.reject();
     }
@@ -192,28 +195,29 @@ function($q, ss, cache, commentsCache, notice, commentService) {
     var deferred = $q.defer();
     var endTime;
     var numOfEntries;
-    cache.getAllEntries().then(function(result) {
+    cache.getAllEntries().then(function (result) {
       entries.push.apply(entries, result); // proper way to extend the array
       numOfEntries = result.length;
 
       if (result.length > 0) {
-        commentsCache.getAllComments().then(function(result) {
+        commentsCache.getAllComments().then(function (result) {
           commentService.comments.items.all.push.apply(commentService.comments.items.all, result);
 
-          cache.getProjectData().then(function(result) {
+          cache.getProjectData().then(function (result) {
             commentService.comments.counts.userPlusOne = result.commentsUserPlusOne;
             endTime = performance.now();
-            console.log('Loaded ' + numOfEntries + ' entries from the cache in ' + ((endTime - startTime) / 1000).toFixed(2) + ' seconds');
+            console.log('Loaded ' + numOfEntries + ' entries from the cache in ' +
+              ((endTime - startTime) / 1000).toFixed(2) + ' seconds');
             deferred.resolve(result);
 
-          }, function() { deferred.reject(); });
-        }, function() { deferred.reject(); });
+          }, function () { deferred.reject(); });
+        }, function () { deferred.reject(); });
       } else {
         // we got zero entries
         deferred.reject();
       }
 
-    }, function() { deferred.reject(); });
+    }, function () { deferred.reject(); });
 
     return deferred.promise;
   }
@@ -225,11 +229,12 @@ function($q, ss, cache, commentsCache, notice, commentService) {
       commentService.comments.counts.userPlusOne = result.data.commentsUserPlusOne;
       if (!updateOnly) {
         entries.push.apply(entries, result.data.entries); // proper way to extend the array
-        commentService.comments.items.all.push.apply(commentService.comments.items.all, result.data.comments);
+        commentService.comments.items.all.push
+          .apply(commentService.comments.items.all, result.data.comments);
       } else {
 
         // splice updates into entry lists
-        angular.forEach(result.data.entries, function(e) {
+        angular.forEach(result.data.entries, function (e) {
           var i;
 
           // splice into entries
@@ -248,7 +253,7 @@ function($q, ss, cache, commentsCache, notice, commentService) {
         });
 
         // splice comment updates into comments list
-        angular.forEach(result.data.comments, function(c) {
+        angular.forEach(result.data.comments, function (c) {
           var i = getIndexInList(c.id, commentService.comments.items.all);
           if (angular.isDefined(i)) {
             commentService.comments.items.all[i] = c;
@@ -266,11 +271,12 @@ function($q, ss, cache, commentsCache, notice, commentService) {
         sortList(visibleEntries);
       }
 
-      if (result.data.itemCount && result.data.itemCount + result.data.offset < result.data.itemTotalCount) {
+      if (result.data.itemCount &&
+          result.data.itemCount + result.data.offset < result.data.itemTotalCount) {
         isLastRequest = false;
       }
 
-      storeDataInOfflineCache(result.data, isLastRequest).then(function() { deferred.resolve(); });
+      storeDataInOfflineCache(result.data, isLastRequest).then(function () { deferred.resolve(); });
 
       commentService.updateGlobalCommentCounts();
       deferred.resolve(result);
@@ -296,16 +302,18 @@ function($q, ss, cache, commentsCache, notice, commentService) {
     var lexemeA = '';
     var lexemeB = '';
     list.sort(function (a, b) {
-      for (var x = 0; x < inputSystems.length; x++) {
-        var ws = inputSystems[x];
+      var x;
+      var ws;
+      for (x = 0; x < inputSystems.length; x++) {
+        ws = inputSystems[x];
         if (angular.isDefined(a.lexeme[ws])) {
           lexemeA = a.lexeme[ws].value;
           break;
         }
       }
 
-      for (var x = 0; x < inputSystems.length; x++) {
-        var ws = inputSystems[x];
+      for (x = 0; x < inputSystems.length; x++) {
+        ws = inputSystems[x];
         if (angular.isDefined(b.lexeme[ws])) {
           lexemeB = b.lexeme[ws].value;
           break;
