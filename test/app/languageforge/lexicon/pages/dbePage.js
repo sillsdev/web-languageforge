2fd4e7b2c68b70b9d2a3b1d3331be152cd62f7ac
Ira Hopkinson <ira_hopkinson@sil.org>
2016-06-15 15:28:46 +0700

    Add E2E Dictionary Configuration check, cleanup after E2E

--- test/app/languageforge/lexicon/pages/dbePage.js
+++ test/app/languageforge/lexicon/pages/dbePage.js
@@ -12,10 +12,10 @@ var projectTypes = {
 var util = require('../../../bellows/pages/util');
 var dbeUtil = require('./dbeUtil');
 
-var LfDbePage = function() {
+var LfDbePage = function () {
   var _this = this;
   this.url = '/app/lexicon';
-  this.get = function(projectId) {
+  this.get = function (projectId) {
     var extra = projectId ? ('/' + projectId) : '';
     browser.get(browser.baseUrl + _this.url + extra);
   };
@@ -30,8 +30,8 @@ var LfDbePage = function() {
     // Top row UI elements
     newWordBtn: _this.browseDiv.element(by.partialButtonText('New Word')),
     entryCountElem: _this.browseDiv.element(by.binding('entries.length')),
-    getEntryCount: function() {
-      return this.entryCountElem.getText().then(function(s) {
+    getEntryCount: function () {
+      return this.entryCountElem.getText().then(function (s) {
         return parseInt(s, 10);
       });
     },
@@ -40,12 +40,14 @@ var LfDbePage = function() {
     search: {
       input: _this.browseDiv.element(by.css('div.typeahead')).element(by.css('input')),
       clearBtn: _this.browseDiv.element(by.css('div.typeahead')).element(by.css('i.icon-remove')),
-      results: _this.browseDiv.element(by.css('div.typeahead')).all(by.repeater('e in typeahead.searchResults')),
-      matchCountElem: _this.browseDiv.element(by.css('div.typeahead')).element(by.binding('typeahead.searchResults.length')),
-      getMatchCount: function() {
+      results: _this.browseDiv.element(by.css('div.typeahead'))
+        .all(by.repeater('e in typeahead.searchResults')),
+      matchCountElem: _this.browseDiv.element(by.css('div.typeahead'))
+        .element(by.binding('typeahead.searchResults.length')),
+      getMatchCount: function () {
 
         // Inside this function, "this" == _this.browse.search
-        return this.matchCountElem.getText().then(function(s) {
+        return this.matchCountElem.getText().then(function (s) {
           return parseInt(s, 10);
         });
       }
@@ -53,16 +55,16 @@ var LfDbePage = function() {
 
     // Entries list (main body of view)
     entriesList: _this.browseDiv.all(by.repeater('entry in visibleEntries')),
-    findEntryByLexeme: function(lexeme) {
-      return this.entriesList.filter(function(row) {
-        return row.element(by.binding('entry.word')).getText().then(function(word) {
+    findEntryByLexeme: function (lexeme) {
+      return this.entriesList.filter(function (row) {
+        return row.element(by.binding('entry.word')).getText().then(function (word) {
           return (word.indexOf(lexeme) > -1);
         });
       });
     },
 
-    clickEntryByLexeme: function(lexeme) {
-      this.findEntryByLexeme(lexeme).then(function(matchedRows) {
+    clickEntryByLexeme: function (lexeme) {
+      this.findEntryByLexeme(lexeme).then(function (matchedRows) {
         matchedRows[0].click();
       });
     }
@@ -80,20 +82,20 @@ var LfDbePage = function() {
       show: 'Show All Fields',
       hide: 'Hide Uncommon Fields'
     },
-    showUncommonFields: function() {
+    showUncommonFields: function () {
 
       // Only click the button if it will result in fields being shown
-      this.toggleUncommonFieldsBtn.getText().then(function(text) {
+      this.toggleUncommonFieldsBtn.getText().then(function (text) {
         if (text == _this.edit.toggleUncommonFieldsBtnText.show) {
           _this.edit.toggleUncommonFieldsBtn.click();
         }
       });
     },
 
-    hideUncommonFields: function() {
+    hideUncommonFields: function () {
 
       // Only click the button if it will result in fields being hidden
-      this.toggleUncommonFieldsBtn.getText().then(function(text) {
+      this.toggleUncommonFieldsBtn.getText().then(function (text) {
         if (text == _this.edit.toggleUncommonFieldsBtnText.hide) {
           _this.edit.toggleUncommonFieldsBtn.click();
         }
@@ -103,31 +105,32 @@ var LfDbePage = function() {
     // Left sidebar UI elements
     newWordBtn: _this.editDiv.element(by.css('button[data-ng-click="newEntry()')),
     entryCountElem: _this.editDiv.element(by.binding('entries.length')),
-    getEntryCount: function() {
-      return this.entryCountElem.getText().then(function(s) {
+    getEntryCount: function () {
+      return this.entryCountElem.getText().then(function (s) {
         return parseInt(s, 10);
       });
     },
 
     entriesList: _this.editDiv.all(by.repeater('entry in visibleEntries')),
-    findEntryByLexeme: function(lexeme) {
+    findEntryByLexeme: function (lexeme) {
       var div = _this.editDiv.element(by.css('#compactEntryListContainer'));
       return div.element(by.cssContainingText('[ng-bind-html="getWordForDisplay(entry)"', lexeme));
     },
 
-    clickEntryByLexeme: function(lexeme) {
-      this.findEntryByLexeme(lexeme).then(function(elem) {
+    clickEntryByLexeme: function (lexeme) {
+      this.findEntryByLexeme(lexeme).then(function (elem) {
         elem.click();
       });
     },
 
-    findEntryByDefinition: function(definition) {
+    findEntryByDefinition: function (definition) {
       var div = _this.editDiv.element(by.css('#compactEntryListContainer'));
-      return div.element(by.cssContainingText('[ng-bind-html="getMeaningForDisplay(entry)"', definition));
+      return div.element(by.cssContainingText('[ng-bind-html="getMeaningForDisplay(entry)"',
+        definition));
     },
 
-    clickEntryByDefinition: function(definition) {
-      this.findEntryByDefinition(definition).then(function(elem) {
+    clickEntryByDefinition: function (definition) {
+      this.findEntryByDefinition(definition).then(function (elem) {
         elem.click();
       });
     },
@@ -135,11 +138,13 @@ var LfDbePage = function() {
     search: {
       input: _this.editDiv.element(by.css('div.typeahead')).element(by.css('input')),
       clearBtn: _this.editDiv.element(by.css('div.typeahead')).element(by.css('i.icon-remove')),
-      results: _this.editDiv.element(by.css('div.typeahead')).all(by.repeater('e in typeahead.searchResults')),
-      matchCountElem: _this.editDiv.element(by.css('div.typeahead')).element(by.binding('typeahead.searchResults.length')),
-      getMatchCount: function() {
+      results: _this.editDiv.element(by.css('div.typeahead'))
+        .all(by.repeater('e in typeahead.searchResults')),
+      matchCountElem: _this.editDiv.element(by.css('div.typeahead'))
+        .element(by.binding('typeahead.searchResults.length')),
+      getMatchCount: function () {
         // Inside this function, "this" == _this.edit.search
-        return this.matchCountElem.getText().then(function(s) {
+        return this.matchCountElem.getText().then(function (s) {
           return parseInt(s, 10);
         });
       }
@@ -151,7 +156,7 @@ var LfDbePage = function() {
     saveBtn: _this.editDiv.element(by.css('button[data-ng-click="saveCurrentEntry(true)"]')),
 
     // Helper functions for retrieving various field values
-    getLexemes: function() {
+    getLexemes: function () {
 
       // Returns lexemes in the format [{wsid: 'en', value: 'word'}, {wsid:
       // 'de', value: 'Wort'}]
@@ -159,30 +164,30 @@ var LfDbePage = function() {
       return dbeUtil.dcMultitextToArray(lexeme);
     },
 
-    getLexemesAsObject: function() {
+    getLexemesAsObject: function () {
 
       // Returns lexemes in the format [{en: 'word', de: 'Wort'}]
       var lexeme = this.fields.get(0);
       return dbeUtil.dcMultitextToObject(lexeme);
     },
 
-    getFirstLexeme: function() {
+    getFirstLexeme: function () {
 
       // Returns the first (topmost) lexeme regardless of its wsid
       var lexeme = this.fields.get(0);
       return dbeUtil.dcMultitextToFirstValue(lexeme);
     },
 
-    getLexemeByWsid: function(searchWsid) {
+    getLexemeByWsid: function (searchWsid) {
       var lexeme = this.fields.get(0);
-      return dbeUtil.dcMultitextToObject(lexeme).then(function(lexemes) {
+      return dbeUtil.dcMultitextToObject(lexeme).then(function (lexemes) {
         return lexemes[searchWsid];
       });
     },
 
-    clickFirstSense: function() {
+    clickFirstSense: function () {
       var senses = element.all(by.css('dc-sense'));
-      senses.get(0).then(function(elem) {
+      senses.get(0).then(function (elem) {
         elem.click();
       });
     },
@@ -192,14 +197,14 @@ var LfDbePage = function() {
       images: dbeUtil.getOneField('Pictures').all(by.css('img')),
       captions: dbeUtil.getOneField('Pictures').all(by.css('.input-prepend > input')),
       removeImages: dbeUtil.getOneField('Pictures').all(by.css('.icon-remove')),
-      getFileName: function(index) {
-        return dbeUtil.getOneFieldValue('Pictures').then(function(pictures) {
+      getFileName: function (index) {
+        return dbeUtil.getOneFieldValue('Pictures').then(function (pictures) {
           return pictures[index].fileName;
         });
       },
 
-      getCaption: function(index) {
-        return dbeUtil.getOneFieldValue('Pictures').then(function(pictures) {
+      getCaption: function (index) {
+        return dbeUtil.getOneFieldValue('Pictures').then(function (pictures) {
           return pictures[index].caption;
         });
       },
@@ -211,6 +216,10 @@ var LfDbePage = function() {
 
     getMultiTextInputs: function getMultiTextInputs(searchLabel) {
       return dbeUtil.getOneField(searchLabel).all(by.css('.input-prepend > input'));
+    },
+
+    getMultiTextInputSystems: function getMultiTextInputSystems(searchLabel) {
+      return dbeUtil.getOneField(searchLabel).all(by.css('.input-prepend > span.wsid'));
     },
 
     getFields: dbeUtil.getFields,
@@ -229,25 +238,25 @@ var LfDbePage = function() {
       byTextElem: _this.commentDiv.element(by.model('commentFilter.text')),
       byStatusElem: _this.commentDiv.element(by.model('commentFilter.status')),
       clearElem: _this.commentDiv.element(by.css('[title="Clear Filter] > i.icon-remove')),
-      byText: function(textToFilterBy) {
+      byText: function (textToFilterBy) {
         this.byTextElem.sendKeys(textToFilterBy);
       },
 
-      byStatus: function(statusToFilterBy) {
+      byStatus: function (statusToFilterBy) {
         util.clickDropdownByValue(this.byStatusElem, statusToFilterBy);
       },
 
-      clearByText: function() {
+      clearByText: function () {
         this.clearElem.click();
       },
 
-      clearByStatus: function() {
+      clearByStatus: function () {
         this.byStatus('Show All');
       }
     },
     commentCountElem: _this.commentDiv.element(by.binding('currentEntryCommentCounts.total')),
-    getCommentCount: function() {
-      return this.commentCountElem.getText().then(function(s) {
+    getCommentCount: function () {
+      return this.commentCountElem.getText().then(function (s) {
         return parseInt(s, 10);
       });
     },
@@ -266,16 +275,21 @@ var LfDbePage = function() {
     newComment: {
       textarea: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('textarea')),
       postBtn: _this.commentDiv.element(by.css('.newCommentForm')).element(by.buttonText('Post')),
-      regardingDiv: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('.commentRegarding')),
+      regardingDiv: _this.commentDiv.element(by.css('.newCommentForm'))
+        .element(by.css('.commentRegarding')),
       regarding: {
-        clearBtn: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('.commentRegarding')).element(by.css('i.icon-remove')),
-        fieldLabel: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('.commentRegarding')).element(by.css('.regardingFieldName')),
-        fieldWsid: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('.commentRegarding')).element(by.css('.regardingInputSystem')),
-        fieldValue: _this.commentDiv.element(by.css('.newCommentForm')).element(by.css('.commentRegarding')).element(by.css('.regardingFieldValue'))
+        clearBtn: _this.commentDiv.element(by.css('.newCommentForm'))
+          .element(by.css('.commentRegarding')).element(by.css('i.icon-remove')),
+        fieldLabel: _this.commentDiv.element(by.css('.newCommentForm'))
+          .element(by.css('.commentRegarding')).element(by.css('.regardingFieldName')),
+        fieldWsid: _this.commentDiv.element(by.css('.newCommentForm'))
+          .element(by.css('.commentRegarding')).element(by.css('.regardingInputSystem')),
+        fieldValue: _this.commentDiv.element(by.css('.newCommentForm'))
+          .element(by.css('.commentRegarding')).element(by.css('.regardingFieldValue'))
       }
     },
     commentsList: _this.commentDiv.all(by.repeater('comment in currentEntryCommentsFiltered')),
-    getComment: function(commentNum) {
+    getComment: function (commentNum) {
       return _this.getComment(this.commentsList, commentNum);
     }
   };
@@ -289,7 +303,7 @@ var LfDbePage = function() {
   // expect(_this.comments.getComment(0).regarding.inputSystem).toBe("th")
   // commentNum can be -1 to get the last comment, any other number is a 0-based
   // index
-  this.getComment = function(commentsList, commentNum) {
+  this.getComment = function (commentsList, commentNum) {
     if (typeof (commentNum) === 'undefined') {
       commentNum = 0;
     }
@@ -302,7 +316,7 @@ var LfDbePage = function() {
   // (via partsOfReply() below)
   // replyNum can be -1 to get the last reply, any other number is a 0-based
   // index
-  this.getReply = function(repliesList, replyNum) {
+  this.getReply = function (repliesList, replyNum) {
     if (typeof (replyNum) === 'undefined') {
       replyNum = 0;
     }
@@ -315,7 +329,7 @@ var LfDbePage = function() {
   // reply button, etc.) of a comment
   // Usage example:
   // expect(partsOfDcComment(commentDiv).regarding.inputSystem).toBe("th")
-  this.partsOfComment = function(div) {
+  this.partsOfComment = function (div) {
     var replies = div.all(by.repeater('reply in model.replies')); // used in
     // getReply()
     // below
@@ -350,7 +364,7 @@ var LfDbePage = function() {
 
       // Replies (below content but above bottom controls)
       replies: replies,
-      getReply: function(replyNum) {
+      getReply: function (replyNum) {
         return _this.getReply(replies, replyNum);
       },
 
@@ -365,7 +379,7 @@ var LfDbePage = function() {
 
   // Like partsOfComment, returns a Javascript object giving access to the parts
   // of a reply
-  this.partsOfReply = function(div) {
+  this.partsOfReply = function (div) {
     return {
       wholeReply: div,
       content: div.element(by.model('reply.content')),
