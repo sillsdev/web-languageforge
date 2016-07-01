267aecde8e58c89ea07017ae527687977c932ee2
megahirt <chris@hirtfamily.net>
2016-07-01 15:51:40 +0700

    Refactored method to determine if help file is present on server
    An API method is now unnecessary as the help file paths are passed down on the session data.

--- src/angular-app/container/js/xforge.helpButton.js
+++ src/angular-app/container/js/xforge.helpButton.js
@@ -1,22 +1,54 @@
 'use strict';
 
-angular.module('xforge.helpButton', ['jsonRpc', 'ui.bootstrap', 'pascalprecht.translate'])
+angular.module('xforge.helpButton', ['jsonRpc', 'ui.bootstrap', 'pascalprecht.translate', 'bellows.services'])
+  /*
   .service('helpButtonService', ['jsonRpc', function(jsonRpc) {
     jsonRpc.connect('/api/sf');
     this.checkIfCanShowPageButton = function(urlPath, hashPath, callback) {
       jsonRpc.call('xforge_frame_can_show_page_help_button', [urlPath, hashPath], callback);
     };
   }])
-  .controller('helpButtonController', ['$scope', '$modal', 'helpButtonService', function($scope, $modal, helpButtonService) {
-    $scope.showButton = false;
+  */
+  .controller('helpButtonController', ['$scope', '$modal', '$location', 'sessionService', function($scope, $modal, $location, ss) {
+    
     $scope.helpFilePath = '';
 
+    // this function should be run whenever the location changes
+    function isHelpFilePresentOnServer() {
+      var helpFilePathsAvailable = ss.session.helps.filePaths;
+      var hashPath = $location.hash();
+      var appName = $location.path().split('/')[2];
+
+      var partialPath = "/helps/en/page/" + appName;
+      if (hashPath) {
+        partialPath += $location.hash.substring(1).replace('/', '-');
+      }
+      partialPath += ".html";
+
+      var foundFile = false;
+      angular.forEach(helpFilePathsAvailable, function(path) {
+        if (path.indexOf(partialPath)) {
+          foundFile = true;
+          $scope.helpFilePath = "/" + path;
+        }
+
+      });
+      return foundFile;
+
+    }
+
+    $scope.showButton = isHelpFilePresentOnServer();
+
+
+
+    /*
     helpButtonService.checkIfCanShowPageButton(location.pathname, location.hash, function(result) {
       if (result.data.showButton) {
         $scope.helpFilePath = result.data.helpFilePath;
         $scope.showButton = true;
       }
     });
+    */
 
 
     $scope.showHelpContent = function showHelpContent() {
