fe4a5512e02cba5a7151e284c818857dd6dcf1ae
Ira Hopkinson <ira_hopkinson@sil.org>
2016-07-21 13:50:50 +0700

    Fix JSCS

--- src/angular-app/bellows/directive/palaso.ui.comments.regarding-field.js
+++ src/angular-app/bellows/directive/palaso.ui.comments.regarding-field.js
@@ -1,27 +1,29 @@
-"use strict";
+'use strict';
 angular.module('palaso.ui.comments')
+
 // Palaso UI Dictionary Control: Comments
-  .directive('regardingField', [function() {
+  .directive('regardingField', [function () {
     return {
       restrict: 'E',
       templateUrl: '/angular-app/bellows/directive/palaso.ui.comments.regarding-field.html',
       scope: {
-        content: "=",
-        control: "=",
-        commentRegardingFieldConfig: "="
+        content: '=',
+        control: '=',
+        commentRegardingFieldConfig: '='
       },
-      controller: ['$scope', function($scope) {
-          if (!angular.isUndefined($scope.content)) {
-            $scope.contentArr = $scope.content.split("#");
+      controller: ['$scope', function ($scope) {
+        if (!angular.isUndefined($scope.content)) {
+          $scope.contentArr = $scope.content.split('#');
+        }
+
+        $scope.$watch('content', function (newVal) {
+          if (newVal && !angular.isUndefined($scope.content)) {
+            $scope.contentArr = $scope.content.split('#');
           }
-          
-          $scope.$watch("content", function(newVal) {
-            if (newVal && !angular.isUndefined($scope.content)) {
-                $scope.contentArr = $scope.content.split("#");
-              }
-            });
-       }],
-      link: function(scope, element, attrs, controller) {
+        });
+      }],
+
+      link: function (scope, element, attrs, controller) {
       }
     };
   }]);
