76a640a62a3d043420e58d26a6709cf3958d1965
Ira Hopkinson <ira_hopkinson@sil.org>
2016-08-02 15:51:36 +0700

    Fix failing SF E2E test in projectSettingsPage

--- test/app/rune2eLF.sh
+++ test/app/rune2eLF.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]
   then
     node ./node_modules/protractor/bin/protractor protractorConf.languageforge.js
   else
-    node ./node_modules/protractor/bin/protractor protractorConf.languageforge.js --specs "`find . -wholename "*languageforge*e2e*$1*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
+    node ./node_modules/protractor/bin/protractor protractorConf.languageforge.js --specs "./allspecs/e2e/*.spec.js,`find . -wholename "*languageforge*e2e*$1*.spec.js" -printf "%p,"|perl -p -e 's/,$//'`"
 fi
 sudo -u www-data php teardownTestEnvironment.php
 sh useLiveConfig.sh
