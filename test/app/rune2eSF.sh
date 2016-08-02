5cd9a52a56347f59dd2d25bc334f57aabb95b376
Ira Hopkinson <ira_hopkinson@sil.org>
2016-08-02 15:51:36 +0700

    Fix failing SF E2E test in projectSettingsPage
    Former-commit-id: c990420f906c56de8d8b265ec589fae2c007f383

--- test/app/rune2eSF.sh
+++ test/app/rune2eSF.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]
   then
     node ./node_modules/protractor/bin/protractor protractorConf.scriptureforge.js
   else
-    node ./node_modules/protractor/bin/protractor protractorConf.scriptureforge.js --specs "`find . -wholename "*scriptureforge*e2e*$1*.spec.js" -printf "%p,"|perl -pi -e 's/,$//'`"
+    node ./node_modules/protractor/bin/protractor protractorConf.scriptureforge.js --specs "./allspecs/e2e/*.spec.js,`find . -wholename "*scriptureforge*e2e*$1*.spec.js" -printf "%p,"|perl -p -e 's/,$//'`"
 fi
 sudo -u www-data php teardownTestEnvironment.php
 sh useLiveConfig.sh
