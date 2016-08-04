8b78ad9c89de07e18b379e0ea831ccc3639a953d
Darcy Wong <darcy_wong@sil.org>
2016-08-04 09:21:54 +0700

    Set project owner role view in lexicon service
    Copy the project_manager role view for project owner.  This way,  the PHP side doesn't need to be updated.
    
    Former-commit-id: ff25e4fcaaf7682ae9d76eda4bc299b8d0dc3c1d

--- src/angular-app/languageforge/lexicon/js/lexicon.services.config.js
+++ src/angular-app/languageforge/lexicon/js/lexicon.services.config.js
@@ -18,6 +18,9 @@ angular.module('lexicon.services')
     var role = sessionService.session.projectSettings.currentUserRole;
     var fieldsConfig;
 
+    // Copy project_owner role view from project_manager
+    config.roleViews['project_owner'] = config.roleViews['project_manager'];
+
     // copy option lists to config object
     config.optionlists = {};
     angular.forEach(sessionService.session.projectSettings.optionlists, function (optionlist) {
