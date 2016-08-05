701cedab48abce9f1512c38b7fc290a94adfc494
Ira Hopkinson <ira_hopkinson@sil.org>
2016-08-05 14:33:21 +0700

    Allow Site Admin to archive projects
    Former-commit-id: 2f51c6f01de3f0ccc62f70c153e59dddd53ec357

--- src/angular-app/bellows/apps/projectmanagement/ng-app.js
+++ src/angular-app/bellows/apps/projectmanagement/ng-app.js
@@ -1,16 +1,19 @@
 'use strict';
 
-angular.module('projectmanagement', ['projectManagement.services', 'bellows.services', 'palaso.ui.listview', 'ui.bootstrap', 'palaso.ui.notice', 'palaso.ui.utils', 'wc.Directives'])
-  .controller('ProjectManagementCtrl', ['$scope', 'projectService', 'projectManagementService', 'sessionService', 'silNoticeService', 'modalService', '$window',
-    function($scope, projectService, appService, ss, notice, modalService, $window) {
+angular.module('projectmanagement', ['projectManagement.services', 'bellows.services',
+  'palaso.ui.listview', 'ui.bootstrap', 'palaso.ui.notice', 'palaso.ui.utils', 'wc.Directives'])
+  .controller('ProjectManagementCtrl', ['$scope', 'projectService', 'projectManagementService',
+    'sessionService', 'silNoticeService', 'modalService', '$window',
+    function ($scope, projectService, appService, ss, notice, modalService, $window) {
 
       $scope.project = ss.session.project;
       $scope.actionInProgress = false;
+
       // Rights
       $scope.rights = {};
       $scope.rights.archive = (!ss.session.project.isArchived &&
-                              (ss.session.project.userIsProjectOwner || ss.hasSiteRight(ss.domain.PROJECTS, ss.operation.ARCHIVE) )
-                              );
+        (ss.session.project.userIsProjectOwner ||
+        ss.hasSiteRight(ss.domain.PROJECTS, ss.operation.ARCHIVE)));
 
       $scope.report = {
         output: '',
@@ -19,25 +22,26 @@ angular.module('projectmanagement', ['projectManagement.services', 'bellows.serv
       $scope.reportOutput = '';
 
       $scope.loadDto = function loadDto() {
-        appService.getDto(function(result) {
+        appService.getDto(function (result) {
           if (result.ok) {
             $scope.reports = result.data.reports;
             $scope.dtoLoaded = true;
           }
         });
       };
+
       $scope.loadDto();
 
-      $scope.$watch('report.currentId', function(reportId) {
+      $scope.$watch('report.currentId', function () {
         $scope.runReport();
       });
 
       $scope.runReport = function runReport() {
         if ($scope.report.currentId) {
           $scope.report.output = 'Running Report...';
-          appService.runReport($scope.report.currentId, [], function(result) {
+          appService.runReport($scope.report.currentId, [], function (result) {
             if (result.ok) {
-              $scope.report.output = result.data.output.replace(/\\n/g,"\n");
+              $scope.report.output = result.data.output.replace(/\\n/g, "\n"); // jscs:ignore
             }
           });
         } else {
@@ -46,21 +50,19 @@ angular.module('projectmanagement', ['projectManagement.services', 'bellows.serv
       };
 
       // Archive the project
-      $scope.archiveProject = function() {
-        var message = "Are you sure you want to archive this project?";
+      $scope.archiveProject = function () {
+        var message = 'Are you sure you want to archive this project?';
         var modalOptions = {
           closeButtonText: 'Cancel',
           actionButtonText: 'Archive',
           headerText: 'Archive Project?',
           bodyText: message
         };
-        modalService.showModal({}, modalOptions).then(function (result) {
+        modalService.showModal({}, modalOptions).then(function () {
           $scope.actionInProgress = true;
-          var archiveFunction;
-          archiveFunction = projectService.archive;
-          archiveFunction(function(result) {
+          projectService.archive(function (result) {
             if (result.ok) {
-              notice.push(notice.SUCCESS, "The project was archived successfully");
+              notice.push(notice.SUCCESS, 'The project was archived successfully');
               $window.location.href = '/app/projects';
             } else {
               $scope.actionInProgress = false;
@@ -69,6 +71,5 @@ angular.module('projectmanagement', ['projectManagement.services', 'bellows.serv
         });
       };
 
-
-    }])
-;
+    }
+  ]);
