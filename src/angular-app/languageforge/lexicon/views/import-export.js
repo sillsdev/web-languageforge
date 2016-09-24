bd23d40faa6f8a00b377d36ea015c23807a1525b
Ira Hopkinson <ira_hopkinson@sil.org>
2016-09-24 12:33:04 +0700

    fix JSCS

--- src/angular-app/languageforge/lexicon/views/import-export.js
+++ src/angular-app/languageforge/lexicon/views/import-export.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'palaso.ui.notice', 'palaso.ui.language', 'ngAnimate', 'ngFileUpload', 'lexicon.upload'])
-  .controller('LiftImportCtrl', ['$scope', '$upload', '$modal', 'silNoticeService', 'lexProjectService', '$filter', '$location', 'sessionService',
-  function($scope, $upload, $modal, notice, lexProjectService, $filter, $location, ss) {
+angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'palaso.ui.notice', 'palaso.ui.language',
+  'ngAnimate', 'ngFileUpload', 'lexicon.upload'])
+  .controller('LiftImportCtrl', ['$scope', '$upload', '$modal', 'silNoticeService', 'lexProjectService', '$filter',
+    '$location', 'sessionService',
+  function ($scope, $upload, $modal, notice, lexProjectService, $filter,
+            $location, sessionService) {
     lexProjectService.setBreadcrumbs('importExport', 'Import/export');
 
     $scope.upload = {};
@@ -18,7 +21,7 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
     };
 
     $scope.importLift = function importLift() {
-      if ($scope.upload.file.size <= ss.fileSizeMax()) {
+      if ($scope.upload.file.size <= sessionService.fileSizeMax()) {
         var uploadUrl = '/upload/lf-lexicon/import-lift';
         if ($scope.upload.isLift) {
           notice.setLoading('Importing LIFT file...');
@@ -26,53 +29,58 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
           notice.setLoading('Importing zipped file...');
           uploadUrl = '/upload/lf-lexicon/import-zip';
         }
+
         $scope.upload.importStarted = true;
         $scope.upload.progress = 0;
         $upload.upload({
 
           // Upload.php script
-          'url': uploadUrl,
+          url: uploadUrl,
+
           // 'headers': {'myHeaderKey': 'myHeaderVal'},
-          'data': {
-            'filename': $scope.upload.file.name,
-            'mergeRule': $scope.upload.mergeRule,
-            'skipSameModTime': $scope.upload.skipSameModTime,
-            'deleteMatchingEntry': $scope.upload.deleteMatchingEntry
+          data: {
+            filename: $scope.upload.file.name,
+            mergeRule: $scope.upload.mergeRule,
+            skipSameModTime: $scope.upload.skipSameModTime,
+            deleteMatchingEntry: $scope.upload.deleteMatchingEntry
           },
-          'file': $scope.upload.file
-        }).progress(function(evt) {
+          file: $scope.upload.file
+        }).progress(function (evt) {
           $scope.upload.progress = parseInt(100.0 * evt.loaded / evt.total);
-        }).success(function(data) {
+        }).success(function (data) {
           notice.cancelLoading();
           if (data.result) {
             $scope.upload.progress = 100.0;
             var modalInstance = $modal.open({
               templateUrl: '/angular-app/languageforge/lexicon/views/import-results.html',
-              controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
+              controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
                 $scope.show = {};
                 $scope.show.importErrors = false;
                 $scope.result = {
-                  'stats': data.data.stats,
-                  'importErrors': data.data.importErrors
+                  stats: data.data.stats,
+                  importErrors: data.data.importErrors
                 };
-                $scope.ok = function() {
+                $scope.ok = function () {
                   $modalInstance.close();
                 };
+
                 $scope.hasImportErrors = function hasImportErrorrs() {
                   return ($scope.result.importErrors !== '');
                 };
+
                 $scope.showImportErrorsButtonLabel = function showImportErrorsButtonLabel() {
                   if ($scope.show.importErrors) {
                     return $filter('translate')('Hide non-critical import errors');
                   }
+
                   return $filter('translate')('Show non-critical import errors');
                 };
               }]
             });
 
             // reload the config after the import is complete
-            modalInstance.result.then()['finally'](function() {
-              ss.refresh(function() {
+            modalInstance.result.then()['finally'](function () {
+              sessionService.refresh(function () {
                 notice.push(notice.SUCCESS, $filter('translate')('Import completed successfully'));
                 notice.push(notice.INFO, $filter('translate')('Your project was successfully imported.  Carefully review the dictionary configuration below before continuing, especially the input systems and fields tabs'));
                 $location.path('/configuration');
@@ -82,9 +90,10 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
             $scope.upload.progress = 0;
             notice.push(notice.ERROR, data.data.errorMessage);
           }
+
           $scope.upload.file = null;
           $scope.upload.importStarted = false;
-        }).error(function(data, status) {
+        }).error(function (data, status) {
           notice.cancelLoading();
           var errorMessage = $filter('translate')('Import failed.');
           if (status > 0) {
@@ -93,11 +102,12 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
               errorMessage += '- ' + data;
             }
           }
+
           notice.push(notice.ERROR, errorMessage);
         });
       } else {
         notice.push(notice.ERROR, '<b>' + $scope.upload.file.name + '</b> (' + $filter('bytes')($scope.upload.file.size) +
-            ') is too large. It must be smaller than ' + $filter('bytes')(ss.fileSizeMax()) + '.');
+            ') is too large. It must be smaller than ' + $filter('bytes')(sessionService.fileSizeMax()) + '.');
         $scope.upload.progress = 0;
         $scope.upload.file = null;
       }
@@ -109,6 +119,7 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
       if (a.length === 1 || (a[0] === '' && a.length === 2)) {
         return '';
       }
+
       return a.pop().toLowerCase();
     }
 
