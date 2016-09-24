1cba387c779eab32a866bb1b4f077cc9a68b0d52
Ira Hopkinson <ira_hopkinson@sil.org>
2016-09-24 18:08:52 +0700

    fix more upload (SF & LF)

--- src/angular-app/languageforge/lexicon/views/import-export.js
+++ src/angular-app/languageforge/lexicon/views/import-export.js
@@ -2,9 +2,9 @@
 
 angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'palaso.ui.notice', 'palaso.ui.language',
   'ngAnimate', 'ngFileUpload', 'lexicon.upload'])
-  .controller('LiftImportCtrl', ['$scope', '$upload', '$modal', 'silNoticeService', 'lexProjectService', '$filter',
+  .controller('LiftImportCtrl', ['$scope', 'Upload', '$modal', 'silNoticeService', 'lexProjectService', '$filter',
     '$location', 'sessionService',
-  function ($scope, $upload, $modal, notice, lexProjectService, $filter,
+  function ($scope, Upload, $modal, notice, lexProjectService, $filter,
             $location, sessionService) {
     lexProjectService.setBreadcrumbs('importExport', 'Import/export');
 
@@ -13,43 +13,44 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
     $scope.upload.skipSameModTime = true;
     $scope.upload.deleteMatchingEntry = false;
 
-    $scope.onFileSelect = function onFileSelect($files) {
-
-      // take the first file only
-      $scope.upload.file = $files[0];
+    $scope.onFileSelect = function onFileSelect($file) {
+      $scope.upload.file = $file;
       $scope.upload.isLift = (fileExtension($scope.upload.file.name) === 'lift');
     };
 
     $scope.importLift = function importLift() {
-      if ($scope.upload.file.size <= sessionService.fileSizeMax()) {
-        var uploadUrl = '/upload/lf-lexicon/import-lift';
-        if ($scope.upload.isLift) {
-          notice.setLoading('Importing LIFT file...');
-        } else {
-          notice.setLoading('Importing zipped file...');
-          uploadUrl = '/upload/lf-lexicon/import-zip';
-        }
-
-        $scope.upload.importStarted = true;
+      if (!$scope.upload.file || $scope.upload.file.$error) return;
+      if ($scope.upload.file.size > sessionService.fileSizeMax()) {
+        notice.push(notice.ERROR, '<b>' + $scope.upload.file.name + '</b> (' +
+          $filter('bytes')($scope.upload.file.size) + ') is too large. It must be smaller than ' +
+          $filter('bytes')(sessionService.fileSizeMax()) + '.');
         $scope.upload.progress = 0;
-        $upload.upload({
+        $scope.upload.file = null;
+        return;
+      }
 
-          // Upload.php script
-          url: uploadUrl,
+      var uploadUrl = '/upload/lf-lexicon/import-lift';
+      if ($scope.upload.isLift) {
+        notice.setLoading('Importing LIFT file...');
+      } else {
+        notice.setLoading('Importing zipped file...');
+        uploadUrl = '/upload/lf-lexicon/import-zip';
+      }
 
-          // 'headers': {'myHeaderKey': 'myHeaderVal'},
-          data: {
-            filename: $scope.upload.file.name,
-            mergeRule: $scope.upload.mergeRule,
-            skipSameModTime: $scope.upload.skipSameModTime,
-            deleteMatchingEntry: $scope.upload.deleteMatchingEntry
-          },
-          file: $scope.upload.file
-        }).progress(function (evt) {
-          $scope.upload.progress = parseInt(100.0 * evt.loaded / evt.total);
-        }).success(function (data) {
+      $scope.upload.importStarted = true;
+      $scope.upload.progress = 0;
+      Upload.upload({
+        url: uploadUrl,
+        data: {
+          file: $scope.upload.file,
+          mergeRule: $scope.upload.mergeRule,
+          skipSameModTime: $scope.upload.skipSameModTime,
+          deleteMatchingEntry: $scope.upload.deleteMatchingEntry
+        }
+      }).then(function (response) {
           notice.cancelLoading();
-          if (data.result) {
+          var isUploadSuccess = response.data.result;
+          if (isUploadSuccess) {
             $scope.upload.progress = 100.0;
             var modalInstance = $modal.open({
               templateUrl: '/angular-app/languageforge/lexicon/views/import-results.html',
@@ -57,8 +58,8 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
                 $scope.show = {};
                 $scope.show.importErrors = false;
                 $scope.result = {
-                  stats: data.data.stats,
-                  importErrors: data.data.importErrors
+                  stats: response.data.data.stats,
+                  importErrors: response.data.data.importErrors
                 };
                 $scope.ok = function () {
                   $modalInstance.close();
@@ -88,29 +89,33 @@ angular.module('lexicon.import-export', ['ui.bootstrap', 'bellows.services', 'pa
             });
           } else {
             $scope.upload.progress = 0;
-            notice.push(notice.ERROR, data.data.errorMessage);
+            notice.push(notice.ERROR, response.data.data.errorMessage);
           }
 
           $scope.upload.file = null;
           $scope.upload.importStarted = false;
-        }).error(function (data, status) {
+        },
+
+        function (response) {
           notice.cancelLoading();
           var errorMessage = $filter('translate')('Import failed.');
-          if (status > 0) {
-            errorMessage += ' Status: ' + status;
-            if (data) {
-              errorMessage += '- ' + data;
+          if (response.status > 0) {
+            errorMessage += ' Status: ' + response.status;
+            if (response.statusText) {
+              errorMessage += ' ' + response.statusText;
+            }
+
+            if (response.data) {
+              errorMessage += '- ' + response.data;
             }
           }
 
           notice.push(notice.ERROR, errorMessage);
+        },
+
+        function (evt) {
+          notice.setPercentComplete(100.0 * evt.loaded / evt.total);
         });
-      } else {
-        notice.push(notice.ERROR, '<b>' + $scope.upload.file.name + '</b> (' + $filter('bytes')($scope.upload.file.size) +
-            ') is too large. It must be smaller than ' + $filter('bytes')(sessionService.fileSizeMax()) + '.');
-        $scope.upload.progress = 0;
-        $scope.upload.file = null;
-      }
     };
 
     // see http://stackoverflow.com/questions/190852/how-can-i-get-file-extensions-with-javascript
