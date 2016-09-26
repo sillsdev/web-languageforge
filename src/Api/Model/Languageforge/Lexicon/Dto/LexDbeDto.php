57e91a7d36404ac036ba20d15d7138a08c65da7e
Ira Hopkinson <ira_hopkinson@sil.org>
2016-09-26 10:04:26 +0700

    deploy: fix unit test folder permissions

--- src/Api/Model/Languageforge/Lexicon/Dto/LexDbeDto.php
+++ src/Api/Model/Languageforge/Lexicon/Dto/LexDbeDto.php
@@ -12,16 +12,16 @@ use Api\Model\Shared\UserGenericVoteModel;
 
 class LexDbeDto
 {
+    const MAX_ENTRIES_PER_REQUEST = 2500;
+
     /**
-     * @param  string     $projectId
-     * @param $userId
-     * @param  null       $lastFetchTime
+     * @param string $projectId
+     * @param string $userId
+     * @param null $lastFetchTime
+     * @param int $offset
      * @throws \Exception
      * @return array
      */
-
-    const MAX_ENTRIES_PER_REQUEST = 2500;
-
     public static function encode($projectId, $userId, $lastFetchTime = null, $offset = 0)
     {
         $data = array();
