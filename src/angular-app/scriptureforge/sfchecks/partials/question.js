c3a41170ad96ff39b3ad7b6a54391fc73712a52c
Ira Hopkinson <ira_hopkinson@sil.org>
2016-09-28 16:02:15 +0700

    fix rangy

--- src/angular-app/scriptureforge/sfchecks/partials/question.js
+++ src/angular-app/scriptureforge/sfchecks/partials/question.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sfchecks.question', ['ui.bootstrap', 'bellows.services', 'sgw.soundmanager',
-  'sfchecks.services', 'ngRoute', 'palaso.ui.listview', 'palaso.ui.jqte', // 'palaso.ui.selection',
+  'sfchecks.services', 'ngRoute', 'palaso.ui.listview', 'palaso.ui.jqte', 'palaso.ui.selection',
   'palaso.ui.tagging', 'palaso.ui.notice'])
   .controller('QuestionCtrl', ['$scope', '$routeParams', 'questionService', 'sessionService',
     'utilService', 'breadcrumbService', 'silNoticeService', 'sfchecksLinkService', 'modalService',
