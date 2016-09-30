0edd2f1469605e8bd5629ffcdce4f52d78542420
Ira Hopkinson <ira_hopkinson@sil.org>
2016-09-30 09:43:40 +0700

    change to use one package.json
    Former-commit-id: 2b049645bf52dac01af620c606dac844a6626b21

--- test/app/rune2eLF.sh
+++ test/app/rune2eLF.sh
@@ -4,9 +4,9 @@ sh useTestConfig.sh
 sudo -u www-data php setupTestEnvironment.php "languageforge.local"
 if [ $# -eq 0 ]
   then
-    node ./node_modules/protractor/bin/protractor protractorConf.languageforge.js
+    node ../../node_modules/protractor/bin/protractor protractorConf.languageforge.js
   else
-    node ./node_modules/protractor/bin/protractor protractorConf.languageforge.js $2 --specs "./allspecs/e2e/*.spec.js,`find . -wholename "*languageforge*e2e*$1*.spec.js" -printf "%p,"|perl -p -e 's/,$//'`"
+    node ../../node_modules/protractor/bin/protractor protractorConf.languageforge.js $2 --specs "./allspecs/e2e/*.spec.js,`find . -wholename "*languageforge*e2e*$1*.spec.js" -printf "%p,"|perl -p -e 's/,$//'`"
 fi
 sudo -u www-data php teardownTestEnvironment.php
 sh useLiveConfig.sh
--- test/app/start webdriver.bat
+++
@@ -1,2 +0,0 @@
-node .\node_modules\protractor\bin\webdriver-manager update --standalone
-node .\node_modules\protractor\bin\webdriver-manager start
