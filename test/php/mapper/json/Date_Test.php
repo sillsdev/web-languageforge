4ff889628bdf9e5f8abe48db3432734d9e4c0de3
megahirt <chris@hirtfamily.net>
2016-10-07 17:09:35 +0700

    fix up tests for php 7.0

--- test/php/mapper/json/Date_Test.php
+++ test/php/mapper/json/Date_Test.php
@@ -11,8 +11,8 @@ class TestJsonDateModel
 {
     public function __construct()
     {
-        $this->dateTime =  new DateTime('2013-08-01 12:01:01.321');
-        $this->universalTimestamp = UniversalTimestamp::fromStringTimestamp('2013-08-01 12:01:01.321');
+        $this->dateTime =  new DateTime();
+        $this->universalTimestamp = UniversalTimestamp::now();
     }
 
     public $dateTime;
@@ -22,10 +22,13 @@ class TestJsonDateModel
 
 class TestJsonDateMapper extends UnitTestCase
 {
+    const StringTimestamp = '2001-01-01 12:01:01';
+    const StringTimestampWithMilliseconds = '2001-01-01 12:01:01.321';
     public function testEncodeDecodeDateTime_HistoricalDate_Same()
     {
         $model = new TestJsonDateModel();
-        $model->dateTime = new DateTime('2001-01-01 12:01:01.321');
+        $model->dateTime = new DateTime(self::StringTimestamp);
+        $model->universalTimestamp = UniversalTimestamp::fromStringTimestamp(self::StringTimestamp);
         $encoded = JsonEncoder::encode($model);
         $this->assertIsA($encoded['dateTime'], 'string');
 //        var_dump($encoded);
@@ -35,13 +38,13 @@ class TestJsonDateMapper extends UnitTestCase
         $iso8601 = $decodedModel->dateTime->format(DateTime::ISO8601);
         $this->assertEqual($encoded['dateTime'], $iso8601);
         $this->assertEqual($model->dateTime, $decodedModel->dateTime);
-//        var_dump($iso8601);
+ //       var_dump($iso8601);
     }
 
     public function testEncodeDecodeUniversalTimestamp_HistoricalDate_Same()
     {
         $model = new TestJsonDateModel();
-        $model->universalTimestamp = UniversalTimestamp::fromStringTimestamp('2001-01-01 12:01:01.321');
+        $model->universalTimestamp = UniversalTimestamp::fromStringTimestamp(self::StringTimestampWithMilliseconds);
         $encoded = JsonEncoder::encode($model);
         $this->assertIsA($encoded['universalTimestamp'], 'string');
 //        var_dump($encoded);
