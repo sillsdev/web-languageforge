f6bf5d7a525a49a3d9fd4ea6ae414e751d35cda7
Ira Hopkinson <ira_hopkinson@sil.org>
2016-10-17 12:00:56 +0700

    fix JSCS
    Former-commit-id: 3655c89c9d37b2126687ac0ee8e989011566c478

--- src/angular-app/languageforge/lexicon/directive/dc-formattedtext.js
+++ src/angular-app/languageforge/lexicon/directive/dc-formattedtext.js
@@ -3,34 +3,38 @@
 angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular'])
 
 // Custom textAngular tool for language spans
-.config(['$provide', function($provide) {
-  
+.config(['$provide', function ($provide) {
+
   // add custom tools. Note: $delegate is the taOptions we are decorating
-  $provide.decorator('taOptions', ['taRegisterTool', '$delegate',  'taTranslations', 'taTools', 'sessionService', '$window', '$compile', '$animate',
-  function(taRegisterTool, taOptions, taTranslations, taTools, ss, $window, $compile, $animate) {
+  $provide.decorator('taOptions', ['taRegisterTool', '$delegate',  'taTranslations', 'taTools',
+    'sessionService', '$window', '$compile', '$animate',
+  function (taRegisterTool, taOptions, taTranslations, taTools,
+            ss, $window, $compile, $animate) {
 
     // remove the built-in insertLink tool
     //delete taTools.insertLink;
 
     // register the tool with textAngular
     taRegisterTool('lexInsertLink', {
-//      tooltiptext: taTranslations.lexInsertLink.tooltip,
+      //      tooltiptext: taTranslations.lexInsertLink.tooltip,
       tooltiptext: 'Insert/edit link',
       iconclass: 'fa fa-link',
-      action: function() {
+      action: function () {
         var urlLink;
         urlLink = $window.prompt(taTranslations.lexInsertLink.dialogPrompt, 'http://');
         if (urlLink && urlLink !== '' && urlLink !== 'http://') {
           return this.$editor().wrapSelection('createLink', urlLink, true);
         }
       },
-      activeState: function(commonElement) {
+
+      activeState: function (commonElement) {
         if (commonElement) return commonElement[0].tagName === 'A';
         return false;
       },
+
       onElementSelect: {
         element: 'a',
-        action: function(event, $element, editorScope) {
+        action: function (event, $element, editorScope) {
 
           // setup the editor toolbar
           // Credit to the work at http://hackerwins.github.io/summernote/ for this editbar logic
@@ -39,48 +43,62 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
           var container = editorScope.displayElements.popoverContainer;
           container.empty();
           container.css('line-height', '28px');
-          var link = angular.element('<a href="' + $element.attr('href') + '" target="_blank">' + $element.attr('href') + '</a>');
+          var link = angular.element('<a href="' + $element.attr('href') + '" target="_blank">' +
+            $element.attr('href') + '</a>');
           link.css({
-            'display': 'inline-block',
+            display: 'inline-block',
             'max-width': '200px',
-            'overflow': 'hidden',
+            overflow: 'hidden',
             'text-overflow': 'ellipsis',
             'white-space': 'nowrap',
             'vertical-align': 'middle'
           });
           container.append(link);
-          var buttonGroup = angular.element('<div class="btn-group pull-right">'),
-              reLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-edit icon-edit"></i></button>');
-          reLinkButton.on('click', function(event) {
+          var buttonGroup = angular.element('<div class="btn-group pull-right">');
+          var reLinkButton = angular.element('<button type="button" ' +
+            'class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">' +
+            '<i class="fa fa-edit icon-edit"></i></button>');
+          reLinkButton.on('click', function (event) {
             event.preventDefault();
-            var urlLink = $window.prompt(taTranslations.lexInsertLink.dialogPrompt, $element.attr('href'));
+            var urlLink = $window.prompt(taTranslations.lexInsertLink.dialogPrompt,
+              $element.attr('href'));
             if (urlLink && urlLink !== '' && urlLink !== 'http://') {
               $element.attr('href', urlLink);
               editorScope.updateTaBindtaTextElement();
             }
+
             editorScope.hidePopover();
           });
-          buttonGroup.append(reLinkButton);
-          var unLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-unlink icon-unlink"></i></button>');
 
-          // directly before this click event is fired a digest is fired off whereby the reference to $element is orphaned off
-          unLinkButton.on('click', function(event) {
+          buttonGroup.append(reLinkButton);
+          var unLinkButton = angular.element('<button type="button" ' +
+            'class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">' +
+            '<i class="fa fa-unlink icon-unlink"></i></button>');
+
+          // directly before this click event is fired a digest is fired off whereby the reference
+          // to $element is orphaned off
+          unLinkButton.on('click', function (event) {
             event.preventDefault();
             $element.replaceWith($element.contents());
             editorScope.updateTaBindtaTextElement();
             editorScope.hidePopover();
           });
+
           buttonGroup.append(unLinkButton);
-          var targetToggle = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">Open in New Window</button>');
+          var targetToggle = angular.element('<button type="button" ' +
+            'class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">' +
+            'Open in New Window</button>');
           if ($element.attr('target') === '_blank') {
             targetToggle.addClass('active');
           }
-          targetToggle.on('click', function(event) {
+
+          targetToggle.on('click', function (event) {
             event.preventDefault();
             $element.attr('target', ($element.attr('target') === '_blank') ? '' : '_blank');
             targetToggle.toggleClass('active');
             editorScope.updateTaBindtaTextElement();
           });
+
           buttonGroup.append(targetToggle);
           container.append(buttonGroup);
           editorScope.showPopover($element);
@@ -88,7 +106,8 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
       }
     });
 
-    // Written by the author of Rangy, see http://stackoverflow.com/questions/4652734/return-html-from-a-user-selected-text
+    // Written by the author of Rangy
+    // see http://stackoverflow.com/questions/4652734/return-html-from-a-user-selected-text
     function getSelectionHtml() {
       var html = '';
       if (typeof window.getSelection != 'undefined') {
@@ -98,6 +117,7 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
           for (var i = 0, len = sel.rangeCount; i < len; ++i) {
             container.appendChild(sel.getRangeAt(i).cloneContents());
           }
+
           html = container.innerHTML;
         }
       } else if (typeof document.selection != 'undefined') {
@@ -105,35 +125,40 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
           html = document.selection.createRange().htmlText;
         }
       }
+
       return html;
     }
-    
+
     taRegisterTool('languageSpan', {
       tooltiptext: 'Create language span',
       iconclass: 'fa fa-language fa-lg',
       action: function createLanguageSpan() {
         var selectedHtml = getSelectionHtml();
         if (selectedHtml && selectedHtml !== '') {
-          var languageSpan = '<span lang="">' + selectedHtml + '</span>',
-            element = angular.element(languageSpan),
-            result = this.$editor().wrapSelection('insertHTML', languageSpan, false);
+          var languageSpan = '<span lang="">' + selectedHtml + '</span>';
+          var element = angular.element(languageSpan);
+          var result = this.$editor().wrapSelection('insertHTML', languageSpan, false);
           this.onElementSelect.action(null, element, this.$editor());
           return result;
         }
+
         return false;
       },
-      activeState: function(commonElement){
-        
+
+      activeState: function (commonElement) {
+
         // only works if span is inside another element
-        if(commonElement) {
+        if (commonElement) {
           if (commonElement[0].parentElement.tagName === 'SPAN') return true;
           return commonElement[0].tagName === 'SPAN';
         }
+
         return false;
       },
+
       onElementSelect: {
         element: 'span',
-        action: function(event, $element, editorScope) {
+        action: function (event, $element, editorScope) {
           var inputSystems = ss.session.projectSettings.config.inputSystems;
           editorScope.selects = {};
           editorScope.selects.language = {};
@@ -145,10 +170,11 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
             if (languageName === 'Unlisted Language') {
               languageName += ' (' + tag + ')';
             }
+
             editorScope.selects.language.options[tag] = languageName;
             editorScope.selects.language.optionsOrder.push(tag);
           });
-          
+
           editorScope.displayElements.popover.css('width', '300px');
           editorScope.displayElements.popover.attr('data-container', 'body');
           editorScope.displayElements.popover.off('mousedown');
@@ -156,39 +182,44 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
           container.empty();
           container.css('line-height', '28px');
           var langSelect = angular.element(
-                '<select data-ng-model="selects.language.tag"' +
-                  'data-ng-options="selects.language.options[tag] for tag in selects.language.optionsOrder">' +
-                  '<option value="">-- choose a language --</option></select>'
-              );
+            '<select data-ng-model="selects.language.tag" ' +
+            'data-ng-options="selects.language.options[tag] ' +
+            'for tag in selects.language.optionsOrder">' +
+            '<option value="">-- choose a language --</option></select>'
+          );
           container.append(langSelect);
-          var buttonGroup = angular.element('<div class="btn-group pull-right">'),
-              unLinkButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on"><i class="fa fa-unlink icon-unlink"></i></button>');
+          var buttonGroup = angular.element('<div class="btn-group pull-right">');
+          var unLinkButton = angular.element('<button type="button" ' +
+            'class="btn btn-default btn-sm btn-small" tabindex="-1" unselectable="on">' +
+            '<i class="fa fa-unlink icon-unlink"></i></button>');
 
-          // directly before this click event is fired a digest is fired off whereby the reference to $element is orphaned off
-          unLinkButton.on('click', function(event) {
+          // directly before this click event is fired a digest is fired off whereby the reference
+          // to $element is orphaned off
+          unLinkButton.on('click', function (event) {
             event.preventDefault();
             $element.replaceWith($element.contents());
             editorScope.updateTaBindtaTextElement();
             editorScope.hidePopover();
           });
+
           buttonGroup.append(unLinkButton);
           container.append(buttonGroup);
           $compile(editorScope.displayElements.popover)(editorScope);
-          
+
           // change event declared after $compile so that the bindings are in place
-          langSelect.on('change', function(event) {
+          langSelect.on('change', function (event) {
             event.preventDefault();
             $element.attr('lang', editorScope.selects.language.tag);
             editorScope.updateTaBindtaTextElement();
             editorScope.hidePopover();
-            
+
             console.log('langSelect change: ', $element.attr('lang'));
           });
-          
+
           if (event) {
             editorScope.$apply();
           }
-          
+
           // use code below (removes close event) instead of editorScope.showPopover($element);
           editorScope.displayElements.popover.css('display', 'block');
           editorScope.reflowPopover($element);
@@ -201,39 +232,39 @@ angular.module('palaso.ui.dc.formattedtext', ['bellows.services', 'textAngular']
     taOptions.toolbar[0].push('lexInsertLink', 'languageSpan');
     return taOptions;
   }]);
-  
+
   // add element selector strings that are used to catch click events within a taBind
-  $provide.decorator('taSelectableElements', ['$delegate', function(taSelectableElements) {
+  $provide.decorator('taSelectableElements', ['$delegate', function (taSelectableElements) {
     taSelectableElements.push('span');
     return taSelectableElements;
   }]);
 }])
 
 // Dictionary Control Formatted Text Editor
-.directive('dcFormattedtext', [function() {
+.directive('dcFormattedtext', [function () {
   return {
     restrict: 'E',
     templateUrl: '/angular-app/languageforge/lexicon/directive/dc-formattedtext.html',
     scope: {
-      fteModel: "=",
-      fteToolbar: "=",
-      fteDisabled: "=",
-      fteDir: "="
+      fteModel: '=',
+      fteToolbar: '=',
+      fteDisabled: '=',
+      fteDir: '='
     },
-    controller: ['$scope', 'sessionService', function($scope, ss) {
+    controller: ['$scope', 'sessionService', function ($scope, ss) {
       $scope.fte = {};
       if (angular.isDefined($scope.fteToolbar)) {
         $scope.fte.toolbar = $scope.fteToolbar;
       } else if (ss.hasSiteRight(ss.domain.PROJECTS, ss.operation.EDIT)) {
-        
+
         // if site administrator enable development controls
-//        $scope.fte.toolbar = "[['lexInsertLink', 'languageSpan'], ['html']]";
+        //        $scope.fte.toolbar = "[['lexInsertLink', 'languageSpan'], ['html']]";
         // html toggle for development only
         $scope.fte.toolbar = "[['lexInsertLink', 'languageSpan']]";
       } else {
-//        $scope.fte.toolbar = "[['lexInsertLink', 'languageSpan']]";
+        //        $scope.fte.toolbar = "[['lexInsertLink', 'languageSpan']]";
         // disable unfinished link and language span controls
-        $scope.fte.toolbar = "[[]]";
+        $scope.fte.toolbar = '[[]]';
       }
     }]
   };
