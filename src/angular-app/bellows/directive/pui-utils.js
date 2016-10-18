68ba82fe94ac2d18ff084fc3717b1a39f9cbe7f8
megahirt <chris@hirtfamily.net>
2016-10-18 10:37:55 +0700

    add Korean composition support to typeahead search

--- src/angular-app/bellows/directive/pui-utils.js
+++ src/angular-app/bellows/directive/pui-utils.js
@@ -48,4 +48,27 @@ angular.module('palaso.ui.utils', [])
     };
   }])
 
+  // see http://stackoverflow.com/questions/27663149/angularjs-trigger-ng-change-ng-keyup-or-scope-watch-while-composing-korean-c
+  // this might not work on firefox.  See link above for alternate code
+
+  // Angular's ng-change, ng-keyup and $scope.$watch don't get triggered
+  // while composing (e.g. when writing Korean syllables).
+  // See: https://github.com/angular/angular.js/issues/10588
+  // This custom directive uses element.on('input') instead, which gets
+  // triggered while composing.
+  .directive('compositionInput', function() {
+    return {
+      restrict: 'A',
+      require: '^ngModel',
+      link: function (scope, element, attrs, ngModel) {
+        element.on('input', function() {
+          scope.$apply(function(){
+            scope.ngModel = element.val();
+            scope.$eval(attrs.cstInput, {'answer': scope.ngModel}); // only works if no scope has been defined in directive
+          });
+        });
+      }
+    };
+  })
+
 ;
