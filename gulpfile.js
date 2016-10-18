93e92c3268eca6601813511bae6dcdd328796512
Darcy Wong <darcy_wong@sil.org>
2016-10-18 22:56:45 +0700

    Update to gulp4 and use gulp to run E2E tests
    -update gulpfile.js for gulp4 syntax
    -create gulp tasks for running E2E
    syntax is `gulp test-e2e-run --hostname [host] --specs [test specs] --verbosity [bool]`
    -cleaned up legacy protractor conf files
    -created new rune2e.sh at the root dir
    -updates to use jasmine2
    
    
    Former-commit-id: e31076618c6d1c01af8b4c05c0b3479202761b37

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,63 @@
-// npm install gulp gulp-util async gulp-livereload
+// *************************************
+//
+//   Gulpfile
+//
+// *************************************
+//
+// Available tasks:
+//   'gulp'
+//   'do-reload'
+//   'reload'
+//   'lint'
+//   'upload'
+//   'mongodb-backup-prod-db'
+//   'mongodb-copy-backup-to-local'
+//   'mongodb-cleanup-backup-prod-db'
+//   'mongodb-restore-local-db'
+//   'mongodb-copy-prod-db'
+//   'test-php'
+//   'test-php-coverage-open'
+//   'test-php-coverage-close'
+//   'test-js-unit'
+//   'test-e2e-webdriver_update'
+//   'test-e2e-webdriver_standalone'
+//   'test-e2e-parseArgs'
+//   'test-e2e-useTestConfig'
+//   'test-e2e-useLiveConfig'
+//   'test-e2e-setupTestEnvironment'
+//   'test-e2e-teardownTestEnvironment'
+//   'test-e2e-doTest'
+//   'test-e2e-run'
+//   'tasks'
+//   'default'
 
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var exec = require('child_process').exec;
+// -------------------------------------
+//   Modules
+// -------------------------------------
+//
+// async             : Higher-order functions and common patterns for asynchronous code
+// exec              : Call a child process with the ease of exec and safety of spawn
+// gulp              : The streaming build system
+// gulp-jshint       : JSHint plugin for gulp
+// gulp-livereload   : Gulp plugin for livereload
+// gulp-markdown     : Markdown to HTML
+// gulp-protractor   : A helper for protactor and gulp
+// gulp-util         : Utility functions for gulp plugins
+// karma             : Spectacular Test Runner for JavaScript
+// sylish            : Stylish reporter for JSHint
+// yargs             : yargs the modern, pirate-themed, successor to optimist
 var async = require('async');
-var markdown = require('gulp-markdown');
+var exec = require('child_process').exec;
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var livereload = require('gulp-livereload');
-
-var reloadPatterns = [
-  'src/angular-app/**',
-  'src/Api/**',
-  'src/Site/**',
-  'test/**'
-];
+var markdown = require('gulp-markdown');
+var protractor = require('gulp-protractor').protractor;
+var webdriverStandalone = require('gulp-protractor').webdriver_standalone;
+var webdriverUpdate = require('gulp-protractor').webdriver_update;
+var gutil = require('gulp-util');
+var Server = require('karma').Server;
+var stylish = require('jshint-stylish');
 
 var execute = function (command, callback) {
   gutil.log(gutil.colors.green(command));
@@ -23,22 +68,40 @@ var execute = function (command, callback) {
   });
 };
 
-gulp.task('default', function () {
-  return gulp.src('src/angular-app/**/helps/**/*.md').pipe(markdown())
-    .pipe(gulp.dest('src/angular-app'));
+var srcPatterns = [
+  'src/angular-app/**',
+  'src/Api/**',
+  'src/Site/**',
+  'test/**'
+];
 
-  // place code for your default task here
-});
-
+// -------------------------------------
+//   Task: Do Reload
+// -------------------------------------
 gulp.task('do-reload', function () {
   return gulp.src('src/index.php').pipe(livereload());
 });
 
+// -------------------------------------
+//   Task: Reload
+// -------------------------------------
 gulp.task('reload', function () {
   livereload.listen();
-  gulp.watch(reloadPatterns, ['do-reload']);
+  gulp.watch(srcPatterns, gulp.series('do-reload'));
 });
 
+// -------------------------------------
+//   Task: Lint
+// -------------------------------------
+gulp.task('lint', function () {
+  return gulp.src(srcPatterns)
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
+});
+
+// -------------------------------------
+//   Task: Upload
+// -------------------------------------
 gulp.task('upload', function () {
   var options = {
     silent: false,
@@ -51,25 +114,90 @@ gulp.task('upload', function () {
     '--rsh="ssh -i <%= options.key %>" <%= options.src %>/ <%= options.dest %>', options));
 });
 
+//region MongoDB
+
+// -------------------------------------
+//   Task: MongoDB: Backup Production
+// -------------------------------------
+gulp.task('mongodb-backup-prod-db', function (cb) {
+  execute('ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/backupMongoOnServer.sh',
+      function () {
+        cb(null);
+      });
+});
+
+// -------------------------------------
+//   Task: MongoDB: Copy Backup to Local
+// -------------------------------------
+gulp.task('mongodb-copy-backup-to-local', function (cb) {
+  var padLeft = function (i, len, ch) {
+    var val = i.toString();
+    while (val.length < len)
+      val = ch + val;
+    return val;
+  };
+
+  var formatDateYMD = function (date) {
+    return date.getFullYear() + '-' + padLeft(date.getMonth() + 1, 2, '0') + '-' +
+        padLeft(date.getDate(), 2, '0');
+  };
+
+  var today = formatDateYMD(new Date());
+  execute('scp scriptureforge.org:mongodb_backup_' + today + '.tgz /tmp/', function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: MongoDB: Clean Backup Production
+// -------------------------------------
+gulp.task('mongodb-cleanup-backup-prod-db', function (cb) {
+  execute('ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/deleteTarFileOnServer.sh',
+      function () {
+        cb(null);
+      });
+});
+
+// -------------------------------------
+//   Task: MongoDB: Restore Mongo on Local
+// -------------------------------------
+gulp.task('mongodb-restore-local-db', function (cb) {
+  // To set the username, edit your ssh config file (~/.ssh/config) and add an entry:
+  // Host scriptureforge.org
+  //     User jdoe
+  execute('scripts/server/mongodb/restoreMongoOnLocal.sh /tmp', function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: MongoDB: Copy Production (Backup MongoDB on server and restore on local machine)
+// -------------------------------------
+gulp.task('mongodb-copy-prod-db',
+    gulp.series(
+        'mongodb-backup-prod-db',
+        'mongodb-copy-backup-to-local',
+        gulp.parallel('mongodb-cleanup-backup-prod-db', 'mongodb-restore-local-db')
+    )
+);
+
+//endregion
+
+//region PHPTests
+
+// -------------------------------------
+//   Task: test-php
+// -------------------------------------
 gulp.task('test-php', function (cb) {
   execute('php test/php/languageforge/lexicon/AllTests.php', function (err) {
     cb(err);
   });
 });
 
-gulp.task('test-local', function (cb) {
-  execute('php test/php/languageforge/lexicon/LiftImportFlex_Test.php', function () {
-    cb(null);
-  });
-});
-
-gulp.task('test-local-watch', function () {
-  gulp.watch(['./src/Api/Model/**/*.php', './src/Api/Library/**/*.php',
-    './test/php/languageforge/lexicon/**/*.php', '!./src/vendor/'], ['test-local']);
-});
-
-var coverageFolder = 'src/vendor/simpletest/simpletest/extensions/coverage/';
-gulp.task('coverage-open', function (cb) {
+// -------------------------------------
+//   Task: test-php-coverage-open
+// -------------------------------------
+gulp.task('test-php-coverage-open', function (cb) {
   var options = {
     includes: [
       'src/Api/Library/.*\.php$',
@@ -83,6 +211,7 @@ gulp.task('coverage-open', function (cb) {
       'lib/.*'
     ]
   };
+  var coverageFolder = 'src/vendor/simpletest/simpletest/extensions/coverage/';
   var args = '--';
   var command = function (commandName, args) {
     return 'php ' + coverageFolder + commandName + ' ' + args;
@@ -102,7 +231,10 @@ gulp.task('coverage-open', function (cb) {
 
 });
 
-gulp.task('coverage-close', function (cb) {
+// -------------------------------------
+//   Task: test-php-coverage-close
+// -------------------------------------
+gulp.task('test-php-coverage-close', function (cb) {
   var command = function (commandName, args) {
     return 'php ' + coverageFolder + commandName + ' ' + args;
   };
@@ -125,57 +257,187 @@ gulp.task('coverage-close', function (cb) {
 
 });
 
-gulp.task('backup-prod-db', function (cb) {
-  execute('ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/backupMongoOnServer.sh',
-    function () {
-      cb(null);
-    });
+//endregion
+
+// -------------------------------------
+//   Task: test-js-unit
+// -------------------------------------
+gulp.task('test-js-unit', function (cb) {
+  console.log('cwd: ', __dirname);
+  new Server({
+    configFile: __dirname + '/test/app/karma.conf.js',
+    reporters: 'teamcity',
+    singleRun: true
+  }, cb).start();
 });
 
-gulp.task('cleanup-backup-prod-db', ['backup-prod-db', 'copy-backup-to-local'], function (cb) {
-  execute('ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/deleteTarFileOnServer.sh',
-    function () {
-      cb(null);
-    });
-});
+//region E2ETest
 
-gulp.task('copy-backup-to-local', ['backup-prod-db'], function (cb) {
-  padLeft = function (i, len, ch) {
-    var val = i.toString();
-    while (val.length < len)
-      val = ch + val;
-    return val;
-  };
+// Global vars for configuring protractor
+var protractorOptions = {
+  configFile: './test/app/protractorConf.js',
+  args: [],
+  debug: false
+};
+var hostname = '';
 
-  formatDateYMD = function (date) {
-    return date.getFullYear() + '-' + padLeft(date.getMonth() + 1, 2, '0') + '-' +
-      padLeft(date.getDate(), 2, '0');
-  };
+// list of specs to test.  More specs added in later tasks
+var specs = ['test/app/allspecs/e2e/*.spec.js'];
 
-  var today = formatDateYMD(new Date());
-  execute('scp scriptureforge.org:mongodb_backup_' + today + '.tgz /tmp/', function () {
-    cb(null);
-  });
-});
+// -------------------------------------
+//   Task: E2E Test: Webdriver Update
+// -------------------------------------
+gulp.task('test-e2e-webdriver_update', webdriverUpdate);
 
-gulp.task('restore-local-db', ['backup-prod-db', 'copy-backup-to-local'], function (cb) {
-  // To set the username, edit your ssh config file (~/.ssh/config) and add an entry:
-  // Host scriptureforge.org
-  //     User jdoe
-  execute('scripts/server/mongodb/restoreMongoOnLocal.sh /tmp', function () {
-    cb(null);
-  });
-});
+// -------------------------------------
+//   Task: E2E Test: Webdriver Standalone
+// -------------------------------------
+gulp.task('test-e2e-webdriver_standalone', webdriverStandalone);
 
-// Backup database on server and restore on local machine
-gulp.task('copy-prod-db', ['backup-prod-db', 'cleanup-backup-prod-db', 'copy-backup-to-local',
-  'restore-local-db'], function (cb) {
-    cb(null);
+// -------------------------------------
+//   Task: E2E Test: Parse Args
+// -------------------------------------
+gulp.task('test-e2e-parseArgs', function (cb) {
+  var options = require('yargs')
+      .usage(
+        'Usage: $0 test-e2e-parseArgs --hostname [hostname] --specs [testSpecs] --verbosity [bool]')
+      .option('hostname', {
+        demand: true,
+        describe: "local hostname is 'languageforge.local' or " +
+          "'scriptureforge.local' or 'jamaicanpsalms.scriptureforge.local'",
+        type: 'string' })
+      .option('specs', {
+        demand: false,
+        describe: 'testSpecs are the names of the e2e test specs to run',
+        type: 'string' })
+      .option('verbosity', {
+        demand: false,
+        describe: 'bool for jasmine reporter verbosity.  true for more detail',
+        type: 'boolean' })
+      .help('?')
+      .alias('?', 'help')
+      .example('$0 test-e2e-parseArgs --hostname languageforge.local',
+        'Runs all the E2E tests for languageforge')
+      .example('$0 test-e2e-parseArgs --hostname scriptureforge.local --specs projectSettingsPage',
+        'Runs the scriptureforge E2E test for projectSettingsPage').argv;
+
+  // Get the hostname from the first parameter
+  if (options.hostname && options.hostname.length > 0) {
+    hostname = options.hostname.toLowerCase();
+    if (hostname == 'jamaicanpsalms.scriptureforge.local') {
+      protractorOptions.args.push('--baseUrl', 'https://' + hostname);
+    } else {
+      protractorOptions.args.push('--baseUrl', 'http://' + hostname);
+    }
+  } else {
+    console.log('Failed to provide a valid hostname.  Exiting');
+    process.exit(-1);
   }
+
+  // Generate list of specs to test (glob format so protractor will test whatever files exist)
+  var specString = ((options.specs) && (options.specs.length > 0)) ? options.specs : '*';
+  if (hostname.includes('languageforge')) {
+    specs.push('test/app/bellows/**/e2e/' + specString + '.spec.js',
+      'test/app/languageforge/**/e2e/' + specString + '.spec.js');
+  } else {
+    specs.push('test/app/bellows/**/e2e/' + specString + '.spec.js',
+      'test/app/scriptureforge/**/e2e/' + specString + '.spec.js');
+  }
+
+  if (options.verbosity) {
+    protractorOptions.args.push('--params.verbosity', 3);
+  } else {
+    protractorOptions.args.push('--params.verbosity', 0);
+  }
+
+  cb(null);
+});
+
+// -------------------------------------
+//   Task: E2E Test: Use Test Config
+// -------------------------------------
+gulp.task('test-e2e-useTestConfig', function (cb) {
+  execute('cp ./src/config.php.fortest ./src/config.php', function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: E2E Test: Use Live Config
+// -------------------------------------
+gulp.task('test-e2e-useLiveConfig', function (cb) {
+  execute('cp ./src/config.php.live ./src/config.php', function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: E2E Test: Setup Test Environment
+// -------------------------------------
+gulp.task('test-e2e-setupTestEnvironment', function (cb) {
+  execute('sudo -u www-data php ./test/app/setupTestEnvironment.php ' + hostname, function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: E2E Test: Teardown Test Environment
+// -------------------------------------
+gulp.task('test-e2e-teardownTestEnvironment', function (cb) {
+  execute('sudo -u www-data php ./test/app/teardownTestEnvironment.php', function () {
+    cb(null);
+  });
+});
+
+// -------------------------------------
+//   Task: E2E Test: Do Test
+// -------------------------------------
+gulp.task('test-e2e-doTest', function (cb) {
+  // It's better to pass the specs array of files to test, and not use the --exclude parameter
+  console.log('specs: ', specs);
+  gulp.src(specs)
+    .pipe(protractor(protractorOptions))
+
+    // error handling
+    .on('error', function (e) {
+      console.log(e);
+      throw e;
+    })
+    .on('end', cb);
+});
+
+// -------------------------------------
+//   Task: E2E Test: Run
+// -------------------------------------
+gulp.task('test-e2e-run',
+  gulp.series(
+    'test-e2e-parseArgs',
+    'test-e2e-useTestConfig',
+    'test-e2e-setupTestEnvironment',
+    'test-e2e-doTest',
+    'test-e2e-teardownTestEnvironment',
+    'test-e2e-useLiveConfig',
+    function (cb) {
+      cb(null);
+    }
+  )
 );
+gulp.task('test-e2e-run').description = 'Run the E2E test';
+
+//endregion
 
 gulp.task('tasks', function (cb) {
   execute('grep gulp\.task gulpfile.js', function () {
     cb(null); // Swallow the error propagation so that gulp doesn't display a nodejs backtrace.
   });
 });
+
+gulp.task('default', function () {
+  return gulp.src('src/angular-app/**/helps/**/*.md').pipe(markdown())
+    .pipe(gulp.dest('src/angular-app'));
+
+  // place code for your default task here
+});
+
+gulp.task('default').description = 'Generate helps markdown files';
+
---
+++ rune2e.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Usage
+# rune2e.sh lf                                   : runs E2E tests for LF
+# rune2e.sh sf                                   : runs E2E tests for SF
+# rune2e.sh jp                                   : runs E2E tests for JP
+# rune2e.sh lf --specs lexicon-new-project       : runs lexicon-new-project spec for LF
+#
+# Note, make sure webdriver-manager is running
+# gulp test-e2e-webdriver_standalone
+
+if [ "$1" = "lf" ]
+  then
+    E2EHOSTNAME="languageforge.local"
+elif [ "$1" = "sf" ]
+  then
+    E2EHOSTNAME="scriptureforge.local"
+elif [ "$1" = "jp" ]
+  then
+    E2EHOSTNAME="jamaicanpsalms.scriptureforge.local"
+else
+    E2EHOSTNAME="languageforge.local"
+fi
+gulp test-e2e-run --hostname $E2EHOSTNAME ${@:2}
+
+# Ensure cleanup
+gulp test-e2e-teardownTestEnvironment
+gulp test-e2e-useLiveConfig
