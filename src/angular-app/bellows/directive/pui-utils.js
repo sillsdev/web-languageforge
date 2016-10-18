489eaca736402e02eed82406844f2450ec44e775
megahirt <chris@hirtfamily.net>
2016-10-18 10:36:46 +0700

    indent pui-utils.js

--- src/angular-app/bellows/directive/pui-utils.js
+++ src/angular-app/bellows/directive/pui-utils.js
@@ -1,51 +1,51 @@
 'use strict';
 
 angular.module('palaso.ui.utils', [])
-.directive('idleValidate', ['$interval', function ($interval) {
-  return {
-    restrict: 'A',
-    scope: {
-      idleValidate: '&',
-      idleValidateKeypress: '&',
-      idleValidateMsec: '='
-    },
-    link: function (scope, element) {
-      var intervalTimer;
-      var milliseconds = scope.idleValidateMsec;
-      if (!angular.isNumber(scope.idleValidateMsec)) {
-        milliseconds = 1000;
+  .directive('idleValidate', ['$interval', function ($interval) {
+    return {
+      restrict: 'A',
+      scope: {
+        idleValidate: '&',
+        idleValidateKeypress: '&',
+        idleValidateMsec: '='
+      },
+      link: function (scope, element) {
+        var intervalTimer;
+        var milliseconds = scope.idleValidateMsec;
+        if (!angular.isNumber(scope.idleValidateMsec)) {
+          milliseconds = 1000;
+        }
+
+        element.bind('keyup', function () {
+          if (angular.isFunction(scope.idleValidateKeypress)) {
+            scope.$apply(scope.idleValidateKeypress);
+          }
+
+          if (angular.isDefined(intervalTimer)) {
+            $interval.cancel(intervalTimer);
+          }
+
+          intervalTimer = $interval(function () { scope.idleValidate(); }, milliseconds, 1);
+        });
       }
+    };
+  }])
 
-      element.bind('keyup', function () {
-        if (angular.isFunction(scope.idleValidateKeypress)) {
-          scope.$apply(scope.idleValidateKeypress);
-        }
+  // see http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs
+  .directive('setFocus', ['$timeout', function ($timeout) {
+    return {
+      link: function (scope, element, attrs) {
+        scope.$watch(attrs.setFocus, function (value) {
+          if (value === true) {
+            $timeout(function () {
+              element[0].focus();
 
-        if (angular.isDefined(intervalTimer)) {
-          $interval.cancel(intervalTimer);
-        }
-
-        intervalTimer = $interval(function () { scope.idleValidate(); }, milliseconds, 1);
-      });
-    }
-  };
-}])
-
-// see http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs
-.directive('setFocus', ['$timeout', function ($timeout) {
-  return {
-    link: function (scope, element, attrs) {
-      scope.$watch(attrs.setFocus, function (value) {
-        if (value === true) {
-          $timeout(function () {
-            element[0].focus();
-
-            //scope[attrs.setFocus] = false;
-          });
-        }
-      });
-    }
-  };
-}])
+              //scope[attrs.setFocus] = false;
+            });
+          }
+        });
+      }
+    };
+  }])
 
 ;
