277277e3c5f2e4e4e52aad9c8f5340055ee22dc3
Ira Hopkinson <ira_hopkinson@sil.org>
2016-10-22 23:03:14 +0700

    fix models for PHP5 (still used on server; split not needed in PHP7)

--- src/Api/Model/Scriptureforge/Sfchecks/TextModel.php
+++ src/Api/Model/Scriptureforge/Sfchecks/TextModel.php
@@ -3,9 +3,7 @@
 namespace Api\Model\Scriptureforge\Sfchecks;
 
 use Api\Model\Shared\Mapper\Id;
-use Api\Model\Shared\Mapper\MapperListModel;
 use Api\Model\Shared\Mapper\MapperModel;
-use Api\Model\Shared\Mapper\MongoMapper;
 use Api\Model\Shared\ProjectModel;
 
 class TextModel extends MapperModel
@@ -65,39 +63,5 @@ class TextModel extends MapperModel
         $questionList->read();
 
         return $questionList;
-    }
-}
-
-class TextModelMongoMapper extends MongoMapper
-{
-    /**
-     * @param string $databaseName
-     * @return TextModelMongoMapper
-     */
-    public static function connect($databaseName)
-    {
-        if (! isset(static::$_pool[$databaseName])) {
-            static::$_pool[$databaseName] = new TextModelMongoMapper($databaseName, 'texts');
-        }
-        return static::$_pool[$databaseName];
-    }
-
-    /** @var TextModelMongoMapper[] */
-    private static $_pool = array();
-}
-
-class TextListModel extends MapperListModel
-{
-    /**
-     * TextListModel constructor.
-     * @param ProjectModel $project
-     */
-    public function __construct($project)
-    {
-        parent::__construct(
-            TextModelMongoMapper::connect($project->databaseName()),
-            array('title' => array('$regex' => '')),
-            array('title')
-        );
     }
 }
