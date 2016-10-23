db83741aee1df5781150ed878ab1c8ddc3aab12c
Ira Hopkinson <ira_hopkinson@sil.org>
2016-10-23 18:01:02 +0700

    fix more models for PHP5

--- src/Api/Model/Scriptureforge/Sfchecks/QuestionModel.php
+++ src/Api/Model/Scriptureforge/Sfchecks/QuestionModel.php
@@ -202,24 +202,6 @@ class QuestionModel extends MapperModel
     }
 }
 
-class QuestionModelMongoMapper extends MongoMapper
-{
-    /**
-     * @param string $databaseName
-     * @return QuestionModelMongoMapper
-     */
-    public static function connect($databaseName)
-    {
-        if (!isset(static::$_pool[$databaseName])) {
-            static::$_pool[$databaseName] = new QuestionModelMongoMapper($databaseName, 'questions');
-        }
-        return static::$_pool[$databaseName];
-    }
-
-    /** @var QuestionModelMongoMapper[] */
-    private static $_pool = array();
-}
-
 class QuestionListModel extends MapperListModel
 {
     /**
