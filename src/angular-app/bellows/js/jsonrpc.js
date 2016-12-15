40ccc2386679de50b04b10d41d53687cd847e06d
Ira Hopkinson <ira_hopkinson@sil.org>
2016-12-15 12:46:50 +0700

    fix JSCS

--- src/angular-app/bellows/js/jsonrpc.js
+++ src/angular-app/bellows/js/jsonrpc.js
@@ -4,120 +4,131 @@
 // from scratch since that project doesn't specify a license. It's pretty
 // simple code anyway.
 
-var json_rpc = angular.module('jsonRpc', ['sf.error']);
+angular.module('jsonRpc', ['sf.error'])
+  .factory('jsonRpc', ['$http', '$window', 'error', function ($http, $window, error) {
+    this.params = {};
+    this.last_id = 0;
 
-json_rpc.factory('jsonRpc', ['$http', '$window', 'error', function($http, $window, error) {
-  this.params = {};
-  this.last_id = 0;
-  
-  this.next_id = function() {
-    this.last_id = this.last_id + 1;
-    return this.last_id;
-  };
-  
-  this.connect = function(params) {
-    if (typeof(params) == "string") {
-      // We were called as connect("http://url/goes/here")
-      params = {"url": params};
-    }
-    if (params.url === undefined || params.url === "") {
-      throw "Valid URL required";
-    }
-    if (params.version === undefined) {
-      params.version = "2.0";
-    }
-    this.params = params;
-  };
-  
-  this.call = function(remote_func, remote_params, callback, id) {
-    // remote_func should be a string
-    // remote_params should be an object (e.g., {"param_name": "value"})
-    // callback is the function that will be called on success or failure. It
-    // should take a single parameter, a result object with five attributes:
-    //   ok = boolean, true for a 2xx code and false for a 3xx, 4xx or 5xx code
-    //   data, status, headers, config = as described in Angular's $http docs
-    if (this.params.url === undefined) {
-      throw "No URL: should use connect() before using call()";
-    }
-    if (id === undefined) {
-      id = this.next_id();
-    }
-    var json_request = {
-      "version": this.params.version,
-      "method": remote_func,
-      "params": remote_params,
-      "id": id
+    this.next_id = function () {
+      this.last_id = this.last_id + 1;
+      return this.last_id;
     };
-    var http_request = {
-      "url": this.params.url,
-      "method": "POST",
-      "data": JSON.stringify(json_request),
-      "headers": {"Content-Type": "application/json"} // Might not be necessary, since this appears to be the default for Angular's $http service
+
+    this.connect = function (params) {
+      if (typeof (params) == 'string') {
+        // We were called as connect("http://url/goes/here")
+        params = { url: params };
+      }
+
+      if (params.url === undefined || params.url === '') {
+        throw 'Valid URL required';
+      }
+
+      if (params.version === undefined) {
+        params.version = '2.0';
+      }
+
+      this.params = params;
     };
-    var result = {};
-    var request = $http(http_request);
-    this.requestsuccess = function(data, status, headers, config) {
-      if (data == null) {
-        // TODO error handling for jsonRpc CP 2013-07
-        error.error('RPC Error', "data is null");
-        
-        return;
+
+    this.call = function (remoteFunction, remoteParams, callback, id) {
+      // remoteFunction should be a string
+      // remoteParams should be an object (e.g., {"paramName": "value"})
+      // callback is the function that will be called on success or failure. It
+      // should take a single parameter, a result object with five attributes:
+      //   ok = boolean, true for a 2xx code and false for a 3xx, 4xx or 5xx code
+      //   data, status, headers, config = as described in Angular's $http docs
+      if (this.params.url === undefined) {
+        throw 'No URL: should use connect() before using call()';
       }
-      if (typeof data == 'string') {
-        error.error('RPC Error', data);
-        return;
+
+      if (id === undefined) {
+        id = this.next_id();
       }
-      if (data.error != null) {
-        var type = '';
-        switch(data.error.type) {
-          case 'ResourceNotAvailableException':
-            type = "The requested resource is not available.";
-            break;
-          case 'UserNotAuthenticatedException':
-            type = "You're not currently signed in.";
-            // redirect to login page with message
-            error.error("You will now be redirected to the login page.");
-            $window.location.href = "/auth/login";
-            return;
-            break;
-          case 'UserUnauthorizedException':
-            type = "You don't have sufficient privileges.";
-            break;
-          default:
-            type = 'Exception';
+
+      var jsonRequest = {
+        version: this.params.version,
+        method: remoteFunction,
+        params: remoteParams,
+        id: id
+      };
+      var httpRequest = {
+        url: this.params.url,
+        method: 'POST',
+        data: JSON.stringify(jsonRequest),
+
+        // Might not be necessary, since this appears to be the default for Angular's $http service
+        headers: { 'Content-Type': 'application/json' }
+      };
+      var result = {};
+      var request = $http(httpRequest);
+      this.requestsuccess = function (data, status, headers, config) {
+        if (data == null) {
+          // TODO error handling for jsonRpc CP 2013-07
+          error.error('RPC Error', 'data is null');
+
+          return;
         }
-        error.error(type, data.error.message);
-        
-        return;
-      }
-      if (data.error == null) {
-        result.ok = true;
-        result.data = data.result;
-        result.status = status;
-        result.headers = headers;
-        result.config = config;
-        (callback||angular.noop)(result);
-      }
-      
-    };
-    this.requesterror = function(data, status, headers, config) {
-      if (status == 0 || status == "0") {
-        // if we get here it basically means the browser/network is OFFLINE
-        // we should fail silently (the browser will console log a failed connection anyway)
-      } else {
-        error.error('RPC Error', "Server Status Code " + status);
-        result.ok = false;
-        result.data = data;
-        result.status = status;
-        result.headers = headers;
-        result.config = config;
-        (callback||angular.noop)(result);
-      }
-    };
-    request.success(this.requestsuccess);
-    request.error(this.requesterror);
-    return request;
-  };
 
-  return this;
-}]);
+        if (typeof data == 'string') {
+          error.error('RPC Error', data);
+          return;
+        }
+
+        if (data.error != null) {
+          var type = '';
+          switch (data.error.type) {
+            case 'ResourceNotAvailableException':
+              type = 'The requested resource is not available.';
+              break;
+            case 'UserNotAuthenticatedException':
+              type = "You're not currently signed in.";
+
+              // redirect to login page with message
+              error.error('You will now be redirected to the login page.');
+              $window.location.href = '/auth/login';
+              return;
+              break;
+            case 'UserUnauthorizedException':
+              type = "You don't have sufficient privileges.";
+              break;
+            default:
+              type = 'Exception';
+          }
+          error.error(type, data.error.message);
+
+          return;
+        }
+
+        if (data.error == null) {
+          result.ok = true;
+          result.data = data.result;
+          result.status = status;
+          result.headers = headers;
+          result.config = config;
+          (callback || angular.noop)(result);
+        }
+
+      };
+
+      this.requesterror = function (data, status, headers, config) {
+        // only report error if the browser/network is not OFFLINE
+        // otherwise fail silently (the browser will console log a failed connection anyway)
+        if (status != 0 && status != '0') {
+          error.error('RPC Error', 'Server Status Code ' + status);
+          result.ok = false;
+          result.data = data;
+          result.status = status;
+          result.headers = headers;
+          result.config = config;
+          (callback || angular.noop)(result);
+        }
+      };
+
+      request.success(this.requestsuccess);
+      request.error(this.requesterror);
+      return request;
+    };
+
+    return this;
+  }]);
