b601ba3d100544d4d13ea813c37eebf7fd716a7e
megahirt <chris@hirtfamily.net>
2017-01-06 10:37:43 +0700

    first draft of bootstrap 4 header and footer

--- src/Site/Controller/Base.php
+++ src/Site/Controller/Base.php
@@ -83,9 +83,14 @@ class Base
     protected function renderPage(Application $app, $viewName) {
 
         if ($this->data['isBootstrap4']) {
+            // TODO: move to app_dependencies once bootstrap4 migration is complete
             $this->addCssFiles($this->getThemePath()."/cssBootstrap4", array(), false);
-            array_unshift($this->data['cssFiles'], "vendor_bower/bootstrap/dist/css/bootstrap.css");
             array_unshift($this->data['cssFiles'], "vendor_bower/bootstrap/dist/css/bootstrap-flex.css");
+            array_unshift($this->data['cssFiles'], "vendor_bower/bootstrap/dist/css/bootstrap.css");
+            array_unshift($this->data['vendorFilesJs'], "vendor_bower/bootstrap/dist/js/bootstrap.js");
+            array_unshift($this->data['vendorFilesMinJs'], "vendor_bower/bootstrap/dist/js/bootstrap.min.js");
+            array_unshift($this->data['vendorFilesJs'], "vendor_bower/tether/dist/js/tether.js");
+            array_unshift($this->data['vendorFilesMinJs'], "vendor_bower/tether/dist/js/tether.min.js");
         } else {
             $this->addCssFiles($this->getThemePath()."/cssBootstrap2", array(), false);
             $this->addCssFiles("Site/views/shared/cssBootstrap2", array('bootstrap.css'), false);
