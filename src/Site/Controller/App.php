ff77fbf3054eb785c06da5eaa036ab73e271655c
TrebleNotes <alkoyhylin@gmail.com>
2017-01-11 09:56:51 +0700

    feature/bs-forgot_password: Forgot Password updated to Bootstrap 4

--- src/Site/Controller/App.php
+++ src/Site/Controller/App.php
@@ -270,7 +270,7 @@ class AppModel {
 
         $siteAppsInBootstrap4 = array(
             "scriptureforge" => array("appName"),
-            "languageforge" => array("login", "rapid-words", "userprofile", "changepassword"),
+            "languageforge" => array("login", "rapid-words", "userprofile", "changepassword", "forgot_password"),
             "waaqwiinaagiwritings" => array(),
             "jamaicanpsalms.scriptureforge" => array(),
             "demo.scriptureforge" => array(),
