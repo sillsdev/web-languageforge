04cf376721ee288e3a5145ded6abe8a0a17a92e6
lukejosten <lukejosten@gmail.com>
2017-01-13 12:37:04 +0700

    Copied the definition folder from studio and set it up in the seed

--- src/angular-app/languageforge/review-suggest/src/client/app/auth/auth.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { FormsModule } from '@angular/forms';
 import { AuthService } from '../shared/services/auth.service';
 
 @NgModule({
--- src/angular-app/languageforge/review-suggest/src/client/app/shared/shared.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NameListService } from './name-list/name-list.service';
+import { DictionaryService } from './services/dictionary.service';
+import { AuthService } from './services/auth.service';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -21,7 +23,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [NameListService]
+      providers: [NameListService, DictionaryService, AuthService]
     };
   }
 }
