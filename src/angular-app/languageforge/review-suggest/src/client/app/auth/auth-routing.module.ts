0329835185840e68ef938f29bbe73e66baaf0284
lukejosten <lukejosten@gmail.com>
2017-01-13 13:22:17 +0700

    Edit routing and the navbar component to reflect the studio project's layout and deleted the default seed pages

--- src/angular-app/languageforge/review-suggest/src/client/app/auth/auth-routing.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/auth/auth-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthComponent } from './auth.component';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      { path: '', redirectTo: '/auth', pathMatch: 'full' },
       { path: 'auth', component: AuthComponent }
     ])
   ],
--- src/angular-app/languageforge/review-suggest/src/client/app/shared/shared.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/shared/shared.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NameListService } from './name-list/name-list.service';
 import { DictionaryService } from './services/dictionary.service';
 import { AuthService } from './services/auth.service';
+
+import { MaterializeDirective } from 'angular2-materialize/dist/index';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -15,15 +15,15 @@ import { AuthService } from './services/auth.service';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ToolbarComponent, NavbarComponent],
-  exports: [ToolbarComponent, NavbarComponent,
+  declarations: [NavbarComponent, MaterializeDirective],
+  exports: [NavbarComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [NameListService, DictionaryService, AuthService]
+      providers: [DictionaryService, AuthService]
     };
   }
 }
