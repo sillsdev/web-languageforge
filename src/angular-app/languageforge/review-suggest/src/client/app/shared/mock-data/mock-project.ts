a6cc8c7ad10ece714b66b63223fe6541de4dbe9f
Taylor Dunlop <Taylor Dunlop>
2017-01-17 03:53:44 -0500

    feature/rs: Dashboard
    - Continuing work on the Dashboard
    - Displaying fake data.

---
+++ src/angular-app/languageforge/review-suggest/src/client/app/shared/mock-data/mock-project.ts
@@ -0,0 +1,8 @@
+import { Project } from "../models/project";
+
+export const PROJECTS = [
+    new Project (0, "Greek"),
+    new Project (1, "Latin"),
+    new Project (2, "Thai"),
+    new Project (3, "Russian"),
+]
