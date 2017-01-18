f4e70e84645dfcaba792bd32ce712e7f520f7f27
JacobJinTheAsian <jacobjin1995@gmail.com>
2017-01-18 11:36:47 +0700

    Allow client communicate with the server through two socket, one for messages, the other for sharedb
    Former-commit-id: 997aaae2f88227fb06f35f20483ff901d9000d95

--- src/node/server.js
+++ src/node/server.js
@@ -9,16 +9,6 @@ ShareDB.types.register(richText.type);
 var backend = new ShareDB();
 var connection = backend.connect();
 
-function WordFieldConcat(word, id){
-  var a = word.concat('~',id);
-  return a;
-}
-
-function GetIDfromWordFieldConcat(con){
-  var a = con.split('~');
-  return a[1];
-}
-
 startServer();
 
 // Create initial document then fire callback
@@ -27,7 +17,8 @@ function createDoc(collection, id) {
   doc.fetch(function(err) {
     if (err) throw err;
     if (doc.type === null) {
-      doc.create([{insert: ''}], 'rich-text');
+      doc.create([{insert: ""}], 'rich-text');
+      return;
     }
   });
 }
@@ -37,39 +28,35 @@ function startServer() {
   var app = express();
   app.use(express.static('static'));
   app.use(express.static('node_modules/quill/dist'));
-  var server = http.createServer(app);
-
+  var docServer = http.createServer(app);
+  var messageServer = http.createServer(app);
   // Connect any incoming WebSocket connection to ShareDB
-  var wss = new WebSocket.Server({server: server});
-  wss.on('connection', function(ws, req) {
+  var wss = new WebSocket.Server({server: docServer});
+  var wss2 = new WebSocket.Server({server: messageServer});
+  wss2.on('connection', function(ws, req) {
     ws.on('message', function(msg) {
-      // console.log(wss);
       var JSONMsg = JSON.parse(msg);
       if (typeof(JSONMsg.b) == "string") {
         createDoc(JSONMsg.c,JSONMsg.b);
-        console.log("hello!" + JSONMsg.b);
-        //ws.send(JSON.stringify({"status": "success", "id": JSONMsg.b, "collection": JSONMsg.c}));
-        ws.send(JSON.stringify({"a": "bs", "b": JSONMsg.b, "c": JSONMsg.c, "d": "success"}));
+        ws.send(JSON.stringify({"a": "bs", "b": JSONMsg.b, "c": JSONMsg.c, "d": JSONMsg.d, "e": "success"}));
       } else if (typeof(JSONMsg.b) == "object") {
         for (var i = 0; i<JSONMsg.b.length; i++) {      
           createDoc(JSONMsg.c,JSONMsg.b[i]);
-          console.log("hello2");
-          ws.send(JSON.stringify({"status": "success", "id": JSONMsg.b[i], "collection": JSONMsg.c, "d": "success"}));
+          ws.send(JSON.stringify({"a": "bs", "b": JSONMsg.b, "c": JSONMsg.c, "d": JSONMsg.d, "e": "success"}));
         }
-      } else {
-        // console.log("not here");
       }
-      //  else {
-      //   throw "unknown type of id";
-      // }
-    // console.log("the message is",msg);
     });
+  });
+  wss.on('connection', function(ws, req) {
     var stream = new WebSocketJSONStream(ws);
     backend.listen(stream);
   });
 
-  server.listen(8080);
-  console.log('Listening on http://localhost:8080');
+
+  docServer.listen(8080);
+  messageServer.listen(8079);
+  console.log('Message server listening on http://localhost:8079');
+  console.log('Doc server listening on http://localhost:8080');
 }
 
 function getJsonObjFromFile(sessionId){
