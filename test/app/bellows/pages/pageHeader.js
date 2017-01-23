5618b4531463d3232d84dcd74ace803f367d68b9
Ira Hopkinson <ira_hopkinson@sil.org>
2017-01-23 20:13:25 +0700

    fix  E2E changepassword
    Former-commit-id: 7b3f171e48ed544281033f13138cf53fc0217d5f

--- test/app/bellows/pages/pageHeader.js
+++ test/app/bellows/pages/pageHeader.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var PageHeader = function() {
-  
-  this.myProjects = {
-    button:    element(by.id('myProjects')),
-    links:    element(by.id('myProjects')).all(by.css('ul li'))
-  };
-  
-  this.loginButton = element(by.partialButtonText('Login'));
-
-};
-
 module.exports = new PageHeader();
+
+function PageHeader() {
+  this.myProjects = {
+    button:    element(by.id('myProjectDropdown')),
+    links:    element(by.id('myProjectDropdown')).all(by.css('ul li'))
+  };
+
+  this.loginButton = element(by.partialButtonText('Login'));
+}
