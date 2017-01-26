95b5989523733df14457c78aed4407d6b822bb9a
megahirt <chris@hirtfamily.net>
2017-01-26 10:39:19 +0700

    delete unused bootstrap2 templates

--- src/angular-app/languageforge/lexicon/lexicon.js
+++ src/angular-app/languageforge/lexicon/lexicon.js
@@ -24,6 +24,8 @@ angular.module('lexicon',
   function ($stateProvider, $urlRouterProvider, $translateProvider) {
     $urlRouterProvider.otherwise('/editor/list');
 
+    // TODO: remove dependency on bootstrap JS and jquery calls in the state provider below.  Use UI-bootstrap instead
+
     // State machine from ui.router
     $stateProvider
       .state('configuration', {
@@ -36,29 +38,25 @@ angular.module('lexicon',
       })
       .state('importExport', {
         url: '/importExport',
-        templateUrl: '/angular-app/languageforge/lexicon/views/' + bootstrapVersion +
-          '/import-export.html'
+        templateUrl: '/angular-app/languageforge/lexicon/views/import-export.html'
       })
       .state('settings-project', {
         url: '/settings-project',
-        templateUrl: '/angular-app/languageforge/lexicon/views/' + bootstrapVersion +
-          '/settings.html',
+        templateUrl: '/angular-app/languageforge/lexicon/views/settings.html',
         controller: function () {
           jQuery("#app-container-for-bootstrap .nav-tabs a[href='#settings-project']").tab('show');
         }
       })
       .state('settings-archive', {
         url: '/settings-archive',
-        templateUrl: '/angular-app/languageforge/lexicon/views/' + bootstrapVersion +
-          '/settings.html',
+        templateUrl: '/angular-app/languageforge/lexicon/views/settings.html',
         controller: function () {
           jQuery("#app-container-for-bootstrap .nav-tabs a[href='#settings-archive']").tab('show');
         }
       })
       .state('settings-delete', {
         url: '/settings-delete',
-        templateUrl: '/angular-app/languageforge/lexicon/views/' + bootstrapVersion +
-          '/settings.html',
+        templateUrl: '/angular-app/languageforge/lexicon/views/settings.html',
         controller: function () {
           jQuery("#app-container-for-bootstrap .nav-tabs a[href='#settings-delete']").tab('show');
         }
