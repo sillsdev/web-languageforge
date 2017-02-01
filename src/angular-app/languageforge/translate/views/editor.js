1892cf79c7275ca4f50c785421bb2051fbd83ef3
Ira Hopkinson <ira_hopkinson@sil.org>
2017-02-01 21:11:34 +0700

    add two real-time ng-quill editors. Enable swapping of source and target editors

--- src/angular-app/languageforge/translate/views/editor.js
+++ src/angular-app/languageforge/translate/views/editor.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('translate.editor', ['ui.router', 'ui.bootstrap', 'bellows.services'])
+angular.module('translate.editor', ['ui.router', 'ui.bootstrap', 'bellows.services', 'ngQuill'])
   .config(['$stateProvider', function ($stateProvider) {
 
     // State machine from ui.router
@@ -12,9 +12,55 @@ angular.module('translate.editor', ['ui.router', 'ui.bootstrap', 'bellows.servic
       })
     ;
   }])
-  .controller('EditorCtrl', ['$scope',
-    function ($scope) {
+  .controller('EditorCtrl', ['$scope', function ($scope) {
+    var currentFieldIds = [];
+    var editors = {};
+    var source = {
+      key: 'source',
+      label: 'Source'
+    };
+    var target = {
+      key: 'target',
+      label: 'Target'
+    };
 
-    }])
+    $scope.left = source;
+    $scope.right = target;
+
+    $scope.getFieldId = function getFieldId(key) {
+      return $scope.project.id + ':' + key;
+    };
+
+    $scope.editorChanged = function editorChanged(editor, key, side) {
+      if (currentFieldIds[key] != $scope.getFieldId(key)) {
+        window.realTime.disconnectRichTextDoc(currentFieldIds[key]);
+        delete currentFieldIds[key];
+        $scope.editorCreated(editor, key, side);
+      }
+    };
+
+    $scope.editorCreated = function editorCreated(editor, key, side) {
+      editors[side] = editor;
+      var fieldId = $scope.getFieldId(key);
+      currentFieldIds[key] = fieldId;
+      window.realTime.createAndSubscribeRichTextDoc(fieldId, editor);
+    };
+
+    $scope.swapEditors = function swapEditors() {
+      window.realTime.disconnectRichTextDoc(currentFieldIds[$scope.left.key]);
+      window.realTime.disconnectRichTextDoc(currentFieldIds[$scope.right.key]);
+      currentFieldIds = [];
+
+      var newLeft = $scope.right;
+      var newRight = $scope.left;
+      delete $scope.right;
+      delete $scope.left;
+      $scope.right = newRight;
+      $scope.left = newLeft;
+      $scope.editorCreated(editors.left, newLeft.key, 'left');
+      $scope.editorCreated(editors.right, newRight.key, 'right');
+    };
+
+  }])
 
   ;
