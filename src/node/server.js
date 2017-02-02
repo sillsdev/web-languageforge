d1e9db3f8b89c66fc9c12989e7379092a606dba1
Ira Hopkinson <ira_hopkinson@sil.org>
2017-02-02 16:03:15 +0700

    change to secure WebSockets and refactor WebSocket messages
    Former-commit-id: 5b8ada45692bec83c7c5434dd413c80151610f6b

--- src/node/server.js
+++ src/node/server.js
@@ -1,29 +1,37 @@
-var http = require('http');
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var ShareDB = require('sharedb');
 var richText = require('rich-text');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('websocket-json-stream');
-// var database = require('sharedb-mongo')('mongodb://localhost:27017/operations');
+var database = require('sharedb-mongo')('mongodb://localhost:27017/realtime');
 var MongoClient = require('mongodb').MongoClient;
 
 ShareDB.types.register(richText.type);
-// var backend = new ShareDB({db: database});
-var backend = new ShareDB();
+var backend = new ShareDB({ db: database });
 var connection = backend.connect();
+
+var config = require('./serverConfig');
+var defaultKey = '/etc/letsencrypt/live/cat.languageforge.org/privKey.pem';
+var defaultCert = '/etc/letsencrypt/live/cat.languageforge.org/cert.pem';
+var sslKeyPath = config.sslKeyPath || defaultKey;
+var sslCertPath = config.sslCertPath || defaultCert;
+var webSocketDocServerPort = 8443;
+var webSocketMessageServerPort = 8442;
 
 // Client connection time allowed without editing in minutes
 var connectionTime = 20;
 
 startServer();
 
-MongoClient.connect('mongodb://localhost:27017/operations', function(err, db) {
-  if(!err) {
+MongoClient.connect('mongodb://localhost:27017/realtime', function (err, db) {
+  if (!err) {
     console.log('Connected to MongoDB');
   }
 
   //Example code getting collections out of a database
-  db.collection('collection').find({}).toArray(function(err, docs) {
+  db.collection('collection').find({}).toArray(function (err, docs) {
     // console.log('Found the following records', docs);
   });
 });
@@ -32,21 +40,22 @@ MongoClient.connect('mongodb://localhost:27017/operations', function(err, db) {
 function createDoc(collection, id, value) {
   value = value || '';
   var doc = connection.get(collection, id);
-  doc.fetch(function(err) {
+  doc.fetch(function (err) {
     if (err) throw err;
 
     if (doc.type === null) {
-      doc.create([{insert: value}], 'rich-text');
+      doc.create([{ insert: value }], 'rich-text');
     }
   });
 }
 
 // Check if object is empty
 function isEmpty(object) {
-  for(var key in object) {
-    if(object.hasOwnProperty(key))
+  for (var key in object) {
+    if (object.hasOwnProperty(key))
       return false;
   }
+
   return true;
 }
 
@@ -61,10 +70,12 @@ function docDisconnect(docIds, receivedBytes, agent) {
             delete streams[fieldId][docId];
           }
         }
+
         if (isEmpty(streams[fieldId])) {
           if (streams.hasOwnProperty(fieldId)) {
             delete streams[fieldId];
           }
+
           var sub = backend.pubsub.subscribed;
           if (sub.hasOwnProperty(fieldId)) {
             delete sub[fieldId];
@@ -76,40 +87,39 @@ function docDisconnect(docIds, receivedBytes, agent) {
     receivedBytes = agent.stream.ws.bytesReceived;
     setTimeout(function () {
       docDisconnect(docIds, receivedBytes, agent);
-    }, connectionTime*1000);
+    }, connectionTime * 1000);
   }
 }
 
 function startServer() {
-  // Create a web server to serve files and listen to WebSocket connections
+  // Create web servers to serve files and listen to WebSocket connections
+  var privateKey  = fs.readFileSync(sslKeyPath, 'utf8');
+  var certificate = fs.readFileSync(sslCertPath, 'utf8');
+  var options = { key: privateKey, cert: certificate };
   var app = express();
   app.use(express.static('static'));
   app.use(express.static('node_modules/quill/dist'));
-  var docServer = http.createServer(app);
-  var messageServer = http.createServer(app);
+  var docServer = https.createServer(options, app);
+  var messageServer = https.createServer(options, app);
 
   // Connect any incoming WebSocket connection to ShareDB
-  var docWss = new WebSocket.Server({server: docServer});
-  var messageWss = new WebSocket.Server({server: messageServer});
-  messageWss.on('connection', function(ws, req) {
-    ws.on('message', function(msg) {
-      var JsonMsg = JSON.parse(msg);
-      if (typeof(JsonMsg.b) == 'string') {
-        createDoc(JsonMsg.c, JsonMsg.b, JsonMsg.d);
-        ws.send(JSON.stringify({'a': 'bs', 'b': JsonMsg.b, 'c': JsonMsg.c, 'd': JsonMsg.d, 'e': 'success'}));
-      } else if (typeof(JsonMsg.b) == 'object') {
-        for (var i = 0; i<JsonMsg.b.length; i++) {
-          createDoc(JsonMsg.c, JsonMsg.b[i], JsonMsg.d);
-          ws.send(JSON.stringify({'a': 'bs', 'b': JsonMsg.b, 'c': JsonMsg.c, 'd': JsonMsg.d, 'e': 'success'}));
-        }
+  var docWss = new WebSocket.Server({ server: docServer });
+  var messageWss = new WebSocket.Server({ server: messageServer });
+  messageWss.on('connection', function (ws) {
+    ws.on('message', function (jsonMsg) {
+      var msg = JSON.parse(jsonMsg);
+      if (typeof (msg.docId) == 'string') {
+        createDoc(msg.collection, msg.docId, msg.initialValue);
+        msg.result = 'docReady';
+        ws.send(JSON.stringify(msg));
       }
     });
   });
 
-  docWss.on('connection', function(ws, req) {
+  docWss.on('connection', function (ws) {
     var stream = new WebSocketJSONStream(ws);
     var agent = backend.listen(stream);
-    setTimeout(function(){
+    setTimeout(function () {
       var collection = agent.subscribedDocs.collection;
       var docIds = [];
       for (var key in collection) {
@@ -117,17 +127,23 @@ function startServer() {
           docIds.push(collection[key].id);
         }
       }
+
       docDisconnect(docIds, -1, agent);
     }, 1500);
   });
 
-  docServer.listen(8080);
-  messageServer.listen(8079);
-  console.log('Message server listening on http://localhost:8079');
-  console.log('Doc server listening on http://localhost:8080');
+  docServer.listen(webSocketDocServerPort, function () {
+    console.log((new Date()) + ' Doc Server is listening on port ' + webSocketDocServerPort);
+  });
+
+  messageServer.listen(webSocketMessageServerPort, function () {
+    console.log((new Date()) + ' Message Server is listening on port ' +
+      webSocketMessageServerPort);
+  });
 }
 
-function getJsonObjFromFile(sessionId){
+//noinspection JSUnusedLocalSymbols
+function getJsonObjFromFile(sessionId) {
   if (sessionId == null || sessionId == '') return false;
 
   var tmpDir = '/tmp';
@@ -135,8 +151,9 @@ function getJsonObjFromFile(sessionId){
   try {
     var jsonObj = require(filePath);
   } catch (e) {
-      return false;
+    return false;
   }
+
   return jsonObj;
 }
 
