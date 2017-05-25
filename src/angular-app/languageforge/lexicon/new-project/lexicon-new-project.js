4fc0a0ee4dfa12744a593d880d3f94c5564387de
Nathaniel Paulus <nateowami@gmail.com>
2017-05-25 13:11:43 +0800

    Fix E2E tests form validation on new project page

--- src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.js
+++ src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.js
@@ -159,6 +159,7 @@ angular.module('lexicon-new-project',
       $scope.formStatusClass = (bootstrapVersion == 'bootstrap4' ? '' : 'neutral');
       $scope.forwardBtnClass = (bootstrapVersion == 'bootstrap4' ? 'btn-secondary' : '');
       $scope.formValidationDefer = $q.defer();
+      $scope.formValidationDefer.resolve(true);
       return $scope.formValidationDefer.promise;
     }
 
@@ -267,10 +268,8 @@ angular.module('lexicon-new-project',
       switch ($state.current.name) {
         case 'newProject.chooser':
           return error();
-          break;
         case 'newProject.sendReceiveCredentials':
           return validateSendReceiveCredentialsForm();
-          break;
         case 'newProject.sendReceiveClone':
           if (sendReceive.isInProgress()) {
             return error();
@@ -324,10 +323,10 @@ angular.module('lexicon-new-project',
             }
           });
 
-          break;
         case 'newProject.initialData':
+          return neutral();
         case 'newProject.verifyData':
-          break;
+          return neutral();
         case 'newProject.selectPrimaryLanguage':
           if (!$scope.newProject.languageCode) {
             return error('Please select a primary language for the project.');
