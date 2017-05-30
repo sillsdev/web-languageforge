12db6ca94db08e80c12cf042fff240b8078c79bb
Nathaniel Paulus <nateowami@gmail.com>
2017-05-30 16:53:05 +0800

    Create basic Angular 4 app based on quickstart and angular2-seed

---
+++ src/angular-app/ng4/home/main.ts
@@ -0,0 +1,7 @@
+import './polyfills.browser'
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
