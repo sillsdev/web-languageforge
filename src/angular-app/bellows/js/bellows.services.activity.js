f07c1d300abdcbfb6736678381d3170bbf3b609f
Nathaniel Paulus <nateowami@gmail.com>
2017-05-31 15:43:29 +0800

    Fix e2e tests on ScriptureForge

--- src/angular-app/bellows/js/bellows.services.activity.js
+++ src/angular-app/bellows/js/bellows.services.activity.js
@@ -2,9 +2,12 @@
 
 angular.module('bellows.services')
   .service('activityPageService', ['jsonRpc', 'sessionService', function(jsonRpc, sessionService) {
+
+    var project = sessionService.session.project;
+
     jsonRpc.connect({
       url: '/api/sf',
-      projectId: sessionService.session.project.id
+      projectId: project ? project.id : undefined
     });
     this.list_activity = function(offset, count, callback) {
       jsonRpc.call('activity_list_dto', [offset, count], callback);
