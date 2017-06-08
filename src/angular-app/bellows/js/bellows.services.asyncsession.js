fa087354c97ada992bf12e79141ef128c2c9b130
Nathaniel Paulus <nateowami@gmail.com>
2017-06-08 12:32:23 +0800

    Add async session service
    Former-commit-id: 4b0afef7b59b3004b03460e54f91b5b4eebb34cb

---
+++ src/angular-app/bellows/js/bellows.services.asyncsession.js
@@ -0,0 +1,123 @@
+'use strict';
+
+angular.module('bellows.services')
+.service('asyncSession', ['apiService', '$window', '$q', function (api, $window, $q) {
+  // Because session data is asynchronously loaded, it is not possible to synchronously
+  // obtain a reference to the session instance. getSession() must be called, which returns
+  // a promise (callbacks also accepted) that resolves to the session instance, which can
+  // then be used synchronously.
+
+  var projectId = window.location.pathname.match(/^\/app\/[a-z]+\/([a-z0-9]{24,})$/i);
+  projectId = projectId == null ? undefined : projectId[1];
+
+  this.getProjectId = function () {
+    return projectId;
+  };
+
+  function fn(val) {
+    return function() {
+      return val;
+    }
+  }
+
+  var domain = {
+    ANY:       fn(1000),
+    USERS:     fn(1100),
+    PROJECTS:  fn(1200),
+    TEXTS:     fn(1300),
+    QUESTIONS: fn(1400),
+    ANSWERS:   fn(1500),
+    COMMENTS:  fn(1600),
+    TEMPLATES: fn(1700),
+    TAGS:      fn(1800),
+    ENTRIES:   fn(1900)
+  };
+  var operation = {
+    CREATE:       fn(1),
+    EDIT:         fn(2),
+    DELETE:       fn(3),
+    LOCK:         fn(4),
+    VIEW:         fn(5),
+    VIEW_OWN:     fn(6),
+    EDIT_OWN:     fn(7),
+    DELETE_OWN:   fn(8),
+    ARCHIVE:      fn(9)
+  };
+
+  this.domain = domain;
+  this.operation = operation;
+
+  var sessionData;
+
+  // session instance (singleton) that references the data
+  var Session = new (function () {
+    // Helper function
+    function fnFor(key) {
+      return function() {
+        return sessionData[key];
+      }
+    }
+
+    this.currentUserId = fnFor('userId');
+    this.fileSizeMax = fnFor('fileSizeMax');
+    this.baseSite = fnFor('baseSite');
+    this.projectSettings = fnFor('projectSettings');
+    this.project = fnFor('project');
+
+    // TODO Is it really necessary to have these on the instance as well?
+    this.domain = domain;
+    this.operation = operation;
+
+    this.hasSiteRight = function (domain, operation) {
+      return this.hasRight(sessionData.userSiteRights, domain, operation);
+    };
+
+    this.hasProjectRight = function (domain, operation) {
+      return this.hasRight(sessionData.userProjectRights, domain, operation);
+    };
+
+    this.hasRight = function (rights, domain, operation) {
+      var right = domain() + operation();
+      return rights.indexOf(right) != -1;
+    };
+
+    this.getProjectSetting = function (setting) {
+      return sessionData.projectSettings[setting];
+    }
+
+    this.getCaptchaData = api.method('get_captcha_data');
+
+  })();
+
+  // forceRefresh and callback are both optional (a Promise is returned)
+  // Can be called as (boolean, function), (boolean), (function), or ()
+  this.getSession = function(forceRefresh, callback) {
+    // handle the one edge case (called as getSession(function))
+    if(typeof forceRefresh === 'function') {
+      callback = forceRefresh;
+      forceRefresh = false;
+    }
+    callback = callback || angular.noop;
+
+    return $q(function(resolve, reject) {
+      if(sessionData && !forceRefresh) {
+        callback(Session);
+        resolve(Session);
+        return;
+      }
+
+      // TODO Consider whether errors should result in a retry rather than
+      // making them the responsibility of the caller
+      api.call('session_getSessionData').then(function(response) {
+        sessionData = response.data;
+        callback(Session);
+        resolve(Session);
+      }).catch(function(response) {
+        callback(response);
+        reject(response);
+      });
+    });
+  };
+
+}]);
+
