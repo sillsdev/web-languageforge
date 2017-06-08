4dd7ffc6368a65771501b77ecd64150fe406aff6
Nathaniel Paulus <nateowami@gmail.com>
2017-06-08 12:30:24 +0800

    Promiseify apiService

--- src/angular-app/bellows/js/bellows.services.api.js
+++ src/angular-app/bellows/js/bellows.services.api.js
@@ -1,15 +1,21 @@
 angular.module('bellows.services')
-  .service('apiService', ['jsonRpc', function (jsonRpc) {
+  .service('apiService', ['jsonRpc', '$q', function (jsonRpc, $q) {
 
     // FIXME This same code exists in session service, but can't be used due to circular dependency.
     var projectId = window.location.pathname.match(/^\/app\/[a-z]+\/([a-z0-9]{24,})$/i);
     projectId = projectId == null ? undefined : projectId[1];
 
     this.call = function(method, args, callback) {
-        var options = {
-          projectId: projectId
-        };
-        jsonRpc.call('/api/sf', method, options, args, callback);
+      var options = {
+        projectId: projectId
+      };
+
+      return $q(function(resolve, reject) {
+        jsonRpc.call('/api/sf', method, options, args || [], function(result) {
+          (callback || function(){})(result);
+          result.ok ? resolve(result) : reject(result);
+        });
+      });
     };
 
     this.method = function(method) {
