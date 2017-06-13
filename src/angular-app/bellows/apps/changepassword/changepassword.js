f898c079f8679d4c89e468462dc641eeaa2dd48d
Nathaniel Paulus <nateowami@gmail.com>
2017-06-13 11:20:05 +0800

    Make change password controller use async session

--- src/angular-app/bellows/apps/changepassword/changepassword.js
+++ src/angular-app/bellows/apps/changepassword/changepassword.js
@@ -3,20 +3,19 @@
 angular.module('changepassword', ['ui.bootstrap', 'bellows.services', 'ui.validate',
   'palaso.ui.notice', 'palaso.ui.utils'
 ])
-  .controller('changePasswordCtrl', ['$scope', 'userService', 'sessionService', 'silNoticeService',
+  .controller('changePasswordCtrl', ['$scope', 'userService', 'asyncSession', 'silNoticeService',
     function ($scope, userService, sessionService, notice) {
       $scope.notify = {};
 
       $scope.updatePassword = function () {
         if ($scope.vars.password == $scope.vars.confirm_password) {
-          userService.changePassword(sessionService.currentUserId(), $scope.vars.password,
-            function (result) {
-              if (result.ok) {
-                notice.push(notice.SUCCESS, 'Password Updated successfully');
-                $scope.vars.password = $scope.vars.confirm_password = '';
-              }
-            }
-          );
+          sessionService.getSession().then(function (session) {
+            var user = session.currentUserId(), password = $scope.vars.password;
+            userService.changePassword(user, password).then(function() {
+              notice.push(notice.SUCCESS, 'Password updated successfully');
+              $scope.vars.password = $scope.vars.confirm_password = '';
+            });
+          });
         }
       };
     }
