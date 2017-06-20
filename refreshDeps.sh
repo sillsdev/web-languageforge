04beaa7e6b3ee2f7f69c3480e3deb39bcbf301aa
megahirt <chris@hirtfamily.net>
2017-06-20 08:36:03 +0700

    make refreshDeps.sh clear node_modules and do webdriver update
    Former-commit-id: b443050249a1244e2d5bb3a198b5eb31d8300a67

--- refreshDeps.sh
+++ refreshDeps.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+rm -r node_modules
 npm install &
 cd src
 composer install &
 rm -r vendor_bower
 bower install &
 wait
+gulp test-e2e-webdriver_update &
 ../cleanup_css.sh
 gulp sass
+wait
 echo -e "\n---------Finished refreshing npm, composer and bower----------"
