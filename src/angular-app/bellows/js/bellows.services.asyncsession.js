f0966a13c330bbfe82b29d07cd026680dd612251
Nathaniel Paulus <nateowami@gmail.com>
2017-06-20 22:28:31 +0800

    Convert remaining instances of session.currentUserId to userId

--- src/angular-app/bellows/js/bellows.services.asyncsession.js
+++ src/angular-app/bellows/js/bellows.services.asyncsession.js
@@ -62,7 +62,6 @@ angular.module('bellows.services')
     }
 
     this.userId = fnFor('userId');
-    this.currentUserId = this.userId; // deprecated
     this.fileSizeMax = fnFor('fileSizeMax');
     this.baseSite = fnFor('baseSite');
     this.projectSettings = fnFor('projectSettings');
