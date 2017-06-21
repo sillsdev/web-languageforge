585b55f5ace474c192d112598b4a46f9da108b4d
Nathaniel Paulus <nateowami@gmail.com>
2017-06-21 16:05:28 +0800

    Keep all projectId logic in the api service, reference it from session service
    Former-commit-id: 773a64822905206a47edf39a2e2ebaa1c1acbfd0

--- src/angular-app/bellows/js/bellows.services.api.js
+++ src/angular-app/bellows/js/bellows.services.api.js
@@ -1,9 +1,9 @@
 angular.module('bellows.services')
   .service('apiService', ['jsonRpc', '$q', function (jsonRpc, $q) {
 
-    // FIXME This same code exists in session service, but can't be used due to circular dependency.
     var projectId = window.location.pathname.match(/^\/app\/[a-z]+\/([a-z0-9]{24,})$/i);
     projectId = projectId == null ? undefined : projectId[1];
+    this.projectId = projectId;
 
     this.call = function(method, args, callback) {
       var options = {
