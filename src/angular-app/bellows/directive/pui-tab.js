11578bd9a0e0b4bbb3524270181d7b3938529b1f
Nathaniel Paulus <nateowami@gmail.com>
2017-06-27 23:01:53 +0800

    Heavily improve pui-tabset

--- src/angular-app/bellows/directive/pui-tab.js
+++ src/angular-app/bellows/directive/pui-tab.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Tab directives heavily inspired by https://toddmotto.com/directive-to-directive-communication-with-require/
 angular.module('palaso.ui.tabset', [])
   .directive('puiTabset', [function() {
     return {
@@ -13,32 +14,45 @@ angular.module('palaso.ui.tabset', [])
           this.tabs.push(tab);
         };
         this.selectTab = function(index) {
+          this.tabs[index].onSelect();
           this.tabIndex = index;
           this.tabs.forEach(function(val, i) {
             this.tabs[i].selected = i === index;
           }, this);
         };
+        this.selectedTab = function() {
+          return this.tabs[this.tabIndex];
+        };
       },
       link: function($scope, $element, $attrs, $ctrl) {
         $ctrl.selectTab($attrs.selected || $ctrl.tabIndex);
       },
-      controllerAs: 'tabs',
-      templateUrl: '/angular-app/bellows/directive/' + bootstrapVersion + '/palaso.ui.tabs.html'
+      controllerAs: 'tabset',
+      templateUrl: '/angular-app/bellows/directive/' + bootstrapVersion + '/palaso.ui.tabset.html'
     };
   }])
   .directive('puiTab', [function() {
+    // Note: ng-if should not be used on a pui-tab directive if it will initially be false and afterwards become true.
+    // Doing so causes the tab to be added last, and therefore shown as the last tab, regardless of the order it was
+    // in the original template.
     return {
       restrict: 'E',
       transclude: true,
       require: '^puiTabset',
       scope: {
-        heading: '@'
+        heading: '@',
+        select: '&',
+        tabIf: '='
       },
       template: '<div ng-show="tab.selected" class="tab-pane" ng-class="{active: tab.selected}"><div ng-transclude></div></div>',
       link: function($scope, $element, $attrs, $ctrl) {
         $scope.tab = {
           heading: $scope.heading,
-          selected: false
+          onSelect: $scope.select || angular.noop,
+          selected: false,
+          visible: function() {
+            return $scope.tabIf !== false;
+          }
         };
         $ctrl.addTab($scope.tab);
       }
