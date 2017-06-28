c184fafe1b638c2d4d2e583732c9da1edd224d7a
Nathaniel Paulus <nateowami@gmail.com>
2017-06-28 12:16:51 +0800

    Add JSCS config to package.json and format pui-tab according to style guide
    Former-commit-id: 648104fe259de75379aed68499e3de6e49a9f1e9

--- src/angular-app/bellows/directive/pui-tab.js
+++ src/angular-app/bellows/directive/pui-tab.js
@@ -2,39 +2,43 @@
 
 // Tab directives heavily inspired by https://toddmotto.com/directive-to-directive-communication-with-require/
 angular.module('palaso.ui.tabset', [])
-  .directive('puiTabset', [function() {
+  .directive('puiTabset', [function () {
     return {
       restrict: 'E',
       scope: {},
       transclude: true,
-      controller: function() {
+      controller: function () {
         this.tabs = [];
         this.tabIndex = 0;
-        this.addTab = function(tab) {
+        this.addTab = function (tab) {
           this.tabs.push(tab);
         };
-        this.selectTab = function(index) {
+
+        this.selectTab = function (index) {
           this.tabs[index].onSelect();
           this.tabIndex = index;
-          this.tabs.forEach(function(val, i) {
+          this.tabs.forEach(function (val, i) {
             this.tabs[i].selected = i === index;
           }, this);
         };
-        this.selectedTab = function() {
+
+        this.selectedTab = function () {
           return this.tabs[this.tabIndex];
         };
       },
-      link: function($scope, $element, $attrs, $ctrl) {
+
+      link: function ($scope, $element, $attrs, $ctrl) {
         $ctrl.selectTab($attrs.selected || $ctrl.tabIndex);
       },
+
       controllerAs: 'tabset',
       templateUrl: '/angular-app/bellows/directive/' + bootstrapVersion + '/palaso.ui.tabset.html'
     };
   }])
-  .directive('puiTab', [function() {
-    // Note: ng-if should not be used on a pui-tab directive if it will initially be false and afterwards become true.
-    // Doing so causes the tab to be added last, and therefore shown as the last tab, regardless of the order it was
-    // in the original template.
+  .directive('puiTab', [function () {
+    // Note: ng-if should not be used on a pui-tab directive if it will initially be false and
+    // afterwards become true. Doing so causes the tab to be added last, and therefore shown as
+    // the last tab, regardless of the order it was in the original template.
     return {
       restrict: 'E',
       transclude: true,
@@ -44,17 +48,18 @@ angular.module('palaso.ui.tabset', [])
         select: '&',
         tabIf: '='
       },
-      template: '<div ng-show="tab.selected" class="tab-pane" ng-class="{active: tab.selected}"><div ng-transclude></div></div>',
-      link: function($scope, $element, $attrs, $ctrl) {
+      template: '<div ng-show="tab.selected" class="tab-pane" ng-class="{active: tab.selected}">' +
+        '<div ng-transclude></div></div>',
+      link: function ($scope, $element, $attrs, $ctrl) {
         $scope.tab = {
           heading: $scope.heading,
           onSelect: $scope.select || angular.noop,
           selected: false,
-          visible: function() {
+          visible: function () {
             return $scope.tabIf !== false;
           }
         };
         $ctrl.addTab($scope.tab);
       }
-    }
+    };
   }]);
