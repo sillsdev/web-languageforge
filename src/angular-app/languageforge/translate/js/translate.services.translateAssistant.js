33bf4e5ae59acdc886349246c2532bb72dc96d68
Ira Hopkinson <ira_hopkinson@sil.org>
2017-07-03 09:28:50 +0700

    Work-around for blank source segments
     - also work-around for "Invalid regular expression" error in isPunctuation
    
    
    Former-commit-id: 5b28132717eaeba01ef8224e957bd191d71901a3

--- src/angular-app/languageforge/translate/js/translate.services.translateAssistant.js
+++ src/angular-app/languageforge/translate/js/translate.services.translateAssistant.js
@@ -12,19 +12,31 @@ angular.module('translate.services')
         sourceLanguageTag, targetLanguageTag, projectId);
     };
 
+    /**
+     * translateInteractively currently dies if an empty sourceSegment is passed in.
+     * also replace \u2014 with dash (otherwise get "Invalid regular expression" error in
+     * bridge.js function isPunctuation)
+     * @param {string} sourceSegment
+     * @returns {string}
+     */
+    function fixSourceSegment(sourceSegment) {
+      return (sourceSegment) ? sourceSegment.split('—').join('-') : '_';
+    }
+
     // SIL.Machine.Translation.TranslationEngine.translateInteractively(sourceSegment,
     //    confidenceThreshold, onFinished)
     this.translateInteractively = function translateInteractively(sourceSegment,
                                                                   confidenceThreshold, callback) {
       if (angular.isUndefined(engine)) return;
 
-      engine.translateInteractively(sourceSegment, confidenceThreshold, function (newSession) {
-        if (newSession) {
-          session = newSession;
-        }
+      engine.translateInteractively(fixSourceSegment(sourceSegment), confidenceThreshold,
+        function (newSession) {
+          if (newSession) {
+            session = newSession;
+          }
 
-        (callback || angular.noop)();
-      });
+          (callback || angular.noop)();
+        });
     };
 
     // SIL.Machine.Translation.InteractiveTranslationSession.updatePrefix(prefix)
