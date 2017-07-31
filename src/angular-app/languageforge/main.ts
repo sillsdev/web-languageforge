b8912eff38a94189d655f4888d7d97d5af6ec6c4
Ira Hopkinson <ira_hopkinson@sil.org>
2017-07-31 10:35:20 +0700

    Convert configuration.js to TypeScript
    Former-commit-id: b7d138a276ed89d0986d47a78fe89726e68961bd

--- src/angular-app/languageforge/main.ts
+++ src/angular-app/languageforge/main.ts
@@ -1,1 +1,6 @@
-import '../bellows/polyfills.browser'
+import '../bellows/polyfills.browser.ts'
+
+// this is imported here to ensure JS files can use it
+import '../bellows/js/assets/inputSystems.ts'
+
+import './lexicon/views/configuration.ts'
