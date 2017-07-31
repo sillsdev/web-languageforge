ab3503ccbf2f688f3eca7b3eb876c31d05b83264
Ira Hopkinson <ira_hopkinson@sil.org>
2017-07-31 16:23:41 +0700

    Fix empty sourceSegment and "Invalid regular expression" bugs
     - fix exception on empty sourceSegment
     - fix "Invalid regular expression" bug on non-ascii sourceSegment
     - remove front-end work-around in favour of machine work-around
    
    
    Former-commit-id: 8196f9ccff0e1f25de4b32fd9807894020a934a2

--- src/angular-app/languageforge/translate/js/translate.services.translateAssistant.js
+++ src/angular-app/languageforge/translate/js/translate.services.translateAssistant.js
@@ -11,31 +11,19 @@ angular.module('translate.services')
         projectId);
     };
 
-    /**
-     * translateInteractively currently dies if an empty sourceSegment is passed in.
-     * also replace \u2014 with dash (otherwise get "Invalid regular expression" error in
-     * bridge.js function isPunctuation)
-     * @param {string} sourceSegment
-     * @returns {string}
-     */
-    function fixSourceSegment(sourceSegment) {
-      return (sourceSegment) ? sourceSegment.split('—').join('-') : '_';
-    }
-
     // SIL.Machine.Translation.TranslationEngine.translateInteractively(sourceSegment,
     //    confidenceThreshold, onFinished)
     this.translateInteractively = function translateInteractively(sourceSegment,
                                                                   confidenceThreshold, callback) {
       if (angular.isUndefined(engine)) return;
 
-      engine.translateInteractively(fixSourceSegment(sourceSegment), confidenceThreshold,
-        function (newSession) {
-          if (newSession) {
-            session = newSession;
-          }
+      engine.translateInteractively(sourceSegment, confidenceThreshold, function (newSession) {
+        if (newSession) {
+          session = newSession;
+        }
 
-          (callback || angular.noop)();
-        });
+        (callback || angular.noop)();
+      });
     };
 
     // SIL.Machine.Translation.InteractiveTranslationSession.updatePrefix(prefix)
