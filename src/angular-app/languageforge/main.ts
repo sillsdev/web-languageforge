4baea5e47aaffce84a662b36dc97d4af72059ef0
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-07 13:39:16 +0700

    Remove src/package.json
     - move bootstrap, font-awesome, intl-tel-input, jquery, rangy,
       textangular, zxcvbn
     - update documentation and scripts

--- src/angular-app/languageforge/main.ts
+++ src/angular-app/languageforge/main.ts
@@ -1,6 +1,6 @@
-import '../bellows/main.common'
+import '../bellows/main.common';
 
 // this is imported here to ensure JS files can use it
-import '../bellows/js/assets/inputSystems.ts'
+import '../bellows/js/assets/inputSystems';
 
-import './lexicon/views/configuration.ts'
+import './lexicon/views/configuration';
