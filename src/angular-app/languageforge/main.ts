fa3d3384f590af235f27b3d9a01481e102fd08d6
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-15 14:38:07 +0700

    improve TS call chain for Translate App
    Former-commit-id: 4c74da7e5ac8ddb6a28eb72f87e7c39b6e4d3f25

--- src/angular-app/languageforge/main.ts
+++ src/angular-app/languageforge/main.ts
@@ -1,17 +1,3 @@
 import '../bellows/main.common';
 import './lexicon/lexicon.module';
-
-
-// translate
-import 'angularjs-slider';
-import 'angularjs-slider/dist/rzslider.css';
-import 'quill';
-import 'quill/dist/quill.bubble.css';
-import 'quill/dist/quill.snow.css';
-import 'ng-quill';
-
-import './translate/core/translate-core.module';
-import './translate/core/machine.service';
-import './translate/shared/translate-shared.module';
-import './translate/editor/quill/quill.module'
-import './translate/editor/quill/quill.service'
+import './translate/translate.module';
