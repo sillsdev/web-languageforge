3c88e9319bd42de2c718f45320c23b504cebac6b
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-16 11:56:29 +0700

    convert bellows project service to TypeScript
     - change offline cache and notice from factory to service
     - change notice directive to component
     - convert bellows util to TS
     - convert error, jsonRpc, api services to TS
     - convert offline cache service to function in TS
     - convert project service, session service to TS

---
+++ src/angular-app/bellows/core/notice/notice.service.ts
@@ -0,0 +1,139 @@
+import * as angular from 'angular';
+
+class Notice {
+  cannotClose: boolean;
+
+  constructor(
+    public type: string,
+    public message: string,
+    public id: string,
+    public details: string,
+    public showDetails: boolean
+  ) {}
+
+  toggleDetails () {
+    this.showDetails = !this.showDetails;
+  }
+}
+
+export class NoticeService {
+  private notices: Notice[];
+  private timers: { [id: string]: angular.IPromise<any>; };
+  private percentComplete: number;
+  private isProgressBarShown: boolean;
+  private isLoadingNotice: boolean;
+  private loadingMessage: string;
+
+  static $inject: string[] = ['$interval', 'utilService'];
+  constructor(private $interval: angular.IIntervalService, private util: any) {
+    this.notices = [];
+    this.timers = {};
+    this.percentComplete = 0;
+    this.isProgressBarShown = false;
+    this.isLoadingNotice = false;
+  }
+
+  private getIndexById(id: string): number {
+    for (let i = 0; i < this.notices.length; i++) {
+      if (this.notices[i].id === id) {
+        return i;
+      }
+    }
+  }
+
+  push(type: () => string, message: string, details?: string, cannotClose?: boolean, time?: number): string {
+    let id = this.util.uuid();
+
+    if (!time && type() === this.SUCCESS()) time = 4 * 1000;
+    if (time) {
+      this.timers[id] = this.$interval(() => { this.removeById(id); }, time, 1);
+    }
+
+    let notice = new Notice(type(), message, id, details, false);
+
+    if (details) {
+      details = details.replace(/<p>/gm, '\n');
+      details = details.replace(/<pre>/gm, '\n');
+      details = details.replace(/<\/p>/gm, '\n');
+      details = details.replace(/<\/pre>/gm, '\n');
+      details = details.replace(/<[^>]+>/gm, ''); // remove HTML
+      details = details.replace(/\\\//g, '/');
+      notice.details = details;
+    }
+
+    if (cannotClose) {
+      notice.cannotClose = true;
+    }
+
+    this.notices.push(notice);
+    return id;
+  };
+
+  removeById(id: string) {
+    this.remove(this.getIndexById(id));
+    if (id in this.timers) {
+      this.$interval.cancel(this.timers[id]);
+    }
+  };
+
+  remove(index: number) {
+    if (!angular.isUndefined(index)) {
+      this.notices.splice(index, 1);
+    }
+  };
+
+  get(): Notice[] {
+    return this.notices;
+  };
+
+  getLoadingMessage(): string {
+    return this.loadingMessage;
+  };
+
+  setLoading(message: string) {
+    this.loadingMessage = message;
+    this.isLoadingNotice = true;
+  };
+
+  getPercentComplete(): number {
+    return this.percentComplete;
+  };
+
+  setPercentComplete(percent: number) {
+    this.percentComplete = percent;
+    this.isProgressBarShown = true;
+  };
+
+  cancelProgressBar() {
+    this.isProgressBarShown = false;
+  };
+
+  showProgressBar(): boolean {
+    return this.isProgressBarShown;
+  };
+
+  cancelLoading() {
+    this.loadingMessage = '';
+    this.isLoadingNotice = false;
+  };
+
+  isLoading(): boolean {
+    return this.isLoadingNotice;
+  };
+
+  ERROR(): string {
+    return 'danger';
+  };
+
+  WARN(): string {
+    return 'warning';
+  };
+
+  INFO(): string {
+    return 'info';
+  };
+
+  SUCCESS(): string {
+    return 'success';
+  };
+}
---
+++ src/angular-app/bellows/core/session.service.ts
@@ -0,0 +1,174 @@
+import * as angular from 'angular';
+
+import { ApiMethod, ApiService } from  './api.service';
+
+export class Session {
+  readonly userId: SessionDataFunction;
+  readonly fileSizeMax: SessionDataFunction;
+  readonly baseSite: SessionDataFunction;
+  readonly projectSettings: SessionDataFunction;
+  readonly project: SessionDataFunction;
+  readonly username: SessionDataFunction;
+
+  constructor(public data?: SessionData) {
+    this.userId = this.sessionDataFunctionFor('userId');
+    this.fileSizeMax = this.sessionDataFunctionFor('fileSizeMax');
+    this.baseSite = this.sessionDataFunctionFor('baseSite');
+    this.projectSettings = this.sessionDataFunctionFor('projectSettings');
+    this.project = this.sessionDataFunctionFor('project');
+    this.username = this.sessionDataFunctionFor('username');
+  }
+
+  hasSiteRight(domain: RightsFunction, operation: RightsFunction) {
+    return this.hasRight(this.data.userSiteRights, domain, operation);
+  };
+
+  hasProjectRight(domain: RightsFunction, operation: RightsFunction) {
+    return this.hasRight(this.data.userProjectRights, domain, operation);
+  };
+
+  hasRight(rights: any, domain: RightsFunction, operation: RightsFunction) {
+    if (!rights) return false;
+    let right = domain() + operation();
+    return rights.indexOf(right) !== -1;
+  };
+
+  getProjectSetting(setting: string) {
+    return this.data.projectSettings[setting];
+  };
+
+  private sessionDataFunctionFor(key: string): SessionDataFunction {
+    return () => {
+      return this.data[key];
+    };
+  }
+
+}
+
+export interface SessionCallback { (session: Session): void; }
+
+interface SessionDataFunction { (): any}
+
+class SessionData {
+  userId: string;
+  fileSizeMax: number;
+  baseSite: string;
+  projectSettings: any;
+  project: any;
+  username: string;
+  userSiteRights: any;
+  userProjectRights: any;
+}
+
+interface RightsFunction { (): number }
+
+class Domains {
+  ANY: RightsFunction;
+  USERS: RightsFunction;
+  PROJECTS: RightsFunction;
+  TEXTS: RightsFunction;
+  QUESTIONS: RightsFunction;
+  ANSWERS: RightsFunction;
+  COMMENTS: RightsFunction;
+  TEMPLATES: RightsFunction;
+  TAGS: RightsFunction;
+  ENTRIES: RightsFunction;
+}
+
+class Operations {
+  CREATE: RightsFunction;
+  EDIT: RightsFunction;
+  DELETE: RightsFunction;
+  LOCK: RightsFunction;
+  VIEW: RightsFunction;
+  VIEW_OWN: RightsFunction;
+  EDIT_OWN: RightsFunction;
+  DELETE_OWN: RightsFunction;
+  ARCHIVE: RightsFunction;
+}
+
+// Because session data is asynchronously loaded, it is not possible to synchronously
+// obtain a reference to the session instance. getSession() must be called, which returns
+// a promise (callbacks also accepted) that resolves to the session instance, which can
+// then be used synchronously.
+export class SessionService {
+  domain: Domains;
+  operation: Operations;
+  getCaptchaData: ApiMethod;
+
+  private session: Session;
+  private promiseForSession: angular.IPromise<any>;
+
+  static $inject: string[] = ['apiService', '$q'];
+  constructor(private api: ApiService, private $q: angular.IQService) {
+    const domains: Domains = {
+      ANY:       this.rightsFunction(1000),
+      USERS:     this.rightsFunction(1100),
+      PROJECTS:  this.rightsFunction(1200),
+      TEXTS:     this.rightsFunction(1300),
+      QUESTIONS: this.rightsFunction(1400),
+      ANSWERS:   this.rightsFunction(1500),
+      COMMENTS:  this.rightsFunction(1600),
+      TEMPLATES: this.rightsFunction(1700),
+      TAGS:      this.rightsFunction(1800),
+      ENTRIES:   this.rightsFunction(1900)
+    };
+    const operations: Operations = {
+      CREATE:       this.rightsFunction(1),
+      EDIT:         this.rightsFunction(2),
+      DELETE:       this.rightsFunction(3),
+      LOCK:         this.rightsFunction(4),
+      VIEW:         this.rightsFunction(5),
+      VIEW_OWN:     this.rightsFunction(6),
+      EDIT_OWN:     this.rightsFunction(7),
+      DELETE_OWN:   this.rightsFunction(8),
+      ARCHIVE:      this.rightsFunction(9)
+    };
+
+    this.domain = domains;
+    this.operation = operations;
+    this.getCaptchaData = api.method('get_captcha_data');
+
+    // session instance (singleton) that references the data
+    this.session = new Session();
+  }
+
+  projectId() {
+    return this.api.projectId;
+  };
+
+  getSession(forceRefresh: boolean = false, callback?: SessionCallback) {
+    if (this.session.data && !forceRefresh) {
+      if (callback) callback(this.session);
+      return this.$q.when(this.session); // Wrap session in a promise
+    }
+
+    return this.fetchSessionData(forceRefresh).then((data: SessionData) => {
+      this.session.data = data;
+      if (callback) callback(this.session);
+      return this.session;
+    });
+  };
+
+  private fetchSessionData(forceRefresh: boolean) {
+    if (this.promiseForSession && !forceRefresh) return this.promiseForSession;
+
+    let promise: angular.IPromise<any> = this.api.call('session_getSessionData').then((response) => {
+      return response.data;
+    }).catch((response) => {
+      console.error(response); // TODO decide whether to show to user or just retry
+      return this.fetchSessionData(forceRefresh); // retry
+    });
+
+    if (!this.promiseForSession) this.promiseForSession = promise;
+    return promise;
+  }
+
+  private rightsFunction(val: number): RightsFunction {
+    return function () {
+      return val;
+    };
+  }
+
+}
+
