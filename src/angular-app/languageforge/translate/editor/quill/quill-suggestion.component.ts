09ab0d3231ecba90901286e5556bbf0045f3ac73
Damien Daspit <damien_daspit@sil.org>
2017-08-17 16:58:46 +0700

    Fix wordCombine method in QuillSuggestionComponent
    * pulled out controller into a proper class
    
    
    Former-commit-id: 0a225bd50672ee307b113ada0a083fcae06ecc02

--- src/angular-app/languageforge/translate/editor/quill/quill-suggestion.component.ts
+++ src/angular-app/languageforge/translate/editor/quill/quill-suggestion.component.ts
@@ -1,31 +1,11 @@
 import * as angular from 'angular';
-import { WordParser } from '../word-parser.service';
+import { QuillSuggestionController } from './quill-suggestion.controller';
 
-export const QuillSuggestionComponent = {
+export const QuillSuggestionComponent: angular.IComponentOptions = {
   bindings: {
     qlSuggestions: '<',
     qlInsertSuggestion: '&'
   },
   templateUrl: '/angular-app/languageforge/translate/editor/quill/quill-suggestion.component.html',
-  controller: ['wordParser', function (wordParser: WordParser) {
-    this.wordCombine = wordParser.wordCombine;
-
-    this.wordWidthStyle = function (word: string, isLast: boolean) {
-      let words = this.wordCombine(this.qlSuggestions);
-      if (words.length <= 0) return;
-
-      let space = (isLast) ? 0 : 1;
-      return { width: Math.floor((word.length + space) * 100 / words.length) + '%' };
-    };
-
-    this.selectAll = function () {
-      let text = this.wordCombine(this.qlSuggestions);
-      (this.qlInsertSuggestion || angular.noop)({ text: text });
-    };
-
-    this.selectWord = function (index: number) {
-      let text = this.qlSuggestions[index];
-      (this.qlInsertSuggestion || angular.noop)({ text: text });
-    };
-  }]
+  controller: QuillSuggestionController
 };
