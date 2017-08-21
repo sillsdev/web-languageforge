8fa33b172c14d57553e0ee9f8aad2ca989af59cd
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-21 14:32:46 +0700

    convert translate project service to TS

--- src/angular-app/languageforge/translate/core/translate-core.module.ts
+++ src/angular-app/languageforge/translate/core/translate-core.module.ts
@@ -1,9 +1,11 @@
 import * as angular from 'angular';
+
 import { MachineService } from './machine.service';
+import { TranslateProjectService } from './translate-project.service';
 
 export const TranslateCoreModule = angular
   .module('translateCoreModule', [])
+  .service('translateProjectApi', TranslateProjectService)
   .service('machineService', MachineService)
   .value('realTime', require('../../../../node/client.js').realTime)
-
-  ;
+  .name;
