a9aeb3e999e2e3bc89857f38eadbafe320ee2a93
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-22 17:26:45 +0700

    convert translate rights service to TypeScript
    Former-commit-id: 2dfc26338869131cb677459369f46b79b22e781e

--- src/angular-app/languageforge/translate/core/translate-core.module.ts
+++ src/angular-app/languageforge/translate/core/translate-core.module.ts
@@ -2,10 +2,12 @@ import * as angular from 'angular';
 
 import { MachineService } from './machine.service';
 import { TranslateProjectService } from './translate-project.service';
+import { TranslateRightsService } from './translate-rights.service';
 
 export const TranslateCoreModule = angular
   .module('translateCoreModule', [])
   .service('translateProjectApi', TranslateProjectService)
+  .service('translateRightsService', TranslateRightsService)
   .service('machineService', MachineService)
   .value('realTime', require('../../../../node/client.js').realTime)
   .name;
