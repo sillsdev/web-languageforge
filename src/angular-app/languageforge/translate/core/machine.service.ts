0801dc53e5c65f06bcd2829ae34657d7321683b4
Damien Daspit <damien_daspit@sil.org>
2017-08-22 18:13:53 +0700

    Port DocumentData to Typescript

--- src/angular-app/languageforge/translate/core/machine.service.ts
+++ src/angular-app/languageforge/translate/core/machine.service.ts
@@ -12,48 +12,45 @@ export class MachineService {
   // SIL.Machine.Translation.TranslationEngine.translateInteractively(sourceSegment,
   //    confidenceThreshold, onFinished)
   translateInteractively(sourceSegment: string, confidenceThreshold: number, callback?: () => void): void {
-    if (!this.engine) return;
+    if (this.engine == null) return;
 
     this.engine.translateInteractively(sourceSegment, confidenceThreshold, newSession => {
-      if (newSession) {
-        this.session = newSession;
-      }
-
-      if (callback) callback();
+      this.session = newSession;
+      if (callback != null) callback();
     });
   };
 
   // SIL.Machine.Translation.TranslationEngine.train(onStatusUpdate, onFinished)
   train(onStatusUpdate: (progress: machine.SmtTrainProgress) => void, onFinished: (success: boolean) => void): void {
-    if (!this.engine) return;
+    if (this.engine == null) return;
 
     this.engine.train(onStatusUpdate, onFinished);
   }
 
   // SIL.Machine.Translation.TranslationEngine.listenForTrainingStatus(onStatusUpdate, onFinished)
   listenForTrainingStatus(onStatusUpdate: (progress: machine.SmtTrainProgress) => void, onFinished: (success: boolean) => void): void {
-    if (!this.engine) return;
+    if (this.engine == null) return;
 
     this.engine.listenForTrainingStatus(onStatusUpdate, onFinished);
   }
 
   // SIL.Machine.Translation.InteractiveTranslationSession.updatePrefix(prefix)
   updatePrefix(prefix: string): string[] {
-    if (!this.engine || !this.session) return;
+    if (this.engine == null || this.session == null) return [];
 
     // returns suggestions
     return this.session.updatePrefix(prefix);
   }
 
   getCurrentSuggestion(): string[] {
-    if (!this.engine || !this.session) return;
+    if (this.engine == null || this.session == null) return [];
 
     return this.session.currentSuggestion;
   }
 
   // SIL.Machine.Translation.InteractiveTranslationSession.approve(onFinished)
   learnSegment(callback: (success: boolean) => void): void {
-    if (!this.engine || !this.session) return;
+    if (this.engine == null || this.session == null) return;
 
     this.session.approve(callback);
   }
--- src/angular-app/languageforge/translate/editor/editor.module.ts
+++ src/angular-app/languageforge/translate/editor/editor.module.ts
@@ -1,8 +1,10 @@
 import * as angular from 'angular';
 import { WordParser } from './word-parser.service';
+import { DocumentDataService } from './document-data.service';
 
 export const EditorModule = angular
   .module('editorModule', [])
   .service('wordParser', WordParser)
+  .service('documentDataService', DocumentDataService)
 
   ;
