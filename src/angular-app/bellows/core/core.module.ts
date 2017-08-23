6c6ec013038399d3fc74b0d3255b909a3ac2b347
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-23 12:24:03 +0700

    convert modal service to TypeScript
    Former-commit-id: 83badb2bf5454306b4d48451122a50759449ec18

--- src/angular-app/bellows/core/core.module.ts
+++ src/angular-app/bellows/core/core.module.ts
@@ -2,6 +2,7 @@ import * as angular from 'angular';
 
 import { ApiService } from  './api.service';
 import { BytesFilter, RelativeTimeFilter } from './filters';
+import { ModalService } from './modal/modal.service';
 import { OfflineCacheService } from './offline-cache.service';
 import { ProjectService } from './project.service';
 import { SessionService } from './session.service';
@@ -16,6 +17,7 @@ export const CoreModule = angular
   .service('projectService', ProjectService)
   .service('apiService', ApiService)
   .service('sessionService', SessionService)
+  .service('modalService', ['$uibModal', ModalService])
   .service('offlineCache', ['$window', '$q', OfflineCacheService])
   .service('utilService', UtilityService)
   .filter('bytes', BytesFilter)
