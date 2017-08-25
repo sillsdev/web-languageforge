4d05c29a133f1309292ced6687d454c08ee0b5ee
Ira Hopkinson <ira_hopkinson@sil.org>
2017-08-25 11:14:39 +0700

    Convert real-time client to TS

--- src/angular-app/languageforge/translate/core/translate-core.module.ts
+++ src/angular-app/languageforge/translate/core/translate-core.module.ts
@@ -1,6 +1,7 @@
 import * as angular from 'angular';
 
 import { MachineService } from './machine.service';
+import { RealTimeService } from './realtime.service'
 import { TranslateProjectService } from './translate-project.service';
 import { TranslateRightsService } from './translate-rights.service';
 
@@ -9,5 +10,5 @@ export const TranslateCoreModule = angular
   .service('translateProjectApi', TranslateProjectService)
   .service('translateRightsService', TranslateRightsService)
   .service('machineService', MachineService)
-  .value('realTime', require('../../../../node/client.js').realTime)
+  .service('realTimeService', RealTimeService)
   .name;
