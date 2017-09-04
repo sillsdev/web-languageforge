2756430c2f608194793d6ceeab401c035429a43b
Ira Hopkinson <ira_hopkinson@sil.org>
2017-09-04 20:54:54 +0700

    change settings component to one-way bindings
    Former-commit-id: 561df91c23d086eaeef02f06e6e9bfe566b616d9

--- src/angular-app/languageforge/translate/translate-app.component.ts
+++ src/angular-app/languageforge/translate/translate-app.component.ts
@@ -1,6 +1,7 @@
 import * as angular from 'angular';
 
 import { SessionService } from '../../bellows/core/session.service';
+import { TranslateProjectService } from './core/translate-project.service';
 import { Rights, TranslateRightsService } from './core/translate-rights.service';
 
 export class TranslateAppController implements angular.IController {
@@ -8,21 +9,30 @@ export class TranslateAppController implements angular.IController {
   rights: Rights;
   interfaceConfig: any;
 
-  static $inject = ['$state', 'sessionService', 'translateRightsService', '$q'];
+  static $inject = ['$state', 'sessionService',
+    'translateRightsService',
+    'translateProjectApi', '$q'
+  ];
   constructor(private $state: angular.ui.IStateService, private sessionService: SessionService,
-              private rightsService: TranslateRightsService, private $q: angular.IQService) {}
+              private rightsService: TranslateRightsService,
+              private projectApi: TranslateProjectService, private $q: angular.IQService) {}
 
   $onInit() {
-    this.$q.all([this.sessionService.getSession(), this.rightsService.getRights()]).then(data => {
-      const session = data[0];
-      const rights = data[1];
-
-      this.project = session.project();
+    this.$q.all([this.rightsService.getRights(), this.sessionService.getSession(),
+      this.projectApi.readProject()
+    ]).then(([rights, session, readProjectResult]) => {
       this.rights = rights;
       this.rights.showSettingsDropdown = () => {
         return this.rights.canEditProject() || this.rights.canEditUsers() ||
           this.rights.canEditEntry();
       };
+
+      this.project = session.project();
+      if (readProjectResult.ok) {
+        angular.merge(this.project, readProjectResult.data.project);
+      }
+
+      this.project.config = this.project.config || {};
 
       // this.interfaceConfig = sessionService.session.projectSettings.interfaceConfig;
       this.interfaceConfig = {};
