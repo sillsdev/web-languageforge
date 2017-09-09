a7b8b35ac956f4765a20ebdd7ccf615b1f7bf0e1
Ira Hopkinson <ira_hopkinson@sil.org>
2017-09-09 13:52:34 +0700

    convert user profile app to TypeScript
    Former-commit-id: a810f0d4d98fff2c72d243df5aecd9237975ff61

---
+++ src/angular-app/bellows/shared/model/user-profile.model.ts
@@ -0,0 +1,15 @@
+export class UserProfile {
+  age: string = '';
+  avatar_color: string = '';
+  avatar_shape: string = '';
+  avatar_ref: string;
+  communicate_via: string = '';
+  email: string = '';
+  gender: string = '';
+  id: string = '';
+  last_login: string = '';
+  mobile_phone: string = '';
+  name: string = '';
+  projectUserProfiles: any;
+  username: string = '';
+}
