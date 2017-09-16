54559fa2b0e35d5f8e3457ce3e88778434354d2e
Ira Hopkinson <ira_hopkinson@sil.org>
2017-09-16 21:11:15 +0700

    convert public apps to TypeScript
     - convert forgot password app to TypeScript
     - convert login app to TypeScript
     - convert reset password app to TypeScript
     - convert signup app to TypeScript

--- src/angular-app/bellows/apps/changepassword/change-password-app.module.ts
+++ src/angular-app/bellows/apps/changepassword/change-password-app.module.ts
@@ -4,7 +4,7 @@ import { ChangePasswordAppComponent } from './change-password-app.component';
 import { NoticeServiceModule } from "../../core/notice/notice.module";
 import { PuiUtilityModule } from '../../shared/pui-utils.module';
 
-export const ChangePasswordModule = angular
+export const ChangePasswordAppModule = angular
   .module('changepassword', ['ui.bootstrap', 'bellows.services', 'ui.validate',
     NoticeServiceModule, PuiUtilityModule, 'zxcvbn'
   ])
