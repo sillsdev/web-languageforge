c397048e294945cec9d39cbd235383a6e6c04578
Ira Hopkinson <ira_hopkinson@sil.org>
2017-09-21 11:39:52 +0700

    fix up TS call chain

--- src/angular-app/languageforge/translate/settings/settings.module.ts
+++ src/angular-app/languageforge/translate/settings/settings.module.ts
@@ -3,12 +3,13 @@ import 'angularjs-slider';
 import 'angularjs-slider/dist/rzslider.css';
 
 import { CoreModule } from '../../../bellows/core/core.module';
+import { NoticeModule } from '../../../bellows/core/notice/notice.module';
 import { TranslateSettingsComponent } from './settings.component';
 import { TranslateSharedModule } from '../shared/translate-shared.module';
 
 export const TranslateSettingsModule = angular
-  .module('translateSettingsModule', [CoreModule, 'ui.bootstrap', 'palaso.ui.listview',
-    'palaso.ui.typeahead', 'palaso.ui.archiveProject', 'palaso.ui.deleteProject', 'palaso.ui.notice',
+  .module('translateSettingsModule', ['ui.bootstrap', CoreModule, 'palaso.ui.listview',
+    'palaso.ui.typeahead', 'palaso.ui.archiveProject', 'palaso.ui.deleteProject', NoticeModule,
     'palaso.ui.textdrop', TranslateSharedModule, 'rzModule'])
   .component('translateSettings', TranslateSettingsComponent)
   .name;
