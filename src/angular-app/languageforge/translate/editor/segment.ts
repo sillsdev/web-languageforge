09dbf41fd31a8894024ecdf3eadeab6e175a8108
Damien Daspit <damien_daspit@sil.org>
2017-09-22 16:01:46 +0700

    Train segment when it changes, remove segment format

--- src/angular-app/languageforge/translate/editor/segment.ts
+++ src/angular-app/languageforge/translate/editor/segment.ts
@@ -1,9 +1,9 @@
 import { RangeStatic, StringMap } from 'quill';
-import { SegmentFormat } from './quill/suggestions-theme';
 
 export class Segment {
   private _text: string;
   private _range: RangeStatic;
+  private initialText: string;
 
   constructor(public readonly index: number) { }
 
@@ -15,41 +15,15 @@ export class Segment {
     return this._range;
   }
 
-  update(text: string, range: RangeStatic, format: StringMap): boolean {
+  get isChanged(): boolean {
+    return this._text !== this.initialText;
+  }
+
+  update(text: string, range: RangeStatic): void {
     this._text = text;
     this._range = range;
-    return format.segment == null;
-  }
-
-  getFormat(): StringMap {
-    const segmentFormat: SegmentFormat = {};
-    return { segment: segmentFormat };
-  }
-}
-
-export class TargetSegment extends Segment {
-  isTrained: boolean = false;
-
-  update(text: string, range: RangeStatic, format: StringMap): boolean {
-    const isChanged = this.text != null && this.text !== text;
-    const result = super.update(text, range, format);
-    const segmentFormat: SegmentFormat = format.segment;
-    if (segmentFormat != null) {
-      const isTrained = segmentFormat.isTrained;
-      if (isTrained != null) {
-        this.isTrained = isTrained.toLowerCase() === 'true';
-      }
+    if (this.initialText == null) {
+      this.initialText = text;
     }
-    if (isChanged) {
-      this.isTrained = false;
-      return true;
-    }
-    return result;
-  }
-
-  getFormat(): StringMap {
-    const format = super.getFormat();
-    format.segment.isTrained = this.isTrained.toString();
-    return format;
   }
 }
