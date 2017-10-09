b5c719c71b039e6aa7292fd2d41fd9f0821d2b9e
Ira Hopkinson <ira_hopkinson@sil.org>
2017-10-09 12:37:51 +0700

    Fix type errors

--- src/angular-app/bellows/apps/activity/activity-app.component.ts
+++ src/angular-app/bellows/apps/activity/activity-app.component.ts
@@ -49,11 +49,11 @@ export class ActivityAppController implements angular.IController {
       { label: 'Activity' }
     ]);
 
-    this.activityService.listActivity((result) => {
+    this.activityService.listActivity(result => {
       if (result.ok) {
         this.activities = [];
         this.unread = result.data.unread;
-        for (let key in result.data.activity) {
+        for (const key in result.data.activity) {
           if (result.data.activity.hasOwnProperty(key)) {
             this.activities.push(result.data.activity[key]);
           }
@@ -68,55 +68,54 @@ export class ActivityAppController implements angular.IController {
 
   isUnread(id: string) {
     return (this.unread.findIndex(value => value === id) > -1);
-  };
+  }
 
   decodeActivityList(items: Activity[]) {
-    for (let i = 0; i < items.length; i++) {
-      if ('userRef' in items[i]) {
-        items[i].userHref = this.linkService.user(items[i].userRef.id);
+    for (const item of items) {
+      if ('userRef' in item) {
+        item.userHref = this.linkService.user(item.userRef.id);
       }
 
-      if ('userRef2' in items[i]) {
-        items[i].userHref2 = this.linkService.user(items[i].userRef2.id);
+      if ('userRef2' in item) {
+        item.userHref2 = this.linkService.user(item.userRef2.id);
       }
 
-      if ('projectRef' in items[i]) {
-        items[i].projectHref =
-          this.linkService.project(items[i].projectRef.id, items[i].projectRef.type);
+      if ('projectRef' in item) {
+        item.projectHref =
+          this.linkService.project(item.projectRef.id, item.projectRef.type);
       }
 
-      if ('textRef' in items[i]) {
-        items[i].textHref = this.linkService.text(items[i].textRef, items[i].projectRef.id);
+      if ('textRef' in item) {
+        item.textHref = this.linkService.text(item.textRef, item.projectRef.id);
       }
 
-      if ('questionRef' in items[i]) {
-        items[i].questionHref = this.linkService.question(items[i].textRef,
-          items[i].questionRef, items[i].projectRef.id);
+      if ('questionRef' in item) {
+        item.questionHref = this.linkService.question(item.textRef,
+          item.questionRef, item.projectRef.id);
       }
 
-      if ('entryRef' in items[i]) {
-        items[i].entryHref = this.linkService.entry(items[i].entryRef, items[i].projectRef.id);
+      if ('entryRef' in item) {
+        item.entryHref = this.linkService.entry(item.entryRef, item.projectRef.id);
       }
 
-      if ('content' in items[i]) {
-        if ('answer' in items[i].content) {
-          items[i].content.answer = this.$sce.trustAsHtml(items[i].content.answer);
+      if ('content' in item) {
+        if ('answer' in item.content) {
+          item.content.answer = this.$sce.trustAsHtml(item.content.answer);
         }
-
       }
     }
-  };
+  }
 
   filterAllActivity() {
     this.showAllActivity = true;
     this.filteredActivities = this.activities;
-  };
+  }
 
   filterMyActivity() {
-    this.sessionService.getSession().then((session) => {
+    this.sessionService.getSession().then(session => {
       this.showAllActivity = false;
       this.filteredActivities = [];
-      for (let activity of this.activities) {
+      for (const activity of this.activities) {
         if (activity.userRef && activity.userRef.id === session.userId() ||
           activity.userRef2 && activity.userRef2.id === session.userId()
         ) {
@@ -124,7 +123,7 @@ export class ActivityAppController implements angular.IController {
         }
       }
     });
-  };
+  }
 
 }
 
--- src/angular-app/bellows/shared/model/user.model.ts
+++ src/angular-app/bellows/shared/model/user.model.ts
@@ -1,9 +1,11 @@
 export class User {
   active: boolean = false;
+  // noinspection TsLint
   avatar_ref?: string;
   dateCreated: string = '';
   email: string = '';
   id: string = '';
+  // noinspection TsLint
   last_login?: number;
   name: string = '';
   role: string = '';
