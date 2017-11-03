a8566315843d71a5f1109b4143fd03028ca1c650
megahirt <chris@hirtfamily.net>
2017-11-03 13:53:48 +0700

    browserstack tweaks
    Former-commit-id: 542bb3e9c5dfbd4f74ab63ad00129d45e7173b31

--- test/app/browserStackLFProtractorConf.js
+++ test/app/browserStackLFProtractorConf.js
@@ -4,6 +4,30 @@ exports.config = {
   'rootElement': '[id="app-container-for-bootstrap"]',
 
   'capabilities': {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'os': 'Windows',
+    'os_version': '10'
+  },
+
+  onPrepare: function () {
+    if (process.env.TEAMCITY_VERSION) {
+      var jasmineReporters = require('jasmine-reporters');
+      jasmine.getEnv().addReporter(new jasmineReporters.TeamCityReporter());
+    }
+    else {
+      var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+      jasmine.getEnv().addReporter(new SpecReporter({
+        spec: {
+          displayStacktrace: true
+        }
+      }));
+      var pauseOnFailure = {
+        specDone: function (spec) {
+          if (spec.status === 'failed') {
+            browser.pause();
+          }
+        }
+      }
+    }
   }
 };
