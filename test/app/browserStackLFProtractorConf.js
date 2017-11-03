f5d9f4b08cd5926dc4597db79b008f5cb487d548
megahirt <chris@hirtfamily.net>
2017-11-03 10:55:47 +0700

    more changes toward e2e on browserstack
    Former-commit-id: 9a2f4133540a4e6d9956cdde2fc5547cc3de51d5

---
+++ test/app/browserStackLFProtractorConf.js
@@ -0,0 +1,9 @@
+exports.config = {
+  'baseUrl': 'http://e2etest.languageforge.org',
+  'framework': 'jasmine2',
+  'rootElement': '[id="app-container-for-bootstrap"]',
+
+  'capabilities': {
+    'browserName': 'chrome'
+  }
+};
