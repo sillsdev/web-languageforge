36b04b59ef0f49792f1f0a838eb0f4495324f928
Nathaniel Paulus <nateowami@gmail.com>
2017-11-17 12:51:33 -0500

    Remove SoundManager dependency; use HTML5 <audio>

---
+++ src/angular-app/bellows/directive/soundplayer.js
@@ -0,0 +1,68 @@
+'use strict';
+
+angular.module('palaso.ui.soundplayer', [])
+  .component('puiSoundplayer', {
+      bindings: {
+        url: '<',
+        timeFormat: '<'
+      },
+      controller: function ($scope) {
+        var ctrl = this;
+        var mostRecentlyPlayedAudioElement;
+        $scope.audioElement = document.createElement('audio');
+        $scope.playing = false;
+
+        $scope.audioElement.addEventListener('ended', function () {
+          $scope.$apply(function () {
+            if ($scope.playing) $scope.togglePlayback();
+          });
+        });
+
+        ctrl.$onChanges = function (changes) {
+          if (changes.url.currentValue) {
+            if ($scope.playing) $scope.togglePlayback();
+            $scope.audioElement.src = changes.url.currentValue;
+          }
+        };
+
+        $scope.iconClass = function () {
+          return $scope.playing ? 'fa-pause' : 'fa-play';
+        };
+
+        $scope.togglePlayback = function () {
+          $scope.playing = !$scope.playing;
+
+          if ($scope.playing) {
+            $scope.audioElement.play();
+            mostRecentlyPlayedAudioElement = $scope.audioElement;
+          } else {
+            stop($scope.audioElement);
+          }
+
+          function stop(audio) {
+            audio.pause();
+            audio.currentTime = 0;
+          }
+        };
+
+        $scope.duration = function () {
+          return ctrl.timeFormat($scope.audioElement.duration * 1000);
+        };
+
+        $scope.currentTime = function () {
+          return ctrl.timeFormat($scope.audioElement.currentTime * 1000);
+        };
+
+        $scope.audioElement.addEventListener('loadedmetadata', function () {
+          $scope.$digest();
+        });
+
+        $scope.audioElement.addEventListener('timeupdate', function () {
+          $scope.$digest();
+        });
+      },
+
+      template: '<a ng-click="togglePlayback()"><i class="fa {{iconClass()}}"></i></a>'
+        + '<span data-ng-if="audioElement.duration && $ctrl.timeFormat" class="audioProgress text-muted">{{currentTime()}} / {{duration()}}</span>'
+    }
+  );
