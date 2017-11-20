e32ca40b092e4f88c6b72ce38743bfb467e66d2b
Robin Munn <rmunn@pobox.com>
2017-11-20 17:16:46 +0700

    Put provider name in OAuth callback URL
    This will allow for future Facebook, Paratext, etc. logins

--- src/index.php
+++ src/index.php
@@ -236,7 +236,7 @@ $app->get('/auth/reset_password/{resetPasswordKey}', 'Site\Controller\Auth::view
 $app->get('/auth/{appName}',    'Site\Controller\Auth::view')->value('appName', 'login');
 $app->post('/auth/forgot_password', 'Site\Controller\Auth::forgotPassword')->bind('auth_forgot_password');
 
-$app->get('/oauthcallback', 'Site\Controller\GoogleOAuth::oauthCallback');
+$app->get('/oauthcallback/google', 'Site\Controller\GoogleOAuth::oauthCallback');
 
 $app->get('/download/assets/{appName}/{projectSlug}/audio/{filename}', 'Site\Controller\Download::assets');
 $app->get('/download/assets/{appName}/{projectSlug}/{filename}', 'Site\Controller\Download::assets');
