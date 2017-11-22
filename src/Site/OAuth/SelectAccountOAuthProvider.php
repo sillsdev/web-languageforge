865699d1b34d989c67ac5e8817dfaa5a90a0e6e2
Robin Munn <rmunn@pobox.com>
2017-11-22 13:36:21 +0700

    Remove unused PHP "use" directives
    Now that all the OAuth code is in the same namespace again, a lot of
    the "use" statements that PhpStorm auto-generated are unnecessary.
    
    
    Former-commit-id: 0641679b66be3314dd2f1ea087d200c9a5ca952a

--- src/Site/OAuth/SelectAccountOAuthProvider.php
+++ src/Site/OAuth/SelectAccountOAuthProvider.php
@@ -3,9 +3,8 @@
 namespace Site\OAuth;
 
 use League\OAuth2\Client\Provider\Google as GoogleOAuthProvider;
-use Site\OAuth\SelectAccountAuthorizationParameters;
 
 class SelectAccountOAuthProvider extends GoogleOAuthProvider
 {
-    use SelectAccountAuthorizationParameters;
+    use SelectAccountAuthorizationParametersTrait;
 }
