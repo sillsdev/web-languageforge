fd680548f3d396e051998dab97943fe27a460675
Damien Daspit <damien_daspit@sil.org>
2017-11-22 10:09:07 +0700

    Add initial .NET Core web API for xForge

---
+++ src/netcore-api/SIL.XForge.WebApi.Server/DataAccess/DbNames.cs
@@ -0,0 +1,8 @@
+namespace SIL.XForge.WebApi.Server.DataAccess
+{
+    public static class DbNames
+    {
+        public const string Default = "scriptureforge";
+        public const string Realtime = "realtime";
+    }
+}
---
+++ src/netcore-api/SIL.XForge.WebApi.Server/Services/SendReceiveService.cs
@@ -0,0 +1,17 @@
+using SIL.XForge.WebApi.Server.Models;
+
+namespace SIL.XForge.WebApi.Server.Services
+{
+    public class SendReceiveService
+    {
+        public void StartJob(SendReceiveJob job)
+        {
+            // TODO: perform send/receive on a separate thread
+        }
+
+        public void CancelJob(string id)
+        {
+            // TODO: cancel send/receive job
+        }
+    }
+}
