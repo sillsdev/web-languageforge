20c80d30a9161eb69bfddbdf2762d86c9491fec6
Robin Munn <rmunn@pobox.com>
2017-12-04 13:54:37 +0700

    Improve looks of OAuth login form
    Also remove a couple of debugging statements.
    
    
    Former-commit-id: d6cb2d47cb9f262276477e3a47f22e628bd51bc3

--- src/angular-app/bellows/apps/public/oauth-signup/oauth-signup-app.module.ts
+++ src/angular-app/bellows/apps/public/oauth-signup/oauth-signup-app.module.ts
@@ -6,29 +6,4 @@ import { OAuthSignupAppComponent } from './oauth-signup-app.component';
 export const OAuthSignupAppModule = angular
     .module('oauth-signup', ['ui.bootstrap', CoreModule])
     .component('oauthSignupApp', OAuthSignupAppComponent)
-    // .controller('OAuthSignupAppController', () => {})
     .name;
-
-
-/*
-import * as angular from 'angular';
-
-import { CoreModule } from '../../../core/core.module';
-import { SignupAppComponent } from './signup-app.component';
-
-export const ResetPasswordAppModule = angular
-  .module('signup', ['ui.bootstrap', 'pascalprecht.translate', 'zxcvbn',
-    CoreModule, 'palaso.util.model.transform', 'palaso.ui.captcha'
-  ])
-  .component('signupApp', SignupAppComponent)
-  .config(['$translateProvider', function ($translateProvider: angular.translate.ITranslateProvider) {
-    // configure interface language filepath
-    $translateProvider.useStaticFilesLoader({
-      prefix: '/angular-app/bellows/lang/',
-      suffix: '.json'
-    });
-    $translateProvider.preferredLanguage('en');
-    $translateProvider.useSanitizeValueStrategy('escape');
-  }])
-  .name;
- */
