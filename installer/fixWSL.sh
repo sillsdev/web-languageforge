33c38b8ad81c5bcfa4390e91b3db30770e993edd
megahirt <chris@hirtfamily.net>
2017-12-04 09:51:07 +0700

    correct github url in fixWSL.sh

--- installer/fixWSL.sh
+++ installer/fixWSL.sh
@@ -10,7 +10,7 @@ if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
     BASHRCFILE="/home/$USER/.bashrc"
     ALREADYHASBASHRCMODS=`grep "rw,noatime,fallback=1" $BASHRCFILE`
     if [ -f "$BASHRCFILE" -a ! -n "$ALREADYHASBASHRCMODS" ]; then
-        wget -O- https://github.com/sillsdev/web-languageforge/blob/master/installer/bashrcFileAdditionsWSL.txt >> $BASHRCFILE
+        wget -O- https://raw.githubusercontent.com/sillsdev/web-languageforge/master/installer/bashrcFileAdditionsWSL.txt >> $BASHRCFILE
         echo "Amended $BASHRCFILE with WSL-specific instructions."
     fi
 fi
