c475431f63754081afcd7eec42163592d4ee763c
Ethan Clark <elc3@students.calvin.edu>
2018-01-07 22:55:14 -0500

    Fixed 10 Created the View Model for the Example Fields
    - Updated constructor for the Example Fields
    - Updated some whitespace in code per TSLint suggestions

--- src/angular-app/languageforge/lexicon/settings/configuration/configuration-unified-view.model.ts
+++ src/angular-app/languageforge/lexicon/settings/configuration/configuration-unified-view.model.ts
@@ -3,23 +3,30 @@
 export class ConfigurationUnifiedViewModel {
 
   // Settings objects for Entry Fields
-  entryFields : FieldSettings[];
+  entryFields: FieldSettings[];
 
   // Settings objects for Sense Fields
-  senseFields : FieldSettings[];
+  senseFields: FieldSettings[];
 
-  constructor(numEntryFields : number, numSenseFields : number) {
+  // Settings objects for Example Fields
+  exampleFields: FieldSettings[];
+
+  constructor(numEntryFields: number, numSenseFields: number, numExampleFields: number) {
     this.entryFields = [];
-    for(let i = 0; i < numEntryFields; i++) {
+    for (let i = 0; i < numEntryFields; i++) {
       this.entryFields[i] = new FieldSettings();
     }
 
     this.senseFields = [];
-    for(let i = 0; i < numSenseFields; i++) {
+    for (let i = 0; i < numSenseFields; i++) {
       this.senseFields[i] = new FieldSettings();
     }
+
+    this.exampleFields = [];
+    for (let i = 0; i < numExampleFields; i++) {
+      this.exampleFields[i] = new FieldSettings();
+    }
   }
-  
 }
 
 export class FieldSettings {
