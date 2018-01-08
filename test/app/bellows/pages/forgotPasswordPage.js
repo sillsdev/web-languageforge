a749615776c0dbde9103d92a9bada6363fa3f3d2
Mark Wissink <mwissink.b@gmail.com>
2018-01-08 03:38:10 -0500

    Updates locators on forgot password page to follow Protractor style guide

--- test/app/bellows/pages/forgotPasswordPage.js
+++ test/app/bellows/pages/forgotPasswordPage.js
@@ -5,11 +5,12 @@ var BellowsForgotPasswordPage = function() {
     browser.get(browser.baseUrl + '/auth/forgot_password');
   };
 
-  this.form = element(by.tagName('form'));
-  this.infoMessages = element.all(by.css('.alert-info'));
-  this.errors = element.all(by.css('.alert-danger'));
+  this.form = element(by.id('forgot-password-form'));
+  // infoMessage and errors are dynamic elements, so class name locators seem to be the best option
+  this.infoMessages = element.all(by.className('alert-info'));
+  this.errors = element.all(by.className('alert-danger'));
   this.usernameInput = element(by.id('username'));
-  this.submitButton = element(by.partialButtonText('Submit'));
+  this.submitButton = element(by.id('forgot-password-submit-btn'));
 };
 
 module.exports = new BellowsForgotPasswordPage();
