10d1b8aab0e310992591ebe62a249940101dca83
Mark Wissink <mwissink.b@gmail.com>
2018-01-08 03:49:09 -0500

    Updates locators on reset password page to follow Protractor style guide

--- test/app/bellows/pages/resetPasswordPage.js
+++ test/app/bellows/pages/resetPasswordPage.js
@@ -5,12 +5,13 @@ var BellowsResetPasswordPage = function () {
     browser.get(browser.baseUrl + '/auth/reset_password/' + resetPasswordKey);
   };
 
-  this.form = element(by.tagName('form'));
-  this.infoMessages = element.all(by.css('.alert-info'));
-  this.errors = element.all(by.css('.alert-danger'));
+  this.form = element(by.id('reset-password-form'));
+  // infoMessage and errors are dynamic elements, so class name locators seem to be the best option
+  this.infoMessages = element.all(by.className('alert-info'));
+  this.errors = element.all(by.className('alert-danger'));
   this.passwordInput = element(by.model('$ctrl.record.password'));
   this.confirmPasswordInput = element(by.model('$ctrl.confirmPassword'));
-  this.resetButton = element(by.partialButtonText('Reset Password'));
+  this.resetButton = element(by.id('reset-password-btn'));
 };
 
 module.exports = new BellowsResetPasswordPage();
