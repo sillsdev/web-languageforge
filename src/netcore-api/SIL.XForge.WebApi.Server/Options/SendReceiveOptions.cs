8fb3598cbfb6953852b646e25c3181027ee5b7a1
Damien Daspit <damien_daspit@sil.org>
2018-01-10 17:38:47 +0700

    add support for cloning and receiving PT projects

--- src/netcore-api/SIL.XForge.WebApi.Server/Options/SendReceiveOptions.cs
+++ src/netcore-api/SIL.XForge.WebApi.Server/Options/SendReceiveOptions.cs
@@ -3,5 +3,6 @@ namespace SIL.XForge.WebApi.Server.Options
     public class SendReceiveOptions
     {
         public string TranslateDir { get; set; }
+        public string ShareDBUrl { get; set; }
     }
 }
