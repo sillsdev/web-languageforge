1d9bfe2a20b3c629dd9f2db67abac7eedc3fc6c6
yejoo24 <yo24@LAPTOP-FR0AOREF.localdomain>
2018-01-12 13:37:14 +0700

    Fixed 49 Made a popup window that update when add field button is clicked
    Former-commit-id: f67c7a4a19c3a289e55ebb4784163677b3cc0509

--- src/angular-app/languageforge/lexicon/settings/configuration/configuration-unified.component.ts
+++ src/angular-app/languageforge/lexicon/settings/configuration/configuration-unified.component.ts
@@ -3,10 +3,19 @@ import * as angular from 'angular';
 import {ModalService} from '../../../../bellows/core/modal/modal.service';
 import {User} from '../../../../bellows/shared/model/user.model';
 import {LexiconConfigService} from '../../core/lexicon-config.service';
-import {LexConfigField, LexiconConfig} from '../../shared/model/lexicon-config.model';
+import {
+  LexConfig,
+  LexConfigField,
+  LexConfigFieldList,
+  LexConfigMultiText,
+  LexConfigOptionList,
+  LexiconConfig,
+  LexViewFieldConfig,
+  LexViewMultiTextFieldConfig
+} from '../../shared/model/lexicon-config.model';
 import {LexOptionList} from '../../shared/model/option-list.model';
 import {Field} from './configuration-fields.component';
-import {ConfigurationUnifiedViewModel, InputSystemSettings, RoleType} from './configuration-unified-view.model';
+import {ConfigurationUnifiedViewModel} from './configuration-unified-view.model';
 import {ConfigurationInputSystemsViewModel} from './input-system-view.model';
 
 export class UnifiedConfigurationController implements angular.IController {
@@ -15,11 +24,13 @@ export class UnifiedConfigurationController implements angular.IController {
   uccConfigDirty: LexiconConfig;
   uccInputSystemViewModels: { [inputSystemId: string]: ConfigurationInputSystemsViewModel };
   uccInputSystemsList: ConfigurationInputSystemsViewModel[];
-  uccUsers: { [userId: string]: User };readonly uccConfigPristine: LexiconConfig;
+  uccUsers: { [userId: string]: User };
+  readonly uccConfigPristine: LexiconConfig;
   readonly uccOptionLists: LexOptionList[];
   uccSelectField: (params: { fieldName: string }) => void;
   uccAddInputSystem: (params: {}) => void;
-  uccOnUpdate: (params: { $event: { unifiedViewModel: ConfigurationUnifiedViewModel } }) => void;
+  uccOnUpdate: (params: { $event: { unifiedViewModel?: ConfigurationUnifiedViewModel,
+    configDirty?: LexiconConfig } }) => void;
 
   unifiedViewModel: ConfigurationUnifiedViewModel;
 
@@ -48,6 +59,152 @@ export class UnifiedConfigurationController implements angular.IController {
 
   checkIfAllRowSelected = ConfigurationUnifiedViewModel.checkIfAllRowSelected;
 
+  selectField(fieldName: string) {
+    this.uccSelectField({ fieldName });
+  }
+
+  openNewCustomFieldModal(): void {
+    class NewCustomData {
+      code: string;
+      level: string;
+      listCode?: string;
+      name: string;
+      type: string;
+    }
+
+    const modalInstance = this.$modal.open({
+      scope: this.$scope,
+      templateUrl: '/angular-app/languageforge/lexicon/settings/configuration/new-custom-field.html',
+      controller: ['$scope', '$uibModalInstance',
+        (scope: any, $modalInstance: angular.ui.bootstrap.IModalInstanceService) => {
+          scope.fieldConfig = this.uccFieldConfig;
+          scope.selects = {};
+          scope.selects.level = {
+            optionsOrder: ['entry', 'senses', 'examples'],
+            options: {
+              entry: 'Entry Level',
+              senses: 'Meaning Level',
+              examples: 'Example Level'
+            }
+          };
+          scope.selects.type = {
+            optionsOrder: ['multitext', 'optionlist', 'multioptionlist'],
+            options: {
+              multitext: 'Multi-input-system Text',
+              optionlist: 'Option List',
+              multioptionlist: 'Multi-option List',
+              reference: 'Entry Reference',
+              picture: 'Picture',
+              date: 'Date',
+              number: 'Number'
+            }
+          };
+          scope.selects.listCode = {
+            optionsOrder: [],
+            options: {}
+          };
+          angular.forEach(this.uccOptionLists, optionList => {
+            scope.selects.listCode.optionsOrder.push(optionList.code);
+            scope.selects.listCode.options[optionList.code] = optionList.name;
+          });
+
+          scope.newCustomData = new NewCustomData();
+          scope.newCustomData.name = '';
+          scope.customFieldNameExists = function customFieldNameExists(level: string, code: string) {
+            const customFieldName = 'customField_' + level + '_' + code;
+            return customFieldName in scope.fieldConfig;
+          };
+
+          scope.add = function add() {
+            $modalInstance.close(scope.newCustomData);
+          };
+
+          scope.$watch('newCustomData.name', (newValue: string, oldValue: string) => {
+            if (angular.isDefined(newValue) && newValue !== oldValue) {
+
+              // replace spaces with underscore
+              scope.newCustomData.code = newValue.replace(/ /g, '_');
+            }
+          });
+
+        }
+      ]
+    });
+
+    modalInstance.result.then((newCustomData: NewCustomData) => {
+      const customFieldName = 'customField_' + newCustomData.level + '_' + newCustomData.code;
+      const customField: LexConfigField = new LexConfig();
+      const customViewField: LexViewFieldConfig = new LexViewFieldConfig();
+      customField.label = newCustomData.name;
+      customField.type = newCustomData.type;
+      customField.hideIfEmpty = false;
+      customViewField.type = 'basic';
+      customViewField.show = false;
+      switch (newCustomData.type) {
+        case 'multitext':
+          const customMutliTextField = customField as LexConfigMultiText;
+          const customMultiTextViewField = (customViewField as LexViewMultiTextFieldConfig);
+          customMutliTextField.displayMultiline = false;
+          customMutliTextField.width = 20;
+          customMutliTextField.inputSystems = [this.uccInputSystemsList[0].inputSystem.tag];
+          customMultiTextViewField.type = 'multitext';
+          customMultiTextViewField.overrideInputSystems = false;
+          customMultiTextViewField.inputSystems = [];
+          break;
+        case 'optionlist':
+        case 'multioptionlist':
+          const customOptionlistField = customField as LexConfigOptionList;
+          customOptionlistField.listCode = newCustomData.listCode;
+          break;
+      }
+
+      switch (newCustomData.level) {
+        case 'examples':
+          ((this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fields.examples as LexConfigFieldList)
+            .fields[customFieldName] = customField;
+          this.uccFieldConfig[customFieldName] = ((this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fields
+            .examples as LexConfigFieldList).fields[customFieldName];
+          if (!(customFieldName in ((this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fields
+              .examples as LexConfigFieldList).fieldOrder)
+          ) {
+            ((this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fields.examples as LexConfigFieldList)
+              .fieldOrder.push(customFieldName);
+          }
+
+          break;
+        case 'senses':
+          (this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fields[customFieldName] = customField;
+          this.uccFieldConfig[customFieldName] = (this.uccConfigDirty.entry.fields.senses as LexConfigFieldList)
+            .fields[customFieldName];
+          if (!(customFieldName in (this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fieldOrder)) {
+            (this.uccConfigDirty.entry.fields.senses as LexConfigFieldList).fieldOrder.push(customFieldName);
+          }
+
+          break;
+        case 'entry':
+        default:
+          this.uccConfigDirty.entry.fields[customFieldName] = customField;
+          this.uccFieldConfig[customFieldName] = this.uccConfigDirty.entry.fields[customFieldName];
+          if (!(customFieldName in this.uccConfigDirty.entry.fieldOrder)) {
+            this.uccConfigDirty.entry.fieldOrder.push(customFieldName);
+          }
+      }
+
+      angular.forEach(this.uccConfigDirty.roleViews, roleView => {
+        roleView.fields[customFieldName] = angular.copy(customViewField);
+      });
+
+      const role = 'project_manager';
+      this.uccConfigDirty.roleViews[role].fields[customFieldName].show = true;
+      angular.forEach(this.uccConfigDirty.userViews, userView => {
+        userView.fields[customFieldName] = angular.copy(customViewField);
+      });
+
+      this.uccOnUpdate({ $event: { configDirty: this.uccConfigDirty } });
+      this.selectField(customFieldName);
+    }, angular.noop);
+
+  }
 }
 
 export const UnifiedConfigurationComponent: angular.IComponentOptions = {
