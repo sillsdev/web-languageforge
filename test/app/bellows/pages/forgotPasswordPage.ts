52bcb1fce86fc8341bcc9fc0a9b7cf4a8205ed2f
Benjamin Kastner <benjamin@benjaminkastner.com>
2018-01-16 11:00:47 +0700

    Converted forgotPasswordPage to typescript.

--- test/app/bellows/pages/forgotPasswordPage.ts
+++ test/app/bellows/pages/forgotPasswordPage.ts
@@ -1,9 +1,9 @@
 import {$, $$, browser, by, By, element, ExpectedConditions} from 'protractor';
 
 export class BellowsForgotPasswordPage {
-  get = function() {
+  get = () => {
     browser.get(browser.baseUrl + '/auth/forgot_password');
-  };
+  }
 
   form = element(by.id('forgot-password-form'));
   // infoMessage and errors are dynamic elements, so class name locators seem to be the best option
