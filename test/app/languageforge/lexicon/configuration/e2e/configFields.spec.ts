28942a38b98cb7f73f0c353253af8af3d52b9a61
Benjamin Kastner <benjamin@benjaminkastner.com>
2018-01-18 10:25:04 +0700

    Converted configFields spec to typescript

--- test/app/languageforge/lexicon/configuration/e2e/configFields.spec.ts
+++ test/app/languageforge/lexicon/configuration/e2e/configFields.spec.ts
@@ -1,14 +1,16 @@
-'use strict';
+import {} from 'jasmine';
+// Importing protractor to make usages of protractor.Key work - Ben Kastner 2018-01-18
+import { protractor } from 'protractor';
 
-describe('Configuration Fields', function () {
-  var constants     = require('../../../../testConstants');
-  var loginPage     = require('../../../../bellows/pages/loginPage.js');
-  var projectsPage  = require('../../../../bellows/pages/projectsPage.js');
-  var util          = require('../../../../bellows/pages/utils.js');
-  var configPage    = require('../../pages/configurationPage.js');
-  var displayName = 'cust_name';
+describe('Configuration Fields', () => {
+  const constants     = require('../../../../testConstants');
+  const loginPage     = require('../../../../bellows/pages/loginPage.js');
+  const projectsPage  = require('../../../../bellows/pages/projectsPage.js');
+  const util          = require('../../../../bellows/pages/utils.js');
+  const configPage    = require('../../pages/configurationPage.js');
+  const displayName = 'cust_name';
 
-  it('setup: login as manager, select test project, goto configuration', function () {
+  it('setup: login as manager, select test project, goto configuration', () => {
     loginPage.loginAsManager();
     projectsPage.get();
     projectsPage.clickOnProject(constants.testProjectName);
@@ -18,13 +20,13 @@ describe('Configuration Fields', function () {
     expect(configPage.applyButton.isEnabled()).toBe(false);
   });
 
-  it('can select Fields tab', function () {
+  it('can select Fields tab', () => {
     configPage.tabs.fields.click();
     expect(configPage.fieldsTab.newCustomFieldButton.isDisplayed()).toBe(true);
     expect(configPage.fieldsTab.fieldSetupLabel.isDisplayed()).toBe(true);
   });
 
-  it('can select POS Field; appropriate controls shown', function () {
+  it('can select POS Field; appropriate controls shown', () => {
     configPage.getFieldByName('Part of Speech').click();
     expect(configPage.fieldsTab.fieldSetupLabel.getText()).toEqual('Part of Speech Field Setup');
     expect(configPage.fieldsTab.hiddenIfEmptyCheckbox.isDisplayed()).toBe(true);
@@ -33,7 +35,7 @@ describe('Configuration Fields', function () {
     expect(configPage.fieldsTab.inputSystemTags.count()).toBe(0);
   });
 
-  it('can select Word Field; appropriate controls shown', function () {
+  it('can select Word Field; appropriate controls shown', () => {
     configPage.getFieldByName('Word').click();
     expect(configPage.fieldsTab.fieldSetupLabel.getText()).toEqual('Word Field Setup');
     expect(configPage.fieldsTab.hiddenIfEmptyCheckbox.isDisplayed()).toBe(true);
@@ -42,7 +44,7 @@ describe('Configuration Fields', function () {
     expect(configPage.fieldsTab.inputSystemTags.count()).toBe(4);
   });
 
-  it('can move an Input System upwards', function () {
+  it('can move an Input System upwards', () => {
     expect(configPage.fieldsTab.inputSystemTags.first().getText()).toEqual('th');
     expect(configPage.fieldsTab.inputSystemTags.get(1).getText()).toEqual('tipa');
     expect(configPage.fieldsTab.inputSystemTags.get(2).getText()).toEqual('taud');
@@ -74,7 +76,7 @@ describe('Configuration Fields', function () {
     expect(configPage.fieldsTab.inputSystemTags.last().getText()).toEqual('taud');
   });
 
-  it('can move an Input System downwards', function () {
+  it('can move an Input System downwards', () => {
     configPage.fieldsTab.inputSystemDownButton.click();
     expect(configPage.fieldsTab.inputSystemUpButton.isEnabled()).toBe(true);
     expect(configPage.fieldsTab.inputSystemDownButton.isEnabled()).toBe(true);
@@ -90,16 +92,16 @@ describe('Configuration Fields', function () {
     expect(configPage.fieldsTab.inputSystemTags.last().getText()).toEqual('en');
   });
 
-  it('can save changed field settings', function () {
+  it('can save changed field settings', () => {
     expect(configPage.noticeList.count()).toBe(0);
     configPage.applyButton.click();
     expect(configPage.noticeList.count()).toBe(1);
     expect(configPage.noticeList.get(0).getText()).toContain('Configuration updated successfully');
   });
 
-  describe('Add a new Custom Field modal', function () {
+  describe('Add a new Custom Field modal', () => {
 
-    it('can open the new custom field modal', function () {
+    it('can open the new custom field modal', () => {
       expect(configPage.fieldsTab.removeCustomFieldButton.isDisplayed()).toBe(false);
       expect(configPage.fieldsTab.newCustomFieldButton.isEnabled()).toBe(true);
       configPage.fieldsTab.newCustomFieldButtonClick();
@@ -111,7 +113,7 @@ describe('Configuration Fields', function () {
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('can enter a field name', function () {
+    it('can enter a field name', () => {
       expect(configPage.modal.customField.fieldCodeExists.isPresent()).toBe(true);
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
       configPage.modal.customField.displayNameInput.sendKeys(displayName + protractor.Key.ENTER);
@@ -120,20 +122,20 @@ describe('Configuration Fields', function () {
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('can enter a field level', function () {
+    it('can enter a field level', () => {
       util.clickDropdownByValue(configPage.modal.customField.levelDropdown, 'Entry Level');
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('can enter a field type', function () {
+    it('can enter a field type', () => {
       util.clickDropdownByValue(configPage.modal.customField.typeDropdown,
         'Multi-input-system Text');
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(true);
     });
 
-    it('can add custom field', function () {
+    it('can add custom field', () => {
       configPage.modal.customField.addButton.click();
       expect(configPage.modal.customField.displayNameInput.isPresent()).toBe(false);
       expect(configPage.fieldsTab.fieldSetupLabel.getText()).toEqual(displayName + ' Field Setup');
@@ -141,12 +143,12 @@ describe('Configuration Fields', function () {
       expect(configPage.applyButton.isEnabled()).toBe(true);
     });
 
-    it('can re-open the new custom field modal', function () {
+    it('can re-open the new custom field modal', () => {
       configPage.fieldsTab.newCustomFieldButtonClick();
       expect(configPage.modal.customField.displayNameInput.isDisplayed()).toBe(true);
     });
 
-    it('cannot add a duplicate field name', function () {
+    it('cannot add a duplicate field name', () => {
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
       configPage.modal.customField.displayNameInput.sendKeys(displayName + protractor.Key.ENTER);
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
@@ -156,13 +158,13 @@ describe('Configuration Fields', function () {
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('can add a duplicate field name at a different level', function () {
+    it('can add a duplicate field name at a different level', () => {
       util.clickDropdownByValue(configPage.modal.customField.levelDropdown, 'Meaning Level');
       expect(configPage.modal.customField.fieldCodeExists.isDisplayed()).toBe(false);
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('list code only shows when a list type is selected', function () {
+    it('list code only shows when a list type is selected', () => {
       util.clickDropdownByValue(configPage.modal.customField.typeDropdown,
         'Multi-input-system Text');
       expect(configPage.modal.customField.listCodeDropdown.isPresent()).toBe(false);
@@ -175,12 +177,12 @@ describe('Configuration Fields', function () {
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(false);
     });
 
-    it('can enter a list code', function () {
+    it('can enter a list code', () => {
       util.clickDropdownByValue(configPage.modal.customField.listCodeDropdown, 'Part of Speech');
       expect(configPage.modal.customField.addButton.isEnabled()).toBe(true);
     });
 
-    it('can cancel custom field modal', function () {
+    it('can cancel custom field modal', () => {
       configPage.modal.customField.displayNameInput.sendKeys(protractor.Key.ESCAPE);
       expect(configPage.modal.customField.displayNameInput.isPresent()).toBe(false);
       expect(configPage.applyButton.isEnabled()).toBe(true);
@@ -188,12 +190,12 @@ describe('Configuration Fields', function () {
 
   });
 
-  it('can delete a newly created custom field', function () {
+  it('can delete a newly created custom field', () => {
     configPage.fieldsTab.removeCustomFieldButton.click();
     expect(configPage.fieldsTab.removeCustomFieldButton.isDisplayed()).toBe(false);
   });
 
-  it('can re-create custom field and save configuration', function () {
+  it('can re-create custom field and save configuration', () => {
     configPage.fieldsTab.newCustomFieldButtonClick();
     configPage.modal.customField.displayNameInput.sendKeys(displayName + protractor.Key.ENTER);
     util.clickDropdownByValue(configPage.modal.customField.levelDropdown, 'Entry Level');
@@ -203,12 +205,12 @@ describe('Configuration Fields', function () {
     configPage.applyButton.click();
   });
 
-  it('cannot delete a newly saved custom field', function () {
+  it('cannot delete a newly saved custom field', () => {
     expect(configPage.fieldsTab.removeCustomFieldButton.isDisplayed()).toBe(false);
   });
 
   // this regression test added because the code has been fixed at least once before. IJH 2016-05
-  it('does not regress Input System selection', function () {
+  it('does not regress Input System selection', () => {
     configPage.showAllFieldsButton.click();
     configPage.getFieldByName('Sentence').click();
     expect(configPage.fieldsTab.fieldSetupLabel.getText()).toEqual('Sentence Field Setup');
