8a24753ca95a53ff172ec87d3b50b0cff0e45aa8
Mark Wissink <mwissink.b@gmail.com>
2018-01-18 02:22:53 -0500

    Updated projects spec to es6

--- test/app/bellows/projects/e2e/projects.spec.ts
+++ test/app/bellows/projects/e2e/projects.spec.ts
@@ -1,68 +1,68 @@
-'use strict';
+import { browser, element, by, ExpectedConditions } from 'protractor';
+import { ElementFinder } from 'protractor/built/element';
+import { BellowsLoginPage } from '../../pages/loginPage';
+import { ProjectsPage } from '../../pages/projectsPage';
+const constants = require('../../../testConstants.json');
+const CONDITION_TIMEOUT = 3000;
 
-describe('E2E Projects List App', function () {
-  var constants     = require('../../../testConstants.json');
-  var loginPage     = require('../../pages/loginPage');
-  var projectsPage  = require('../../pages/projectsPage.js');
-  var expectedCondition = protractor.ExpectedConditions;
-  var CONDITION_TIMEOUT = 3000;
+const loginPage = new BellowsLoginPage();
+const projectsPage = new ProjectsPage();
+describe('E2E Projects List App', () => {
 
-  describe('for Normal User', function () {
+  describe('for Normal User', () => {
 
-    it('should list the project of which the user is a member', function () {
+    it('should list the project of which the user is a member', () => {
       loginPage.loginAsMember();
       projectsPage.get();
-
       expect(projectsPage.projectNames.get(0).getText()).toBe(constants.testProjectName);
     });
 
-    it('should not list projects the user is not a member of', function () {
+    it('should not list projects the user is not a member of', () => {
       projectsPage.get();
-      expect(projectsPage.projectsList.count()).toBe(1);
+      expect<any>(projectsPage.projectsList.count()).toBe(1);
     });
 
-    it('can list two projects of which the user is a member', function () {
+    it('can list two projects of which the user is a member', () => {
       loginPage.loginAsAdmin();
       projectsPage.get();
-      browser.wait(expectedCondition.visibilityOf(projectsPage.createBtn), CONDITION_TIMEOUT);
+      browser.wait(ExpectedConditions.visibilityOf(projectsPage.createBtn), CONDITION_TIMEOUT);
       projectsPage.addMemberToProject(constants.otherProjectName, constants.memberName);
       loginPage.loginAsMember();
       projectsPage.get();
-      browser.wait(expectedCondition.visibilityOf(projectsPage.createBtn), CONDITION_TIMEOUT);
-      expect(projectsPage.projectsList.count()).toBe(2);
+      browser.wait(ExpectedConditions.visibilityOf(projectsPage.createBtn), CONDITION_TIMEOUT);
+      expect<any>(projectsPage.projectsList.count()).toBe(2);
     });
-
   });
 
   // Two helper functions to avoid duplicating the same checks in admin test below
-  var shouldProjectBeLinked = function shouldProjectBeLinked(projectName, projectRow, bool) {
-    expect(projectRow.element(by.cssContainingText('a', projectName)).isDisplayed()).toBe(bool);
+  const shouldProjectBeLinked = (projectName: string, projectRow: ElementFinder, bool: boolean) => {
+    expect<any>(projectRow.element(by.cssContainingText('a', projectName)).isDisplayed()).toBe(bool);
   };
 
-  var shouldProjectHaveButtons = function shouldProjectHaveButtons(projectRow, bool) {
-    var addAsManagerBtn = projectRow.element(by.id('managerButton'));
-    expect(addAsManagerBtn.isDisplayed()).toBe(bool);
+  const shouldProjectHaveButtons = (projectRow: ElementFinder, bool: boolean) => {
+    let addAsManagerBtn = projectRow.element(by.id('managerButton'));
+    expect<any>(addAsManagerBtn.isDisplayed()).toBe(bool);
   };
 
-  describe('for System Admin User', function () {
+  describe('for System Admin User', () => {
 
-    it('should list all projects', function () {
+    it('should list all projects', () => {
       loginPage.loginAsAdmin();
       projectsPage.get();
       expect(projectsPage.projectsList.count()).toBeGreaterThan(0);
 
       // Check that the test project is around
-      projectsPage.findProject(constants.testProjectName).then(function (projectRow) {
+      projectsPage.findProject(constants.testProjectName).then((projectRow: ElementFinder) => {
         shouldProjectBeLinked(constants.testProjectName, projectRow, true);
       });
     });
 
-    it('should show add and delete buttons', function () {
+    it('should show add and delete buttons', () => {
       // projectsPage.createBtn.getOuterHtml().then(console.log);
       expect(projectsPage.createBtn.isDisplayed()).toBeTruthy();
     });
 
-    it('should allow the admin to add themselves to the project as member or manager', function () {
+    it('should allow the admin to add themselves to the project as member or manager', () => {
 
       // First remove the admin from the project (must be a project admin is not the owner of)
       loginPage.loginAsManager();
@@ -70,7 +70,7 @@ describe('E2E Projects List App', function () {
 
       // The admin should not see "Add myself to project" buttons when he's already a project member
       // or manager, and the project name should be a clickable link
-      projectsPage.findProject(constants.otherProjectName).then(function (projectRow) {
+      projectsPage.findProject(constants.otherProjectName).then((projectRow: ElementFinder) => {
         shouldProjectBeLinked(constants.otherProjectName, projectRow, true);
         shouldProjectHaveButtons(projectRow, false);
       });
@@ -81,7 +81,7 @@ describe('E2E Projects List App', function () {
 
       // Now the admin should have "Add myself to project" buttons
       // And the project name should NOT be a clickable link
-      projectsPage.findProject(constants.otherProjectName).then(function (projectRow) {
+      projectsPage.findProject(constants.otherProjectName).then((projectRow: ElementFinder) => {
         shouldProjectBeLinked(constants.otherProjectName, projectRow, false);
         shouldProjectHaveButtons(projectRow, true);
 
@@ -90,7 +90,7 @@ describe('E2E Projects List App', function () {
       });
 
       // And the buttons should go away after one of them is clicked
-      projectsPage.findProject(constants.otherProjectName).then(function (projectRow) {
+      projectsPage.findProject(constants.otherProjectName).then((projectRow: ElementFinder) => {
         shouldProjectBeLinked(constants.otherProjectName, projectRow, true);
         shouldProjectHaveButtons(projectRow, false);
       });
