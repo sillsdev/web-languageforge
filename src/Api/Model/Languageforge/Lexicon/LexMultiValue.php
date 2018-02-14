412592e243cba196e996f5f0828ace31a86659e2
Robin Munn <rmunn@pobox.com>
2018-02-14 14:41:30 +0700

    Add "differences" function to various field types
    This will be used in calculating the difference between fields when a
    LexEntry is updated, so that those differences can be stored in the
    activity log.
    
    
    Former-commit-id: 528897696309ad52dfc9051d80a0d0cf80a9475d

--- src/Api/Model/Languageforge/Lexicon/LexMultiValue.php
+++ src/Api/Model/Languageforge/Lexicon/LexMultiValue.php
@@ -32,4 +32,49 @@ class LexMultiValue
             $this->values[] = $value;
         }
     }
+
+    public function differences(LexMultiValue $otherMultiValue)
+    {
+        // Shows differences between the arrays as whole values. Use individualDifferences function to see individual additions or removals.
+        if ($this->values == $otherMultiValue->values)
+        {
+            // Shortcut so we don't have to copy arrays just to find out they were already equal
+            return [];
+        }
+        $thisArr  = $this->values->getArrayCopy();
+        $otherArr = $otherMultiValue->values->getArrayCopy();
+        $thisJson = json_encode($thisArr);
+        $otherJson = json_encode($otherArr);
+        return ["this" => $thisJson, "other" => $otherJson];
+    }
+
+/* If individual differences between arrays (insertions and deletions) are desired, we could do it like this:
+    public function individualDifferences(LexMultiValue $otherMultiValue)
+    {
+        $result = [];
+        $thisArr  = $this->values->getArrayCopy();
+        $otherArr = $otherMultiValue->values->getArrayCopy();
+        $oursOnly = array_diff($thisArr, $otherArr);
+        $theirsOnly = array_diff($otherArr, $thisArr);
+        foreach ($oursOnly as $item)
+        {
+            $value = isset($item) ? (string)$item : "";
+            $result[] = [
+                "value" => $value,
+                "this" => $value,
+                "other" => ""
+            ];
+        }
+        foreach ($theirsOnly as $item)
+        {
+            $value = isset($item) ? (string)$item : "";
+            $result[] = [
+                "value" => $value,
+                "this" => "",
+                "other" => $value
+            ];
+        }
+        return $result;
+    }
+*/
 }
---
+++ test/php/model/shared/LexMultiValueTest.php
@@ -0,0 +1,92 @@
+<?php
+
+use Api\Model\Languageforge\Lexicon\LexMultiValue;
+use PHPUnit\Framework\TestCase;
+
+class LexMultiValueTest extends TestCase
+{
+
+    public function doTest(array $one, array $two, array $expectedDifferences)
+    {
+        $multiValue1 = LexMultiValue::createFromArray($one);
+        $multiValue2 = LexMultiValue::createFromArray($two);
+        $differences = $multiValue1->differences($multiValue2);
+        $this->assertEquals($expectedDifferences, $differences);
+    }
+
+    public function testDifferences()
+    {
+        $one = ["one"];
+        $two = ["two"];
+        $expectedDifferences = [
+            "this" => json_encode($one),
+            "other" => json_encode($two)
+        ];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testInsertionStillShowsWholeArrayDifferences()
+    {
+        $one = ["one"];
+        $two = ["one", "two"];
+        $expectedDifferences = [
+            "this" => json_encode($one),
+            "other" => json_encode($two)
+        ];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testRemovalStillShowsWholeArrayDifferences()
+    {
+        $one = ["one", "two"];
+        $two = ["one"];
+        $expectedDifferences = [
+            "this" => json_encode($one),
+            "other" => json_encode($two)
+        ];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testReorderingStillShowsWholeArrayDifferences()
+    {
+        $one = ["one", "two"];
+        $two = ["two", "one"];
+        $expectedDifferences = [
+            "this" => json_encode($one),
+            "other" => json_encode($two)
+        ];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testSameArrayObject_NoDifferences()
+    {
+        $one = ["one", "two"];
+        $two = $one;
+        $expectedDifferences = [];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testEqualArraysButDifferentObjects_NoDifferences()
+    {
+        $one = ["one", "two"];
+        $two = ["one", "two"];
+        $expectedDifferences = [];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testSameEmptyArrayObject_NoDifferences()
+    {
+        $one = [];
+        $two = $one;
+        $expectedDifferences = [];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+
+    public function testEqualEmptyArraysButDifferentObjects_NoDifferences()
+    {
+        $one = [];
+        $two = [];
+        $expectedDifferences = [];
+        $this->doTest($one, $two, $expectedDifferences);
+    }
+}
