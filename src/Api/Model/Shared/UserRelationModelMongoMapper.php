ac1071a7dbc692df884fd001b429d879505d8029
Robin Munn <rmunn@pobox.com>
2018-02-20 14:02:32 +0700

    Add indexes on userrelation collection
    These are needed so that the "unread items for given user" query doesn't
    have to do a sequential scan through over 27,000 items every time we add
    a new unread item (lex entry update, comment, reply...).
    
    
    Former-commit-id: 10a2cab43d16b1b809639b8cab47ee8205d79fa1

---
+++ src/Api/Model/Shared/UserRelationModelMongoMapper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Api\Model\Shared;
+
+use Api\Model\Shared\Mapper\MongoMapper;
+
+class UserRelationModelMongoMapper extends MongoMapper
+{
+    public $INDEXES_REQUIRED = [
+        ['key' => ['type' => 1]],
+        ['key' => ['userRef' => 1]],
+        ['key' => ['projectRef' => 1]],
+    ];
+
+    public static function instance()
+    {
+        static $instance = null;
+        if (null === $instance) {
+            $instance = new ProjectModelMongoMapper(SF_DATABASE, 'userrelation');
+        }
+
+        return $instance;
+    }
+}
