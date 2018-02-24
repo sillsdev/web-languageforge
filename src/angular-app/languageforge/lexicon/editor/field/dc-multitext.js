3ab65f4a9aa45ec37e485947dae3530240ee1d67
Nathaniel Paulus <nateowami@gmail.com>
2018-02-24 10:24:08 -0500

    Escape/unescape text between dc-multitext model and view

--- src/angular-app/languageforge/lexicon/editor/field/dc-multitext.js
+++ src/angular-app/languageforge/lexicon/editor/field/dc-multitext.js
@@ -49,8 +49,8 @@ angular.module('palaso.ui.dc.multitext', ['palaso.ui.showOverflow', 'palaso.ui.d
           return false;
         }
 
-        var languageSpanPattern = /<span.* lang="/;
-        return languageSpanPattern.test($scope.model[tag].value);
+        var str = $scope.model[tag].value;
+        return (new DOMParser().parseFromString(str, 'text/html').body.children.length) > 0;
       };
 
     }]
