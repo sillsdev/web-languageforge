a4bbfdbb3bbd71e62bb6bf3fbf777a6c9b76cfd8
Ira Hopkinson <ira_hopkinson@sil.org>
2018-03-28 17:33:20 +1300

    E2E TS rename and reorganize
     - convert afterEach.js to TS
     - rename as *.e2e-spec.ts (remove e2e folders)
     - reorganize Bellows, Language Forge and Scripture Forge

---
+++ test/app/bellows/shared/reset-password.page.ts
@@ -0,0 +1,15 @@
+import {browser, by, element} from 'protractor';
+
+export class BellowsResetPasswordPage {
+  static get(resetPasswordKey: string): void {
+    browser.get(browser.baseUrl + '/auth/reset_password/' + resetPasswordKey);
+  }
+
+  form = element(by.id('reset-password-form'));
+  // infoMessage and errors are dynamic elements, so class name locators seem to be the best option
+  infoMessages = element.all(by.className('alert-info'));
+  errors = element.all(by.className('alert-danger'));
+  passwordInput = element(by.model('$ctrl.record.password'));
+  confirmPasswordInput = element(by.model('$ctrl.confirmPassword'));
+  resetButton = element(by.id('reset-password-btn'));
+}
