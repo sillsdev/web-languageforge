3676f9d8afda1f845908d8eddbf1bd7f2bb15645
Ira Hopkinson <ira_hopkinson@sil.org>
2018-04-02 18:24:53 +1200

    convert sound player to TypeScript

--- src/angular-app/languageforge/lexicon/lexicon.module.ts
+++ src/angular-app/languageforge/lexicon/lexicon.module.ts
@@ -1,9 +1,10 @@
 import * as angular from 'angular';
 
 import {InputSystemsModule} from '../../bellows/core/input-systems/input-systems.service';
+import {SoundModule} from '../../bellows/shared/sound.module';
 import {LexiconCoreModule} from './core/lexicon-core.module';
 import {LexiconSettingsModule} from './settings/settings.module';
 
 export const LexiconModule = angular
-  .module('lexiconModule', [InputSystemsModule, LexiconCoreModule, LexiconSettingsModule])
+  .module('lexiconModule', [InputSystemsModule, LexiconCoreModule, LexiconSettingsModule, SoundModule])
   .name;
