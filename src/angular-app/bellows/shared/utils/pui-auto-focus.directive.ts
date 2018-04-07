10f18e06c9076b1f98d3062a1a855291ce2e132d
Ira Hopkinson <ira_hopkinson@sil.org>
2018-04-07 12:06:06 +1200

    Change TS directives signatures.
    Former-commit-id: 9c8075a9809a46735dae7298cbb728d22d5a5254

--- src/angular-app/bellows/shared/utils/pui-auto-focus.directive.ts
+++ src/angular-app/bellows/shared/utils/pui-auto-focus.directive.ts
@@ -1,22 +1,23 @@
 import * as angular from 'angular';
 
 // This directive's code is from http://stackoverflow.com/questions/14833326/how-to-set-focus-in-angularjs
-export const PuiAutoFocus = ($interval: angular.IIntervalService, $parse: angular.IParseService) => ({
-  restrict: 'A',
-  link($scope, $element, $attrs) {
-    const model = $parse($attrs.puiAutoFocus);
-    $scope.$watch(model, newValue => {
-      if (!newValue) {
-        return;
+export function PuiAutoFocus($interval: angular.IIntervalService, $parse: angular.IParseService): angular.IDirective {
+  return {
+    restrict: 'A',
+    link($scope, $element, $attrs) {
+      const model = $parse($attrs.puiAutoFocus);
+      $scope.$watch(model, newValue => {
+        if (!newValue) {
+          return;
+        }
+        $interval(() => $element[0].focus(), 0, 1);
+      });
+
+      // to address @blesh's comment, set attribute value to 'false' on blur event
+      if (typeof model.assign === 'function') {
+        $element.bind('blur', () => $scope.$apply(model.assign($scope, false)));
       }
-      $interval(() => $element[0].focus(), 0, 1);
-    });
-
-    // to address @blesh's comment, set attribute value to 'false' on blur event
-    if (typeof model.assign === 'function') {
-      $element.bind('blur', () => $scope.$apply(model.assign($scope, false)));
     }
-  }
-} as angular.IDirective);
-
+  };
+}
 PuiAutoFocus.$inject = ['$interval', '$parse'];
