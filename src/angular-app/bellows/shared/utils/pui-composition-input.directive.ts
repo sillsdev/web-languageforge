9c8075a9809a46735dae7298cbb728d22d5a5254
Ira Hopkinson <ira_hopkinson@sil.org>
2018-04-07 12:06:06 +1200

    Change TS directives signatures.

--- src/angular-app/bellows/shared/utils/pui-composition-input.directive.ts
+++ src/angular-app/bellows/shared/utils/pui-composition-input.directive.ts
@@ -1,24 +1,28 @@
 import * as angular from 'angular';
 
+interface CompositionInputScope extends angular.IScope {
+  ngModel: any;
+}
+
 // tslint:disable-next-line:max-line-length
 // see http://stackoverflow.com/questions/27663149/angularjs-trigger-ng-change-ng-keyup-or-scope-watch-while-composing-korean-c
 // this might not work on firefox.  See link above for alternate code
 
-// Angular's ng-change, ng-keyup and $scope.$watch don't get triggered
-// while composing (e.g. when writing Korean syllables).
-// See: https://github.com/angular/angular.js/issues/10588
-// This custom directive uses element.on('input') instead, which gets
-// triggered while composing.
-export const PuiCompositionInput = () => ({
-  restrict: 'A',
-  require: '^ngModel',
-  link($scope: any, $element, $attrs) {
-    $element.on('input', () => {
-      $scope.$apply(() => {
-        $scope.ngModel = $element.val();
-        // only works if no scope has been defined in directive
-        $scope.$eval($attrs.cstInput, { answer: $scope.ngModel });
+// Angular's ng-change, ng-keyup and $scope.$watch don't get triggered while composing (e.g. when writing Korean
+// syllables). See: https://github.com/angular/angular.js/issues/10588
+// This custom directive uses element.on('input') instead, which gets triggered while composing.
+export function PuiCompositionInput(): angular.IDirective {
+  return {
+    restrict: 'A',
+    require: '^ngModel',
+    link($scope: CompositionInputScope, $element, $attrs) {
+      $element.on('input', () => {
+        $scope.$apply(() => {
+          $scope.ngModel = $element.val();
+          // only works if no scope has been defined in directive
+          $scope.$eval($attrs.cstInput, { answer: $scope.ngModel });
+        });
       });
-    });
-  }
-} as angular.IDirective);
+    }
+  };
+}
--- src/angular-app/bellows/shared/utils/pui-idle-validate.directive.ts
+++ src/angular-app/bellows/shared/utils/pui-idle-validate.directive.ts
@@ -1,31 +1,38 @@
 import * as angular from 'angular';
 
-export const PuiIdleValidate = ($interval: angular.IIntervalService) => ({
-  restrict: 'A',
-  scope: {
-    idleValidate: '&',
-    idleValidateKeypress: '&',
-    idleValidateMsec: '='
-  },
-  link($scope: any, $element) {
-    let intervalTimer: angular.IPromise<any>;
-    let milliseconds = $scope.idleValidateMsec;
-    if (!angular.isNumber($scope.idleValidateMsec)) {
-      milliseconds = 1000;
+interface IdleValidateScope extends angular.IScope {
+  idleValidate: () => { };
+  idleValidateKeypress: () => { };
+  idleValidateMsec: number;
+}
+
+export function PuiIdleValidate($interval: angular.IIntervalService): angular.IDirective {
+  return {
+    restrict: 'A',
+    scope: {
+      idleValidate: '&',
+      idleValidateKeypress: '&',
+      idleValidateMsec: '<'
+    },
+    link($scope: IdleValidateScope, $element) {
+      let intervalTimer: angular.IPromise<any>;
+      let milliseconds = $scope.idleValidateMsec;
+      if (!angular.isNumber($scope.idleValidateMsec)) {
+        milliseconds = 1000;
+      }
+
+      $element.bind('keyup', () => {
+        if (angular.isFunction($scope.idleValidateKeypress)) {
+          $scope.$apply($scope.idleValidateKeypress);
+        }
+
+        if (angular.isDefined(intervalTimer)) {
+          $interval.cancel(intervalTimer);
+        }
+
+        intervalTimer = $interval(() => $scope.idleValidate(), milliseconds, 1);
+      });
     }
-
-    $element.bind('keyup', () => {
-      if (angular.isFunction($scope.idleValidateKeypress)) {
-        $scope.$apply($scope.idleValidateKeypress);
-      }
-
-      if (angular.isDefined(intervalTimer)) {
-        $interval.cancel(intervalTimer);
-      }
-
-      intervalTimer = $interval(() => $scope.idleValidate(), milliseconds, 1);
-    });
-  }
-} as angular.IDirective);
-
+  };
+}
 PuiIdleValidate.$inject = ['$interval'];
