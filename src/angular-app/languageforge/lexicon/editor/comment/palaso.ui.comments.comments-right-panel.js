359f8af9e0fd083b713055d339c1ebc77d0ef903
Ira Hopkinson <ira_hopkinson@sil.org>
2018-04-11 20:54:41 +1200

    Reorganize comment components

--- src/angular-app/languageforge/lexicon/editor/comment/palaso.ui.comments.comments-right-panel.js
+++ src/angular-app/languageforge/lexicon/editor/comment/palaso.ui.comments.comments-right-panel.js
@@ -1,11 +1,12 @@
 'use strict';
-angular.module('palaso.ui.comments')
+angular.module('lexCommentsModule')
 
   // Palaso UI Dictionary Control: Comments
   .directive('commentsRightPanel', [function () {
     return {
       restrict: 'E',
-      templateUrl: '/angular-app/bellows/directive/palaso.ui.comments.comments-right-panel.html',
+      templateUrl: '/angular-app/languageforge/lexicon/editor/comment/' +
+        'palaso.ui.comments.comments-right-panel.html',
       scope: {
         entry: '=',
         control: '=',
@@ -192,11 +193,7 @@ angular.module('palaso.ui.comments')
 
         $scope.showCommentsInContext = function showCommentsInContext(contextGuid) {
           $scope.commentFilter.contextGuid = contextGuid;
-          if (contextGuid !== '') {
-            $scope.showNewComment = true;
-          } else {
-            $scope.showNewComment = false;
-          }
+          $scope.showNewComment = (contextGuid !== '');
 
           commentService.refreshFilteredComments($scope.commentFilter);
         };
@@ -264,6 +261,7 @@ angular.module('palaso.ui.comments')
           if (!angular.isDefined(regardingField)) {
             return '';
           }
+
           return $scope.getSenseLabel(regardingField, $scope.newComment.contextGuid);
         };
 
--- src/angular-app/languageforge/lexicon/editor/comment/palaso.ui.comments.current-entry-comment-count.js
+++ src/angular-app/languageforge/lexicon/editor/comment/palaso.ui.comments.current-entry-comment-count.js
@@ -1,12 +1,12 @@
 'use strict';
-angular.module('palaso.ui.comments')
+angular.module('lexCommentsModule')
 
   // Palaso UI Dictionary Control: Comments
   .directive('currentEntryCommentCount', [function () {
     return {
       restrict: 'E',
-      templateUrl:
-        '/angular-app/bellows/directive/palaso.ui.comments.current-entry-comment-count.html',
+      templateUrl: '/angular-app/languageforge/lexicon/editor/comment/' +
+        'palaso.ui.comments.current-entry-comment-count.html',
       controller: ['$scope', 'lexCommentService', function ($scope, commentService) {
         $scope.count = commentService.comments.counts.currentEntry;
       }]
