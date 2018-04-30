5653cd384621790320e5cddaef7e5261d199d525
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-01 09:54:11 +1200

    Make Multi & Option List fields valid TypeScript

--- src/angular-app/languageforge/lexicon/editor/field/dc-optionlist.component.ts
+++ src/angular-app/languageforge/lexicon/editor/field/dc-optionlist.component.ts
@@ -1,38 +1,35 @@
-angular.module('palaso.ui.dc.optionlist', [])
+import * as angular from 'angular';
+
+export const OptionListModule = angular
+  .module('palaso.ui.dc.optionlist', [])
 
   // Palaso UI Optionlist
-  .directive('dcOptionlist', [function () {
-    return {
-      restrict: 'E',
-      templateUrl: '/angular-app/languageforge/lexicon/editor/field/dc-optionlist.component.html',
-      scope: {
-        config: '=',
-        model: '=',
-        control: '=',
-        items: '=',
-        fieldName: '='
-      },
-      controller: ['$scope', '$state', function ($scope, $state) {
-        $scope.$state = $state;
-        $scope.contextGuid = $scope.$parent.contextGuid;
-        $scope.getDisplayName = function getDisplayName(value) {
-          var displayName = value;
-          if (angular.isDefined($scope.items)) {
-            for (var i = 0; i < $scope.items.length; i++) {
-              if ($scope.items[i].key == value) {
-                displayName = $scope.items[i].value;
-                break;
-              }
+  .directive('dcOptionlist', [() => ({
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/editor/field/dc-optionlist.component.html',
+    scope: {
+      config: '=',
+      model: '=',
+      control: '=',
+      items: '=',
+      fieldName: '='
+    },
+    controller: ['$scope', '$state', ($scope, $state) => {
+      $scope.$state = $state;
+      $scope.contextGuid = $scope.$parent.contextGuid;
+      $scope.getDisplayName = function getDisplayName(value: string): string {
+        let displayName = value;
+        if (angular.isDefined($scope.items)) {
+          for (const item of $scope.items) {
+            if (item.key === value) {
+              displayName = item.value;
+              break;
             }
           }
+        }
 
-          return displayName;
-        };
-      }],
-
-      link: function (scope, element, attrs, controller) {
-      }
-    };
-  }])
-
-  ;
+        return displayName;
+      };
+    }]
+  })])
+  .name;
