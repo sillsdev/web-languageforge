27fbd25e92cf6782529371b3f0f134a4fa9670b8
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-01 09:54:11 +1200

    Make Multi & Option List fields valid TypeScript
    Former-commit-id: 5653cd384621790320e5cddaef7e5261d199d525

--- src/angular-app/languageforge/lexicon/editor/field/field.module.ts
+++ src/angular-app/languageforge/lexicon/editor/field/field.module.ts
@@ -4,13 +4,17 @@ import {NoticeModule} from '../../../../bellows/core/notice/notice.module';
 import {MockModule} from '../../../../bellows/shared/mock.module';
 import {SoundModule} from '../../../../bellows/shared/sound.module';
 import {FieldAudioComponent} from './dc-audio.component';
+import {MultiOptionListModule} from './dc-multioptionlist.component';
 import {LexiconMultiTextComponent} from './dc-multitext.component';
+import {OptionListModule} from './dc-optionlist.component';
 import {FieldTextComponent} from './dc-text.component';
 
 export const EditorFieldModule = angular
   .module('editorFieldModule', [
     'ngFileUpload',
     'palaso.ui.comments',
+    OptionListModule,
+    MultiOptionListModule,
     MockModule,
     NoticeModule,
     SoundModule
