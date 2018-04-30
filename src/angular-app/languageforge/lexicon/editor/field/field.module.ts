f475dafbdb14406aa09a3d910c9c554135548eb0
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-01 10:22:11 +1200

    Convert Multi Option List field to TypeScript component
    Former-commit-id: 7a10f554822708d8f7f0eeb7e01717ddae1a093b

--- src/angular-app/languageforge/lexicon/editor/field/field.module.ts
+++ src/angular-app/languageforge/lexicon/editor/field/field.module.ts
@@ -4,7 +4,7 @@ import {NoticeModule} from '../../../../bellows/core/notice/notice.module';
 import {MockModule} from '../../../../bellows/shared/mock.module';
 import {SoundModule} from '../../../../bellows/shared/sound.module';
 import {FieldAudioComponent} from './dc-audio.component';
-import {MultiOptionListModule} from './dc-multioptionlist.component';
+import {FieldMultiOptionListComponent} from './dc-multioptionlist.component';
 import {FieldMultiTextComponent} from './dc-multitext.component';
 import {FieldOptionListComponent} from './dc-optionlist.component';
 import {FieldTextComponent} from './dc-text.component';
@@ -13,12 +13,12 @@ export const EditorFieldModule = angular
   .module('editorFieldModule', [
     'ngFileUpload',
     'palaso.ui.comments',
-    MultiOptionListModule,
     MockModule,
     NoticeModule,
     SoundModule
   ])
   .component('dcAudio', FieldAudioComponent)
+  .component('dcMultioptionlist', FieldMultiOptionListComponent)
   .component('dcMultitext', FieldMultiTextComponent)
   .component('dcOptionlist', FieldOptionListComponent)
   .component('dcText', FieldTextComponent)
