8eefa6e589a60c069967a3dfde536ca78536c154
nigel-wells <nigel-wells@hotmail.com>
2018-05-02 16:31:47 +1200

    Changed importing of the breadcrumb module to be referenced correctly

--- src/angular-app/bellows/apps/userprofile/user-profile-app.module.ts
+++ src/angular-app/bellows/apps/userprofile/user-profile-app.module.ts
@@ -1,12 +1,13 @@
 import * as angular from 'angular';
 
+import {BreadcrumbModule} from '../../core/breadcrumbs/breadcrumb.module';
 import {CoreModule} from '../../core/core.module';
 import {NoticeModule} from '../../core/notice/notice.module';
 import {UserProfileAppComponent} from './user-profile-app.component';
 
 export const UserProfileAppModule = angular
   .module('userprofile', ['ui.bootstrap', 'pascalprecht.translate', CoreModule,
-    'palaso.ui.intlTelInput', NoticeModule, 'sgw.ui.breadcrumb'
+    'palaso.ui.intlTelInput', NoticeModule, BreadcrumbModule
   ])
   .component('userProfileApp', UserProfileAppComponent)
   .config(['$translateProvider', ($translateProvider: angular.translate.ITranslateProvider) => {
