d250898a0bbadd4d1fbbf8121bb42fd497905bf6
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-03 16:57:56 +1200

    Refactor field repeat fields to components
     - multiparagraph, picture, semantic domain
    
    
    Former-commit-id: 5488710d9c34a50b9487339e5681548916a07181

--- src/angular-app/languageforge/lexicon/editor/field/field-control.model.ts
+++ src/angular-app/languageforge/lexicon/editor/field/field-control.model.ts
@@ -1,6 +1,6 @@
 import {InterfaceConfig} from '../../../../bellows/shared/model/interface-config.model';
 import {Rights} from '../../core/lexicon-rights.service';
-import {LexiconConfig} from '../../shared/model/lexicon-config.model';
+import {LexConfig, LexiconConfig} from '../../shared/model/lexicon-config.model';
 import {LexiconProject} from '../../shared/model/lexicon-project.model';
 
 // ToDo: When all the fields are converted to TS we can then figure out how to deal with this control - IJH 2018-04
@@ -9,6 +9,10 @@ import {LexiconProject} from '../../shared/model/lexicon-project.model';
 export class FieldControl {
   interfaceConfig: InterfaceConfig;
   config: LexiconConfig;
+  makeValidModelRecursive: (config: LexConfig, data?: any, stopAtNodes?: string | string[]) => any;
   project: LexiconProject;
+  show: {
+    emptyFields: boolean
+  };
   rights: Rights;
 }
--- src/angular-app/languageforge/lexicon/editor/field/field.module.ts
+++ src/angular-app/languageforge/lexicon/editor/field/field.module.ts
@@ -5,10 +5,14 @@ import {MockModule} from '../../../../bellows/shared/mock.module';
 import {SoundModule} from '../../../../bellows/shared/sound.module';
 import {FieldAudioComponent} from './dc-audio.component';
 import {FieldEntryModule} from './dc-entry.component';
+import {FieldRepeatComponent} from './dc-fieldrepeat.component';
 import {FieldMultiOptionListComponent} from './dc-multioptionlist.component';
+import {FieldMultiParagraphComponent} from './dc-multiparagraph.component';
 import {FieldMultiTextComponent} from './dc-multitext.component';
 import {FieldOptionListComponent} from './dc-optionlist.component';
+import {FieldPictureComponent} from './dc-picture.component';
 import {FieldRenderedModule} from './dc-rendered.component';
+import {FieldSemanticDomainComponent} from './dc-semanticdomain.component';
 import {FieldTextComponent} from './dc-text.component';
 
 export const EditorFieldModule = angular
@@ -22,8 +26,12 @@ export const EditorFieldModule = angular
     SoundModule
   ])
   .component('dcAudio', FieldAudioComponent)
+  .component('dcFieldrepeat', FieldRepeatComponent)
   .component('dcMultioptionlist', FieldMultiOptionListComponent)
+  .component('dcMultiparagraph', FieldMultiParagraphComponent)
   .component('dcMultitext', FieldMultiTextComponent)
   .component('dcOptionlist', FieldOptionListComponent)
+  .component('dcPicture', FieldPictureComponent)
+  .component('dcSemanticdomain', FieldSemanticDomainComponent)
   .component('dcText', FieldTextComponent)
   .name;
