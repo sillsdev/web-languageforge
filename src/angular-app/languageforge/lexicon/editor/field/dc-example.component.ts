3de21bcbcff950dc9f5de4a683c25033b89f962e
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-03 13:23:52 +1200

    Make valid TypeScript

--- src/angular-app/languageforge/lexicon/editor/field/dc-example.component.ts
+++ src/angular-app/languageforge/lexicon/editor/field/dc-example.component.ts
@@ -1,8 +1,12 @@
-angular.module('palaso.ui.dc.example', ['palaso.ui.dc.fieldrepeat'])
+import * as angular from 'angular';
 
-// Palaso UI Dictionary Control: Example Sentence
-.directive('dcExample', [function () {
-  return {
+import {FieldRepeatModule} from './dc-fieldrepeat.component';
+
+export const FieldExampleModule = angular
+  .module('palaso.ui.dc.example', [FieldRepeatModule])
+
+  // Palaso UI Dictionary Control: Example Sentence
+  .directive('dcExample', [() => ({
     restrict: 'E',
     templateUrl: '/angular-app/languageforge/lexicon/editor/field/dc-example.component.html',
     scope: {
@@ -12,24 +16,18 @@ angular.module('palaso.ui.dc.example', ['palaso.ui.dc.fieldrepeat'])
       remove: '=',
       control: '='
     },
-    controller: ['$scope', '$state', function ($scope, $state) {
+    controller: ['$scope', '$state', ($scope, $state) => {
       $scope.$state = $state;
       $scope.contextGuid = $scope.$parent.contextGuid + ' example#' + $scope.model.guid;
 
-      angular.forEach($scope.control.config.entry.fields.senses.fields.examples.fields,
-        function (field) {
-          if (!angular.isDefined(field.senseLabel)) {
-            field.senseLabel = [];
-            field.senseLabel[-1] = 'Example';
-          }
+      angular.forEach($scope.control.config.entry.fields.senses.fields.examples.fields, field => {
+        if (!angular.isDefined(field.senseLabel)) {
+          field.senseLabel = [];
+          field.senseLabel[-1] = 'Example';
+        }
 
-          field.senseLabel[$scope.index] = 'Example ' + ($scope.index + 1);
-        });
-    }],
-
-    link: function (scope, element, attrs, controller) {
-    }
-  };
-}])
-
-;
+        field.senseLabel[$scope.index] = 'Example ' + ($scope.index + 1);
+      });
+    }]
+  })])
+  .name;
--- src/angular-app/languageforge/lexicon/editor/field/dc-picture.component.ts
+++ src/angular-app/languageforge/lexicon/editor/field/dc-picture.component.ts
@@ -1,11 +1,19 @@
-'use strict';
+import * as angular from 'angular';
 
-angular.module('palaso.ui.dc.picture', ['editorFieldModule', 'palaso.ui.notice',
-  'ngFileUpload'])
+import {SessionService} from '../../../../bellows/core/session.service';
+import {LexiconProjectService} from '../../core/lexicon-project.service';
+import {UploadFile, UploadResponse} from '../../shared/model/upload.model';
 
-// Palaso UI Dictionary Control: Picture
-.directive('dcPicture', [function () {
-  return {
+class Picture {
+  fileName: string;
+  caption: string;
+}
+
+export const FieldPictureModule = angular
+  .module('palaso.ui.dc.picture', ['ngFileUpload'])
+
+  // Palaso UI Dictionary Control: Picture
+  .directive('dcPicture', [() => ({
     restrict: 'E',
     templateUrl: '/angular-app/languageforge/lexicon/editor/field/dc-picture.component.html',
     scope: {
@@ -17,61 +25,62 @@ angular.module('palaso.ui.dc.picture', ['editorFieldModule', 'palaso.ui.notice',
     },
     controller: ['$scope', '$state', 'Upload', '$filter', 'sessionService', 'lexProjectService',
       'lexConfigService', 'silNoticeService', 'modalService',
-    function ($scope, $state, Upload, $filter, sessionService, lexProjectService,
-              lexConfigService, notice, modalService) {
-      $scope.$state = $state;
-      $scope.upload = {};
-      $scope.upload.progress = 0;
-      $scope.upload.file = null;
-      $scope.contextGuid = $scope.$parent.contextGuid;
+      ($scope, $state, Upload, $filter, sessionService: SessionService, lexProjectService: LexiconProjectService,
+       lexConfigService, notice, modalService) => {
 
-      $scope.fieldContainsData = lexConfigService.fieldContainsData;
+        $scope.$state = $state;
+        $scope.upload = {};
+        $scope.upload.progress = 0;
+        $scope.upload.file = null;
+        $scope.contextGuid = $scope.$parent.contextGuid;
 
-      $scope.getPictureUrl = function getPictureUrl(picture) {
-        if (isExternalReference(picture.fileName))
-          return '/Site/views/shared/image/placeholder.png';
-        return '/assets/lexicon/' + $scope.control.project.slug + '/pictures/' + picture.fileName;
-      };
+        $scope.fieldContainsData = lexConfigService.fieldContainsData;
 
-      $scope.getPictureDescription = function getPictureDescription(picture) {
-        if (!isExternalReference(picture.fileName)) return picture.fileName;
+        $scope.getPictureUrl = function getPictureUrl(picture: Picture): string {
+          if (isExternalReference(picture.fileName)) {
+            return '/Site/views/shared/image/placeholder.png';
+          }
+          return '/assets/lexicon/' + $scope.control.project.slug + '/pictures/' + picture.fileName;
+        };
 
-        return 'This picture references an external file (' +
-          picture.fileName +
-          ') and therefore cannot be synchronized. ' +
-          'To see the picture, link it to an internally referenced file. ' +
-          'Replace the file here or in FLEx, move or copy the file to the Linked Files folder.';
-      };
+        $scope.getPictureDescription = function getPictureDescription(picture: Picture): string {
+          if (!isExternalReference(picture.fileName)) return picture.fileName;
 
-      function isExternalReference(fileName) {
-        var isWindowsLink = (fileName.indexOf(':\\') >= 0);
-        var isLinuxLink = (fileName.indexOf('//') >= 0);
-        return isWindowsLink || isLinuxLink;
-      }
+          return 'This picture references an external file (' +
+            picture.fileName +
+            ') and therefore cannot be synchronized. ' +
+            'To see the picture, link it to an internally referenced file. ' +
+            'Replace the file here or in FLEx, move or copy the file to the Linked Files folder.';
+        };
 
-      // strips the timestamp file prefix (returns everything after the '_')
-      function originalFileName(fileName) {
-        return fileName.substr(fileName.indexOf('_') + 1);
-      }
+        function isExternalReference(fileName: string): boolean {
+          const isWindowsLink = (fileName.indexOf(':\\') >= 0);
+          const isLinuxLink = (fileName.indexOf('//') >= 0);
+          return isWindowsLink || isLinuxLink;
+        }
 
-      function addPicture(fileName) {
-        var newPicture = {};
-        var captionConfig = angular.copy($scope.config);
-        captionConfig.type = 'multitext';
-        newPicture.fileName = fileName;
-        newPicture.caption = $scope.control.makeValidModelRecursive(captionConfig, {});
-        $scope.pictures.push(newPicture);
-      }
+        // strips the timestamp file prefix (returns everything after the '_')
+        function originalFileName(fileName: string): string {
+          return fileName.substr(fileName.indexOf('_') + 1);
+        }
 
-      $scope.deletePicture = function deletePicture(index) {
-        var fileName = $scope.pictures[index].fileName;
-        if (fileName) {
-          var deleteMsg = "Are you sure you want to delete the picture <b>'" +
-            originalFileName(fileName) + "'</b>";
-          modalService.showModalSimple('Delete Picture', deleteMsg, 'Cancel', 'Delete Picture').
-            then(function () {
+        function addPicture(fileName: string): void {
+          const newPicture: Picture = new Picture();
+          const captionConfig = angular.copy($scope.config);
+          captionConfig.type = 'multitext';
+          newPicture.fileName = fileName;
+          newPicture.caption = $scope.control.makeValidModelRecursive(captionConfig, {});
+          $scope.pictures.push(newPicture);
+        }
+
+        $scope.deletePicture = function deletePicture(index: number): void {
+          const fileName = $scope.pictures[index].fileName;
+          if (fileName) {
+            const deleteMsg = 'Are you sure you want to delete the picture <b>\'' +
+              originalFileName(fileName) + '\'</b>';
+            modalService.showModalSimple('Delete Picture', deleteMsg, 'Cancel', 'Delete Picture').then(() => {
               $scope.pictures.splice(index, 1);
-              lexProjectService.removeMediaFile('sense-image', fileName, function (result) {
+              lexProjectService.removeMediaFile('sense-image', fileName, result => {
                 if (result.ok) {
                   if (!result.data.result) {
                     notice.push(notice.ERROR, result.data.errorMessage);
@@ -79,66 +88,68 @@ angular.module('palaso.ui.dc.picture', ['editorFieldModule', 'palaso.ui.notice',
                 }
               });
             }, angular.noop);
-        } else {
-          $scope.pictures.splice(index, 1);
-        }
-      };
+          } else {
+            $scope.pictures.splice(index, 1);
+          }
+        };
 
-      $scope.uploadFile = function uploadFile(file) {
-        if (!file || file.$error) return;
-
-        sessionService.getSession().then(function (session) {
-          if (file.size > session.fileSizeMax()) {
-            $scope.upload.progress = 0;
-            $scope.upload.file = null;
-            notice.push(notice.ERROR, '<b>' + file.name + '</b> (' +
-              $filter('bytes')(file.size) + ') is too large. It must be smaller than ' +
-              $filter('bytes')(session.fileSizeMax()) + '.');
+        $scope.uploadFile = function uploadFile(file: UploadFile): void {
+          if (!file || file.$error) {
             return;
           }
 
-          $scope.upload.file = file;
-          $scope.upload.progress = 0;
-          Upload.upload({
-            url: '/upload/lf-lexicon/sense-image',
-            data: { file: file }
-          }).then(function (response) {
-            var isUploadSuccess = response.data.result;
-            if (isUploadSuccess) {
-              $scope.upload.progress = 100.0;
-              addPicture(response.data.data.fileName);
-              $scope.upload.showAddPicture = false;
-            } else {
+          sessionService.getSession().then(session => {
+            if (file.size > session.fileSizeMax()) {
               $scope.upload.progress = 0;
-              notice.push(notice.ERROR, response.data.data.errorMessage);
+              $scope.upload.file = null;
+              notice.push(notice.ERROR, '<b>' + file.name + '</b> (' +
+                $filter('bytes')(file.size) + ') is too large. It must be smaller than ' +
+                $filter('bytes')(session.fileSizeMax()) + '.');
+              return;
             }
 
-            $scope.upload.file = null;
-          },
+            $scope.upload.file = file;
+            $scope.upload.progress = 0;
+            Upload.upload({
+              url: '/upload/lf-lexicon/sense-image',
+              data: {file}
+            }).then((response: UploadResponse) => {
+                const isUploadSuccess = response.data.result;
+                if (isUploadSuccess) {
+                  $scope.upload.progress = 100.0;
+                  addPicture(response.data.data.fileName);
+                  $scope.upload.showAddPicture = false;
+                } else {
+                  $scope.upload.progress = 0;
+                  notice.push(notice.ERROR, response.data.data.errorMessage);
+                }
 
-          function (response) {
-            var errorMessage = $filter('translate')('Upload failed.');
-            if (response.status > 0) {
-              errorMessage += ' Status: ' + response.status;
-              if (response.statusText) {
-                errorMessage += ' ' + response.statusText;
-              }
+                $scope.upload.file = null;
+              },
 
-              if (response.data) {
-                errorMessage += '- ' + response.data;
-              }
-            }
+              (response: UploadResponse) => {
+                let errorMessage = 'Upload failed.';
+                if (response.status > 0) {
+                  errorMessage += ' Status: ' + response.status;
+                  if (response.statusText) {
+                    errorMessage += ' ' + response.statusText;
+                  }
 
-            $scope.upload.file = null;
-            notice.push(notice.ERROR, errorMessage);
-          },
+                  if (response.data) {
+                    errorMessage += '- ' + response.data;
+                  }
+                }
 
-          function (evt) {
-            $scope.upload.progress = parseInt(100.0 * evt.loaded / evt.total);
+                $scope.upload.file = null;
+                notice.push(notice.ERROR, errorMessage);
+              },
+
+              (evt: ProgressEvent) => {
+                $scope.upload.progress = Math.floor(100.0 * evt.loaded / evt.total);
+              });
           });
-        });
-      };
+        };
 
-    }]
-  };
-}]);
+      }]
+  })])
+  .name;
