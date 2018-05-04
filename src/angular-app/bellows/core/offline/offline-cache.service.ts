18ecd04e14d559c71d52694c3d957ecac465e541
Nathaniel Paulus <nateowami@gmail.com>
2018-05-04 15:30:20 -0400

    Numerous small changes to offline cache

--- src/angular-app/bellows/core/offline/offline-cache.service.ts
+++ src/angular-app/bellows/core/offline/offline-cache.service.ts
@@ -1,15 +1,54 @@
 import * as angular from 'angular';
-import * as localforage from "localforage";
+import * as localforage from 'localforage';
 
 /**
  * implements an offline cache storage system
  */
 export class OfflineCacheService {
-  readonly version = 6;
+  static readonly version = 6;
+
+  static $inject: string[] = ['$q'];
+  constructor(private $q: angular.IQService) { }
+
+  static canCache(): boolean {
+    return localforage.supports(localforage.INDEXEDDB) || localforage.supports(localforage.WEBSQL);
+  }
+
+  deleteObjectInStore(storeName: string, key: string): angular.IPromise<any> {
+    return this.$q.when(this.getStore(storeName).removeItem(key));
+  }
+
+  getAllFromStore(storeName: string, projectId?: string): angular.IPromise<any> {
+    const results: any[] = [];
+    return this.$q.when(this.getStore(storeName).iterate((value, key, iterationNumber) => {
+      if (value.hasOwnProperty('projectId')) {
+        const obj = value as {projectId: string; };
+        if (obj.projectId === projectId) {
+          results.push(OfflineCacheService.removeProjectId(value));
+        }
+      }
+    }).then(() => {
+      return results;
+    }));
+  }
+
+  getOneFromStore(storeName: string, key: string): angular.IPromise<any> {
+    return this.$q.when(this.getStore(storeName).getItem(key).then(item => {
+      return OfflineCacheService.removeProjectId(item);
+    }));
+  }
+
+  setObjectsInStore(storeName: string, projectId: string, items: any[], isAdd: boolean = false): angular.IPromise<any> {
+
+    const store: LocalForage = this.getStore(storeName);
+    return this.$q.all(items.map(item => {
+      return store.setItem(item.id, OfflineCacheService.addProjectId(item, projectId));
+    }));
+  }
 
   private stores = {};
   private getStore(storeName: string): LocalForage {
-    if(!this.stores[storeName]) {
+    if (!this.stores[storeName]) {
       this.stores[storeName] = localforage.createInstance({
         name: storeName
       });
@@ -17,52 +56,17 @@ export class OfflineCacheService {
     return this.stores[storeName];
   }
 
-  private addProjectId(object: any, projectId: string) {
-    if(object.projectId !== projectId) {
+  private static addProjectId(object: any, projectId: string) {
+    if (object.projectId !== projectId) {
       object = angular.copy(object);
       object.projectId = projectId;
     }
     return object;
   }
 
-  private removeProjectId(object: any) {
+  private static removeProjectId(object: any) {
     delete object.projectId;
     return object;
-  }
-
-  static $inject: string[] = ['$q', '$window'];
-  constructor(private $q: angular.IQService, private $window: angular.IWindowService) { }
-
-  canCache = (): boolean => {
-    return localforage.supports(localforage.INDEXEDDB) || localforage.supports(localforage.WEBSQL);
-  }
-
-  deleteObjectInStore = (storeName: string, key: string): angular.IPromise<any> => {
-    return this.$q.when(this.getStore(storeName).removeItem(key));
-  }
-
-  getAllFromStore = (storeName: string, projectId?: string): angular.IPromise<any> => {
-    const results: {}[] = [];
-    return this.$q.when(this.getStore(storeName).iterate((value, key, iterationNumber) => {
-      if(value['projectId'] === projectId) results.push(this.removeProjectId(value));
-    }).then(() => {
-      return results;
-    }));
-  }
-
-  getOneFromStore = (storeName: string, key: string): angular.IPromise<any> => {
-    return this.$q.when(this.getStore(storeName).getItem(key).then(item => {
-      return this.removeProjectId(item);
-    }));
-  }
-
-  setObjectsInStore = (storeName: string, projectId: string, items: any[],
-                       isAdd: boolean = false): angular.IPromise<any> => {
-
-    const store: LocalForage = this.getStore(storeName);
-    return this.$q.all(items.map(item => {
-      return store.setItem(item.id, this.addProjectId(item, projectId));
-    }));
   }
 
 }
