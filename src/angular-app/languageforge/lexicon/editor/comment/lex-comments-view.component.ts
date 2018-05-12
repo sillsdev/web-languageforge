fad4ac40022cac1038b8a04d8c3b181d61d14c5b
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-12 21:17:33 +1200

    Make comments valid TypeScript

--- src/angular-app/languageforge/lexicon/editor/comment/lex-comments-view.component.ts
+++ src/angular-app/languageforge/lexicon/editor/comment/lex-comments-view.component.ts
@@ -1,103 +1,98 @@
-'use strict';
+import * as angular from 'angular';
 
-angular.module('lexCommentsModule')
+import {SessionService} from '../../../../bellows/core/session.service';
+import {LexiconConfigService} from '../../core/lexicon-config.service';
+import {LexField} from '../../shared/model/lex-field.model';
+import {LexMultiValue} from '../../shared/model/lex-multi-value.model';
+import {LexValue} from '../../shared/model/lex-value.model';
 
-  // Palaso UI Dictionary Control: Comments
-  .directive('lexCommentsView', [function () {
-    return {
-      restrict: 'E',
-      templateUrl: '/angular-app/languageforge/lexicon/editor/comment/lex-comments-view.component.html',
-      scope: {
-        entry: '=',
-        entryConfig: '=',
-        control: '='
-      },
-      controller: ['$scope', '$filter', 'lexCommentService', 'sessionService', 'lexConfigService',
-        '$q',
-      function ($scope, $filter, commentService, ss, lexConfig, $q) {
-        // notes by cjh 2015-03
-        // define this method on the control (which happens to be an ancestor scope) because it is
-        // used by a sibling directive (dc-entry)
-        // an alternative implementation to this would be to use the commentService to contain this
-        // method (but then the comment service would become lex specific which is a downside
-        $q.all([lexConfig.refresh(), ss.getSession()]).then(function (data) {
-          var config = data[0];
-          var session = data[1];
-          $scope.config = config;
-          $scope.control.selectFieldForComment =
-          function selectFieldForComment(fieldName, model, inputSystem, multioptionValue,
-                                         pictureFilePath, contextGuid) {
-            var canComment = session.hasProjectRight(ss.domain.COMMENTS, ss.operation.CREATE);
-            if (!canComment) return;
+export function LexCommentsViewComponent() {
+  return {
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/editor/comment/lex-comments-view.component.html',
+    scope: {
+      entry: '=',
+      entryConfig: '=',
+      control: '='
+    },
+    controller: ['$scope', '$q', 'sessionService', 'lexConfigService',
+    ($scope: any, $q: angular.IQService, sessionService: SessionService, lexConfig: LexiconConfigService) => {
+      // notes by cjh 2015-03
+      // define this method on the control (which happens to be an ancestor scope) because it is
+      // used by a sibling directive (dc-entry)
+      // an alternative implementation to this would be to use the commentService to contain this
+      // method (but then the comment service would become lex specific which is a downside
+      $q.all([lexConfig.refresh(), sessionService.getSession()]).then(([config, session]) => {
+        $scope.config = config;
+        $scope.control.selectFieldForComment =
+        function selectFieldForComment(fieldName: string, model: LexField, inputSystemTag: string,
+                                       multioptionValue: string, pictureFilePath: string, contextGuid: string): void {
+          const canComment = session.hasProjectRight(sessionService.domain.COMMENTS, sessionService.operation.CREATE);
+          if (!canComment) return;
 
-            lexConfig.getFieldConfig(fieldName).then(function (fieldConfig) {
-              $scope.newComment.regardingFieldConfig = fieldConfig;
-              $scope.newComment.regarding.field = fieldName;
-              $scope.newComment.regarding.fieldNameForDisplay =
-                $scope.newComment.regardingFieldConfig.label;
-              delete $scope.newComment.regarding.inputSystem;
-              delete $scope.newComment.regarding.inputSystemAbbreviation;
-              $scope.newComment.isRegardingPicture = false;
-              $scope.newComment.contextGuid = contextGuid;
-              if (inputSystem) {
-                $scope.newComment.regarding.fieldValue = getFieldValue(model, inputSystem);
-                $scope.newComment.regarding.inputSystem =
-                  $scope.config.inputSystems[inputSystem].languageName;
-                $scope.newComment.regarding.inputSystemAbbreviation =
-                  $scope.config.inputSystems[inputSystem].abbreviation;
-              } else if (multioptionValue) {
-                $scope.newComment.regarding.fieldValue = multioptionValue;
-              } else if (pictureFilePath) {
-                $scope.newComment.regarding.fieldValue = pictureFilePath;
-                $scope.newComment.isRegardingPicture = true;
-              } else {
-                $scope.newComment.regarding.fieldValue = getFieldValue(model);
-              }
-            });
-          };
-        });
-
-        $scope.control.getNewComment = function getNewComment() {
-          return $scope.newComment;
-        };
-
-        function getFieldValue(model, inputSystem) {
-          // get value of option list
-          if (angular.isDefined(model.value)) {
-
-            // todo return display value
-            return model.value;
-          }
-
-          // get value of multi-option list
-          if (angular.isDefined(model.values)) {
-
-            // todo return display values
-            return model.values.join(' ');
-          }
-
-          // get value of multi-text with specified inputSystem
-          if (angular.isDefined(inputSystem) && angular.isDefined(model[inputSystem])) {
-            return model[inputSystem].value;
-          }
-
-          // get first inputSystem of a multi-text (no inputSystem specified)
-          var valueToReturn = undefined;
-          angular.forEach(model, function (prop) {
-            if (angular.isUndefined(valueToReturn)) {
-              valueToReturn = prop.value;
-              return valueToReturn;
+          lexConfig.getFieldConfig(fieldName).then(fieldConfig => {
+            $scope.newComment.regardingFieldConfig = fieldConfig;
+            $scope.newComment.regarding.field = fieldName;
+            $scope.newComment.regarding.fieldNameForDisplay =
+              $scope.newComment.regardingFieldConfig.label;
+            delete $scope.newComment.regarding.inputSystem;
+            delete $scope.newComment.regarding.inputSystemAbbreviation;
+            $scope.newComment.isRegardingPicture = false;
+            $scope.newComment.contextGuid = contextGuid;
+            if (inputSystemTag) {
+              $scope.newComment.regarding.fieldValue = getFieldValue(model, inputSystemTag);
+              $scope.newComment.regarding.inputSystem =
+                $scope.config.inputSystems[inputSystemTag].languageName;
+              $scope.newComment.regarding.inputSystemAbbreviation =
+                $scope.config.inputSystems[inputSystemTag].abbreviation;
+            } else if (multioptionValue) {
+              $scope.newComment.regarding.fieldValue = multioptionValue;
+            } else if (pictureFilePath) {
+              $scope.newComment.regarding.fieldValue = pictureFilePath;
+              $scope.newComment.isRegardingPicture = true;
+            } else {
+              $scope.newComment.regarding.fieldValue = getFieldValue(model);
             }
           });
+        };
+      });
 
-          return valueToReturn;
+      $scope.control.getNewComment = function getNewComment() {
+        return $scope.newComment;
+      };
+
+      function getFieldValue(model: LexField, inputSystem?: string): string {
+        // get value of option list
+        if ((model as LexValue).value != null) {
+
+          // todo return display value
+          return (model as LexValue).value;
         }
 
-      }],
+        // get value of multi-option list
+        if ((model as LexMultiValue).values != null) {
 
-      link: function (scope, element, attrs, controller) {
+          // todo return display values
+          return (model as LexMultiValue).values.join(' ');
+        }
+
+        // get value of multi-text with specified inputSystem
+        if (inputSystem != null && model[inputSystem] != null) {
+          return model[inputSystem].value;
+        }
+
+        // get first inputSystem of a multi-text (no inputSystem specified)
+        let valueToReturn: string = null;
+        angular.forEach(model, prop => {
+          if (valueToReturn == null) {
+            valueToReturn = prop.value;
+            return;
+          }
+        });
+
+        return valueToReturn;
       }
-    };
-  }])
 
-;
+    }]
+  };
+}
--- src/angular-app/languageforge/lexicon/editor/comment/regarding-field.component.ts
+++ src/angular-app/languageforge/lexicon/editor/comment/regarding-field.component.ts
@@ -1,61 +1,65 @@
-'use strict';
+import * as angular from 'angular';
 
-angular.module('lexCommentsModule')
+import {LexConfigField} from '../../shared/model/lexicon-config.model';
 
-  // Palaso UI Dictionary Control: Comments
-  .directive('regardingField', [function () {
-    return {
-      restrict: 'E',
-      templateUrl: '/angular-app/languageforge/lexicon/editor/comment/regarding-field.component.html',
-      scope: {
-        content: '=',
-        control: '=',
-        field: '=',
-        fieldConfig: '='
-      },
-      controller: ['$scope', function ($scope) {
-        $scope.regarding = '';
-        $scope.$watch('fieldConfig', function (newContent) {
-          if (angular.isDefined(newContent)) {
-            $scope.setContent();
-          }
-        });
+interface WindowService extends angular.IWindowService {
+  semanticDomains_en?: any;
+}
 
-        $scope.setContent = function setContent() {
-          if (angular.isDefined($scope.fieldConfig)) {
-            if (!angular.isUndefined($scope.content)) {
-              if ($scope.fieldConfig.type === 'optionlist' ||
-                  $scope.fieldConfig.type === 'multioptionlist') {
-                if ($scope.field === 'semanticDomain') {
-                  // Semantic domains are in the global scope and appear to be English only
-                  // Will need to be updated once the system provides support for other languages
-                  for (var i in semanticDomains_en) {
-                    if (semanticDomains_en.hasOwnProperty(i) &&
-                      semanticDomains_en[i].key === $scope.content
-                    ) {
-                      $scope.regarding = semanticDomains_en[i].value;
-                    }
+export function RegardingFieldComponent() {
+  return {
+    restrict: 'E',
+    templateUrl: '/angular-app/languageforge/lexicon/editor/comment/regarding-field.component.html',
+    scope: {
+      content: '=',
+      control: '=',
+      field: '=',
+      fieldConfig: '='
+    },
+    controller: ['$scope', '$window', ($scope: any, $window: WindowService) => {
+      $scope.regarding = '';
+      $scope.$watch('fieldConfig', (newContent: LexConfigField) => {
+        if (newContent != null) {
+          $scope.setContent();
+        }
+      });
+
+      $scope.setContent = function setContent(): void {
+        if ($scope.fieldConfig != null) {
+          if ($scope.content != null) {
+            if ($scope.fieldConfig.type === 'optionlist' || $scope.fieldConfig.type === 'multioptionlist') {
+              if ($scope.field === 'semanticDomain') {
+                // Semantic domains are in the global scope and appear to be English only
+                // Will need to be updated once the system provides support for other languages
+                for (const i in $window.semanticDomains_en) {
+                  if ($window.semanticDomains_en.hasOwnProperty(i) &&
+                    $window.semanticDomains_en[i].key === $scope.content
+                  ) {
+                    $scope.regarding = $window.semanticDomains_en[i].value;
                   }
-                } else {
-                  var optionlists = $scope.control.config.optionlists;
-                  for (var listCode in optionlists) {
-                    if (listCode === $scope.fieldConfig.listCode) {
-                      for (var i in optionlists[listCode].items) {
-                        if (optionlists[listCode].items[i].key === $scope.content) {
-                          $scope.regarding = optionlists[listCode].items[i].value;
-                        }
+                }
+              } else {
+                const optionlists = $scope.control.config.optionlists;
+                for (const listCode in optionlists) {
+                  if (optionlists.hasOwnProperty(listCode) && listCode === $scope.fieldConfig.listCode) {
+                    for (const i in optionlists[listCode].items) {
+                      if (optionlists[listCode].items.hasOwnProperty(i) &&
+                        optionlists[listCode].items[i].key === $scope.content
+                      ) {
+                        $scope.regarding = optionlists[listCode].items[i].value;
                       }
                     }
                   }
                 }
-              } else {
-                $scope.regarding = $scope.content;
               }
+            } else {
+              $scope.regarding = $scope.content;
             }
           }
-        };
+        }
+      };
 
-        $scope.setContent();
-      }]
-    };
-  }]);
+      $scope.setContent();
+    }]
+  };
+}
