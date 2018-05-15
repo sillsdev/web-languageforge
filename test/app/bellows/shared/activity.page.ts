19338a72653234b1c1c9e3b7cd2ca324f76f683c
nigel-wells <nigel-wells@hotmail.com>
2018-05-15 15:32:02 +1200

    Updated E2E tests for the new activity layout

--- test/app/bellows/shared/activity.page.ts
+++ test/app/bellows/shared/activity.page.ts
@@ -1,22 +1,25 @@
 import {browser, by, element} from 'protractor';
 import {ElementFinder} from 'protractor/built/element';
+import {Utils} from "./utils";
 
 // This object handles the activity page and provides methods to access items in the activity list
 export class SfActivityPage {
   activityURL = '/app/activity';
-  activitiesList = element.all(by.repeater('item in $ctrl.filteredActivities'));
+  activitiesList = element.all(by.className('activity-content'));
+  activityGroups = element.all(by.className('activity-user-group'));
+  filterByUser = element(by.id('filter_by_user'));
 
   // Navigate to the Activity page
   get() {
     browser.get(browser.baseUrl + this.activityURL);
   }
 
-  static clickOnAllActivity() {
-    element(by.id('activity-showAllActivityButton')).click();
+  clickOnAllActivity() {
+    this.filterByUser.element(by.css('option:nth-child(1)')).click();
   }
 
-  static clickOnShowOnlyMyActivity() {
-    element(by.id('activity-showOnlyMyActivityButton')).click();
+  clickOnShowOnlyMyActivity() {
+    this.filterByUser.element(by.css('option:nth-child(2)')).click();
   }
 
   // Returns the number of items in the activity list
@@ -34,11 +37,23 @@ export class SfActivityPage {
     return this.activitiesList.map((elem: ElementFinder) => elem.getText());
   }
 
+  getActivityGroup(index: number) {
+    return SfActivityPage.getPartsOfActivity(this.activityGroups.get(index));
+  }
+
   // Prints the entire activity list
   //noinspection JSUnusedGlobalSymbols
   printActivitiesNames() {
     (this.activitiesList).each((names: ElementFinder) => {
       names.getText().then(console.log);
     });
+  }
+
+  static getPartsOfActivity(div: ElementFinder) {
+    return {
+      activity: div,
+      user: div.element(by.className('activity-username')).getText(),
+      activities: div.all(by.className('activity-content')).map((elem: ElementFinder) => elem.getText())
+    };
   }
 }
