af8a599c65d01f6eb7c36b27f4968b1a25a83d4d
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-30 13:36:18 +1200

    Convert lexicon app to TypeScript
    - remove unused translate provider

--- src/angular-app/languageforge/lexicon/lexicon-app.module.ts
+++ src/angular-app/languageforge/lexicon/lexicon-app.module.ts
@@ -1,13 +1,24 @@
 import * as angular from 'angular';
 import 'angular-sanitize';
+import uiRouter from 'angular-ui-router';
 
+import {BreadcrumbModule} from '../../bellows/core/breadcrumbs/breadcrumb.module';
+import {CoreModule} from '../../bellows/core/core.module';
 import {LexiconCoreModule} from './core/lexicon-core.module';
 import {LexiconEditorModule} from './editor/editor.module';
+import {LexiconAppComponentModule} from './lexicon-app.component';
 import './new-project/lexicon-new-project.module';
 import {LexiconSettingsModule} from './settings/settings.module';
 
 export const LexiconAppModule = angular
   .module('lexiconModule', [
+    'ui.bootstrap',
+    uiRouter,
+    'ngSanitize',
+    'palaso.ui.typeahead',
+    CoreModule,
+    BreadcrumbModule,
+    LexiconAppComponentModule,
     LexiconCoreModule,
     LexiconEditorModule,
     LexiconSettingsModule
