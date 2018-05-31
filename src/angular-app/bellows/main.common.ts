92c7786f677a8bc5534690e5033c7ce032bf9a67
Ira Hopkinson <ira_hopkinson@sil.org>
2018-05-31 13:23:27 +1200

    Remove 'angular-translate' dependency
    - remove 'pascalprecht.translate' module references
    - remove use of $translateProvider
    - remove use of translate attribute and filter

--- src/angular-app/bellows/main.common.ts
+++ src/angular-app/bellows/main.common.ts
@@ -1,8 +1,6 @@
 import 'angular';
 import 'angular-route';
 import 'angular-sortable-view';
-import 'angular-translate';
-import 'angular-translate-loader-static-files';
 import 'angular-ui-bootstrap-4';
 import 'angular-ui-router';
 import 'angular-ui-validate';
