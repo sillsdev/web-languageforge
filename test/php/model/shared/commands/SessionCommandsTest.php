bc98610dbdb68516b70fd4c89ec6a8ef53e38141
Ira Hopkinson <ira_hopkinson@sil.org>
2018-06-28 14:46:25 +1200

    Transifex language picker working for Bellows apps

--- test/php/model/shared/commands/SessionCommandsTest.php
+++ test/php/model/shared/commands/SessionCommandsTest.php
@@ -29,19 +29,33 @@ class SessionTestEnvironment
 
     public function create()
     {
-        $environ = new MongoTestEnvironment();
+        $environ = $this->getEnviron();
         $environ->clean();
         $this->website = $environ->website;
 
         $this->project = $environ->createProject(SF_TESTPROJECT, SF_TESTPROJECTCODE);
-        $this->project->appName = 'sfchecks';
-        $this->project->write();
         $this->question = new QuestionModel($this->project);
         $this->question->write();
 
         $this->userId = $environ->createUser('test_user', 'Test User', 'test_user@example.com');
         $this->projectId = $this->project->id->asString();
         $this->questionId = $this->question->id->asString();
+    }
+
+    /**
+     * @return MongoTestEnvironment
+     */
+    protected function getEnviron(): MongoTestEnvironment
+    {
+        return new MongoTestEnvironment();
+    }
+
+}
+
+class LfSessionTestEnvironment extends SessionTestEnvironment {
+    protected function getEnviron(): MongoTestEnvironment
+    {
+        return new LexiconMongoTestEnvironment();
     }
 }
 
@@ -52,6 +66,9 @@ class SessionCommandsTest extends TestCase
         @unlink(SessionCommands::getSessionFilePath('mockSessionFile'));
     }
 
+    /**
+     * @throws Exception
+     */
     public function testSessionData_userIsNotPartOfProject()
     {
         $environ = new SessionTestEnvironment();
@@ -85,6 +102,50 @@ class SessionCommandsTest extends TestCase
         $this->assertArrayNotHasKey('userProjectRights', $data);
     }
 
+    /**
+     * @throws Exception
+     */
+    public function testSessionData_userIsNotPartOfProjectInLf()
+    {
+        $environ = new LfSessionTestEnvironment();
+        $environ->create();
+        $data = SessionCommands::getSessionData($environ->projectId, $environ->userId, $environ->website);
+
+        // Session data should contain a userId but not a projectId
+        $this->assertArrayHasKey('userId', $data);
+        $this->assertTrue(is_string($data['userId']));
+        $this->assertEquals($environ->userId, $data['userId']);
+
+        // Session data should also contain "site", a string...
+        $this->assertArrayHasKey('baseSite', $data);
+        $this->assertTrue(is_string($data['baseSite']));
+        // ... and "fileSizeMax", an integer
+        $this->assertArrayHasKey('fileSizeMax', $data);
+        $this->assertTrue(is_integer($data['fileSizeMax']));
+
+        // Session data should contain projectSettings, an array with only interFaceConfig
+        $this->assertArrayHasKey('projectSettings', $data);
+        $this->assertTrue(is_array($data['projectSettings']));
+        $this->assertCount(1, $data['projectSettings']);
+        $this->assertArrayHasKey('interfaceConfig', $data['projectSettings']);
+
+        // Session data should not contain project, an associative array
+        $this->assertArrayNotHasKey('project', $data);
+
+        // Session data should contain user site rights, an array of integers
+        $this->assertArrayHasKey('userSiteRights', $data);
+        $this->assertTrue(is_array($data['userSiteRights']));
+        // ... which should not be empty
+        $this->assertFalse(empty($data['userSiteRights']));
+        $this->assertTrue(is_integer($data['userSiteRights'][0]));
+
+        // Session data should contain user project rights, an array of integers
+        $this->assertArrayNotHasKey('userProjectRights', $data);
+    }
+
+    /**
+     * @throws Exception
+     */
     public function testSessionData_userIsPartOfProject()
     {
         $environ = new SessionTestEnvironment();
@@ -100,6 +161,9 @@ class SessionCommandsTest extends TestCase
         $this->assertTrue(is_integer($data['userProjectRights'][0]));
     }
 
+    /**
+     * @throws Exception
+     */
     public function testGetSessionData_sessionFileExists_dataInFile()
     {
         $environ = new SessionTestEnvironment();
@@ -108,9 +172,10 @@ class SessionCommandsTest extends TestCase
         
         $this->assertFalse(file_exists(SessionCommands::getSessionFilePath('mockSessionFile')));
 
-        $data = SessionCommands::getSessionData($environ->projectId, $environ->userId, $environ->website, '', 'mockSessionFile');
+        $data = SessionCommands::getSessionData($environ->projectId, $environ->userId, $environ->website, 'mockSessionFile');
         
         $this->assertTrue(file_exists(SessionCommands::getSessionFilePath('mockSessionFile')));
         $this->assertJsonStringEqualsJsonFile(SessionCommands::getSessionFilePath('mockSessionFile'), json_encode($data));
     }
+
 }
