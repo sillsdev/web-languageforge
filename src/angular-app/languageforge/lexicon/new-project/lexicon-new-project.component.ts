1ab6f6793b529221ce7dcc7798ca7230357b44d1
Ira Hopkinson <ira_hopkinson@sil.org>
2018-07-07 15:41:24 +1200

    convert views to Angular components
    Former-commit-id: f5a8b84776222383b8ca64d216dba24f738e7e0e

--- src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.component.ts
+++ src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.component.ts
@@ -3,14 +3,11 @@ import * as angular from 'angular';
 import {ProjectService} from '../../../bellows/core/api/project.service';
 import {ApplicationHeaderService} from '../../../bellows/core/application-header.service';
 import {BreadcrumbService} from '../../../bellows/core/breadcrumbs/breadcrumb.service';
-import {BytesFilterFunction} from '../../../bellows/core/filters';
 import {LinkService} from '../../../bellows/core/link.service';
-import {ModalService} from '../../../bellows/core/modal/modal.service';
 import {NoticeService} from '../../../bellows/core/notice/notice.service';
 import {SessionService} from '../../../bellows/core/session.service';
 import {InputSystem} from '../../../bellows/shared/model/input-system.model';
 import {InterfaceConfig} from '../../../bellows/shared/model/interface-config.model';
-import {UploadFile, UploadResponse} from '../../../bellows/shared/model/upload.model';
 import {LexiconProjectService} from '../core/lexicon-project.service';
 import {LexiconSendReceiveApiService} from '../core/lexicon-send-receive-api.service';
 import {LexiconSendReceiveService} from '../core/lexicon-send-receive.service';
@@ -23,12 +20,11 @@ import {
 import {LexiconProjectSettings} from '../shared/model/lexicon-project-settings.model';
 import {LexiconProject, SendReceive} from '../shared/model/lexicon-project.model';
 import {LexOptionList} from '../shared/model/option-list.model';
-import {
-  LexiconNewProjectInitialDataState,
-  LexiconNewProjectNameState, LexiconNewProjectSendReceiveCloneState, LexiconNewProjectSendReceiveCredentialsState
-} from './lexicon-new-project-state.model';
+import {NewProjectInitialDataState} from './non-send-receive/new-project-initial-data.component';
+import {NewProjectSendReceiveCloneState} from './send-receive/new-project-clone.component';
+import {NewProjectSendReceiveCredentialsState} from './send-receive/new-project-credentials.component';
 
-interface NewProject extends LexiconProject {
+export interface NewProject extends LexiconProject {
   editProjectCode?: boolean;
   emptyProjectDesired?: boolean;
   entriesImported: number;
@@ -38,14 +34,7 @@ interface NewProject extends LexiconProject {
   };
 }
 
-interface Show {
-  importErrors: boolean;
-  cloning: boolean;
-  flexHelp: boolean;
-}
-
 export class LexiconNewProjectController implements angular.IController {
-  cloneNotice = this.sendReceive.cloneNotice;
   interfaceConfig: InterfaceConfig = {} as InterfaceConfig;
   newProject: NewProject = {} as NewProject;
   project: LexiconProject = {} as LexiconProject;
@@ -56,7 +45,6 @@ export class LexiconNewProjectController implements angular.IController {
   forwardBtnClass: string;
   projectCodeState: string;
   projectCodeStateDefer: angular.IDeferred<string>;
-  show: Show;
 
   // Shorthand to make things look a touch nicer
   readonly ok = this.makeFormValid;
@@ -64,9 +52,8 @@ export class LexiconNewProjectController implements angular.IController {
   readonly error = this.makeFormInvalid;
 
   static $inject = ['$scope', '$q',
-    '$filter', '$window',
-    '$state', '$uibModal',
-    'Upload', 'applicationHeaderService',
+    '$state', '$window',
+    'applicationHeaderService',
     'breadcrumbService', 'sessionService',
     'silNoticeService', 'linkService',
     'projectService',
@@ -74,9 +61,8 @@ export class LexiconNewProjectController implements angular.IController {
     'lexSendReceiveApi',
     'lexSendReceive'];
   constructor(private readonly $scope: angular.IScope, readonly $q: angular.IQService,
-              private readonly $filter: angular.IFilterService, private readonly $window: angular.IWindowService,
-              readonly $state: angular.ui.IStateService, private readonly $modal: ModalService,
-              private readonly Upload: any, private readonly applicationHeaderService: ApplicationHeaderService,
+              readonly $state: angular.ui.IStateService, private readonly $window: angular.IWindowService,
+              private readonly applicationHeaderService: ApplicationHeaderService,
               private readonly breadcrumbService: BreadcrumbService, readonly sessionService: SessionService,
               readonly notice: NoticeService, private readonly linkService: LinkService,
               readonly projectService: ProjectService,
@@ -96,11 +82,6 @@ export class LexiconNewProjectController implements angular.IController {
     this.newProject.config = new LexiconConfig();
     this.newProject.appName = 'lexicon';
 
-    this.show = {
-      importErrors: false,
-      flexHelp: false,
-      cloning: true
-    } as Show;
     this.resetValidateProjectForm();
 
     this.breadcrumbService.set('top', [{
@@ -111,55 +92,11 @@ export class LexiconNewProjectController implements angular.IController {
     }]);
     this.applicationHeaderService.setPageName('Start or join a Web Dictionary Project');
 
-    // ----- Step 1: Project name -----
-
-    this.$scope.$watch(() => this.projectCodeState, (newVal: string, oldVal: string) => {
-      if (!newVal || newVal === oldVal) {
-        return;
-      }
-
-      if (newVal === 'unchecked') {
-        // User just typed in the project name box.
-        // Need to wait just a bit for the idle-validate to kick in.
-        return;
-      }
-
-      if (oldVal === 'loading') {
-        // Project code state just resolved. Validate rest of form so Forward button can activate.
-        this.validateForm();
-      }
-    });
-
-    this.$scope.$watch(() => this.newProject.editProjectCode, (newVal: boolean, oldVal: boolean) => {
-      if (oldVal && !newVal) {
-        // When user unchecks the "edit project code" box, go back to setting it from project name
-        this.newProject.projectCode = LexiconNewProjectController.projectNameToCode(this.newProject.projectName);
-        this.checkProjectCode();
-      }
-    });
-
-    this.$scope.$watch(() => this.newProject.projectName, (newVal: string, oldVal: string) => {
-      if (newVal == null) {
-        this.newProject.projectCode = '';
-      } else if (newVal !== oldVal) {
-        this.newProject.projectCode = newVal.toLowerCase().replace(/ /g, '_');
-      }
-    });
-
     // ----- Step 2: Send Receive Clone -----
 
     this.sendReceive.clearState();
     this.sendReceive.setCloneProjectStatusSuccessCallback(this.gotoEditor);
     this.$scope.$on('$locationChangeStart', this.sendReceive.cancelCloneStatusTimer);
-
-    // ----- Step 3: Verify initial data -OR- select primary language -----
-
-    this.$scope.$watch(() => this.newProject.languageCode, (newVal: string) => {
-      if (newVal != null) {
-        this.validateForm();
-      }
-    });
-
   }
 
   // ----- Step 2: Send Receive Clone -----
@@ -211,7 +148,7 @@ export class LexiconNewProjectController implements angular.IController {
   }
 
   getProjectFromInternet(): void {
-    this.$state.go(LexiconNewProjectSendReceiveCredentialsState.name);
+    this.$state.go(NewProjectSendReceiveCredentialsState.name);
     this.resetValidateProjectForm();
     this.sessionService.getSession().then(session => {
       if (!this.project.sendReceive.username) {
@@ -220,10 +157,6 @@ export class LexiconNewProjectController implements angular.IController {
 
       this.validateForm();
     });
-  }
-
-  createNew(): void {
-    this.$state.go(LexiconNewProjectNameState.name);
   }
 
   iconForStep(step: number): string[] {
@@ -247,7 +180,7 @@ export class LexiconNewProjectController implements angular.IController {
   }
 
   nextStep(): void {
-    if (this.$state.current.name === LexiconNewProjectInitialDataState.name) {
+    if (this.$state.current.name === NewProjectInitialDataState.name) {
       this.newProject.emptyProjectDesired = true;
     }
 
@@ -258,7 +191,6 @@ export class LexiconNewProjectController implements angular.IController {
     });
   }
 
-  // Form validation requires API calls, so it return a promise rather than a value.
   validateForm = (): angular.IPromise<boolean> => {
     this.formValidationDefer = this.$q.defer();
     return this.$state.current.data.isFormValid(this);
@@ -276,13 +208,6 @@ export class LexiconNewProjectController implements angular.IController {
   }
 
   // ----- Step 1: Project name -----
-
-  private static projectNameToCode(name: string): string {
-    if (name == null) {
-      return undefined;
-    }
-    return name.toLowerCase().replace(/ /g, '_');
-  }
 
   checkProjectCode(): angular.IPromise<string> {
     this.projectCodeStateDefer = this.$q.defer();
@@ -331,134 +256,33 @@ export class LexiconNewProjectController implements angular.IController {
     });
   }
 
-  // ----- Step 2: Initial data upload -----
-
-  uploadFile(file: UploadFile): void {
-    if (!file || file.$error) {
-      return;
-    }
-
-    this.sessionService.getSession().then(session => {
-      if (file.size > session.fileSizeMax()) {
-        this.notice.push(this.notice.ERROR, '<b>' + file.name + '</b> (' +
-          this.$filter<BytesFilterFunction>('bytes')(file.size) + ') is too large. It must be smaller than ' +
-          this.$filter<BytesFilterFunction>('bytes')(session.fileSizeMax()) + '.');
-        return;
-      }
-
-      this.notice.setLoading('Importing ' + file.name + '...');
-      this.Upload.upload({
-        url: '/upload/lf-lexicon/import-zip',
-        data: { file }
-      }).then((response: UploadResponse) => {
-          this.notice.cancelLoading();
-          const isUploadSuccess = response.data.result;
-          if (isUploadSuccess) {
-            this.notice.push(this.notice.SUCCESS, 'Successfully imported ' +
-              file.name);
-            this.newProject.entriesImported = response.data.data.stats.importEntries;
-            this.newProject.importErrors = response.data.data.importErrors;
-            this.gotoNextState();
-          } else {
-            this.newProject.entriesImported = 0;
-            this.notice.push(this.notice.ERROR, response.data.data.errorMessage);
-          }
-        },
-
-        (response: UploadResponse) => {
-          this.notice.cancelLoading();
-          let errorMessage = 'Import failed.';
-          if (response.status > 0) {
-            errorMessage += ' Status: ' + response.status;
-            if (response.statusText) {
-              errorMessage += ' ' + response.statusText;
-            }
-
-            if (response.data) {
-              errorMessage += '- ' + response.data;
-            }
-          }
-
-          this.notice.push(this.notice.ERROR, errorMessage);
-        },
-
-        (evt: ProgressEvent) => {
-          this.notice.setPercentComplete(Math.floor(100.0 * evt.loaded / evt.total));
-        });
-    });
-  }
-
-  hasImportErrors(): boolean {
-    return this.newProject.importErrors !== '';
-  }
-
-  showImportErrorsButtonLabel(): string {
-    if (this.show.importErrors) {
-      return 'Hide non-critical import errors';
-    }
-
-    return 'Show non-critical import errors';
-  }
-
   // ----- Step 1: Send Receive Credentials -----
 
   getProject = (): void => {
     this.sendReceiveApi.receiveProject(result => {
       if (result.ok) {
-        this.$state.go(LexiconNewProjectSendReceiveCloneState.name);
+        this.$state.go(NewProjectSendReceiveCloneState.name);
         this.sendReceive.startCloneStatusTimer();
       } else {
-        this.notice.push(this.notice.ERROR, 'The project could not be synchronized with' +
-          ' LanguageDepot.org. Please try again.');
+        this.notice.push(this.notice.ERROR, 'The project could not be synchronized with LanguageDepot.org. ' +
+          'Please try again.');
         this.gotoEditor();
       }
     });
   }
 
-  // ----- Step 3: Verify initial data -OR- select primary language -----
-
-  primaryLanguage(): string {
-    if (this.newProject.languageCode) {
-      return this.newProject.language.name + ' (' + this.newProject.languageCode + ')';
-    }
-
-    return '';
-  }
-
-  openNewLanguageModal(): void {
-    const modalInstance = this.$modal.open({
-      templateUrl: '/angular-app/languageforge/lexicon/shared/select-new-language.modal.html',
-      controller: ['$scope', '$uibModalInstance',
-        ($scope: any, $modalInstance: angular.ui.bootstrap.IModalInstanceService) => {
-          $scope.selected = {
-            code: '',
-            language: {}
-          };
-          $scope.add = () => {
-            $modalInstance.close($scope.selected);
-          };
-
-          $scope.close = $modalInstance.dismiss;
-        }
-      ],
-      windowTopClass: 'modal-select-language'
-    });
-    modalInstance.result.then(selected => {
-      this.newProject.languageCode = selected.code;
-      this.newProject.language = selected.language;
-    }, () => {});
-  }
+  // ----- Step 3: Select primary language -----
 
   savePrimaryLanguage(callback?: () => void): void {
     let config: LexiconConfig = new LexiconConfig();
-    let optionlist: LexOptionList[] = [];
+    let optionLists: LexOptionList[] = [];
     const inputSystem: InputSystem = new InputSystem();
     this.notice.setLoading('Configuring project for first use...');
     this.sessionService.getSession().then(session => {
       const projectSettings = session.projectSettings<LexiconProjectSettings>();
       if (projectSettings != null) {
         config = projectSettings.config;
-        optionlist = projectSettings.optionlists;
+        optionLists = projectSettings.optionlists;
       }
 
       inputSystem.abbreviation = this.newProject.languageCode;
@@ -470,7 +294,7 @@ export class LexiconNewProjectController implements angular.IController {
         this.replaceFieldInputSystem(config.entry, 'th', this.newProject.languageCode);
       }
 
-      this.lexProjectService.updateConfiguration(config, optionlist, result => {
+      this.lexProjectService.updateConfiguration(config, optionLists, result => {
         this.notice.cancelLoading();
         if (result.ok) {
           if (callback) {
@@ -506,3 +330,16 @@ export class LexiconNewProjectController implements angular.IController {
   }
 
 }
+
+export const LexiconNewProjectComponent: angular.IComponentOptions = {
+  bindings: {
+  },
+  controller: LexiconNewProjectController,
+  templateUrl: '/angular-app/languageforge/lexicon/new-project/lexicon-new-project.component.html'
+};
+
+export const NewProjectAbstractState = {
+  name: 'newProject',
+  abstract: true,
+  template: `<lexicon-new-project></lexicon-new-project>`
+} as angular.ui.IState;
--- src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.module.ts
+++ src/angular-app/languageforge/lexicon/new-project/lexicon-new-project.module.ts
@@ -7,14 +7,24 @@ import {MockModule} from '../../../bellows/shared/mock.module';
 import {SelectLanguageModule} from '../../../bellows/shared/select-language.component';
 import {PuiUtilityModule} from '../../../bellows/shared/utils/pui-utils.module';
 import {LexiconCoreModule} from '../core/lexicon-core.module';
+import {LexiconNewProjectComponent, NewProjectAbstractState} from './lexicon-new-project.component';
+import {NewProjectChooserComponent, NewProjectChooserState} from './new-project-chooser.component';
 import {
-  LexiconNewProjectAbstractState,
-  LexiconNewProjectChooserState, LexiconNewProjectInitialDataState,
-  LexiconNewProjectNameState, LexiconNewProjectSelectPrimaryLanguageState, LexiconNewProjectSendReceiveCloneState,
-  LexiconNewProjectSendReceiveCredentialsState, LexiconNewProjectVerifyDataState
-} from './lexicon-new-project-state.model';
-import {LexiconNewProjectController} from './lexicon-new-project.component';
-import {SendReceiveCredentialsComponent} from './send-receive/new-project-credentials.component';
+  NewProjectInitialDataComponent, NewProjectInitialDataState
+} from './non-send-receive/new-project-initial-data.component';
+import {NewProjectNameComponent, NewProjectNameState} from './non-send-receive/new-project-name.controller';
+import {
+  NewProjectSelectPrimaryLanguageComponent, NewProjectSelectPrimaryLanguageState
+} from './non-send-receive/new-project-select-primary-language.component';
+import {
+  NewProjectVerifyDataComponent, NewProjectVerifyDataState
+} from './non-send-receive/new-project-verify-data.component';
+import {
+  NewProjectSendReceiveCloneComponent, NewProjectSendReceiveCloneState
+} from './send-receive/new-project-clone.component';
+import {
+  NewProjectSendReceiveCredentialsComponent, NewProjectSendReceiveCredentialsState
+} from './send-receive/new-project-credentials.component';
 
 export const LexiconNewProjectModule = angular
   .module('lexicon-new-project', [
@@ -28,26 +38,34 @@ export const LexiconNewProjectModule = angular
     MockModule,
     LexiconCoreModule
   ])
-  .component('sendReceiveCredentials', SendReceiveCredentialsComponent)
-  .controller('NewLexiconProjectCtrl', LexiconNewProjectController)
+  .component('lexiconNewProject', LexiconNewProjectComponent)
+  .component('newProjectChooser', NewProjectChooserComponent)
+  .component('newProjectSendReceiveCredentials', NewProjectSendReceiveCredentialsComponent)
+  .component('newProjectSendReceiveClone', NewProjectSendReceiveCloneComponent)
+  .component('newProjectName', NewProjectNameComponent)
+  .component('newProjectInitialData', NewProjectInitialDataComponent)
+  .component('newProjectVerifyData', NewProjectVerifyDataComponent)
+  .component('newProjectSelectPrimaryLanguage', NewProjectSelectPrimaryLanguageComponent)
   .config(['$stateProvider', '$urlRouterProvider',
     ($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) => {
       // State machine from ui.router
       $stateProvider
-        .state(LexiconNewProjectAbstractState)
-        .state(LexiconNewProjectChooserState)
-        .state(LexiconNewProjectNameState)
-        .state(LexiconNewProjectSendReceiveCredentialsState)
-        .state(LexiconNewProjectInitialDataState)
-        .state(LexiconNewProjectSendReceiveCloneState)
-        .state(LexiconNewProjectVerifyDataState)
-        .state(LexiconNewProjectSelectPrimaryLanguageState)
+        .state(NewProjectAbstractState)
+        .state(NewProjectChooserState)
+
+        .state(NewProjectSendReceiveCredentialsState)
+        .state(NewProjectSendReceiveCloneState)
+
+        .state(NewProjectNameState)
+        .state(NewProjectInitialDataState)
+        .state(NewProjectVerifyDataState)
+        .state(NewProjectSelectPrimaryLanguageState)
         ;
 
       $urlRouterProvider
         .when('', ['$state', ($state: angular.ui.IStateService | any) => {
           if (!$state.$current.navigable) {
-            $state.go(LexiconNewProjectChooserState.name);
+            $state.go(NewProjectChooserState.name);
           }
         }]);
 
