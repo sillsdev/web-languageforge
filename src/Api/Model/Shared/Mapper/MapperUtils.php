87b43eeb25f4d1fe41c7fa39d54ca0c661143687
Robin Munn <rmunn@pobox.com>
2018-07-16 17:49:31 +0700

    Remove no-longer needed function
    The detectMoved function in MapperUtils turned out to be less useful
    than the calculateChanges function, so we'll just keep the latter.

--- src/Api/Model/Shared/Mapper/MapperUtils.php
+++ src/Api/Model/Shared/Mapper/MapperUtils.php
@@ -19,60 +19,6 @@ class MapperUtils
         MongoStore::dropAllCollections($databaseName);
     }
 
-    /** Figure out if subdocuments have moved
-     * Structure expected for oldData and newData: array of documents that have a unique key
-     * Returns a keyed array (dict) with keys being ID values, and values being ["oldPos" => int, "newPos" => int].
-     * Only the key is examined; the other parts of the data are not compared
-     * E.g., if the input is as follows:
-     * $oldData = [ ["id" => "abc", "data" => "foo"], ["id" => "def", "data" => "bar"] ]
-     * $newData = [ ["id" => "def", "data" => "bar"], ["id" => "abc", "data" => "new foo"] ]
-     * $key = "id"
-     * then the result will be:
-     * [ "abc" => [ "oldPos" => 0, "newPos" => 1 ], "def" => [ "oldPos" => 1, "newPos" => 0 ] ]
-     * If any ID has appeared or disappeared, the oldPos or newPos for that ID will be null.
-     * E.g., ["oldPos" => 0, "newPos" => null] means that the item was deleted.
-     *
-     * WARNING: if keys are duplicated, this function's behavior will be undefined
-     *
-     * @param array $oldData
-     * @param array $newData
-     * @param string $keyName
-     * @return array
-     */
-    public static function detectMoved($oldData, $newData, $keyName)
-    {
-        $oldPositionsById = [];
-        $newPositionsById = [];
-        foreach ($oldData as $pos => $value) {
-            if (is_array($value) && array_key_exists($keyName, $value)) {
-                $oldPositionsById[$value[$keyName]] = $pos;
-            }
-        }
-        foreach ($newData as $pos => $value) {
-            if (is_array($value) && array_key_exists($keyName, $value)) {
-                $newPositionsById[$value[$keyName]] = $pos;
-            }
-        }
-
-        // Most of the time there will be no motion, so check that first
-        if ($oldPositionsById === $newPositionsById) {
-            return [];
-        }
-
-        $result = [];
-        foreach ($oldPositionsById as $id => $oldPos) {
-            $newPos = $newPositionsById[$id] ?? null;  // Without the "?? null", we'd get an error if the ID didn't exist
-            $result[$id] = ["oldPos" => $oldPos, "newPos" => $newPos];
-            // Keep track of which keys we've handled
-            unset($newPositionsById[$id]);
-        }
-        // Any remaining IDs in newData are items that were added
-        foreach ($newPositionsById as $id => $newPos) {
-            $result[$id] = ["oldPos" => null, "newPos" => $newPos];
-        }
-        return $result;
-    }
-
     // Returns an array with three keys:
     // [ 'added' => [ 'new1', 'new2' ], 'removed' => [ 'old1', 'old2' ], 'moved' => [ 0 => 1, 1 => 0 ] ]
     // The indices in "moved" are calculated after removing all the old keys, and then adding all the new keys *at the end*
