2a7c26f9cd547285d76bffc8cc3bb25588fc02a5
Ira Hopkinson <ira_hopkinson@sil.org>
2018-07-18 18:41:34 +0700

    refactor to Angular components
    Former-commit-id: 10266de9cef4b802257d2d1c86abb2ecb2afbc16

--- src/angular-app/bellows/shared/pick-list-editor.module.ts
+++ src/angular-app/bellows/shared/pick-list-editor.module.ts
@@ -1,85 +1,64 @@
 import * as angular from 'angular';
+import 'ng-drag-to-reorder';
 
-export function PickListEditor(): angular.IDirective {
-  return {
-    restrict: 'E',
-    templateUrl: '/angular-app/bellows/shared/pick-list-editor.component.html',
-    scope: {
-      items: '=',
-      defaultKey: '=?',
-      pristineItems: '=?'
-    },
-    controller: ['$scope', ($scope: any) => {
-      $scope.defaultKeyFunc = function defaultKeyFunc(value: string): string {
-        return value.replace(/ /gi, '_');
-      };
-
-      $scope.showDefault = angular.isDefined($scope.defaultKey);
-
-      $scope.deletableIndexes = [] as number[];
-
-      $scope.pickAddItem = function pickAddItem() {
-        if ($scope.newValue) {
-          const keyFunc = $scope.keyFunc || $scope.defaultKeyFunc;
-          const key = keyFunc($scope.newValue);
-          $scope.items.push({ key, value: $scope.newValue });
-          $scope.deletableIndexes.push($scope.items.length - 1);
-          $scope.newValue = undefined;
-        }
-      };
-
-      $scope.pickRemoveItem = function pickRemoveItem(index: number): void {
-        // Remove index from deletableIndexes, shift all indexes *after* it up by 1
-        $scope.deletableIndexes = $scope.deletableIndexes.map((i: number) => {
-          if (i === index) return -1;
-          else if (i < index) return i;
-          else return i - 1;
-        }).filter((i: number) => i !== -1);
-
-        $scope.items.splice(index, 1);
-      };
-
-      // only unsaved items can be removed
-      // TODO: implement search and replace to allow remove on any item. IJH 2015-03
-      $scope.showRemove = function showRemove(index: number) {
-        return ($scope.deletableIndexes.indexOf(index) !== -1);
-      };
-
-      $scope.onSort = function onSort(indexFrom: number, indexTo: number): void {
-        if (indexFrom === indexTo) {
-          return; // Nothing to do
-        }
-
-        // Ensure deletableIndexes stays up-to-date with the reordered items
-        const lo = Math.min(indexFrom, indexTo);
-        const hi = Math.max(indexFrom, indexTo);
-        $scope.deletableIndexes = $scope.deletableIndexes.map((i: number) => {
-          // Items before or after the rearranged block don't need to be touched
-          if ((i < lo) || (i > hi)) {
-            return i;
-          } else if (i === indexFrom) {
-            return indexTo;
-          } else {
-            if (indexFrom > indexTo) {
-              // Something moved back, other items shift forward
-              return i + 1;
-            } else {
-              // Something moved forward, other items shift back
-              return i - 1;
-            }
-          }
-        });
-      };
-
-      $scope.blur = function blur(elem: HTMLElement): void {
-        elem.blur();
-      };
-    }]
-  };
+interface Item {
+  key: string;
+  value: string;
 }
+
+export class PickListEditorController implements angular.IController {
+  items: Item[];
+  defaultKey: string;
+
+  showDefault: boolean = this.defaultKey != null;
+  newValue: string;
+
+  private isDeletable: { [key: string]: boolean } = {};
+
+  pickAddItem(): void {
+    if (this.newValue) {
+      const key = PickListEditorController.keyFromValue(this.newValue);
+      this.items.push({ key, value: this.newValue });
+      this.isDeletable[key] = true;
+      this.newValue = undefined;
+    }
+  }
+
+  pickRemoveItem(index: number): void {
+    delete this.isDeletable[this.items[index].key];
+    this.items.splice(index, 1);
+  }
+
+  // only unsaved items can be removed
+  // TODO: implement search and replace to allow remove on any item. IJH 2015-03
+  showRemove(index: number): boolean {
+    const key = this.items[index].key;
+    return key in this.isDeletable && this.isDeletable[key];
+  }
+
+  // noinspection JSMethodCanBeStatic
+  blur(elem: HTMLElement): void {
+    elem.blur();
+  }
+
+  static keyFromValue(value: string): string {
+    return value.replace(/ /gi, '_');
+  }
+
+}
+
+export const PickListEditorComponent: angular.IComponentOptions = {
+  bindings: {
+    items: '=',
+    defaultKey: '=?'
+  },
+  controller: PickListEditorController,
+  templateUrl: '/angular-app/bellows/shared/pick-list-editor.component.html'
+};
 
 export function OnEnter(): angular.IDirective {
   return {
+    restrict: 'A',
     link(scope, elem, attrs) {
       elem.bind('keydown keypress', (evt: Event) => {
         if ((evt as KeyboardEvent).which === 13) {
@@ -109,8 +88,8 @@ export function NoDirtyCheck(): angular.IDirective {
 }
 
 export const PickListEditorModule = angular
-  .module('palaso.ui.picklistEditor', ['angular-sortable-view'])
-  .directive('picklistEditor', PickListEditor)
+  .module('palaso.ui.picklistEditor', ['ngDragToReorder'])
+  .component('picklistEditor', PickListEditorComponent)
   .directive('onEnter', OnEnter)
   .directive('noDirtyCheck', NoDirtyCheck)
   .name;
