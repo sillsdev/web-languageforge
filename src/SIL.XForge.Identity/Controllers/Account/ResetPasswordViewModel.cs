09e99adb4962f6d85aa42c2cee189b2eed6a5abc
Umapraajit <uma9809@gmail.com>
2018-11-01 06:03:07 -0400

    Fix: SF-68 Reset password page should accept only 7+ character passwords and maximum limit needs to be removed

--- src/SIL.XForge.Identity/Controllers/Account/ResetPasswordViewModel.cs
+++ src/SIL.XForge.Identity/Controllers/Account/ResetPasswordViewModel.cs
@@ -5,13 +5,13 @@ namespace SIL.XForge.Identity.Controllers.Account
     public class ResetPasswordViewModel
     {
         [Required]
-        [StringLength(12, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [MinLength(7, ErrorMessage = "{0} must be at least {1} characters ")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Required]
-        [StringLength(12, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [MinLength(7, ErrorMessage = "{0} must be at least {1} characters ")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
