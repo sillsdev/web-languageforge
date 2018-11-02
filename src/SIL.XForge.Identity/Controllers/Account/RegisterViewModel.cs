bd8d1b6ce2f238826377df1b2d9d55707e9fb963
sam <sam147doss@gmail.com>
2018-11-02 18:22:07 +0530

    Fix: SF-79 "Email Address" label should not move along with password
     * Fixed the problem - moving the email label control.
     * Removed the bootstrap css and included the material lite ui design
     * Set the fields to the minimum length to 7 characters.
    * fix centered mdl layout of login, sign up, forgot password, reset password

--- src/SIL.XForge.Identity/Controllers/Account/RegisterViewModel.cs
+++ src/SIL.XForge.Identity/Controllers/Account/RegisterViewModel.cs
@@ -5,13 +5,13 @@ namespace SIL.XForge.Identity.Controllers.Account
     public class RegisterViewModel
     {
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 7)]
         [DataType(DataType.Text)]
         [Display(Name = "Full Name")]
         public string Fullname { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 7)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
