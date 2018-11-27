4879c3d855787e3fa6b76ecefc6a32e60bbceb59
Ira Hopkinson <ira_hopkinson@sil.org>
2018-11-27 19:31:31 +0700

    Fix identity routing after PR#465 (temporary fix)

--- src/SIL.XForge.Scripture/ClientApp/src/identity/identity-routing.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/identity/identity-routing.module.ts
@@ -5,14 +5,14 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 import { LogInComponent } from './log-in/log-in.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [
+export const identityRoutes: Routes = [
   { path: 'log-in', component: LogInComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(identityRoutes)],
   exports: [RouterModule]
 })
 export class IdentityRoutingModule { }
