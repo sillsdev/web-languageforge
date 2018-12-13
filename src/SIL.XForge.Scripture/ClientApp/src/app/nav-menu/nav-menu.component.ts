f7b2a85ef1228eabd29a35301f0623af174dccb9
nigel-wells <nigel-wells@hotmail.com>
2018-12-13 16:47:50 +1300

    Import Bootstrap 4 grid and mixins in to global SCSS file Updated header/footer styles to work better responsively using mixins from Bootstrap Update menu to open/close based on viewport - basic placement of hamburger menu for now Made use of SubscriptionDisposable class Fixed import of Bootstrap SCSS files

--- src/SIL.XForge.Scripture/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
 import { AuthService } from '@xforge-common/auth.service';
+import { SubscriptionDisposable } from '@xforge-common/subscription-disposable';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent extends SubscriptionDisposable {
   isExpanded = false;
+  drawerType = 'permanent';
 
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly media: ObservableMedia) {
+    super();
+    this.subscribe(media.asObservable(), (change: MediaChange) => {
+      if (['xs', 'sm'].indexOf(change.mqAlias) > -1) {
+        this.collapseDrawer();
+        this.drawerType = 'modal';
+      } else {
+        this.drawerType = 'permanent';
+        this.openDrawer();
+      }
+    });
+  }
 
   get isLoggedIn(): Promise<boolean> {
     return this.authService.isLoggedIn;
   }
 
-  collapse() {
+  collapseDrawer() {
     this.isExpanded = false;
   }
 
-  toggle() {
+  openDrawer() {
+    this.isExpanded = true;
+  }
+
+  toggleDrawer() {
     this.isExpanded = !this.isExpanded;
   }
 }
