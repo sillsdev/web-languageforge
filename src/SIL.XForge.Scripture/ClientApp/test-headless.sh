7bb87e1e67f599980e9a16fb1936ada56594448c
MarkS <marksvc@gmail.com>
2019-02-22 18:34:27 -0500

    Add test runner script

---
+++ src/SIL.XForge.Scripture/ClientApp/test-headless.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run tests in terminal.
+# Usage:
+#   src/SIL.XForge.Scripture/ClientApp/test-headless.sh
+
+ROOT_PATH="$(dirname "$0")"
+
+echo -e "\033[1;34mRunning tests\033[0m"
+date +"%F %T"
+cd "$ROOT_PATH" &&
+  time ng test --browsers ChromiumHeadless --watch false |
+    grep -v '^.#' |
+    grep -v '^Headless.*LOG' |
+    # Highlight select text, and use |$ to make otherwise not-matching output come thru.
+    grep --color -E '(.*FAILED|ts\?:[^:]*|^TOTAL.*$|^[[:blank:]]*Error:|$)'
+date +"%F %T"
