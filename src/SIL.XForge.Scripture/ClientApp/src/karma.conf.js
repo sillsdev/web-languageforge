40878874b9c305b0a4da07928ec53305396e826c
Damien Daspit <damien_daspit@sil.org>
2019-04-30 17:04:41 +0700

    Fix Angular unit tests on CI build (#683)
    - turn off source maps by default
    - upgrade Karma and Jasmine to the latest
    - set random seed on CI build

--- src/SIL.XForge.Scripture/ClientApp/src/karma.conf.js
+++ src/SIL.XForge.Scripture/ClientApp/src/karma.conf.js
@@ -17,7 +17,11 @@ module.exports = function(config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: true,
+        seed: isTC ? '12345' : null
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
