a95f338b712e8924d0ccb9bb61693fb5cf62604a
Andrew <andrewdt97@gmail.com>
2019-08-09 10:27:50 +0700

    Convert login to angular component

--- src/angular-app/bellows/apps/public/login/login-app.module.ts
+++ src/angular-app/bellows/apps/public/login/login-app.module.ts
@@ -1,11 +1,15 @@
 import * as angular from 'angular';
 
 import {CoreModule} from '../../../core/core.module';
+import {LoginAppComponent} from './login-app.component';
 
 export const LoginAppModule = angular
   .module('login', [
     'ui.bootstrap',
     CoreModule
   ])
-  .controller('LoginCtrl', () => {})
+  .value('loginPath', "pasta")
+  .value('last_username', '{{ last_username }}')
+  // .controller('LoginCtrl', () => {})
+  .component('loginApp', LoginAppComponent)
   .name;
