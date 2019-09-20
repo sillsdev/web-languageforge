33d5a69808a0c558450cdbb2b35933aa7822c99b
Nathaniel Paulus <nateowami@gmail.com>
2019-09-20 12:48:46 -0400

    Add site-wide notice about SFv2 to Scripture Forge

--- src/angular-app/bellows/core/site-wide-notice-service.ts
+++ src/angular-app/bellows/core/site-wide-notice-service.ts
@@ -1,23 +1,42 @@
 import * as angular from 'angular';
 
 import { NoticeModule } from './notice/notice.module';
-import { NoticeService } from './notice/notice.service';
+import { NoticeService, NoticeType } from './notice/notice.service';
+import { SessionService } from './session.service';
 import { UtilityService } from './utility.service';
 
+interface SiteWideNotice {
+  site: string;
+  type: NoticeType;
+  message: string;
+}
+
 export class SiteWideNoticeService {
-  static $inject: string[] = ['silNoticeService'];
-  static message: string = 'It looks like you\'re using Internet Explorer. ' +
-  'This website is not designed for Internet Explorer, and some things may not work as expected. ' +
-  'Please use a different browser (such as <a href="https://www.microsoft.com/windows/microsoft-edge">Edge</a>, ' +
-  '<a href="https://mozilla.org/firefox">Firefox</a>, or <a href="https://google.com/chrome">Chrome</a>) ' +
-  'to browse this site.';
+  static $inject: string[] = ['silNoticeService', 'sessionService', '$http'];
+  static ieMessage: string = 'It looks like you\'re using Internet Explorer. ' +
+    'This website is not designed for Internet Explorer, and some things may not work as expected. ' +
+    'Please use a different browser (such as <a href="https://www.microsoft.com/windows/microsoft-edge">Edge</a>, ' +
+    '<a href="https://mozilla.org/firefox">Firefox</a>, or <a href="https://google.com/chrome">Chrome</a>) ' +
+    'to browse this site.';
 
-  constructor(private noticeService: NoticeService) { }
+  constructor(
+    private noticeService: NoticeService,
+    private sessionService: SessionService,
+    private $http: angular.IHttpService
+  ) { }
 
-  displayNotices() {
+  async displayNotices() {
     if (UtilityService.isIE(window.navigator.userAgent)) {
-      this.noticeService.push(this.noticeService.ERROR, SiteWideNoticeService.message);
+      this.noticeService.push(this.noticeService.ERROR, SiteWideNoticeService.ieMessage);
     }
+
+    const site = (await this.sessionService.getSession()).baseSite();
+    // using await instead of then seems to prevent the digest cycle from running
+    this.$http.get('/site_wide_notices.json').then(notices => {
+      for (const notice of notices.data as SiteWideNotice[]) {
+        if (notice.site === site) this.noticeService.push(() => notice.type, notice.message);
+      }
+    });
   }
 }
 
