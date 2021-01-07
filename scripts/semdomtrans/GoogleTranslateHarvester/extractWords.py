d9ae8e98c94b3b10d45a3bb232c4e36f782cfece
Chris Hirt <chris@hirtfamily.net>
2021-01-07 14:53:15 +0700

    remove semdomtrans app

--- scripts/semdomtrans/GoogleTranslateHarvester/extractWords.py
+++
@@ -1,4 +0,0 @@
-from lxml import etree
-tree = etree.parse('../../../src/resources/languageforge/semdomtrans/SemDom_en.xml')
-notags = etree.tostring(tree, encoding='utf8', method='text')
-print(notags)
