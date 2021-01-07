66cbd8f68b04781df179a7d8c92b9d2784f16896
Chris Hirt <chris@hirtfamily.net>
2021-01-07 16:03:50 +0700

    remove scripture forge references in various tests

--- test/app/setupTestEnvironment.php
+++ test/app/setupTestEnvironment.php
@@ -5,10 +5,6 @@ require_once('e2eTestConfig.php');
 // use commands go here (after the e2eTestConfig)
 use Api\Library\Shared\Website;
 use Api\Model\Languageforge\Lexicon\LexRoles;
-use Api\Model\Scriptureforge\Sfchecks\Command\TextCommands;
-use Api\Model\Scriptureforge\Sfchecks\Command\QuestionCommands;
-use Api\Model\Scriptureforge\Sfchecks\Command\QuestionTemplateCommands;
-use Api\Model\Scriptureforge\SfProjectModel;
 use Api\Model\Languageforge\Lexicon\Command\LexEntryCommands;
 use Api\Model\Languageforge\Lexicon\Command\LexUploadCommands;
 use Api\Model\Languageforge\Lexicon\Config\LexConfig;
@@ -203,11 +199,7 @@ $resetUser->resetPasswordExpirationDate = $today->add(new DateInterval('P5D'));
 $resetUser->write();
 
 $projectType = null;
-if ($site == 'scriptureforge') {
-    $projectType = SfProjectModel::SFCHECKS_APP;
-} else if ($site == 'languageforge') {
-    $projectType = LfProjectModel::LEXICON_APP;
-}
+$projectType = LfProjectModel::LEXICON_APP;
 $testProjectId = ProjectCommands::createProject(
     $constants['testProjectName'],
     $constants['testProjectCode'],
@@ -267,162 +259,90 @@ ProjectCommands::updateUserRole($otherProjectId, $adminUserId, ProjectRoles::MAN
 ProjectCommands::updateUserRole($fourthProjectId, $adminUserId, ProjectRoles::MANAGER);
 ProjectCommands::updateUserRole($srTestProjectId, $adminUserId, ProjectRoles::MANAGER);
 
-if ($site == 'scriptureforge') {
-    $text1 = TextCommands::updateText($testProjectId, [
-        'id' => '',
-        'title' => $constants['testText1Title'],
-        'content' => $constants['testText1Content']
-    ], $adminUserId);
-    $text2 = TextCommands::updateText($testProjectId, [
-        'id' => '',
-        'title' => $constants['testText2Title'],
-        'content' => $constants['testText2Content']
-    ], $adminUserId);
+// Set up LanguageForge E2E test envrionment here
+ProjectCommands::updateUserRole($testProjectId, $observerUserId, LexRoles::OBSERVER);
+$testProjectModel = new LexProjectModel($testProjectId);
+$testProjectModel->addInputSystem('th-fonipa', 'tipa', 'Thai');
+$testProjectModel->config->entry->fields[LexConfig::LEXEME]->inputSystems[] = 'th-fonipa';
+$testProjectModel->addInputSystem('th-Zxxx-x-audio', 'taud', 'Thai Voice');
+$testProjectModel->config->entry->fields[LexConfig::LEXEME]->inputSystems[] = 'th-Zxxx-x-audio';
+$testProjectId = $testProjectModel->write();
 
-    $question1 = QuestionCommands::updateQuestion($testProjectId, [
-        'id' => '',
-        'textRef' => $text1,
-        'title' => $constants['testText1Question1Title'],
-        'description' => $constants['testText1Question1Content']
-    ], $adminUserId);
-    $question2 = QuestionCommands::updateQuestion($testProjectId, [
-        'id' => '',
-        'textRef' => $text1,
-        'title' => $constants['testText1Question2Title'],
-        'description' => $constants['testText1Question2Content']
-    ], $adminUserId);
+// setup to mimic file upload
+$fileName = $constants['testEntry1']['lexeme']['th-Zxxx-x-audio']['value'];
+$file = [];
+$file['name'] = $fileName;
+$_FILES['file'] = $file;
 
-    $template1 = QuestionTemplateCommands::updateTemplate($testProjectId, [
-        'id' => '',
-        'title' => 'first template',
-        'description' => 'not particularly interesting'
-    ]);
+// put a copy of the test file in tmp
+$tmpFilePath = sys_get_temp_dir() . "/CopyOf$fileName";
+copy(TestPath . "common/$fileName", $tmpFilePath);
 
-    $template2 = QuestionTemplateCommands::updateTemplate($testProjectId, [
-        'id' => '',
-        'title' => 'second template',
-        'description' => 'not entirely interesting'
-    ]);
+$response = LexUploadCommands::uploadAudioFile($testProjectId, 'audio', $tmpFilePath);
 
-    $answer1 = QuestionCommands::updateAnswer($testProjectId, $question1, [
-        'id' => '',
-        'content' => $constants['testText1Question1Answer']
-    ],
-        $managerUserId);
-    $answer1Id = array_keys($answer1)[0];
-    $answer2 = QuestionCommands::updateAnswer($testProjectId, $question2, [
-        'id' => '',
-        'content' => $constants['testText1Question2Answer']
-    ],
-        $managerUserId);
-    $answer2Id = array_keys($answer2)[0];
-
-    $comment1 = QuestionCommands::updateComment($testProjectId, $question1, $answer1Id, [
-        'id' => '',
-        'content' => $constants['testText1Question1Answer1Comment']
-    ],
-        $managerUserId);
-    $comment2 = QuestionCommands::updateComment($testProjectId, $question2, $answer2Id, [
-        'id' => '',
-        'content' => $constants['testText1Question2Answer2Comment']
-    ],
-        $managerUserId);
-} elseif ($site == 'languageforge') {
-    // Set up LanguageForge E2E test envrionment here
-    ProjectCommands::updateUserRole($testProjectId, $observerUserId, LexRoles::OBSERVER);
-    $testProjectModel = new LexProjectModel($testProjectId);
-    $testProjectModel->addInputSystem('th-fonipa', 'tipa', 'Thai');
-    $testProjectModel->config->entry->fields[LexConfig::LEXEME]->inputSystems[] = 'th-fonipa';
-    $testProjectModel->addInputSystem('th-Zxxx-x-audio', 'taud', 'Thai Voice');
-    $testProjectModel->config->entry->fields[LexConfig::LEXEME]->inputSystems[] = 'th-Zxxx-x-audio';
-    $testProjectId = $testProjectModel->write();
-
-    // setup to mimic file upload
-    $fileName = $constants['testEntry1']['lexeme']['th-Zxxx-x-audio']['value'];
-    $file = [];
-    $file['name'] = $fileName;
-    $_FILES['file'] = $file;
-
-    // put a copy of the test file in tmp
-    $tmpFilePath = sys_get_temp_dir() . "/CopyOf$fileName";
-    copy(TestPath . "common/$fileName", $tmpFilePath);
-
-    $response = LexUploadCommands::uploadAudioFile($testProjectId, 'audio', $tmpFilePath);
-
-    // cleanup tmp file if it still exists
-    if (file_exists($tmpFilePath) && ! is_dir($tmpFilePath)) {
-        @unlink($tmpFilePath);
-    }
-
-    // put uploaded file into entry1
-    $constants['testEntry1']['lexeme']['th-Zxxx-x-audio']['value'] = $response->data->fileName;
-
-    // setup to mimic file upload
-    $fileName = $constants['testEntry1']['senses'][0]['pictures'][0]['fileName'];
-    $file = [];
-    $file['name'] = $fileName;
-    $_FILES['file'] = $file;
-
-    // put a copy of the test file in tmp
-    $tmpFilePath = sys_get_temp_dir() . "/CopyOf$fileName";
-    copy(TestPath . "common/$fileName", $tmpFilePath);
-
-    $response = LexUploadCommands::uploadImageFile($testProjectId, 'sense-image', $tmpFilePath);
-
-    // cleanup tmp file if it still exists
-    if (file_exists($tmpFilePath) && ! is_dir($tmpFilePath)) {
-        @unlink($tmpFilePath);
-    }
-
-    // put uploaded file into entry1
-    $constants['testEntry1']['senses'][0]['pictures'][0]['fileName'] = $response->data->fileName;
-
-    $entry1 = LexEntryCommands::updateEntry($testProjectId,
-        [
-            'id' => '',
-            'lexeme' => $constants['testEntry1']['lexeme'],
-            'senses' => $constants['testEntry1']['senses']
-        ], $managerUserId);
-    $entry2 = LexEntryCommands::updateEntry($testProjectId,
-        [
-            'id' => '',
-            'lexeme' => $constants['testEntry2']['lexeme'],
-            'senses' => $constants['testEntry2']['senses']
-        ], $managerUserId);
-    $multipleMeaningEntry1 = LexEntryCommands::updateEntry($testProjectId,
-        [
-            'id' => '',
-            'lexeme' => $constants['testMultipleMeaningEntry1']['lexeme'],
-            'senses' => $constants['testMultipleMeaningEntry1']['senses']
-        ], $managerUserId);
-
-    // put mock uploaded zip import (jpg file)
-    $fileName = $constants['testMockJpgImportFile']['name'];
-    $tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
-    copy(TestPath . "common/$fileName", $tmpFilePath);
-
-    // put mock uploaded zip import (zip file)
-    $fileName = $constants['testMockZipImportFile']['name'];
-    $tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
-    copy(TestPath . "common/$fileName", $tmpFilePath);
-
-    // put mock uploaded audio (png file)
-    $fileName = $constants['testMockPngUploadFile']['name'];
-    $tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
-    copy(TestPath . "common/$fileName", $tmpFilePath);
-
-    // put mock uploaded audio (mp3 file)
-    $fileName = $constants['testMockMp3UploadFile']['name'];
-    $tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
-    copy(TestPath . "common/$fileName", $tmpFilePath);
+// cleanup tmp file if it still exists
+if (file_exists($tmpFilePath) && ! is_dir($tmpFilePath)) {
+    @unlink($tmpFilePath);
 }
 
-if ($website->domain == 'jamaicanpsalms.scriptureforge.localhost') {
-    $jpProject = ProjectCommands::createProject(
-        "The Jamaican Psalms Project",
-        "jamaican_psalms",
-        SfProjectModel::SFCHECKS_APP,
-        $adminUserId,
-        $website
-    );
+// put uploaded file into entry1
+$constants['testEntry1']['lexeme']['th-Zxxx-x-audio']['value'] = $response->data->fileName;
+
+// setup to mimic file upload
+$fileName = $constants['testEntry1']['senses'][0]['pictures'][0]['fileName'];
+$file = [];
+$file['name'] = $fileName;
+$_FILES['file'] = $file;
+
+// put a copy of the test file in tmp
+$tmpFilePath = sys_get_temp_dir() . "/CopyOf$fileName";
+copy(TestPath . "common/$fileName", $tmpFilePath);
+
+$response = LexUploadCommands::uploadImageFile($testProjectId, 'sense-image', $tmpFilePath);
+
+// cleanup tmp file if it still exists
+if (file_exists($tmpFilePath) && ! is_dir($tmpFilePath)) {
+    @unlink($tmpFilePath);
 }
+
+// put uploaded file into entry1
+$constants['testEntry1']['senses'][0]['pictures'][0]['fileName'] = $response->data->fileName;
+
+$entry1 = LexEntryCommands::updateEntry($testProjectId,
+    [
+        'id' => '',
+        'lexeme' => $constants['testEntry1']['lexeme'],
+        'senses' => $constants['testEntry1']['senses']
+    ], $managerUserId);
+$entry2 = LexEntryCommands::updateEntry($testProjectId,
+    [
+        'id' => '',
+        'lexeme' => $constants['testEntry2']['lexeme'],
+        'senses' => $constants['testEntry2']['senses']
+    ], $managerUserId);
+$multipleMeaningEntry1 = LexEntryCommands::updateEntry($testProjectId,
+    [
+        'id' => '',
+        'lexeme' => $constants['testMultipleMeaningEntry1']['lexeme'],
+        'senses' => $constants['testMultipleMeaningEntry1']['senses']
+    ], $managerUserId);
+
+// put mock uploaded zip import (jpg file)
+$fileName = $constants['testMockJpgImportFile']['name'];
+$tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+copy(TestPath . "common/$fileName", $tmpFilePath);
+
+// put mock uploaded zip import (zip file)
+$fileName = $constants['testMockZipImportFile']['name'];
+$tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+copy(TestPath . "common/$fileName", $tmpFilePath);
+
+// put mock uploaded audio (png file)
+$fileName = $constants['testMockPngUploadFile']['name'];
+$tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+copy(TestPath . "common/$fileName", $tmpFilePath);
+
+// put mock uploaded audio (mp3 file)
+$fileName = $constants['testMockMp3UploadFile']['name'];
+$tmpFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+copy(TestPath . "common/$fileName", $tmpFilePath);
--- test/php/library/shared/WebsiteTest.php
+++ test/php/library/shared/WebsiteTest.php
@@ -7,9 +7,9 @@ class WebsiteTest extends TestCase
 {
     public function testGet_Works()
     {
-        $website = Website::get('scriptureforge.org');
-        $this->assertEquals('scriptureforge.org', $website->domain);
-        $this->assertEquals('scriptureforge', $website->base);
+        $website = Website::get('languageforge.org');
+        $this->assertEquals('languageforge.org', $website->domain);
+        $this->assertEquals('languageforge', $website->base);
         $this->assertEquals('default', $website->theme);
 
         $website = Website::get('randomdomain.com');
@@ -21,25 +21,10 @@ class WebsiteTest extends TestCase
         $redirect = Website::getRedirect('randomdomain.com');
         $this->assertEquals('', $redirect);
 
-        $redirect = Website::getRedirect('scriptureforge.org');
+        $redirect = Website::getRedirect('languageforge.org');
         $this->assertEquals('', $redirect);
 
-        $redirect = Website::getRedirect('www.scriptureforge.org');
-        $this->assertEquals('https://scriptureforge.org', $redirect);
-
-        $redirect = Website::getRedirect('jamaicanpsalms.org');
-        $this->assertEquals('https://jamaicanpsalms.scriptureforge.org', $redirect);
+        $redirect = Website::getRedirect('www.languageforge.org');
+        $this->assertEquals('https://languageforge.org', $redirect);
     }
-
-    /* -- currently not in use so commented out cjh 2014-06
-    function testGetProjectThemeNamesForSite_Works()
-    {
-        $themeNames = Website::getProjectThemeNamesForSite('scriptureforge');
-        $this->assertEquals('default', $themeNames[0]);
-        $this->assertEquals('jamaicanpsalms', $themeNames[1]);
-
-        $themeNames = Website::getProjectThemeNamesForSite('languageforge');
-        $this->assertEquals('default', $themeNames[0]);
-    }
-    */
 }
