0c2eb0af9324f463c2a7bf90e77b978c80446d27
Robin Munn <rmunn@pobox.com>
2021-01-14 12:24:21 +0700

    Clean up unused code
    Lots of now-unused code now that I've refactored the LD project
    membership view to be shared in common. Get rid of it.

--- src/angular-app/bellows/apps/siteadmin/ldapi-projects-view.ts
+++ src/angular-app/bellows/apps/siteadmin/ldapi-projects-view.ts
@@ -1,11 +1,10 @@
 import * as angular from 'angular';
 import * as ngTable from 'ng-table';
 import {ProjectService} from '../../core/api/project.service';
-import {UserService} from '../../core/api/user.service';
-import {SendReceiveProject} from '../../../languageforge/lexicon/shared/model/lexicon-project.model';
 import { NgTableParams } from 'ng-table';
-import {RolesService} from '../../core/api/roles.service';
+import {LdapiProjectDto} from '../../../languageforge/ldproject/ldproject-app.component';
 
+// TODO: move this and LdapiProjectInfo to an ldapi-models.ts file
 export interface LdapiMembershipInfo {
   [username: string]: string;
 }
@@ -18,39 +17,20 @@ export interface LdapiProjectInfo {
 }
 
 export class LdapiProjectsController implements angular.IController {
-  projects = [{name: 'foo', identifier: 'foo'}, {name: 'bar', identifier: 'bar'}];
-  loadedProjects: LdapiProjectInfo[];
+  loadedProjects: LdapiProjectInfo[];  // TODO: Replace with LdapiProjectDto if possible. TODO: Rename to just "projects"
   tableParams: NgTableParams<LdapiProjectInfo>;
   selectedProject: LdapiProjectDto;
-  membership: [string, string][] = [];
-  memberNames: {[username: string]: string};
-  roles: string[];
-  selectedRole: {[username: string]: string};
-  pristineRoles: {[username: string]: string};
-  inProgress: {[username: string]: boolean} = {};
-  filterUsers: string;
-  users: any;
 
   static $inject = [
     'projectService',
-    'userService',
-    'rolesService',
   ];
 
-  constructor(
-    private readonly projectService: ProjectService,
-    private readonly userService: UserService,
-    private readonly rolesService: RolesService) {
-      this.rolesService.roles.then(roles => {
-        this.roles = roles.map(([roleId, roleName]) => roleName);
-      });
-  }
+  constructor(private readonly projectService: ProjectService) { }
 
   $onInit() {
     this.projectService.getAllLdapiProjects().then(result => {
       if (result.ok) {
         this.loadedProjects = result.data;
-        console.log("Loaded projects", this.loadedProjects);
         this.tableParams = new NgTableParams({}, {dataset: this.loadedProjects});
       }
     });
@@ -58,71 +38,8 @@ export class LdapiProjectsController implements angular.IController {
 
   select(project: LdapiProjectInfo) {
     this.projectService.getLdapiProjectDto(project.code).then(result => {
-      console.log("Project DTO result:", result);
       if (result.ok) {
         this.selectedProject = result.data;
-      }
-    });
-  }
-
-  lookupUserName(username: string): void {
-    this.userService.getLdapiUser(username).then(result => {
-      if (result.ok) {
-        const firstName = result.data.firstName;
-        const lastName = result.data.lastName;
-        const name = firstName && lastName ? firstName + ' ' + lastName : firstName ? firstName : lastName;
-        if (name) {
-          this.memberNames[username] = name;
-        }
-      }
-    });
-  }
-
-  areEqual(obj1:any, obj2:any) {
-    // The === operator does reference equality, and we need value equality here
-    return angular.equals(obj1, obj2);
-  }
-
-  saveMembership() {
-    console.log('Would save', this.selectedRole);
-    // promise.then(() => {this.pristineRoles = roles;})
-  }
-
-  changeRole(member: string, oldRole: string, newRole: string) {
-    if (oldRole !== newRole) {
-      this.inProgress[member] = true;
-      this.projectService.updateLdapiUserRole(this.selectedProject.code, member, newRole).then(() => this.inProgress[member] = false);
-    }
-  }
-
-  deleteFromProject(member: string) {
-    this.inProgress[member] = true;
-    this.projectService.removeUserFromLdapiProject(this.selectedProject.code, member).then(() => {
-      this.inProgress[member] = false;
-      // Remove user from UI by creating a *new* membership list (Angular defaults to tracking arrays by reference)
-      this.membership = this.membership.filter(([username, role]) => username !== member);
-    });
-  }
-
-  addToProject(member: string) {
-    // Adds user as a contributor; if other role is desired, admin can update it with dropdown after adding
-    this.rolesService.contributor.then(contributor => {
-      this.projectService.updateLdapiUserRole(this.selectedProject.code, member, contributor).then(() => {
-        this.lookupUserName(member);
-        // Add user to UI by creating a *new* membership list (Angular defaults to tracking arrays by reference)
-        this.selectedRole[member] = contributor;
-        const newMember: [string,string] = [member, contributor];
-        this.membership = [...this.membership, newMember];
-      });
-    });
-  }
-
-  queryUsers(): void {
-    this.userService.getAllLdapiUsers().then(result => {
-      if (result.ok) {
-        this.users = result.data;
-      } else {
-        this.users = [];
       }
     });
   }
--- src/angular-app/languageforge/ldproject/ldproject-app.component.ts
+++ src/angular-app/languageforge/ldproject/ldproject-app.component.ts
@@ -1,20 +1,10 @@
 import * as angular from 'angular';
 
-import {SiteWideNoticeService} from '../../bellows/core/site-wide-notice-service';
-import {HelpHeroService} from '../../bellows/core/helphero.service';
-import {NoticeService} from '../../bellows/core/notice/notice.service';
-import {InterfaceConfig} from '../../bellows/shared/model/interface-config.model';
-import {User} from '../../bellows/shared/model/user.model';
 import { ProjectService } from '../../bellows/core/api/project.service';
 import { Session, SessionService } from '../../bellows/core/session.service';
-import { LdapiProjectInfo } from '../../bellows/apps/siteadmin/ldapi-projects-view';
-import { UserService } from '../../bellows/core/api/user.service';
 import { LdapiUserInfo } from '../../bellows/apps/siteadmin/ldapi-users-view';
 
-// TODO: siteadmin LDAPI projects component is very similar, only differing in a few things like
-// "Can we search for users" and "how do we add users: directly, or via an invite email"?
-// But also, the "Invite a friend" dialog is very similar, too.
-
+// TODO: Move to a models file so siteadmin app can import from there instead of from here
 export interface LdapiProjectDto {
   code: string;
   description: string;
@@ -23,34 +13,21 @@ export interface LdapiProjectDto {
 }
 
 export class LdProjectAppController implements angular.IController {
-  finishedLoading: boolean = false;
-  interfaceConfig: InterfaceConfig = {} as InterfaceConfig;
-  users: { [userId: string]: User } = {};
   project: LdapiProjectDto = undefined;
   session: angular.IPromise<Session>;
   isAdmin: boolean = false;
   projectId: string = "";
-  membership: [LdapiUserInfo, string][] = [];
 
-  private online: boolean;
-  private pristineLanguageCode: string;
-
-  static $inject = ['$scope', '$location',
-    '$q',
-    'silNoticeService',
+  static $inject = [
+    '$location',
     'projectService',
-    'userService',
     'sessionService',
-    'siteWideNoticeService',
-    ];
-  constructor(private readonly $scope: angular.IScope, private readonly $location: angular.ILocationService,
-              private readonly $q: angular.IQService,
-              private readonly notice: NoticeService,
-              private readonly projectService: ProjectService,
-              private readonly userService: UserService,
-              private readonly sessionService: SessionService,
-              private readonly siteWideNoticeService: SiteWideNoticeService,
-              ) { }
+  ];
+  constructor(
+    private readonly $location: angular.ILocationService,
+    private readonly projectService: ProjectService,
+    private readonly sessionService: SessionService,
+  ) { }
 
   $onInit(): void {
     this.session = this.sessionService.getSession();
@@ -59,28 +36,15 @@ export class LdProjectAppController implements angular.IController {
         this.isAdmin = true;
       };
     });
+    // TODO find a better way to extract this; is there a path-component splitter available?
     var match = this.$location.path().match(/\/app\/ldproject\/([^\/]+)/);
-    console.log(match);
     if (match.length > 1) {
       this.projectId = match[1];
       this.projectService.getLdapiProjectDto(this.projectId).then(result => {
         if (result.ok) {
           this.project = result.data;
-          this.membership = this.project.membership;
         }
       });
-    }
-  }
-
-  $onDestroy(): void {
-  }
-
-  onUpdate = (
-    $event: {
-      foo?: string,
-    }
-  ): void => {
-    if ($event.foo) {
     }
   }
 }
