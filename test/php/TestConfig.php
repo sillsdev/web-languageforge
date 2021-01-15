0d3ba241ec4e4ed3761515836b1510b959c4e6a7
Billy Clark <billy_clark@sil.org>
2021-01-15 15:57:05 -0500

    no localhost inside a docker container.

--- test/php/TestConfig.php
+++ test/php/TestConfig.php
@@ -24,7 +24,7 @@ define('TestLibPath', $rootPath . 'test/lib/');
 define('SourcePath', $rootPath . 'src/');
 
 define('SF_DATABASE', 'scriptureforge_test');
-define('MONGODB_CONN', 'mongodb://localhost:27017');
+define('MONGODB_CONN', 'mongodb://db:27017');
 define('SF_TESTPROJECT',      'Test Project');
 define('SF_TESTPROJECTCODE',  'testcode1');
 define('SF_TESTPROJECT2',     'Test Project2');
