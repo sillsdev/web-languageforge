b03d7627159becc3587dab95de5a848fcf431599
billy clark <billy_clark@sil.org>
2021-02-25 21:29:00 -0500

    DRY wait orchestration (#839)
    * DRY on container ochestration
    
    * wasn't using wait image
    
    * wasn't using wait image
    
    * wasn't using wait image
    
    * needed a ref to config options
    
    * don't think this is needed anymore
    
    * wanted to clean up remnants of old runner location

--- docker/e2e-api/run.sh
+++ docker/e2e-api/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/wait
 cd /var/www/test/app
 php setupTestEnvironment.php localhost
 apache2-foreground
--- docker/test-api/run.sh
+++ docker/test-api/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-/wait
 cd /var/www/
 src/vendor/bin/phpunit --configuration test/php/phpunit.xml
