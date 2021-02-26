b95fd886c36074b93fd2f0032c06d57ce35b719f
Billy <billy.govols@gmail.com>
2021-02-25 20:24:55 -0500

    wanted to clean up remnants of old runner location

--- docker/e2e-ui/run.sh
+++ docker/e2e-ui/run.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-# Usage
-# rune2e.sh lf                                   : runs E2E tests for LF
-# rune2e.sh sf                                   : runs E2E tests for SF
-# rune2e.sh jp                                   : runs E2E tests for JP
-# rune2e.sh lf --specs lexicon-new-project       : runs lexicon-new-project spec for LF
-#
-# Note, make sure webdriver-manager is running
-# gulp test-e2e-webdriver_standalone
 
 attach_debugger () {
   # Searches for the Node process running Protractor tests, then signals for it to attach the debugger
@@ -23,14 +15,9 @@ attach_debugger () {
   echo "Protractor process signaled to have debugger listen."
 }
 
-# docker-compose-wait
-/wait
-
 attach_debugger &
-gulp test-e2e-clean-compile
+cd /data
 gulp test-e2e-doTest --webserverHost e2e ${@:2}
 STATUS=$?
 
-# we cannot actually do a teardown because these scripts are written in PHP
-# gulp test-e2e-teardownForLocalDev
 exit $STATUS
