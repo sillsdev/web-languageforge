2da3110e2ea4a4559eafdd9ba4008c6331b2d59d
Robin Munn <rmunn@pobox.com>
2021-03-01 17:20:47 +0700

    WIP: Fix calling /api/v2/roles in LD API
    Roles API call doesn't need authorization and shouldn't fail if there's
    no username available.

--- docker/e2e-api/run.sh
+++ docker/e2e-api/run.sh
@@ -2,4 +2,6 @@
 
 cd /var/www/test/app
 php setupTestEnvironment.php localhost
+chmod -R g+w /var/www/html/assets /var/www/html/cache
+chown -R :www-data /var/www/html/assets /var/www/html/cache
 apache2-foreground
