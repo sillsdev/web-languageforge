e9e8759a87e4436bdffed3cf39739ec4953d4328
Robin Munn <rmunn@pobox.com>
2021-03-09 13:55:24 +0700

    Fix permissions issue in E2E tests
    This fixes all remaining E2E tests except the Send/Receive tests, which
    are expected failures until we have lfmerge in the container. Since
    there are only two lfmerge Send/Receive tests in our E2E test suite,
    I've disabled those two tests until we add lfmerge. That way we'll get a
    green build, and then we can just ensure that when we add lfmerge we
    continue to get a green build.

--- docker/e2e-api/run.sh
+++ docker/e2e-api/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd /var/www/test/app
-php setupTestEnvironment.php localhost
+# Ensure test setup creates files with correct permissions, including allowing group-writable
+umask 0002
+su www-data -s /bin/sh -c 'php setupTestEnvironment.php localhost'
 apache2-foreground
