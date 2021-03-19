d3c1d410da0c91e0e452f9b47ccabd34ff5e1715
Chris Hirt <chris@hirtfamily.net>
2021-03-19 09:30:08 +0700

    first pass to remove dead wood from the gulpfile
    All the tasks left are either in use or are still useful as documentation on future production-related tasks.  Future PRs will move functionality out of gulp and into shell scripts or npm scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -19,25 +19,15 @@ require('es6-shim');
 var _execute = require('child_process').exec;
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
 var protractor = require('gulp-protractor').protractor;
-var webdriverStandalone = require('gulp-protractor').webdriver_standalone;
-var webdriverUpdate = require('gulp-protractor').webdriver_update;
-var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 var _template = require('lodash.template');
 var Server = require('karma').Server;
 var path = require('path');
-var stylish = require('jshint-stylish');
-var fs = require('fs');
 var del = require('del');
-var data = require('gulp-data');
-var ejs = require('gulp-ejs');
-var dest = require('gulp-dest');
 var webpack = require('webpack');
-
 var webpackConfig = require('./webpack.config.js');
 
 // -------------------------------------
@@ -65,39 +55,6 @@ var phpPatterns = [
   'src/Site/**/*.php',
   'test/**/*.php'
 ];
-
-// -------------------------------------
-//   Task: Lint
-// -------------------------------------
-gulp.task('lint', function () {
-  return gulp.src(srcPatterns)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
-
-// -------------------------------------
-//   Task: Auto-generate language picker asset files
-// -------------------------------------
-gulp.task('generate-language-picker-assets', function (cb) {
-  // Manual prerequisite:
-  // copy from libpalaso:palaso-trusty64-master/SIL.WritingSystems/Resources/*.txt
-  // into scripts/language picker/
-  var options = {
-    dryRun: false,
-    silent: false,
-    cwd: './scripts/language picker/'
-  };
-
-  // auto-generated files written to src/angular-app/bellows/core/input-systems/
-  execute(
-    './build-json-language-data.py',
-    options,
-    cb
-  );
-});
-
-gulp.task('generate-language-picker-assets').description =
-  'Update asset files used for language picker';
 
 // region sass
 
@@ -171,108 +128,7 @@ gulp.task('webpack-lf:analyze', function (cb) {
   runWebpack('languageforge', cb, false, true);
 });
 
-// -------------------------------------
-//   Task: webpack-sf
-// -------------------------------------
-gulp.task('webpack-sf', function (cb) {
-  runWebpack('scriptureforge', cb);
-});
-
-// -------------------------------------
-//   Task: webpack-sf watch
-// -------------------------------------
-gulp.task('webpack-sf:watch', function (cb) {
-  runWebpack('scriptureforge', cb, true);
-});
-
-// -------------------------------------
-//   Task: webpack-sf analyze
-// -------------------------------------
-gulp.task('webpack-sf:analyze', function (cb) {
-  runWebpack('scriptureforge', cb, false, true);
-});
-
 // endregion
-
-//region MongoDB
-
-// -------------------------------------
-//   Task: MongoDB: Backup Production
-// -------------------------------------
-gulp.task('mongodb-backup-prod-db', function (cb) {
-  execute(
-    'ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/backupMongoOnServer.sh',
-    null,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: MongoDB: Copy Backup to Local
-// -------------------------------------
-gulp.task('mongodb-copy-backup-to-local', function (cb) {
-  var padLeft = function (i, len, ch) {
-    var val = i.toString();
-    while (val.length < len)
-      val = ch + val;
-    return val;
-  };
-
-  var formatDateYMD = function (date) {
-    return date.getFullYear() + '-' + padLeft(date.getMonth() + 1, 2, '0') + '-' +
-        padLeft(date.getDate(), 2, '0');
-  };
-
-  var today = formatDateYMD(new Date());
-  execute(
-    'scp scriptureforge.org:mongodb_backup_' + today + '.tgz /tmp/',
-    null,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: MongoDB: Clean Backup Production
-// -------------------------------------
-gulp.task('mongodb-cleanup-backup-prod-db', function (cb) {
-  // To set the username, edit your ssh config file (~/.ssh/config) and add an entry:
-  // Host scriptureforge.org
-  //     User jdoe
-  execute(
-    'ssh scriptureforge.org \'bash -s\' < scripts/server/mongodb/deleteTarFileOnServer.sh',
-    null,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: MongoDB: Restore Mongo on Local
-// -------------------------------------
-gulp.task('mongodb-restore-local-db', function (cb) {
-  execute(
-    'scripts/server/mongodb/restoreMongoOnLocal.sh /tmp',
-    null,
-    cb
-  );
-});
-
-gulp.task('mongodb-restore-local-db').description =
-  'Restore mongodb from a local archive file';
-
-// -------------------------------------
-//   Task: MongoDB: Copy Production
-// -------------------------------------
-gulp.task('mongodb-copy-prod-db',
-  gulp.series(
-    'mongodb-backup-prod-db',
-    'mongodb-copy-backup-to-local',
-    gulp.parallel('mongodb-cleanup-backup-prod-db', 'mongodb-restore-local-db')
-  )
-);
-gulp.task('mongodb-copy-prod-db').description =
-  'Backup MongoDB on server and restore on local machine';
-
-//endregion
 
 //region Test (PHP, JS, .NET, and E2E)
 
@@ -340,159 +196,6 @@ gulp.task('test-ts-lf:watch', function (cb) {
 // -------------------------------------
 gulp.task('test-ts-lf:debug', function (cb) {
   runKarmaTests('languageforge', cb, 'debug');
-});
-
-// -------------------------------------
-//   Task: test-ts-sf
-// -------------------------------------
-gulp.task('test-ts-sf', function (cb) {
-  runKarmaTests('scriptureforge', cb);
-});
-
-// -------------------------------------
-//   Task: test-ts-sf:watch
-// -------------------------------------
-gulp.task('test-ts-sf:watch', function (cb) {
-  runKarmaTests('scriptureforge', cb, 'watch');
-});
-
-// -------------------------------------
-//   Task: test-ts-sf:debug
-// -------------------------------------
-gulp.task('test-ts-sf:debug', function (cb) {
-  runKarmaTests('scriptureforge', cb, 'debug');
-});
-
-// -------------------------------------
-//   Task: E2E Test: Webdriver Update
-// -------------------------------------
-gulp.task('test-e2e-webdriver_update', webdriverUpdate);
-
-// -------------------------------------
-//   Task: E2E Test: Webdriver Standalone
-// -------------------------------------
-gulp.task('test-e2e-webdriver_standalone', webdriverStandalone);
-
-function copy(src, dest) {
-  return gulp.src(src)
-    .pipe(rename(dest.file))
-    .pipe(gulp.dest(dest.path));
-}
-
-// -------------------------------------
-//   Task: E2E Test: Use Test Config
-// -------------------------------------
-gulp.task('test-e2e-useTestConfig', function () {
-  var src = 'src/config.php.fortest';
-  var dest = {
-    file: 'config.php',
-    path: 'src/' };
-  return copy(src, dest);
-});
-
-// -------------------------------------
-//   Task: E2E Test: Use Live Config
-// -------------------------------------
-gulp.task('test-e2e-useLiveConfig', function () {
-  var src = 'src/config.php.live';
-  var dest = {
-    file: 'config.php',
-    path: 'src/' };
-  return copy(src, dest);
-});
-
-// -------------------------------------
-//   Task: E2E Test: Setup Test Environment
-// -------------------------------------
-gulp.task('test-e2e-setupTestEnvironment', function (cb) {
-  var params = require('yargs')
-    .option('webserverHost', {
-      demand: true,
-      describe: 'hostname (without the protocol) for E2E testing',
-      type: 'string' })
-    .option('dest', {
-      demand: false,
-      describe: 'destination of test environment',
-      type: 'string' })
-    .fail(yargFailure)
-    .argv;
-  var options = {
-    dryRun: false,
-    silent: false,
-    cwd: getTestCwd(params.dest)
-  };
-  execute(
-    'sudo -u www-data php setupTestEnvironment.php ' + params.webserverHost,
-    options,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: Remote Restart PHP-FPM
-// -------------------------------------
-gulp.task('remote-restart-php-fpm', function (cb) {
-  var params = require('yargs')
-    .option('dest', {
-      demand: true,
-      type: 'string' })
-    .option('uploadCredentials', {
-      demand: true,
-      type: 'string' })
-    .fail(yargFailure)
-    .argv;
-  var options = {
-    credentials: params.uploadCredentials,
-    destination: params.dest.slice(0, params.dest.indexOf(':'))
-  };
-
-  execute(
-    "ssh -i <%= credentials %> <%= destination %> 'service php7.3-fpm restart'",
-    options,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: E2E Test: Modify Environment
-// -------------------------------------
-gulp.task('test-e2e-env', function () {
-  var params = require('yargs')
-    .option('applicationName', {
-      demand: true,
-      type: 'string' })
-    .option('dest', {
-      demand: false,
-      type: 'string' })
-    .option('webserverHost', {
-      demand: false,
-      default: 'localhost',
-      type: 'string' })
-    .fail(yargFailure)
-    .argv;
-  var cwd = getTestCwd(params.dest);
-  var src = [
-    'setupTestEnvironment.php',
-    'e2eTestConfig.php',
-    'testConstants.json'];
-
-  // noinspection RegExpRedundantEscape
-  return gulp.src(src, { cwd: cwd })
-
-    // e2eTestConfig.php
-    .pipe(replace('src/', 'htdocs/'))
-
-    // testConstants.json
-    .pipe(replace(
-      /(\s*\"siteType\"\s*:\s*\").*$/m,
-      '$1' + params.applicationName + '",'))
-    .pipe(replace(
-      /(\s*\"siteHostname\"\s*:\s*\").*$/m,
-      '$1' + params.webserverHost + '",'))
-    .pipe(replace(
-      /(\s*\"baseUrl\"\s*:\s*\").*$/m,
-      '$1http://' + params.webserverHost + '",'))
-    .pipe(gulp.dest(cwd));
 });
 
 // -------------------------------------
@@ -626,13 +329,6 @@ gulp.task('test-e2e-compile', function (cb) {
 });
 
 // -------------------------------------
-//   Task: E2E Test: Watch and Compile TS files
-// -------------------------------------
-gulp.task('test-e2e-compile:watch', function (cb) {
-  return execute('node_modules/typescript/bin/tsc -p test/app --watch', null, cb);
-});
-
-// -------------------------------------
 //   Task: E2E Test: Run
 // -------------------------------------
 gulp.task('test-e2e-clean-compile',
@@ -641,52 +337,10 @@ gulp.task('test-e2e-clean-compile',
     'test-e2e-compile')
 );
 
-// -------------------------------------
-//   Task: E2E Test: Run
-// -------------------------------------
-gulp.task('test-e2e-run',
-  gulp.series(
-    'test-e2e-clean-compile',
-    'test-e2e-useTestConfig',
-    'test-e2e-setupTestEnvironment',
-    'test-e2e-doTest')
-);
-
 //endregion
 
 //region build
 
-// -------------------------------------
-//   Task: Build Composer
-// -------------------------------------
-gulp.task('build-composer', function (cb) {
-  var options = {
-    dryRun: false,
-    silent: false,
-    cwd: './src'
-  };
-  execute(
-    'composer install',
-    options,
-    cb
-  );
-});
-
-// -------------------------------------
-//   Task: Build npm front-end
-// -------------------------------------
-gulp.task('build-npm-front-end', function (cb) {
-  var options = {
-    dryRun: false,
-    silent: false,
-    cwd: '.'
-  };
-  execute(
-    'npm install',
-    options,
-    cb
-  );
-});
 
 // -------------------------------------
 //   Task: Build Remove test fixtures (directives) in HTML only on live build
@@ -781,21 +435,6 @@ gulp.task('build-version', function () {
       '$1' + params.buildNumber + '\');'))
     .pipe(gulp.dest('src'));
 });
-
-// -------------------------------------
-//   Task: Change Group to www-data
-// -------------------------------------
-gulp.task('build-changeGroup', function (cb) {
-  execute(
-    'sudo chgrp -R www-data src; sudo chgrp -R www-data test; ' +
-    'sudo chmod -R g+w src; sudo chmod -R g+w test',
-    null,
-    cb
-  );
-});
-
-gulp.task('build-changeGroup').description =
-  'Ensure www-data is the group and can write for src and test folder';
 
 // -------------------------------------
 //   Task: Build Production Config
@@ -929,25 +568,6 @@ gulp.task('build-productionConfig', function () {
 });
 
 // -------------------------------------
-//   Task: Build Clear Local Cache
-// -------------------------------------
-gulp.task('build-clearLocalCache', function (cb) {
-  var options = {
-    dryRun: false,
-    silent: false,
-    cwd: 'src/cache/'
-  };
-  execute(
-    'git clean -d -x -f',
-    options,
-    cb
-  );
-});
-
-gulp.task('build-clearLocalCache').description =
-  'Clear all subdirectories of local cache/';
-
-// -------------------------------------
 //   Task: Build Upload to destination
 // -------------------------------------
 gulp.task('build-upload', function (cb) {
@@ -995,129 +615,45 @@ gulp.task('build-upload', function (cb) {
   }
 });
 
-// ------------------------------------------
-// Create WebsiteInstances.php from template
-// ------------------------------------------
-// gulp.task('build-createWebsiteDefsPhp', function () {
-//   return gulp.src('src/Api/Library/Shared/WebsiteInstances.ejs')
-//     .pipe(data(function () {
-//       return JSON.parse(fs.readFileSync('src/Api/Library/Shared/WebsiteInstances.json'));
-//     }))
-//     .pipe(ejs())
-//     .pipe(dest('src/Api/Library/Shared/:name.php'))
-//     .pipe(gulp.dest('src/Api/Library/Shared/'));
-// });
-
-// ------------------------------------------
-// Create website-instances.generated-data.ts from template
-// ------------------------------------------
-// gulp.task('build-createWebsiteDefsTs', function () {
-//   return gulp.src('src/angular-app/bellows/core/website-instances.ejs')
-//     .pipe(data(function () {
-//       return JSON.parse(fs.readFileSync('src/Api/Library/Shared/WebsiteInstances.json'));
-//     }))
-//     .pipe(ejs())
-//     .pipe(dest('src/angular-app/bellows/core/:name.generated-data.ts'))
-//     .pipe(gulp.dest('src/angular-app/bellows/core/'));
-// });
-
-// ------------------------------------------
-// Create files from templates
-// ------------------------------------------
-// gulp.task('build-createWebsiteDefs',
-//   gulp.parallel(
-//     'build-createWebsiteDefsPhp',
-//     'build-createWebsiteDefsTs'
-//   )
-// );
 
 // -------------------------------------
 //   Task: Build (General)
 // -------------------------------------
-gulp.task('build',
-  gulp.series(
-    gulp.parallel(
-      'build-composer',
-      'build-npm-front-end',
-      'build-version',
-      'build-productionConfig',
-      'build-clearLocalCache',
-      'build-remove-test-fixtures',
-      // 'build-createWebsiteDefs'
-    ),
-    'sass',
-    'build-webpack',
-    'build-minify',
-    'build-changeGroup'
-  )
-);
-
-// -------------------------------------
-//   Task: Build get dependencies
-// -------------------------------------
-gulp.task('get-dependencies',
-  gulp.parallel(
-    'build-composer',
-    'build-npm-front-end'
-  )
-);
-
-// -------------------------------------
-//   Task: Developer Build
-// -------------------------------------
-gulp.task('dev-build',
-  gulp.parallel(
-    'sass',
-    'test-e2e-webdriver_update',
-    'test-e2e-clean-compile',
-    'build-webpack'
-  )
-);
-
-// -------------------------------------
-//   Task: Developer Get Dependencies and Build
-// -------------------------------------
-gulp.task('dev-dependencies-and-build',
-  gulp.series(
-    'get-dependencies',
-    // 'build-createWebsiteDefs',
-    'dev-build'
-  )
-);
+// gulp.task('build',
+//   gulp.series(
+//     gulp.parallel(
+//       'build-composer',
+//       'build-npm-front-end',
+//       'build-version',
+//       'build-productionConfig',
+//       'build-clearLocalCache',
+//       'build-remove-test-fixtures',
+//       // 'build-createWebsiteDefs'
+//     ),
+//     'sass',
+//     'build-webpack',
+//     'build-minify',
+//     'build-changeGroup'
+//   )
+// );
 
 // -------------------------------------
 //   Task: Build and Upload to destination
 // -------------------------------------
-gulp.task('build-and-upload',
-  gulp.series(
-    'build',
-    'build-upload',
-    'remote-restart-php-fpm'
-  )
-);
-
-// -------------------------------------
-//   Task: Build E2E Target
-// -------------------------------------
-gulp.task('build-e2e',
-  gulp.series(
-    'test-e2e-useTestConfig',
-    'build',
-    'build-upload',
-    'test-e2e-env',
-    'test-e2e-setupTestEnvironment',
-    'test-e2e-doTest'
-  )
-);
-gulp.task('build-e2e').description =
-  'Build and Run E2E tests on CI server';
+// gulp.task('build-and-upload',
+//   gulp.series(
+//     'build',
+//     'build-upload',
+//     'remote-restart-php-fpm'
+//   )
+// );
 
 //endregion
 
 // -------------------------------------
 //   Task: default (build)
 // -------------------------------------
-gulp.task('default', gulp.series('build'));
+// gulp.task('default', gulp.series('build'));
 
 // -------------------------------------
 //   Functions
