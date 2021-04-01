92480fb4581aa7c44d947ea4c1165449f09c3d12
Christopher Hirt <chris@hirtfamily.net>
2021-04-02 00:07:50 +0700

    remove karma and filters.spec.ts test (#894)

--- src/angular-app/languageforge/main.specs.ts
+++
@@ -1,7 +0,0 @@
-import './main';
-
-import '../bellows/main.common.specs';
-
-const webpackRequire = require as any;
-const testsContext = webpackRequire.context('.', true, /\.spec$/);
-testsContext.keys().forEach(testsContext);
