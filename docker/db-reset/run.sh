8d3b48529f78e7f04826a996ec03caaa858a5978
Billy <billy.govols@gmail.com>
2021-04-14 15:44:39 -0400

    no need for manual steps...

--- docker/db-reset/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-php FactoryReset.php run
---
+++ docker/db/init.js
@@ -0,0 +1,14 @@
+// https://docs.mongodb.com/manual/tutorial/write-scripts-for-the-mongo-shell/#differences-between-interactive-and-scripted-mongo
+// https://docs.mongodb.com/manual/reference/method
+
+db.createCollection('users');
+
+db.users.insert({
+  id: 1,
+  username: 'admin',
+  password: '$2y$07$NRGaBfMC2pS14TcMZQQlBObbpHJgStkdxrpXFuA5yLGnqGlgaHlTi',
+  active: true,
+  email: 'admin@example.com',
+  name: 'Admin',
+  role: 'system_admin'
+});
