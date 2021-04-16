0c0827842727ca7963810d977572c52d34014ce7
Billy <billy.govols@gmail.com>
2021-04-16 15:09:07 -0400

    appear to be unused

--- src/Api/Library/Shared/WebsiteInstances.php
+++ src/Api/Library/Shared/WebsiteInstances.php
@@ -8,89 +8,6 @@ use Api\Model\Shared\Rights\SiteRoles;
 
 class WebsiteInstances
 {
-
-    /**
-     * This function contains the site "definitions" for Scripture Forge sites
-     * @return array
-     * @throws \Exception
-     */
-    public static function getScriptureForgeSites()
-    {
-        $sites = array();
-
-        /*
-         * **************************
-         * SCRIPTURE FORGE WEBSITES
-         * **************************
-         */
-
-
-        // scriptureforge.localhost sites
-        $w = new Website('scriptureforge.localhost', Website::SCRIPTUREFORGE);
-        $w->name = 'Scripture Forge';
-        $w->ssl = false;
-        $w->userDefaultSiteRole = SiteRoles::PROJECT_CREATOR;
-        $w->releaseStage = 'local';
-        $sites['scriptureforge.localhost'] = $w;
-
-        $w = new Website('jamaicanpsalms.scriptureforge.localhost', Website::SCRIPTUREFORGE);
-        $w->name = 'The Jamaican Psalms Project';
-        $w->ssl = false;
-        $w->theme = 'jamaicanpsalms';
-        $w->defaultProjectCode = 'jamaican_psalms';
-        $w->releaseStage = 'local';
-        $sites['jamaicanpsalms.scriptureforge.localhost'] = $w;
-
-        $w = new Website('demo.scriptureforge.localhost', Website::SCRIPTUREFORGE);
-        $w->name = 'Scripture Forge';
-        $w->ssl = true;
-        $w->theme = 'simple';
-        $w->userDefaultSiteRole = SiteRoles::PROJECT_CREATOR;
-        $w->releaseStage = 'local';
-        $sites['demo.scriptureforge.localhost'] = $w;
-
-        // qa.scriptureforge.org
-        $w = new Website('qa.scriptureforge.org', Website::SCRIPTUREFORGE);
-        $w->name = 'Scripture Forge';
-        $w->ssl = true;
-        $w->userDefaultSiteRole = SiteRoles::PROJECT_CREATOR;
-        $w->releaseStage = 'qa';
-        $sites['qa.scriptureforge.org'] = $w;
-
-        // scriptureforge.org
-        $w = new Website('scriptureforge.org', Website::SCRIPTUREFORGE);
-        $w->name = 'Scripture Forge';
-        $w->ssl = true;
-        $w->userDefaultSiteRole = SiteRoles::PROJECT_CREATOR;
-        $w->isProduction = true;
-        $w->releaseStage = 'live';
-        $sites['scriptureforge.org'] = $w;
-
-        // jamaicanpsalms.com
-        $w = new Website('jamaicanpsalms.scriptureforge.org', Website::SCRIPTUREFORGE);
-        $w->name = 'The Jamaican Psalms Project';
-        $w->ssl = true;
-        $w->theme = 'jamaicanpsalms';
-        $w->defaultProjectCode = 'jamaican_psalms';
-        $w->isProduction = true;
-        $w->releaseStage = 'live';
-        $sites['jamaicanpsalms.scriptureforge.org'] = $w;
-
-        // waaqwiinaagiwritings.org
-        $w = new Website('waaqwiinaagiwritings.org', Website::SCRIPTUREFORGE);
-        $w->name = 'Waaqwiinaagi Writings';
-        $w->ssl = true;
-        $w->theme = 'simple';
-        $w->defaultProjectCode = 'waaqwiinaagiwritings';
-        $w->isProduction = true;
-        $w->releaseStage = 'live';
-        $sites['waaqwiinaagiwritings.org'] = $w;
-
-
-        return $sites;
-    }
-
-
     /**
      * This function contains the site "definitions" for Language Forge sites
      * @throws \Exception
@@ -133,10 +50,8 @@ class WebsiteInstances
         $w->releaseStage = 'live';
         $sites['languageforge.org'] = $w;
 
-
         return $sites;
     }
-
 
     /**
      * @return array
@@ -145,14 +60,8 @@ class WebsiteInstances
 
         $redirects = array();
 
-        $redirects['www.scriptureforge.org'] = 'scriptureforge.org';
         $redirects['www.languageforge.org'] = 'languageforge.org';
-        $redirects['www.jamaicanpsalms.com'] = 'jamaicanpsalms.scriptureforge.org';
-        $redirects['www.jamaicanpsalms.org'] = 'jamaicanpsalms.scriptureforge.org';
-        $redirects['jamaicanpsalms.org'] = 'jamaicanpsalms.scriptureforge.org';
-        $redirects['jamaicanpsalms.com'] = 'jamaicanpsalms.scriptureforge.org';
 
         return $redirects;
     }
-
 }
