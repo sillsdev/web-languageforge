9a206aa9dcbee0f49918e244cf513621fc99827f
palmtreefrb <palmtreefrb@hotmail.com>
2021-04-21 06:56:25 -0700

    Restructured angular app to allow vscode debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ROOT = path.resolve(__dirname, './src/angular-app/languageforge');
+const ROOT = path.resolve(__dirname, './src/angular-app');
 
 /**
  * Webpack Plugins
@@ -51,14 +51,14 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                  { from: '../../../node_modules/font-awesome/', to: 'font-awesome' },
-                  { from: '../../../node_modules/intl-tel-input/build/', to: 'intl-tel-input' },
-                  { from: '../../../node_modules/jquery/dist/', to: 'jquery' },
-                  { from: '../../../node_modules/offline-js/offline.min.js', to: 'offline-js' },
-                  { from: '../../../node_modules/rangy/lib/', to: 'rangy' },
-                  { from: '../../../node_modules/zxcvbn/dist/', to: 'zxcvbn' },
-                  { from: '../../../node_modules/ng-table/bundles/ng-table.min.css', to: 'ng-table' },
-                  { from: '../../../node_modules/ng-table/bundles/ng-table.min.css.map', to: 'ng-table' },
+                  { from: '../../node_modules/font-awesome/', to: 'font-awesome' },
+                  { from: '../../node_modules/intl-tel-input/build/', to: 'intl-tel-input' },
+                  { from: '../../node_modules/jquery/dist/', to: 'jquery' },
+                  { from: '../../node_modules/offline-js/offline.min.js', to: 'offline-js' },
+                  { from: '../../node_modules/rangy/lib/', to: 'rangy' },
+                  { from: '../../node_modules/zxcvbn/dist/', to: 'zxcvbn' },
+                  { from: '../../node_modules/ng-table/bundles/ng-table.min.css', to: 'ng-table' },
+                  { from: '../../node_modules/ng-table/bundles/ng-table.min.css.map', to: 'ng-table' },
             ],
           }),
           new webpack.ProvidePlugin({
