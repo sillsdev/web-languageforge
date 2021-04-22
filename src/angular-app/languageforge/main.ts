a6f87323113412dc7ac17e24d2385d2b7a5c78f1
Rick <palmtreefrb@hotmail.com>
2021-04-22 07:24:45 -0700

    Restructured angular app to allow vscode debugging (#921)
    * Restructured angular app to allow vscode debugging
    
    * removed debug launch configurations

--- src/angular-app/languageforge/main.ts
+++
@@ -1,6 +0,0 @@
-import '../bellows/main.common';
-
-import '../bellows/apps/usermanagement/user-management-app.module';
-
-import './lexicon/lexicon-app.module';
-import './ldproject/ldproject-app.module';
