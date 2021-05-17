5ef841b37511cb242d2d290e187dffe308ee55db
Chris Hirt <chris@hirtfamily.net>
2021-05-17 22:11:18 +0700

    make languageforge.org the default for Website::get()
    This is a one-line change necessary for the website to function with production data everywhere.  There is no longer a business case to keep a separate site distinction in this code-base.  A future change may remove the Website class entirely.

--- src/Api/Library/Shared/Website.php
+++ src/Api/Library/Shared/Website.php
@@ -69,7 +69,7 @@ class Website
      * @param string $hostname
      * @return Website
      */
-    public static function get($hostname = '')
+    public static function get($hostname = 'languageforge.org')
     {
         if (!$hostname) {
             $hostname = self::getHostname();
