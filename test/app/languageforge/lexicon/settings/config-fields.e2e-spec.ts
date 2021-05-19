ecfcefe6196a1f8a749aeae27f13572e6e2d2fc6
Rick <palmtreefrb@hotmail.com>
2021-05-19 07:31:11 -0700

    e2e still failing on "select all" changing false to true

--- test/app/languageforge/lexicon/settings/config-fields.e2e-spec.ts
+++ test/app/languageforge/lexicon/settings/config-fields.e2e-spec.ts
@@ -608,14 +608,14 @@ describe('Lexicon E2E Configuration Fields', () => {
       expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.inputSystem.groupColumnCheckboxes(1), true)).toBe(true);
       expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.inputSystem.columnCheckboxes('select-row'), true))
         .toBe(true);
-      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.entry.groupColumnCheckboxes(1), false)).toBe(true);
+      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.entry.groupColumnCheckboxes(1), true)).toBe(true);
       expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.entry.columnCheckboxes('select-row'), false))
         .toBe(true);
-      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.sense.groupColumnCheckboxes(1), false)).toBe(true);
+      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.sense.groupColumnCheckboxes(1), true)).toBe(true);
       expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.sense.columnCheckboxes('select-row'), false))
         .toBe(true);
-      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.example.groupColumnCheckboxes(1), false)).toBe(true);
-      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.example.columnCheckboxes('select-row'), false))
+      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.example.groupColumnCheckboxes(1), true)).toBe(true);
+      expect<any>(await Utils.isAllCheckboxes(configPage.unifiedPane.example.columnCheckboxes('select-row'), true))
         .toBe(true);
     });
 
