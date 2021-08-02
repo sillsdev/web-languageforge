9e2fa98c0c39b365f815dbfb214978ff069426fb
Chris Hirt <chris@hirtfamily.net>
2021-08-02 13:29:50 +0700

    address review comments
    This change should have made it in with the original PR but didn't.

--- docker/deployment/scripts/kubeRsyncSendReceive.sh
+++ docker/deployment/scripts/kubeRsyncSendReceive.sh
@@ -5,4 +5,6 @@
 # -p 55555 this port is being forwarded to 22 on the container
 # -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" these are a hack to bypass security checks for "localhost".  localhost isn't a real host so I don't care about "host key verification failed" errors when doing localhost port forwarding
 
-ssh -A -p 55555 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@localhost "rsync -avzhP chirt@sysops.languageforge.org:/var/lib/languageforge/lexicon/ /var/lib/languageforge/lexicon/"
+USERANDHOSTNAME=user@hostname
+
+ssh -A -p 55555 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@localhost "rsync -avzhP ${USERANDHOSTNAME}:/var/lib/languageforge/lexicon/ /var/lib/languageforge/lexicon/"
