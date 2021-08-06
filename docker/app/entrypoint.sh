c55736ba1b553ebfa850f4870567f104ac52a8e2
Robin Munn <rmunn@pobox.com>
2021-08-06 15:08:07 +0700

    Fix mistake in entrypoint.sh script
    It's not `if [ which some-cmd ]`, it's `if which some-cmd`. It can also
    be a good idea to redirect the output of `which` to `/dev/null` so that
    you don't end up printing the full path of the command in your server
    logs (which is almost never necessary).

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -12,7 +12,8 @@ fi
 /etc/init.d/rsyslog start
 
 # run lfmergeqm on startup to clear out any failed send/receive sessions from previous container
-if [ which /lfmergeqm-background.sh ]; then
+if (which /lfmergeqm-background.sh > /dev/null)
+then
     # MUST be run as a background process as it kicks off an infinite loop to run every 24 hours
     /lfmergeqm-background.sh &
 fi
