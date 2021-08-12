f889970d4994c0828da675ccd0351f744825ae74
Billy <billy.govols@gmail.com>
2021-08-12 14:11:47 -0400

    removed unnecessary check

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,11 +4,7 @@
 /etc/init.d/rsyslog start
 
 # run lfmergeqm on startup to clear out any failed send/receive sessions from previous container
-if which /lfmergeqm-background.sh > /dev/null
-then
-    # MUST be run as a background process as it kicks off an infinite loop to run every 24 hours
-    /lfmergeqm-background.sh &
-fi
+/lfmergeqm-background.sh & # MUST be run as a background process as it kicks off an infinite loop to run every 24 hours
 
 # Now chain to Docker entrypoint from base php:apache image
 exec docker-php-entrypoint "$@"
