5367adc62274677bfe2f2b440c3db79e273b6bbd
Chris Hirt <chris@hirtfamily.net>
2021-08-18 22:02:28 +0700

    remove user-specific hostname in kubeRsyncAssets.sh

--- docker/deployment/scripts/kubeRsyncAssets.sh
+++ docker/deployment/scripts/kubeRsyncAssets.sh
@@ -5,4 +5,6 @@
 # -p 55555 this port is being forwarded to 22 on the container
 # -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" these are a hack to bypass security checks for "localhost".  localhost isn't a real host so I don't care about "host key verification failed" errors when doing localhost port forwarding
 
-ssh -A -p 55555 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@localhost "rsync -avzhP chirt@sysops.languageforge.org:/var/www/languageforge.org/htdocs/assets/lexicon/ /var/www/html/assets/lexicon/"
+USERANDHOSTNAME=user@hostname
+
+ssh -A -p 55555 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@localhost "rsync -avzhP $USERANDHOSTNAME:/var/www/languageforge.org/htdocs/assets/lexicon/ /var/www/html/assets/lexicon/"
