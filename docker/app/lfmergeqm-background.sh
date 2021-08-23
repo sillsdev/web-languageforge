d5b64b6c0524d2ffe962db5c213140623fdef700
Robin Munn <rmunn@pobox.com>
2021-08-23 16:14:45 +0700

    Slightly longer wait time for first lfmergeqm run
    There's a chance that 1 minute is not quite long enough, so we'll wait 2
    minutes to be safe.

--- docker/app/lfmergeqm-background.sh
+++ docker/app/lfmergeqm-background.sh
@@ -3,9 +3,9 @@
 # Run lfmergeqm every 24 hours to clean up any failed syncs that aren't in the queue
 # This script MUST be run as a background process ("script.sh &")!
 
-# First time we're run is before Apache starts up, so wait 60 seconds to allow Apache time to start up
+# First time we're run is before Apache starts up, so wait 120 seconds to allow Apache time to start up
 # (because LfMerge depends on Apache and PHP being up so that the RunClass.php code will work right)
-sleep 60
+sleep 120
 
 # Now enter an infinite loop that will run eery 24*60*60 = 86400 seconds
 while :
