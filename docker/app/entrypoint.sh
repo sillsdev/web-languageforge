f9844c919ef190c6e373787f941f047449d5713c
Robin Munn <rmunn@pobox.com>
2021-09-03 15:39:10 +0700

    Send lfmerge logs to stdout in lf-app container
    This will ensure that lfmerge logs get sent to stdout whether or not
    https://github.com/sillsdev/LfMerge/pull/134 is merged. Currently
    lfmerge runs in the lf-app container so its log messages end up mixed
    together with the PHP logs in `kubectl logs` output, but it's pretty
    easy to sort them out. If that mixing is not desired, then the
    entrypoint.sh changes can be reverted and `kubectl exec` can be used to
    run `tail -f /var/log/syslog` on the container instead.

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,6 +3,11 @@
 # rsyslog needs to run so that lfmerge can log to /var/log/syslog
 /etc/init.d/rsyslog start
 
+# Ensure /var/log/syslog exists so tail -f /var/log/syslog will run
+logger "Starting container..."
+# echo /var/log/syslog to container stdout so it shows up in `kubectl logs`
+tail -f /var/log/syslog &
+
 # run lfmergeqm on startup to clear out any failed send/receive sessions from previous container
 /lfmergeqm-background.sh & # MUST be run as a background process as it kicks off an infinite loop to run every 24 hours
 
