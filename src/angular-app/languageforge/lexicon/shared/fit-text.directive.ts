2df5f2e3fdab51bd4470dd45e656b6ad55ac6452
Rick Bartlett <palmtreefrb@hotmail.com>
2021-09-17 00:44:36 -0700

    Fix white space on mobile and desktop; Fix issue with empty input auto-shrinking (#1143)
    Added css styles and tweaks to remove excess whitespace.
    
    Adjusted FitTextDirective directive class to enable textarea more robust autosizing.

--- src/angular-app/languageforge/lexicon/shared/fit-text.directive.ts
+++ src/angular-app/languageforge/lexicon/shared/fit-text.directive.ts
@@ -4,17 +4,44 @@ export class FitTextDirective implements angular.IDirective {
 
   link(scope: angular.IScope, element: angular.IAugmentedJQuery, attr: angular.IAttributes) {
     let kInput: boolean = false;
+    var defaultHeight = 24;
+    element.height(40);
+    element.css({ 'max-height': '40px' });
+    element.css({ height: '40px' });
+
     function updateHeight(): any {
       let height = element.prop('scrollHeight');
       element.height = height;
       element.css({ 'max-height': height + 'px' });
       element.css({ height: height + 'px' });
+      updateContainersHeight();
     }
+
+    function updateContainersHeight(): any {
+      var wHeight = angular.element(window).height();
+      var editorTitleTextElement = angular.element(document).find('#editor-title-text');
+      var tHeight = editorTitleTextElement.height();
+      var primaryNavigationElement = angular.element(document).find('#primary-navigation');
+      var primaryNavigationHeight = primaryNavigationElement.height();
+      var scrollingEditorContainerElement = angular.element(document).find('#scrolling-editor-container');
+      var compactEntryListContainerElement = angular.element(document).find('#compactEntryListContainer');
+
+      var adjHeight = wHeight - (tHeight - defaultHeight);
+      var sHeight = adjHeight - (177 + primaryNavigationHeight);
+      var lHeight = adjHeight - (447 + primaryNavigationHeight);
+      scrollingEditorContainerElement.css({ height: sHeight + 'px' });
+      compactEntryListContainerElement.css({ height: lHeight + 'px' });
+    }
+
     element.on('keyup', () => {
       kInput = true;
       scope.$apply(() => {
         updateHeight();
       });
+    });
+
+    angular.element(window).on('resize',() => { 
+      updateHeight();
     });
 
     scope.$watch(() => {
@@ -31,3 +58,4 @@ export class FitTextDirective implements angular.IDirective {
   }
 
 }
+
