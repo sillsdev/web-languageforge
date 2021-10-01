6f971570378d5c0aaae888e215348adade095078
Chris Hirt <chris@hirtfamily.net>
2021-10-01 21:46:15 +0700

    add clarifying comments

--- src/angular-app/languageforge/lexicon/shared/fit-text.directive.ts
+++ src/angular-app/languageforge/lexicon/shared/fit-text.directive.ts
@@ -4,8 +4,16 @@ export class FitTextDirective implements angular.IDirective {
 
   link(scope: angular.IScope, element: angular.IAugmentedJQuery, attr: angular.IAttributes) {
     function updateHeight(): void {
+
+        // element is a JQuery object, and element[0] is the textarea DOM object
+        // the DOM object is the only way we can access the calculated "scrollHeight",
+        // since the JQuery object doesn't have that property
         let el = element[0] as HTMLTextAreaElement;
+
+        // start with a small minimum height
       	el.style.height = "10px";
+
+        // grow to the height necessary to fit all the text
         el.style.height = el.scrollHeight+"px";
     }
 
@@ -19,6 +27,7 @@ export class FitTextDirective implements angular.IDirective {
       updateHeight();
     });
 
+    // this is required in order to resize the text area when switching between entries
     scope.$watch(() => {
       updateHeight();
     });
