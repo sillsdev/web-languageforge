90d28d814d59c4e2c223609a6c21860770faebdb
Robin Munn <rmunn@pobox.com>
2021-10-19 12:05:21 +0700

    Convenience: run only LexEntryCommands unit tests
    Will revert this commit before merging the PR.
    
    Rationale: there are some slow unit tests in other files, and I want to
    be able to iterate more quickly.

--- docker/test-php/run.sh
+++ docker/test-php/run.sh
@@ -2,4 +2,5 @@
 
 cd /var/www/
 echo "##teamcity[importData type='junit' path='PhpUnitTests.xml']"
-src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml
+# src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml
+src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml --testsuite lexEntryCommands
