e09f89e28743b5017f9fc9bc3a171d362a461653
Robin Munn <rmunn@pobox.com>
2021-10-19 14:17:13 +0700

    Revert "Convenience: run only LexEntryCommands unit tests"
    This reverts commit 90d28d814d59c4e2c223609a6c21860770faebdb. Now all
    the unit tests run again, not just the ones I was iterating on.

--- docker/test-php/run.sh
+++ docker/test-php/run.sh
@@ -2,5 +2,4 @@
 
 cd /var/www/
 echo "##teamcity[importData type='junit' path='PhpUnitTests.xml']"
-# src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml
-src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml --testsuite lexEntryCommands
+src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml
