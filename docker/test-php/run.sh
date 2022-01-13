d6c58412dae7d55f4cd23f5d70a9a18042648032
Chris Hirt <chris@hirtfamily.net>
2022-01-14 00:08:15 +0700

    test-php based off base-php image for fast build
    Re-architect the test-php image to be built off the base-php image for faster building and execution of unit tests.  This should get unit tests running in under 1 minute without all the UI building happening in the full app image (not necessary for PHP unit tests)

--- docker/test-php/run.sh
+++ docker/test-php/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /var/www/
-echo "##teamcity[importData type='junit' path='PhpUnitTests.xml']"
 src/vendor/bin/phpunit --configuration test/php/phpunit.xml --log-junit PhpUnitTests.xml
