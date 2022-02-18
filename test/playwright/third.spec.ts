0cf4adfd3ceb91b567f816821d28606640e9bba4
Robin Munn <rmunn@pobox.com>
2022-02-18 16:42:13 +0700

    Get project config in a test
    This allows me to get the project config for the project the admin and
    manager users are currently logged into, which includes the update timer
    interval. This is needed because we don't have an API to update parts of
    the project settings, only to update the whole thing at once. So we need
    to fetch the settings, edit the part we need, and write it back, in
    order to set up the test conditions.

--- test/playwright/third.spec.ts
+++ test/playwright/third.spec.ts
@@ -1,8 +1,9 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, APIRequestContext, request } from '@playwright/test';
 import { getLoggedInPage } from './login';
 import constants from '../app/testConstants.json';
+import { jsonRpc } from './utils/json-rpc';
 
-test('Multiple users in different tabs', async ({ browser }) => {
+test.only('Multiple users in different tabs', async ({ browser }) => {
   const adminPage = await getLoggedInPage(browser, 'admin');
   const memberPage = await getLoggedInPage(browser, 'member');
 
@@ -11,4 +12,16 @@ test('Multiple users in different tabs', async ({ browser }) => {
 
   await expect(adminPage.locator('#userDropdown')).toContainText(constants.adminUsername);
   await expect(memberPage.locator('#userDropdown')).toContainText(constants.memberUsername);
+
+  const adminRequest = await request.newContext({ storageState: 'admin-storageState.json', baseURL: 'http://app-for-e2e' });
+  const session = await getSession(adminRequest);
+  console.log('Got session. Looking up poll request interval...');
+  console.log(session?.projectSettings?.config?.pollUpdateIntervalMs);
+  expect(session).toBeDefined();
+  expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toBeDefined();
+  expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toBeGreaterThanOrEqual(0);
 });
+
+function getSession(requestContext: APIRequestContext) {
+  return jsonRpc(requestContext, 'session_getSessionData');
+}
---
+++ test/playwright/utils/json-rpc.ts
@@ -0,0 +1,28 @@
+import type { APIRequestContext } from '@playwright/test';
+
+const nextId = (() => {
+  let id = 0;
+  return () => {
+    id = id + 1;
+    return id;
+  };
+})();
+
+export function jsonRpcParams(method: string, params: any = {}, orderedParams: any[] = []) {
+  if (Object.prototype.hasOwnProperty.call(params, 'orderedParams')) {
+    // Leave orderedParams alone
+  } else {
+    params.orderedParams = orderedParams;
+  }
+  return { version: '2.0', method: 'session_getSessionData', params, id: nextId() };
+}
+
+export async function jsonRpc(request: APIRequestContext, method: string, params: any = {}, orderedParams: any[] = []) {
+  const result = await request.post('/api/sf', { data: jsonRpcParams(method, params, orderedParams) });
+  const json = await result.json();
+  if (json.result) {
+    return json.result;
+  } else {
+    throw json.error;
+  }
+}
