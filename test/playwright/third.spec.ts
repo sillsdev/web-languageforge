3dae3fe8c2b4e4c9b73dcca2b32a74ba0bf4c28a
Robin Munn <rmunn@pobox.com>
2022-02-21 15:19:21 +0700

    Can now edit a text field in an entry

--- test/playwright/third.spec.ts
+++ test/playwright/third.spec.ts
@@ -6,6 +6,8 @@ import { jsonRpc } from './utils/json-rpc';
 test.describe.only('Multiple users editing the same project', () => {
   let adminPage: Page;
   let memberPage: Page;
+  const entryName: string = constants.testEntry2.lexeme.th.value;
+
   test.beforeEach(async ({browser}) => {
     const adminRequest = await request.newContext({ storageState: 'admin-storageState.json', baseURL: 'http://app-for-e2e' });
     const session = await getSession(adminRequest);
@@ -25,6 +27,50 @@ test.describe.only('Multiple users editing the same project', () => {
       expect(memberPage.locator('#userDropdown')).toContainText(constants.memberUsername),
     ]);
   });
+
+  test('Load project and entry', async () => {
+    await Promise.all([
+      adminPage.goto('/app/projects'),
+      memberPage.goto('/app/projects'),
+    ]);
+    await Promise.all([
+      adminPage.screenshot({path: 'admin-projects.png'}),
+      memberPage.screenshot({path: 'member-projects.png'}),
+    ]);
+    await Promise.all([
+      adminPage.locator(`div.listview a:has-text("${constants.testProjectName}")`).click(),
+      memberPage.locator(`div.listview a:has-text("${constants.testProjectName}")`).click(),
+    ]);
+    await Promise.all([
+      adminPage.locator(`text=${entryName}`).click(),
+      memberPage.locator(`text=${entryName}`).click(),
+    ]);
+    await Promise.all([
+      adminPage.screenshot({path: 'admin-project.png'}),
+      memberPage.screenshot({path: 'member-project.png'}),
+    ]);
+  });
+
+  test('Edit data in one entry', async () => {
+    await Promise.all([
+      adminPage.goto('/app/projects'),
+      // memberPage.goto('/app/projects'),
+    ]);
+    await Promise.all([
+      adminPage.locator(`div.listview a:has-text("${constants.testProjectName}")`).click(),
+      // memberPage.locator(`div.listview a:has-text("${constants.testProjectName}")`).click(),
+    ]);
+    await Promise.all([
+      adminPage.locator(`text=${entryName}`).click(),
+      // memberPage.locator(`text=${entryName}`).click(),
+    ]);
+    await Promise.all([
+      getField(adminPage, "Word", "tipa").fill('tipa for Word'),
+    ]);
+    await Promise.all([
+      adminPage.screenshot({path: 'admin-filled-in.png'}),
+    ]);
+  });
 });
 
 function getSession(requestContext: APIRequestContext) {
@@ -33,4 +79,8 @@ function getSession(requestContext: APIRequestContext) {
 
 function updateProjectConfig(requestContext: APIRequestContext, config: any) {
   return jsonRpc(requestContext, 'lex_configuration_update', [config, []]);
+}
+
+function getField(page: Page, fieldName: string, ws: string) {
+  return page.locator(`div.dc-entry form:has-text("${fieldName}") .input-group:has(span.wsid:text-is("${ws}")) textarea`)
 }
