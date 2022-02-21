61dd9d5c07524891c6accbfb12bc135eb0cb1d43
Robin Munn <rmunn@pobox.com>
2022-02-21 14:20:31 +0700

    Use beforeEach to set up test conditions
    Also use Promise.all to let the two halves of the test run in parallel.

--- test/playwright/third.spec.ts
+++ test/playwright/third.spec.ts
@@ -1,30 +1,29 @@
-import { expect, test, APIRequestContext, request } from '@playwright/test';
+import { expect, test, APIRequestContext, request, Page } from '@playwright/test';
 import { getLoggedInPage } from './login';
 import constants from '../app/testConstants.json';
 import { jsonRpc } from './utils/json-rpc';
 
-test.describe('Multiple users editing the same project', () => {
-
-  test.only('Multiple users in different tabs', async ({ browser }) => {
-    const adminPage = await getLoggedInPage(browser, 'admin');
-    const memberPage = await getLoggedInPage(browser, 'member');
-
-    await adminPage.goto('/');
-    await memberPage.goto('/');
-
-    await expect(adminPage.locator('#userDropdown')).toContainText(constants.adminUsername);
-    await expect(memberPage.locator('#userDropdown')).toContainText(constants.memberUsername);
-
+test.describe.only('Multiple users editing the same project', () => {
+  let adminPage: Page;
+  let memberPage: Page;
+  test.beforeEach(async ({browser}) => {
     const adminRequest = await request.newContext({ storageState: 'admin-storageState.json', baseURL: 'http://app-for-e2e' });
     const session = await getSession(adminRequest);
-    expect(session).toBeDefined();
-    expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toBeDefined();
-    expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toBeGreaterThanOrEqual(0);
     session.projectSettings.config.pollUpdateIntervalMs = 10 * 1000;
-    expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toEqual(10 * 1000);
     await updateProjectConfig(adminRequest, session.projectSettings.config);
-    const sessionAfterUpdate = await getSession(adminRequest);
-    expect(sessionAfterUpdate?.projectSettings?.config?.pollUpdateIntervalMs).toEqual(10 * 1000);
+    adminPage = await getLoggedInPage(browser, 'admin');
+    memberPage = await getLoggedInPage(browser, 'member');
+  });
+
+  test('Multiple users in different tabs', async () => {
+    await Promise.all([
+      adminPage.goto('/'),
+      memberPage.goto('/'),
+    ]);
+    await Promise.all([
+      expect(adminPage.locator('#userDropdown')).toContainText(constants.adminUsername),
+      expect(memberPage.locator('#userDropdown')).toContainText(constants.memberUsername),
+    ]);
   });
 });
 
