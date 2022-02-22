8c3d50ab6ea6e051fa73cad6784565414497d86a
Robin Munn <rmunn@pobox.com>
2022-02-22 16:11:23 +0700

    Stop retrying E2E tests in dev mode
    Now we don't bother with retries in dev mode, and just save a trace
    every time but delete it when the test passes.

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -42,7 +42,7 @@ const config: PlaywrightTestConfig = {
     ignoreHTTPSErrors: !process.env.CI,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
