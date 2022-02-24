a8818cc6a7d16ecdd83ab7be1bad1771f1ca85e5
Robin Munn <rmunn@pobox.com>
2022-02-24 10:27:51 +0700

    Add comments explaining the complicated test

--- test/playwright/third.spec.ts
+++ test/playwright/third.spec.ts
@@ -27,17 +27,27 @@ test.describe.only('Multiple users editing the same project', () => {
   });
 
   test('Edit data in one entry', async ({ baseURL }) => {
-    const session = await getSession(adminPage.request);
-    expect(session?.projectSettings?.config?.pollUpdateIntervalMs).toEqual(500);
     // Ensure new page load by going to / before project page
     await Promise.all([
       adminPage.goto('/'),
       memberPage.goto('/'),
     ]);
+
+    // Now go to project page, causing the project settings (including poll update interval) to be loaded now
     await Promise.all([
       projectId ? adminPage.goto(`/app/lexicon/${projectId}`) : adminPage.goto('/app/projects'),
       projectId ? memberPage.goto(`/app/lexicon/${projectId}`) : memberPage.goto('/app/projects'),
     ]);
+
+    // Paranoia check to make sure the session did load the proper update interval
+    const [adminSession, memberSession] = await Promise.all([
+      getSession(adminPage.request),
+      getSession(memberPage.request),
+    ]);
+    expect(adminSession?.projectSettings?.config?.pollUpdateIntervalMs).toEqual(500);
+    expect(memberSession?.projectSettings?.config?.pollUpdateIntervalMs).toEqual(500);
+
+    // TODO: Refactor these functions to be *outside* the test since they're generally useful
     const routeHandler = (method: string) => {
       let allowMethod = false;
       const handler = (route: Route) => {
@@ -64,51 +74,77 @@ test.describe.only('Multiple users editing the same project', () => {
         return (json?.method === method);
       };
     };
+
+    // Give names to the four route handlers so we can call .allow() on them later on
     const adminUpdateRouteHandler = routeHandler('lex_entry_update');
     const memberUpdateRouteHandler = routeHandler('lex_entry_update');
     const adminDtoUpdateRouteHandler = routeHandler('lex_dbeDtoUpdatesOnly');
     const memberDtoUpdateRouteHandler = routeHandler('lex_dbeDtoUpdatesOnly');
+
+    // Block the routes that send updates to the API and read updates from the API, so we can control when those happen
     await adminPage.route('**/api/sf', adminUpdateRouteHandler);
     await memberPage.route('**/api/sf', memberUpdateRouteHandler);
     await adminPage.route('**/api/sf', adminDtoUpdateRouteHandler);
     await memberPage.route('**/api/sf', memberDtoUpdateRouteHandler);
+
+    // Member loads page 100 milliseconds after admin, to ensure ordering of the writes: admin first, then member
     await Promise.all([
       adminPage.locator(`#scrolling-entry-words-container >> text=${entryName}`).click(),
       memberPage.waitForTimeout(100).then(() => memberPage.locator(`#scrolling-entry-words-container >> text=${entryName}`).click()),
     ]);
+
+    // Verify the old value of the fields before proceeding with the test
     await Promise.all([
       expect(await getField(adminPage, "Word", "th").inputValue()).toContain(constants.testEntry2.lexeme['th'].value),
       expect(await getField(memberPage, "Word", "tipa").inputValue()).toContain(constants.testEntry2.lexeme['th-fonipa'].value),
     ]);
+
+    // Admin changes the Thai IPA field, while member changes the Thai field
     await Promise.all([
       getField(adminPage, "Word", "tipa").fill('tipa for Word from admin'),
       memberPage.waitForTimeout(100).then(() => getField(memberPage, "Word", "th").fill('th for Word from member')),
     ]);
+
+    // Click on another field to ensure that the model has been updated (probably not needed)
     await Promise.all([
       getField(adminPage, "Word", "th").click(),
       getField(memberPage, "Word", "tipa").click(),
     ]);
+
+    // Screenshots to examine later (redundant with Playwright traces, so I'll get rid of them)
+    // These are before allowing the API calls, for comparison to the post-call screenshots
     await Promise.all([
       adminPage.screenshot({path: 'admin-filled-in.png'}),
       memberPage.screenshot({path: 'member-filled-in.png'}),
     ]);
-    test.setTimeout(45 * 1000); // Otherwise waiting 30s will run over the default 30s timeout
+
+    // Note that we wait for responses *before* we allow the routes to proceed, to ensure that we don't create a
+    // race condition that could cause us to miss the API call when it goes through
+
+    // First, allow the "write" update to go through, writing our change(s) to Mongo
     await Promise.all([
       adminPage.waitForResponse(responseAwaiter('lex_entry_update')),
       memberPage.waitForResponse(responseAwaiter('lex_entry_update')),
       adminUpdateRouteHandler.allowOnce(),
       memberUpdateRouteHandler.allowOnce(),
-      ]);
+    ]);
+
+    // Now allow the "read" update to go through, seeing the other person's change(s)
     await Promise.all([
       adminPage.waitForResponse(responseAwaiter('lex_dbeDtoUpdatesOnly')),
       memberPage.waitForResponse(responseAwaiter('lex_dbeDtoUpdatesOnly')),
       memberDtoUpdateRouteHandler.allowOnce(),
       adminDtoUpdateRouteHandler.allowOnce(),
     ]);
+
+    // Screenshots to examine later (redundant with Playwright traces, so I'll get rid of them)
+    // These should show the other person's changes having arrived
     await Promise.all([
       adminPage.screenshot({path: 'admin-waited.png'}),
       memberPage.screenshot({path: 'member-waited.png'}),
     ]);
+
+    // Verify that both see the other one's changes: admin sees changed Thai field, member sees changed Thai IPA field
     await Promise.all([
       expect(await getField(adminPage, "Word", "th").inputValue()).toContain('th for Word from member'),
       expect(await getField(memberPage, "Word", "tipa").inputValue()).toContain('tipa for Word from admin'),
