4923922b7e03aac1bf830e67de93258df5e89b70
Robin Munn <rmunn@pobox.com>
2022-03-10 14:58:50 +0700

    Test API to init test projects
    This is ugly, and I plan to clean it up soon. It works, but we really
    should be taking parameters from the E2E test code, and not hardcoding
    things like the admin user ownership. I'll be rewriting this soon.

--- test/e2e/TestControl.php
+++ test/e2e/TestControl.php
@@ -1,6 +1,15 @@
 <?php
 
 namespace Api\Service;
+use Api\Model\Shared\Mapper\MongoStore;
+use Api\Model\Shared\Command\UserCommands;
+use Api\Model\Shared\ProjectModel;
+use Api\Model\Languageforge\Lexicon\LexProjectModel;
+use Api\Model\Shared\Rights\ProjectRoles;
+use Api\Model\Shared\Rights\SystemRoles;
+use Api\Model\Shared\Mapper\IdReference;
+
+// use MongoDB\Client;
 
 use Api\Library\Shared\Website;
 use Silex\Application;
@@ -13,6 +22,10 @@ class TestControl
     {
         $this->app = $app;
         $this->website = Website::get();
+        // $rootPath = realpath(__DIR__.'/../../');
+
+        // $TestPath = $rootPath.'/test/';
+        // $this->constants = json_decode(file_get_contents($TestPath . 'app/testConstants.json'), true);
     }
 
     /** @var Application */
@@ -20,6 +33,8 @@ class TestControl
 
     /** @var Website */
     private $website;
+
+    private $constants;
 
     public function checkPermissions($methodName)
     {
@@ -35,5 +50,54 @@ class TestControl
     public function check_test_api()
     {
         return ['api_is_working' => true];
+    }
+
+    public function init_test_project($projectCode = null, $projectName = null)
+    {
+        $constants = $this->constants;
+
+        if (! $projectCode) {
+            $projectCode = 'test_project';
+        }
+        if (! $projectName) {
+            $projectName = 'Test Project';
+        }
+
+        // TODO: Handle this with MongoStore instead of through Commands library
+        $adminUserId = UserCommands::createUser([
+            'name' => 'Test Admin', // $constants['adminName'],
+            'email' => 'test_runner_admin@example.com', // $constants['adminEmail'],
+            'password' =>'hammertime', // $constants['adminPassword']
+        ],
+            $this->website
+        );
+        $adminUserId = UserCommands::updateUser([
+            'id' => $adminUserId,
+            'name' => 'Test Admin', // $constants['adminName'],
+            'email' => 'test_runner_admin@example.com', // $constants['adminEmail'],
+            'username' => 'test_runner_admin', // $constants['adminUsername'],
+            'password' =>'hammertime', // $constants['adminPassword'],
+            'active' => true,
+            'languageDepotUsername' => 'admin',
+            'role' => SystemRoles::SYSTEM_ADMIN
+        ],
+            $this->website
+        );
+
+        $db = MongoStore::connect(DATABASE);
+        $db->dropCollection('projects');
+        $db->createCollection('projects');
+        $coll = $db->selectCollection('projects');
+        $projectModel = new ProjectModel();
+        $projectModel->projectName = $projectName;
+        $projectModel->projectCode = $projectCode;
+        $projectModel->appName = LexProjectModel::LEXICON_APP;
+        $projectModel->siteName = $this->website->domain;
+        $projectModel->ownerRef = new IdReference($adminUserId);
+        $projectModel->addUser($adminUserId, ProjectRoles::MANAGER);
+        MongoStore::dropAllCollections($projectModel->databaseName());
+        MongoStore::dropDB($projectModel->databaseName());
+        $projectModel->write();
+        return $projectModel->id->asString();
     }
 }
--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,9 +1,17 @@
-import { test, expect, Page } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
+import constants from '../app/testConstants.json';
 import { testControl } from './jsonrpc';
+import { login } from './login';
 
-test.only('API call', async ({ request }) => {
+test('API call', async ({ request }) => {
   const result = await testControl(request, 'check_test_api');
   expect(result).toBeDefined();
   expect(result).toHaveProperty('api_is_working');
   expect(result.api_is_working).toBeTruthy();
+});
+
+test('Reset project', async ({ request, page }) => {
+  const result = await testControl(request, 'init_test_project');
+  await login(page, constants.adminUsername, constants.adminPassword);
+  await page.screenshot({ path: 'post-login.png' });
 });
