ad5b85f2661ed44399681b740c698232eee60e20
Robin Munn <rmunn@pobox.com>
2022-03-11 16:00:57 +0700

    Add types to example tests

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,16 +1,17 @@
-import { expect, Page, test } from '@playwright/test';
+import type { APIRequestContext, Page } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import constants from '../app/testConstants.json';
 import { testControl } from './jsonrpc';
 import { login } from './login';
 
-test('API call', async ({ request }) => {
+test('API call', async ({ request }: { request: APIRequestContext }) => {
   const result = await testControl(request, 'check_test_api');
   expect(result).toBeDefined();
   expect(result).toHaveProperty('api_is_working');
   expect(result.api_is_working).toBeTruthy();
 });
 
-test('Reset project', async ({ request, page }) => {
+test('Reset project', async ({ request, page }: { request: APIRequestContext, page: Page }) => {
   const adminId = await testControl(request, 'create_user', [
     constants.adminUsername,
     constants.adminName,
