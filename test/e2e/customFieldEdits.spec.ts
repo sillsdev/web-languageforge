b4dfe9c4ed1411ba6474632146d6916b0616607c
Robin Munn <rmunn@pobox.com>
2022-03-29 13:17:23 +0700

    Remove example tests from customFieldEdits.spec.ts
    Now that I have the testControl API working correctly, these tests
    (which used to be in example.spec.ts) are no longer needed.

--- test/e2e/customFieldEdits.spec.ts
+++ test/e2e/customFieldEdits.spec.ts
@@ -1,55 +1,10 @@
-import type { APIRequestContext } from '@playwright/test';
 import { expect } from '@playwright/test';
 import constants from './testConstants.json';
 import { test } from './utils/fixtures';
 import { testControl } from './utils/jsonrpc';
 import { addCustomField, addLexEntry, getProjectJson } from './utils/testSetup';
 
-test('API call', async ({ request }: { request: APIRequestContext }) => {
-  const result = await testControl(request, 'check_test_api');
-  expect(result).toBeDefined();
-  expect(result).toHaveProperty('api_is_working');
-  expect(result.api_is_working).toBeTruthy();
-});
-
-test('Reset project', async ({ request }: { request: APIRequestContext }) => {
-  await testControl(request, 'reset_projects');
-  const result = await testControl(request, 'init_test_project', [
-    constants.testProjectCode,
-    constants.testProjectName,
-    constants.adminUsername,
-  ]);
-  const json = await getProjectJson(request, constants.testProjectCode);
-  expect(json.projectCode).toEqual(constants.testProjectCode);
-  expect(json.missingProp).toBeUndefined();
-});
-
-test('Create custom field', async ({ request }: { request: APIRequestContext }) => {
-  const result = await addCustomField(request, constants.testProjectCode, 'customField_entry_Foo', 'entry', 'MultiString', {inputSystems: ['th']});
-  const json = await getProjectJson(request, constants.testProjectCode);
-  expect(json?.config?.roleViews?.project_manager?.fields?.customField_entry_Foo).toBeDefined();
-  expect(json?.config?.roleViews?.project_manager?.fields?.customField_entry_Foo?.type).toEqual('multitext');
-});
-
-test('Second test', async ({ request }: { request: APIRequestContext }) => {
-  const result = await testControl(request, 'new_method');
-  console.log(result);
-  expect(result).toBe('hello');
-});
-
-test('Try this', async ({request}) => {
-  const data = { ...constants.testEntry1 } as any;
-  data.customFields = {
-    customField_entry_Foo: { th: { value: 'something' } }
-  };
-  // const d = new Date();
-  // const uniqueId = '_new_' + d.getSeconds() + d.getMilliseconds();
-  console.log('Calling PHP with data', data);
-  const result = await addLexEntry(request, constants.testProjectCode, data);
-  console.log('Result was', result);
-});
-
-test.describe.only('Custom fields', () => {
+test.describe('Custom fields', () => {
   let projectId: string;
   test.beforeEach(async ({ request }) => {
     projectId = await testControl(request, 'init_test_project', [
