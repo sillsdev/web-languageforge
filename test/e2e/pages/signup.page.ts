a624462f3f38ba588f91e4a27b5deadd41845567
JeanneSon <43119728+JeanneSon@users.noreply.github.com>
2022-03-29 16:55:37 +0700

    convert signup tests to Playwright
    The tests are converted but not yet executed as test set-up functionality is still missing

--- test/e2e/pages/signup.page.ts
+++ test/e2e/pages/signup.page.ts
@@ -5,22 +5,36 @@ type Captcha = {
   blueSquareButton: Locator;
   yellowCircleButton: Locator;
   redTriangleButton: Locator;
+  setInvalidCaptcha: Function;
+  setValidCaptcha: Function;
 };
 
 export class SignupPage {
   readonly page: Page;
   readonly emailInput: Locator;
+  readonly emailInvalid: Locator;
+  readonly emailTaken: Locator;
+
   readonly nameInput: Locator;
   readonly passwordInput: Locator;
+  readonly passwordIsWeak: Locator;
+  readonly showPassword: Locator;
   readonly captchaDiv: Locator;
   readonly captcha: Captcha;
+  readonly captchaInvalid: Locator;
+  readonly signupButton: Locator;
   static readonly url: string = '/public/signup';
 
   constructor(page: Page) {
     this.page = page;
     this.emailInput = page.locator('#email');
+    this.emailInvalid = page.locator('#emailInvalid');
+    this.emailTaken = page.locator('#emailTaken');
+
     this.nameInput = page.locator('#name');
     this.passwordInput = page.locator('#password');
+    this.passwordIsWeak = page.locator('#passwordIsWeak');
+    this.showPassword = page.locator('data-ng-model="$ctrl.showPassword"');
 
     this.captchaDiv = page.locator('#pui-captcha');
     this.captcha = {
@@ -28,11 +42,43 @@ export class SignupPage {
       blueSquareButton: this.captchaDiv.locator('#captcha0'),
       yellowCircleButton: this.captchaDiv.locator('#captcha1'),
       redTriangleButton: this.captchaDiv.locator('#captcha2'),
-    }
+
+      setInvalidCaptcha: async () => {
+        await this.captcha.blueSquareButton.click();
+        if (await this.captcha.expectedItemName.innerText() === 'Blue Square') {
+          this.captcha.yellowCircleButton.click();
+        }
+      },
+
+      setValidCaptcha: async () => {
+        switch (await this.captcha.expectedItemName.innerText()) {
+          case 'Blue Square':
+            this.captcha.blueSquareButton.click();
+          case 'Yellow Circle':
+            this.captcha.yellowCircleButton.click();
+          case 'Red Triangle':
+            this.captcha.redTriangleButton.click();
+        }
+      }
+    };
+    this.captchaInvalid = page.locator('#captchaInvalid');
+
+    this.signupButton = page.locator('#submit');
   }
 
-  async goto() {
-    await this.page.goto(SignupPage.url);
+  async goto(email: string = '') {
+    let appendEmailtoURL: string = '';
+    if (email != '') {
+      appendEmailtoURL = '#!/?e=' + encodeURIComponent(email);
+    }
+    await this.page.goto(SignupPage.url + appendEmailtoURL);
     await expect(this.emailInput).toBeVisible();
+  }
+
+  async setInvalidCaptcha() {
+    await this.captcha.blueSquareButton.click();
+    if (await this.captcha.expectedItemName.innerText() === 'Blue Square') {
+      this.captcha.yellowCircleButton.click();
+    }
   }
 }
---
+++ test/e2e/signup.spec.ts
@@ -0,0 +1,137 @@
+import { expect } from '@playwright/test';
+import { test } from './utils/fixtures';
+import { SignupPage } from './pages/signup.page';
+
+test.describe('E2E Signup app', () => {
+  const constants = require('./../testConstants.json');
+
+  // this name is not very descriptive
+  test('Can verify required information filled', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+
+    await expect(signupPage.signupButton).toBeDisabled();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setInvalidCaptcha();
+    await expect(signupPage.signupButton).toBeEnabled();
+  });
+
+  test('Cannot submit if email is invalid', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.emailNoAt);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setInvalidCaptcha();
+    await expect(signupPage.emailInvalid).toBeVisible();
+    await expect(signupPage.signupButton).toBeDisabled();
+  });
+
+  test('Cannot submit if password is weak', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await expect(signupPage.signupButton).toBeEnabled();
+    await signupPage.passwordInput.fill(constants.passwordTooShort);
+    await signupPage.captcha.setInvalidCaptcha();
+    await expect(signupPage.passwordIsWeak).toBeVisible();
+    await expect(signupPage.signupButton).not.toBeEnabled();
+  });
+
+  test('Can submit if password not weak', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setInvalidCaptcha();
+    await expect(signupPage.passwordIsWeak).not.toBeVisible();
+    await expect(signupPage.signupButton).toBeEnabled();
+  });
+
+  test('Can submit if password is showing and not weak', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.showPassword.click();
+    await signupPage.captcha.setInvalidCaptcha();
+    await expect(signupPage.passwordIsWeak).not.toBeVisible();
+    await expect(signupPage.signupButton).toBeEnabled();
+  });
+
+  test('Cannot submit if captcha not selected', async ({ page }) => {
+
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await expect(signupPage.signupButton).toBeDisabled();
+  });
+
+  test('Can submit a user registration request and captcha is invalid', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setInvalidCaptcha();
+    await signupPage.signupButton.click();
+    await expect(signupPage.captchaInvalid).toBeVisible();
+  });
+
+  test('Finds the admin user (case insensitive) already exists', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.emailInput.fill(constants.adminEmail.toUpperCase());
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setValidCaptcha();
+    await expect(signupPage.signupButton).toBeEnabled();
+    await signupPage.signupButton.click();
+    await expect(signupPage.emailTaken).toBeVisible();
+  });
+
+  test('Can prefill email address that can\'t be changed', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto(constants.unusedEmail);
+    await expect(signupPage.emailInput).toBeDisabled();
+  });
+
+  test('Can prefill email address that already exists', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto(constants.adminEmail);
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setValidCaptcha();
+    await signupPage.signupButton.click();
+    await expect(signupPage.emailTaken).toBeVisible();
+  });
+
+  test('Can signup a new user', async ({ page }) => {
+    const signupPage = new SignupPage(page);
+    await signupPage.goto();
+    await signupPage.emailInput.fill(constants.unusedEmail);
+    await signupPage.nameInput.fill(constants.unusedName);
+    await signupPage.passwordInput.fill(constants.passwordValid);
+    await signupPage.captcha.setValidCaptcha();
+    await expect(signupPage.signupButton).toBeEnabled();
+    await signupPage.signupButton.click();
+
+    // Verify new user logged in and redirected to projects page
+    // maybe add await navigation
+    expect(page.url()).toContain('/app/projects');
+  });
+
+  test('Redirects to projects page if already logged in', async ({ memberTab }) => {
+    const signupPage = new SignupPage(memberTab);
+    await signupPage.goto();
+    expect(memberTab.url()).toContain('/app/projects');
+  });
+});
