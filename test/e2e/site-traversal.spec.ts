d22e432c5a767148fcfd865a80db6209b05256f1
Hanna <43119728+JeanneSon@users.noreply.github.com>
2022-03-31 09:57:59 +0700

    Improve efficiency of site-traversal tests by implementing new design pattern (#1363)
    * the new design pattern
    
    Naturally, Playwright takes a brand-new page/tab for every single test.
    If one is, however, only traversing pages without making any bigger
    changes, it is not worth taking a brand-new page for every single
    test. 
    
    * implementing the design pattern
    
    The implementation is accomplished through fixtures. 
    They can be designed to be reusable tabs across multiple tests.
    For logged in users, these tabs were already provided.
    Now, a reusable tab without a user - the anonTab - was implemented.
    
    * improve efficiency of site-traversal tests
    
    The tests run now twice as fast.

--- test/e2e/site-traversal.spec.ts
+++ test/e2e/site-traversal.spec.ts
@@ -13,9 +13,27 @@ import { UserProfilePage } from './pages/user-profile.page';
  * page traversal without testing functionality
  */
 test.describe('E2E Page Traversal', () => {
+  let signupPage: SignupPage;
+  let forgotPasswordPage: ForgotPasswordPage;
+  let loginPage: LoginPage;
+  let changePasswordPage: ChangePasswordPage;
+  let activityPage: ActivityPage;
+  let projectsPage: ProjectsPage;
+  let siteAdminPage: SiteAdminPage;
+  let userProfilePage: UserProfilePage;
 
-  test('Explore signup page', async ({ page }) => {
-    const signupPage = new SignupPage(page);
+  test.beforeAll(async ({ anonTab, adminTab }) => {
+    signupPage = new SignupPage(anonTab);
+    forgotPasswordPage = new ForgotPasswordPage(anonTab);
+    loginPage = new LoginPage(anonTab);
+    changePasswordPage = new ChangePasswordPage(adminTab);
+    activityPage = new ActivityPage(adminTab);
+    projectsPage = new ProjectsPage(adminTab);
+    siteAdminPage = new SiteAdminPage(adminTab);
+    userProfilePage = new UserProfilePage(adminTab);
+  })
+
+  test('Explore signup page', async () => {
     await signupPage.goto();
 
     await signupPage.emailInput.fill('');
@@ -26,16 +44,14 @@ test.describe('E2E Page Traversal', () => {
     await signupPage.captcha.redTriangleButton.click();
   });
 
-  test('Explore forgot password page', async ({ page }) => {
-    const forgotPasswordPage = new ForgotPasswordPage(page);
+  test('Explore forgot password page', async () => {
     await forgotPasswordPage.goto();
 
     await forgotPasswordPage.usernameInput.fill('');
     await forgotPasswordPage.submitButton.click();
   });
 
-  test('Explore login page', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+  test('Explore login page', async () => {
     await loginPage.goto();
 
     await loginPage.usernameInput.type('');
@@ -43,8 +59,7 @@ test.describe('E2E Page Traversal', () => {
     await loginPage.submitButton.click();
   });
 
-  test('Explore change passsword page (admin)', async ({ adminTab }) => {
-    const changePasswordPage = new ChangePasswordPage(adminTab);
+  test('Explore change passsword page (admin)', async () => {
     await changePasswordPage.goto();
 
     await changePasswordPage.passwordInput.type('');
@@ -52,15 +67,13 @@ test.describe('E2E Page Traversal', () => {
     await expect(changePasswordPage.submitButton).toBeDisabled();
   });
 
-  test('Explore activity page (admin)', async ({ adminTab }) => {
-    const activityPage = new ActivityPage(adminTab);
+  test('Explore activity page (admin)', async () => {
     await activityPage.goto();
 
     await activityPage.activitiesList.count();
   });
 
-  test('Explore project page (admin)', async ({ adminTab }) => {
-    const projectsPage = new ProjectsPage(adminTab);
+  test('Explore project page (admin)', async () => {
     await projectsPage.goto();
 
     await projectsPage.projectsList.count();
@@ -68,8 +81,7 @@ test.describe('E2E Page Traversal', () => {
     await projectsPage.createButton.click();
   });
 
-  test('Explore site admin page', async ({ adminTab }) => {
-    const siteAdminPage = new SiteAdminPage(adminTab);
+  test('Explore site admin page', async () => {
     await siteAdminPage.goto();
 
     await siteAdminPage.tabs.archivedProjects.click();
@@ -78,8 +90,7 @@ test.describe('E2E Page Traversal', () => {
     await siteAdminPage.archivedProjectsTab.projectsList.count();
   });
 
-  test('Explore user profile page (admin)', async ({ adminTab }) => {
-    const userProfilePage = new UserProfilePage(adminTab);
+  test('Explore user profile page (admin)', async () => {
     await userProfilePage.goto();
 
     await userProfilePage.activitiesList.count();
--- test/e2e/utils/fixtures.ts
+++ test/e2e/utils/fixtures.ts
@@ -32,6 +32,7 @@ const userTab = (username: usernamesForFixture) => async ({ browser, browserName
 // Add user fixtures to test function
 // Two kinds of fixtures: userTab and user, where "user" is one of "admin", "manager", "member", "member2", or "observer"
 // The userTab fixture represents a browser tab (a "page" in Playwright terms) that's already logged in as that user
+// The anonTab fixture represents a browser tab (a "page" in Playwright terms) where nobody is logged in; this tab can be used across different tests (like userTab)
 // The user fixture just carries that user's details (username, password, name and email)
 // Note: "Tab" was chosen instead of "Page" to avoid confusion with Page Object Model classes like SiteAdminPage
 export const test = (base
@@ -41,6 +42,7 @@ export const test = (base
     memberTab: UserTab,
     member2Tab: UserTab,
     observerTab: UserTab,
+    anonTab: Page,
     admin: UserDetails,
     manager: UserDetails,
     member: UserDetails,
@@ -52,6 +54,12 @@ export const test = (base
     memberTab: userTab('member'),
     member2Tab: userTab('member2'),
     observerTab: userTab('observer'),
+    anonTab: async ({ browser }: { browser: Browser }, use: (r: Page) => Promise<void>) => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      const tab = page;
+      await use(tab);
+    },
     admin: async ({}, use) => {
       let admin = {} as UserDetails;
       setupUserDetails(admin, 'admin');
