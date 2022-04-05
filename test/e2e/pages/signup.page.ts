649c6046c3dbecb5aecefd28a14ec91748654b14
Hanna <43119728+JeanneSon@users.noreply.github.com>
2022-04-05 17:27:17 +0700

    Signup page playwright tests (#1364)
    The following tests have been converted to playwright
    
    Bellows E2E Signup app
    ✓ cannot submit if email is invalid
    ✓ cannot submit if password is weak
    ✓ can submit if password not weak 
        -> new name: Can submit if the password is strong
    ✓ can submit if password is showing and not weak
    ✓ cannot submit if captcha not selected
    ✓ can submit a user registration request and captcha is invalid 
        -> new name: Captcha is invalid but user can still submit form
    ✓ finds the admin user (case insensitive) already exists
    ✓ can prefill email address that can't be changed
    ✓ can prefill email address that already exists
    ✓ can signup a new user
    ✓ redirects to projects page if already logged in
    
    * Improve email-taken error text in signup page
    
    @jeanneson and @megahirt discussed ways to improve this error message to make it
    clearer to the user what they can do in the unfortunate scenario where the
    email is taken but they cannot modify the disabled/pre-filled email field.
    
    * Convert signup tests to Playwright
    * Implemented new design pattern for signup tests
    * Apply suggestions from code review
    
    Co-authored-by: Christopher Hirt <chris@hirtfamily.net>

--- test/e2e/pages/signup.page.ts
+++ test/e2e/pages/signup.page.ts
@@ -1,26 +1,40 @@
-import { expect, Locator, Page } from '@playwright/test';
+import { ElementHandle, expect, Locator, Page } from '@playwright/test';
 
 type Captcha = {
   expectedItemName: Locator;
   blueSquareButton: Locator;
   yellowCircleButton: Locator;
   redTriangleButton: Locator;
+  setInvalidCaptcha: Function;
+  setValidCaptcha: Function;
 };
 
 export class SignupPage {
   readonly page: Page;
   readonly emailInput: Locator;
+  readonly emailInvalid: Locator;
+  readonly emailTaken: Locator;
+
   readonly nameInput: Locator;
   readonly passwordInput: Locator;
+  readonly passwordIsWeak: Locator;
+  readonly showPassword: Locator;
   readonly captchaDiv: Locator;
   readonly captcha: Captcha;
+  readonly captchaInvalid: Locator;
+  readonly signupButton: Locator;
   static readonly url: string = '/public/signup';
 
   constructor(page: Page) {
     this.page = page;
     this.emailInput = page.locator('#email');
+    this.emailInvalid = page.locator('#emailInvalid');
+    this.emailTaken = page.locator('#emailTaken');
+
     this.nameInput = page.locator('#name');
     this.passwordInput = page.locator('#password');
+    this.passwordIsWeak = page.locator('#passwordIsWeak');
+    this.showPassword = page.locator('[data-ng-model="$ctrl.showPassword"]');
 
     this.captchaDiv = page.locator('#pui-captcha');
     this.captcha = {
@@ -28,11 +42,51 @@ export class SignupPage {
       blueSquareButton: this.captchaDiv.locator('#captcha0'),
       yellowCircleButton: this.captchaDiv.locator('#captcha1'),
       redTriangleButton: this.captchaDiv.locator('#captcha2'),
-    }
+
+      setInvalidCaptcha: async () => {
+        await this.captcha.blueSquareButton.click();
+        if (await this.captcha.expectedItemName.innerText() === 'Blue Square') {
+          await this.captcha.yellowCircleButton.click();
+        }
+      },
+
+      setValidCaptcha: async () => {
+        await expect(this.captcha.expectedItemName).not.toHaveText('');
+        // Could also have done this: -RM
+        // const itemNameHandle = (await this.captcha.expectedItemName.elementHandle()) as ElementHandle<HTMLElement>;
+        // await this.page.waitForFunction(elem => elem.innerText, itemNameHandle);
+
+        switch (await this.captcha.expectedItemName.innerText()) {
+          case 'Blue Square':
+            await this.captcha.blueSquareButton.click();
+            break;
+          case 'Yellow Circle':
+            await this.captcha.yellowCircleButton.click();
+            break;
+          case 'Red Triangle':
+            await this.captcha.redTriangleButton.click();
+        }
+      }
+    };
+    this.captchaInvalid = page.locator('#captchaInvalid');
+    this.signupButton = page.locator('#submit');
   }
 
-  async goto() {
-    await this.page.goto(SignupPage.url);
+  async goto(email: string = '') {
+    if (email === '') {
+      await this.page.goto(SignupPage.url);
+    }
+    else {
+      await this.page.goto(SignupPage.url + '#!/?e=' + encodeURIComponent(email));
+      await this.page.reload();
+    }
     await expect(this.emailInput).toBeVisible();
+  }
+
+  async setInvalidCaptcha() {
+    await this.captcha.blueSquareButton.click();
+    if (await this.captcha.expectedItemName.innerText() === 'Blue Square') {
+      this.captcha.yellowCircleButton.click();
+    }
   }
 }
