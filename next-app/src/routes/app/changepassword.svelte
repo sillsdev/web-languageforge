2e8e307539a0fab5ca191d7dea0b32af34c7938e
billy clark <billy_clark@sil.org>
2022-04-07 13:27:45 -0400

    dmeo noth techniques

--- next-app/src/routes/app/changepassword.svelte
+++ next-app/src/routes/app/changepassword.svelte
@@ -1,12 +1,15 @@
 <script>
-// import { change } from '$lib/data/password'
+import { change } from '$lib/data/password'
 
 let data = null
 let new_password = ''
 let new_password_confirm = ''
 
-async function change_password() {
-    // data = await change(new_password, new_password_confirm)
+async function change_password_chatty() {
+    data = await change(new_password, new_password_confirm)
+}
+
+async function change_password_sleek() {
     const response = await fetch('/password', {
         method: 'put',
         headers: {
@@ -24,11 +27,12 @@ async function change_password() {
 
 <h1>LFNext app - Change password</h1>
 
-<form on:submit|preventDefault={change_password}>
+<form on:submit|preventDefault={() => {}}>
     <input type=password bind:value={new_password}         placeholder="New password" />
     <input type=password bind:value={new_password_confirm} placeholder="Repeat new password" />
 
-    <button>Change my password</button>
+    <button on:click|preventDefault={change_password_sleek}>Change my password (sleek)</button>
+    <button on:click|preventDefault={change_password_chatty}>Change my password (chatty)</button>
 </form>
 
 <pre>{JSON.stringify(data, null, 2)}</pre>
