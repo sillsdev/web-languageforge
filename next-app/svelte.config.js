503d8e15190ce3e35d3e51293c65ab868b3c8b18
billy clark <billy_clark@sil.org>
2022-04-07 13:27:46 -0400

    reinsstalled preprocess after mistakenly removing it

--- next-app/svelte.config.js
+++ next-app/svelte.config.js
@@ -1,23 +1,30 @@
+import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  kit: {
-    adapter: adapter(),
-	prerender: {
-		// https://kit.svelte.dev/docs/configuration#prerender
-		onError: ({ path, status }) => {
-			const externalLinks = [
-				'/auth/login',
-			]
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			// https://kit.svelte.dev/docs/configuration#prerender
+			onError: ({ path, status }) => {
+				const externalLinks = [
+					'/auth/login',
+				]
 
-			if (externalLinks.includes(path)) {
-				// :-( https://github.com/sveltejs/kit/issues/3402
-				console.info(`skipping ${status} error while crawling ${path} since it's an external link`)
-			} else {
-				throw new Error(`${status} error while crawling ${path}`)
+				if (externalLinks.includes(path)) {
+					// :-( https://github.com/sveltejs/kit/issues/3402
+					console.info(`skipping ${status} error while crawling ${path} since it's an external link`)
+				} else {
+					throw new Error(`${status} error while crawling ${path}`)
+				}
 			}
-		}
+		},
 	},
-  },
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 }
