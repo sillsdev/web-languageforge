3f8a2ee57664ae51ca97b1033f4aa315d193ff64
billy clark <billy_clark@sil.org>
2022-04-07 13:27:45 -0400

    applied tw to progress

--- next-app/src/lib/progress/Progress.svelte
+++ next-app/src/lib/progress/Progress.svelte
@@ -1,20 +1,38 @@
 <script>
 import { loading } from '$lib/progress'
+
+let value = 0
+
+$: $loading ? start() : stop()
+
+let intervalId = 0;
+function start() {
+	intervalId = setInterval(() => {
+		value += 0.1
+
+		if (value >= 1) {
+			value = 0
+		}
+
+	}, 100)
+}
+function stop() {
+	clearInterval(intervalId)
+}
 </script>
 
-<progress class:show={$loading} class:hide={!$loading} />
+<progress {value} class:visible={$loading} class:invisible={!$loading} />
+<!-- TODO: watch for https://github.com/saadeghi/daisyui/issues/605 -->
 
 <style>
 progress {
-	width: 100%;
-	position: relative;
-	top: -0.6rem;
-}
+	@apply
+		progress
+		progress-secondary
 
-.hide {
-	visibility: hidden;
-}
-.show {
-	visibility: visible;
+		w-screen
+
+		relative
+		-top-4;
 }
 </style>
