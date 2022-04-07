d8a362a2d2a41e91c2ce76017b990646a02fbc97
billy clark <billy_clark@sil.org>
2022-04-07 13:27:45 -0400

    enabled prerendering on suitable pages

--- next-app/src/routes/index.svelte
+++ next-app/src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context=module>
+	export const prerender = true
+</script>
+
 <script>
 import { browser } from '$app/env'
 import { throwError } from '$lib/error'
--- next-app/src/routes/password/change.svelte
+++ next-app/src/routes/password/change.svelte
@@ -1,3 +1,7 @@
+<script context=module>
+	export const prerender = true
+</script>
+
 <script>
 import { goto } from '$app/navigation'
 import { UPDATE } from '$lib/fetch/client'
