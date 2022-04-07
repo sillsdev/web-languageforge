e6c2724d152f510017aad62affc34fcd82810688
billy clark <billy_clark@sil.org>
2022-04-07 13:27:45 -0400

    integrated fix from daisyUI

--- next-app/src/lib/progress/Progress.svelte
+++ next-app/src/lib/progress/Progress.svelte
@@ -1,30 +1,8 @@
 <script>
 	import { loading } from '$lib/progress'
-
-	let value = 0
-
-	$: $loading ? start() : stop()
-
-	let intervalId = 0;
-	function start() {
-		intervalId = setInterval(() => {
-			value += 0.1
-
-			if (value >= 1) {
-				value = 0
-			}
-
-		}, 100)
-	}
-	function stop() {
-		clearInterval(intervalId)
-
-		value = 0
-	}
 </script>
 
-<progress {value} class:visible={$loading} class:invisible={!$loading} />
-<!-- TODO: watch for https://github.com/saadeghi/daisyui/issues/605 -->
+<progress  class:visible={$loading} class:invisible={!$loading} />
 
 <style>
 progress { @apply
