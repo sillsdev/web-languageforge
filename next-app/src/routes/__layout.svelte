191f7a0690fa68757347910fb6faae4f218c778b
billy clark <billy_clark@sil.org>
2022-04-25 13:21:28 -0400

    wanted a few refinements

--- next-app/src/routes/__layout.svelte
+++ next-app/src/routes/__layout.svelte
@@ -1,23 +1,24 @@
 <script>
+	import { page } from '$app/stores'
 	import '$lib/app.css'
 	import Header from '$lib/Header.svelte'
 
-	// TODO: add 'active menu item' class to current page
+	let menu_toggle = false
 
-	let checked = false
+	$: current_page = $page.url.pathname
 
 	function open() {
-		checked = true
+		menu_toggle = true
 	}
 
 	function close() {
-		checked = false
+		menu_toggle = false
 	}
 </script>
 
 <!-- https://daisyui.com/components/drawer -->
 <div class='drawer drawer-end'>
-	<input type=checkbox {checked} class=drawer-toggle>
+	<input type=checkbox checked={menu_toggle} class=drawer-toggle>
 
 	<div class=drawer-content>
 		<Header on:menuopen={open} />
@@ -34,7 +35,7 @@
 	  <ul class='menu bg-base-100'>
 		<li class=menu-title>User</li>
 		<li><a href=/app/userprofile rel=external>My profile</a></li>
-		<li><a href=/password/change>Change password</a></li>
+		<li><a href=/password/change  class:active={current_page.startsWith('/password/change')}>Change password</a></li>
 		<li><a href=/app/siteadmin rel=external>Site administration</a></li>
 		<li><a href=/auth/logout rel=external>Logout</a></li>
 
@@ -42,13 +43,13 @@
 		<li><a href=/app rel=external>My projects</a></li>
 
 		<li class=menu-title>About</li>
-		<li><a href=https://community.software.sil.org/t/w/5454 rel=external>What's new</a></li>
-		<li><a href='https://www.youtube.com/playlist?list=PLJLUPwIFOI8d8lmQVAcBapyw87jCtmDNA' rel=external>Videos</a></li>
-		<li><a href=https://github.com/sillsdev/web-languageforge/wiki/Known-Issues-and-Limitations rel=external>Known issues and limitations</a></li>
+		<li><a href=https://community.software.sil.org/t/w/5454 target=_blank rel=external>What's new</a></li>
+		<li><a href='https://www.youtube.com/playlist?list=PLJLUPwIFOI8d8lmQVAcBapyw87jCtmDNA' target=_blank rel=external>Videos</a></li>
+		<li><a href=https://github.com/sillsdev/web-languageforge/wiki/Known-Issues-and-Limitations target=_blank rel=external>Known issues and limitations</a></li>
 
 		<li class=menu-title>Help</li>
-		<li><a href=https://community.software.sil.org/c/language-forge rel=external>Community Support</a></li>
-		<li><a href=mailto:issues@languageforge.org rel=external>Report a Problem (email issues@languageforge.org)</a></li>
+		<li><a href=https://community.software.sil.org/c/language-forge target=_blank rel=external>Community Support</a></li>
+		<li><a href=mailto:issues@languageforge.org target=_blank rel=external>Report a Problem (email issues@languageforge.org)</a></li>
 	  </ul>
 	</div>
   </div>
