89f2273f27560ffa56ab898c15208de604dc793f
billy clark <billy_clark@sil.org>
2022-06-23 15:05:30 -0400

    reduced prominence of show all

--- next-app/src/lib/forms/Button.svelte
+++ next-app/src/lib/forms/Button.svelte
@@ -1,14 +1,24 @@
 <script>
 	export let danger = false
+	export let outlined = false
+	export let small = false
 </script>
 
 <!-- https://daisyui.com/components/button -->
-<button on:click class:danger class='btn btn-primary'>
+<button on:click class:danger class:outlined class:small class='btn btn-primary'>
 	<slot />
 </button>
 
 <style>
 .danger { @apply
 	btn-error;
+}
+
+.outlined { @apply
+	btn-outline;
+}
+
+.small { @apply
+	btn-sm;
 }
 </style>
