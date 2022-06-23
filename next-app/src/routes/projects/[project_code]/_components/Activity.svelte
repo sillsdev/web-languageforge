535bbfa6bba26971d87ee606b34f127059d544e8
billy clark <billy_clark@sil.org>
2022-06-23 15:05:30 -0400

    sorted the activity

--- next-app/src/routes/projects/[project_code]/_components/Activity.svelte
+++ next-app/src/routes/projects/[project_code]/_components/Activity.svelte
@@ -1,12 +1,34 @@
 <script>
 	export let activities
 
+	$: sorted_activities = transform(activities).sort(byDateThenUser)
+
 	// mappings in src/Api/Model/Shared/ActivityModel.php
 	const action_display = {
 		'add_entry': 'Add',
 		'update_entry': 'Update',
 		'add_user_to_project': 'Joined',
 	}
+
+	function transform(_activities) {
+		return _activities.map(activity => {
+			const date = new Date(activity.date)
+
+			return {
+				...activity,
+				date: date.toLocaleDateString(),
+				time: date.getTime(),
+				fields: toNames(activity.fields),
+		 	}
+		})
+	}
+
+	function byDateThenUser(a, b) {
+		return a.date === b.date ? a.user === b.user ? des(a.time, b.time) : asc(a.user, b.user) : des(a.date, b.date)
+	}
+
+	const asc = (a, b) => a < b ? -1 : 1
+	const des = (a, b) => a < b ? 1 : -1
 
 	function toNames(fields = []) {
 		return fields.map(field => field.fieldName).join(', ')
@@ -25,13 +47,13 @@
 		</tr>
 	</thead>
 	<tbody>
-		{#each activities as activity}
+		{#each sorted_activities as activity}
 			<tr>
 				<td>{ activity.user }</td>
-				<td>{ new Date(activity.date).toLocaleString() }</td>
+				<td>{ activity.date }</td>
 				<td>{ action_display[activity.action] || activity.action }</td>
 				<td>{ activity.entry || '—' }</td>
-				<td>{ toNames(activity.fields) || '—' }</td>
+				<td>{ activity.fields || '—' }</td>
 			</tr>
 		{:else}
 			<tr><td>No activity yet</td></tr>
