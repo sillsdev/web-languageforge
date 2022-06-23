608017e4b58b5f78adc46be1f15d7e5d63eee0c3
Chris Hirt <chris@hirtfamily.net>
2022-06-23 15:05:30 -0400

    delete unused ActivityTypes API
    This is no longer necessary since we removed Scripture Forge from the code base.  We always have the same activity types defined and an API to retrieve those is overkill and unnecessary

--- src/Api/Model/Shared/Dto/ActivityListDto.php
+++ src/Api/Model/Shared/Dto/ActivityListDto.php
@@ -30,15 +30,6 @@ class ActivityListDto
     const DELETED_FIELD = 'deleted_field';
 
     /**
-     * @param Website $site
-     * @return array
-     */
-    public static function getActivityTypes($site)
-    {
-        return ActivityModel::getActivityTypesForSiteBase($site->base);
-    }
-
-    /**
      * @param ProjectModel $projectModel
      * @param array $filterParams
      * @return array - the DTO array
