d3abd7fd954eabfaa7a03b10e8a1ca880e682e13
billy clark <billy_clark@sil.org>
2022-06-23 15:05:31 -0400

    incorporated PR feedback

--- next-app/src/lib/forms/Button.svelte
+++ next-app/src/lib/forms/Button.svelte
@@ -1,9 +1,11 @@
 <script>
 	export let danger = false
+	let classes = ''
+	export { classes as class }
 </script>
 
 <!-- https://daisyui.com/components/button -->
-<button on:click class:danger class='btn btn-primary { $$props.class }'>
+<button on:click class:danger class='btn btn-primary { classes }'>
 	<slot />
 </button>
 
