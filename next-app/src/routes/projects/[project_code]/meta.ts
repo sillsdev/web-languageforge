8453fcedb60fcd7b46c4e4fbdf3437863736197a
billy clark <billy_clark@sil.org>
2022-07-13 16:47:55 -0400

    make multiple calls in parallel

--- next-app/src/routes/projects/[project_code]/meta.ts
+++ next-app/src/routes/projects/[project_code]/meta.ts
@@ -1,18 +1,13 @@
 import { sf } from '$lib/fetch/server'
 
 export async function get({ project_code, cookie }) {
-	//TODO: multiple calls that can be run in parallel...
-	const { id, projectName: name, users } = await sf({
-		name: 'project_read_by_code', // src/Api/Model/Shared/ProjectModel.php->getByProjectCode
-		args: [ project_code ],
-		cookie,
-	})
-
-	const { entries, comments } = await sf({
-		name: 'lex_stats',
-		args: [ project_code ],
-		cookie,
-	})
+	const [
+		{ id, projectName: name, users },
+		{ entries, comments }
+	] = await Promise.all([
+		sf({ name: 'project_read_by_code', args: [ project_code ], cookie }),
+		sf({ name: 'lex_stats', args: [ project_code ], cookie }),
+	])
 
 	const entries_with_picture = entries.filter(has_picture)
 	const unresolved_comments = comments.filter(({ status }) => status !== 'resolved')
@@ -22,7 +17,7 @@ export async function get({ project_code, cookie }) {
 		code: project_code,
 		name,
 		num_entries: entries.length,
-		num_entries_with_audio: 1234,
+		num_entries_with_audio: 1234, //TODO: need to derive this one
 		num_entries_with_pictures: entries_with_picture.length,
 		num_unresolved_comments: unresolved_comments.length,
 		num_users: Object.keys(users).length,
