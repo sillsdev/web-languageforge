81b1127b82552908fa679bfbb8d9d90fa6a12f69
billy clark <billy_clark@sil.org>
2022-07-14 11:15:04 -0400

    derived num_entries_with_audio

--- next-app/src/routes/projects/[project_code]/meta.ts
+++ next-app/src/routes/projects/[project_code]/meta.ts
@@ -10,6 +10,7 @@ export async function get({ project_code, cookie }) {
 	])
 
 	const entries_with_picture = entries.filter(has_picture)
+	const entries_with_audio = entries.filter(has_audio)
 	const unresolved_comments = comments.filter(({ status }) => status !== 'resolved')
 
 	return {
@@ -17,7 +18,7 @@ export async function get({ project_code, cookie }) {
 		code: project_code,
 		name,
 		num_entries: entries.length,
-		num_entries_with_audio: 1234, //TODO: need to derive this one
+		num_entries_with_audio: entries_with_audio.length,
 		num_entries_with_pictures: entries_with_picture.length,
 		num_unresolved_comments: unresolved_comments.length,
 		num_users: Object.keys(users).length,
@@ -26,4 +27,8 @@ export async function get({ project_code, cookie }) {
 
 function has_picture(entry) {
 	return entry.senses.some(sense => sense.pictures)
+}
+
+function has_audio(entry) {
+	return Object.keys(entry.lexeme).some(key => key.endsWith('-audio')) // naming convention imposed by src/angular-app/languageforge/lexicon/settings/configuration/input-system-view.model.ts L81
 }
