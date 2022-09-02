e59348e3a9c28b26851ba9ce6ba8700fd05c05e3
laineyhm <laineyhm@gmail.com>
2022-09-02 09:51:55 +0700

    core-js removed

--- src/angular-app/bellows/polyfills.browser.ts
+++ src/angular-app/bellows/polyfills.browser.ts
@@ -1,24 +1,3 @@
 // Polyfills
 
-import 'core-js/es/array';
-import 'core-js/es/date';
-import 'core-js/es/function';
-import 'core-js/es/map';
-import 'core-js/es/math';
-import 'core-js/es/number';
-import 'core-js/es/object';
-import 'core-js/es/parse-float';
-import 'core-js/es/parse-int';
-import 'core-js/es/reflect';
-import 'core-js/es/regexp';
-import 'core-js/es/set';
-import 'core-js/es/string';
-import 'core-js/es/symbol';
-import 'core-js/es/weak-map';
-import 'core-js/es/weak-set';
-// see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
-// import 'core-js/es6/promise';
 
-import 'core-js/es/reflect';
-
-import 'core-js/es/array/includes';
