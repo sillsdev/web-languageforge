8403eb5330eaefdab821f31fc4f8a01e664bda0b
JeanneSon <43119728+JeanneSon@users.noreply.github.com>
2022-09-06 23:24:13 +0200

    Convert interface language tests

--- test/e2e/pages/page-header.page.ts
+++ test/e2e/pages/page-header.page.ts
@@ -9,6 +9,8 @@ export class PageHeader {
   readonly page: Page;
   readonly myProjects: MyProjects;
   readonly loginButton: Locator;
+  readonly languageDropdownButton: Locator;
+  readonly languageDropdownItem: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -17,5 +19,7 @@ export class PageHeader {
       links: page.locator('#myProjectDropdownMenu >> .dropdown-item')
     };
     this.loginButton = page.locator('text=Login').nth(0);
+    this.languageDropdownButton = page.locator('#languageDropdownButton');
+    this.languageDropdownItem = page.locator('#languageDropdownMenu >> .dropdown-item');
   }
 }
