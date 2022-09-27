34623ff27c133583a337e9a2edfe9bbee2dbd54c
laineyhm <laineyhm@gmail.com>
2022-09-27 14:11:35 +0700

    remove buttons that add custom entry, sense, or example

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import constants from './testConstants.json';
 import { test } from './utils/fixtures';
 import { testControl } from './utils/jsonrpc';
-import { addCustomField, addLexEntry, initTestProject } from './utils/testSetup';
+import { addLexEntry, initTestProject } from './utils/testSetup';
 
 test.skip('Reset project', async ({ request }) => {
   await initTestProject(request,
@@ -18,20 +18,12 @@ test.skip('Reset project and add test data', async ({ request, adminTab }) => {
     constants.testProjectName,
     constants.managerUsername,
   );
-  const customFieldName = await addCustomField(request,
-    constants.testProjectCode,
-    'CustomField',
-    'entry',
-    'MultiString',
-    {inputSystems: ['th']}
-  );
   // Lexical entry from testConstants.json with no changes
   await addLexEntry(request, constants.testProjectCode, constants.testEntry1);
   // Example of adding data in the custom field
   const data = {
     ...constants.testEntry2,
     customFields: {
-      [customFieldName]: { th: { value: 'contents of custom field' } }
     }
   };
   // The [customFieldName] syntax is how you can assign a property without knowing it at compile-time
