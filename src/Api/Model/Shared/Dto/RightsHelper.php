179f6c35845139a58ca466864a567d48f44258b0
billy clark <billy_clark@sil.org>
2022-11-14 14:20:23 -0500

    refactor activity call into consistent convention

--- src/Api/Model/Shared/Dto/RightsHelper.php
+++ src/Api/Model/Shared/Dto/RightsHelper.php
@@ -210,9 +210,6 @@ class RightsHelper
             case "activity_list_dto_for_current_project":
                 return $this->userHasSiteRight(Domain::PROJECTS + Operation::VIEW_OWN);
 
-            case "activity_list_dto_for_project":
-                return $this->userHasSiteRight(Domain::PROJECTS + Operation::VIEW_OWN);
-
             case "activity_list_dto_for_lexical_entry":
                 return $this->userHasProjectRight(Domain::ENTRIES + Operation::VIEW);
 
