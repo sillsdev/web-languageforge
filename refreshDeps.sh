68ca2f6d781fc7689dd2678f7a754b6579bc1554
billy clark <billy_clark@sil.org>
2022-12-03 12:12:35 -0500

    doesn't seem to be necessary
    it's not referenced in the DEVELOPER.md so I assume it's not important.

--- refreshDeps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Usage
-# ./refreshDeps.sh : refreshes PHP and NPM dependencies for LF
-
-(cd src && composer install)
-npm install
-echo -e "\n---------Finished refreshing npm and composer----------"
