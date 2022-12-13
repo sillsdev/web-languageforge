1ef0e9e76844c0530a400a5440dec996ab63c9b5
billy clark <billy_clark@sil.org>
2022-12-13 18:07:13 -0500

    unused

--- next-app/svelte.config.js
+++ next-app/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
-const yellow = message => `\x1b[33m${message}\x1b[0m`
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: preprocess(),
