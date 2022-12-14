2a11ca08510566ae65740948ae25920fb239d625
Tim Haasdyk <tim_haasdyk@sil.org>
2022-12-14 16:59:47 +0100

    Remove protractor code

--- test/app/bellows/shared/mock-upload.element.ts
+++
@@ -1,8 +0,0 @@
-import {by, element} from 'protractor';
-
-export class MockUploadElement {
-  enableButton = element(by.id('showMockUploadButton'));
-  fileNameInput = element(by.id('mockFileName'));
-  fileSizeInput = element(by.id('mockFileSize'));
-  uploadButton = element(by.id('mockUploadButton'));
-}
