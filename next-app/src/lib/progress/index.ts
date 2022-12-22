22b01b7d0cd493ed6165888462ec3c4752064cd4
billy clark <billy_clark@sil.org>
2022-12-22 18:31:05 -0500

    typed Progress comp

--- next-app/src/lib/progress/index.ts
+++ next-app/src/lib/progress/index.ts
@@ -1,17 +1,17 @@
-import { writable } from 'svelte/store'
+import { writable, type Writable } from 'svelte/store'
 
-export const loading = writable(false)
+export const loading: Writable<boolean> = writable(false)
 
-const pending = []
+const pending: Id[] = []
 
-export function start(id) {
+export function start(id: Id) {
   loading.set(true)
 
   pending.push(id)
 }
 
-export function stop(id) {
-  const i = pending.findIndex(anId => anId === id)
+export function stop(id: Id) {
+  const i = pending.findIndex(_id => _id === id)
 
   if (i >= 0) {
     pending.splice(i,1)
