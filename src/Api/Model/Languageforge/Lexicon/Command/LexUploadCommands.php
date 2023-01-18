a93babced519739dfcd81bab94368d157007dc08
laineyhm <laineyhm@gmail.com>
2023-01-18 15:38:39 +0700

    Boost SR limit to 10MB for audio conversion settings

--- src/Api/Model/Languageforge/Lexicon/Command/LexUploadCommands.php
+++ src/Api/Model/Languageforge/Lexicon/Command/LexUploadCommands.php
@@ -101,8 +101,8 @@ class LexUploadCommands
 
                 if (
                     strcmp($project->whenToConvertAudio, "always") == 0 ||
-                    (strcmp($project->whenToConvertAudio, "sr") == 0 && // restrictions on send/receive files 12-2022
-                        (filesize($filePath) > 1000000 ||
+                    (strcmp($project->whenToConvertAudio, "sr") == 0 && // restrictions on send/receive files 01-2023
+                        (filesize($filePath) > 10000000 ||
                             (strcmp(strtolower($fileExt), ".mp3") !== 0 &&
                                 strcmp(strtolower($fileExt), ".wav") !== 0 &&
                                 strcmp(strtolower($fileExt), ".webm") !== 0)))
