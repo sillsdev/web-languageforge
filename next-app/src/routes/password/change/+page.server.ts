0f3b63692394cccd007966ec38d26bf878a03a28
billy clark <billy_clark@sil.org>
2023-01-26 08:19:21 -0500

    incorporated PR feedback

--- next-app/src/routes/password/change/+page.server.ts
+++ next-app/src/routes/password/change/+page.server.ts
@@ -4,30 +4,30 @@ import { fail, redirect } from '@sveltejs/kit'
 import type { RequestEvent } from '../$types'
 
 export const actions = {
-  default: async ({ request }: RequestEvent) => {
-	const submissions = await request.formData()
+	default: async ({ request }: RequestEvent) => {
+		const submissions = await request.formData()
 
-	const new_password = submissions.get('new_password')
-	const new_password_confirm = submissions.get('new_password_confirm')
+		const new_password = submissions.get('new_password')
+		const new_password_confirm = submissions.get('new_password_confirm')
 
-	if (! new_password) {
-		return fail(400, { failed: 'Password is required' })
+		if (! new_password) {
+			return fail(400, { failed: 'Password is required' })
+		}
+
+		if (new_password !== new_password_confirm) {
+			return fail(400, { failed: 'Passwords do not match' })
+		}
+
+		const cookie = request.headers.get('cookie') || ''
+
+		const { id } = await fetch_current_user(cookie)
+
+		await sf({
+			name: 'change_password',
+			args: [id, new_password],
+			cookie,
+		})
+
+		throw redirect(303, '/password/changed')
 	}
-
-	if (new_password !== new_password_confirm) {
-		return fail(400, { failed: 'Passwords do not match' })
-	}
-
-	const cookie = request.headers.get('cookie') || ''
-
-	const { id } = await fetch_current_user(cookie)
-
-	await sf({
-		name: 'change_password',
-		args: [id, new_password],
-		cookie,
-	})
-
-	throw redirect(303, '/password/changed')
-  }
 }
