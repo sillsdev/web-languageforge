d479184e3e0bc150522751d5acd2b685d7002c45
billy clark <billy_clark@sil.org>
2023-01-30 13:14:16 -0500

    needed to extend Button impl

--- next-app/src/lib/forms/Button.svelte
+++ next-app/src/lib/forms/Button.svelte
@@ -1,11 +1,12 @@
 <script>
 	export let danger = false
+	export let disabled = false
 	export {clazz as class}
 
 	let clazz = ''
 </script>
 
 <!-- https://daisyui.com/components/button -->
-<button on:click class:btn-error={danger} class='btn btn-primary { clazz }'>
+<button on:click class:btn-error={danger} class:btn-disabled={disabled} class='btn btn-primary { clazz }'>
 	<slot />
 </button>
